0fa53d5c3562f81f7e2dc05d047cc7b887a87abe
lcp <none@none>
1994-08-16 19:03:45 +0200

    ZF/Finite: added the finite function space, A-||>B ZF/InfDatatype: added rules for the above

--- src/ZF/Finite.thy
+++ src/ZF/Finite.thy
@@ -7,7 +7,10 @@ Finite powerset operator
 *)
 
 Finite = Arith + 
-consts Fin :: "i=>i"
+consts
+  Fin 	    :: "i=>i"
+  FiniteFun :: "[i,i]=>i"		("(_ -||>/ _)" [61, 60] 60)
+
 inductive
   domains   "Fin(A)" <= "Pow(A)"
   intrs
@@ -15,4 +18,12 @@ inductive
     consI   "[| a: A;  b: Fin(A) |] ==> cons(a,b) : Fin(A)"
   type_intrs "[empty_subsetI, cons_subsetI, PowI]"
   type_elims "[make_elim PowD]"
+
+inductive
+  domains   "FiniteFun(A,B)" <= "Fin(A*B)"
+  intrs
+    emptyI  "0 : A -||> B"
+    consI   "[| a: A;  b: B;  h: A -||> B;  a ~: domain(h)   \
+\	     |] ==> cons(<a,b>,h) : A -||> B"
+  type_intrs "Fin.intrs"
 end
