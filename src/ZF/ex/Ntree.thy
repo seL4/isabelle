dfb08f44f363bdf191d4b038c2d91bb372d97334
lcp <none@none>
1994-08-17 10:34:44 +0200

    ZF/ex/Ntree: two new examples, maptree and maptree2

--- src/ZF/ex/Ntree.thy
+++ src/ZF/ex/Ntree.thy
@@ -5,19 +5,30 @@
 
 Datatype definition n-ary branching trees
 Demonstrates a simple use of function space in a datatype definition
-   and also "nested" monotonicity theorems
 
 Based upon ex/Term.thy
 *)
 
 Ntree = InfDatatype +
 consts
-  ntree :: "i=>i"
+  ntree    :: "i=>i"
+  maptree  :: "i=>i"
+  maptree2 :: "[i,i] => i"
 
 datatype
   "ntree(A)" = Branch ("a: A", "h: (UN n:nat. n -> ntree(A))")
-  monos	      "[[subset_refl, Pi_mono] MRS UN_mono]"
+  monos	      "[[subset_refl, Pi_mono] MRS UN_mono]"	(*MUST have this form*)
   type_intrs  "[nat_fun_univ RS subsetD]"
   type_elims  "[UN_E]"
+
+datatype
+  "maptree(A)" = Sons ("a: A", "h: maptree(A) -||> maptree(A)")
+  monos	      "[FiniteFun_mono1]"	(*Use monotonicity in BOTH args*)
+  type_intrs  "[FiniteFun_univ1 RS subsetD]"
+
+datatype
+  "maptree2(A,B)" = Sons2 ("a: A", "h: B -||> maptree2(A,B)")
+  monos	      "[subset_refl RS FiniteFun_mono]"
+  type_intrs  "[FiniteFun_in_univ']"
 
 end
