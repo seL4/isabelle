5c8a4ca48123cb8f6162fde0b8779231f39068ca
nipkow <none@none>
1995-03-07 14:57:37 +0100

    Hoare logic

---
+++ src/HOL/IMP/Hoare.thy
@@ -0,0 +1,14 @@
+(*  Title: 	HOL/IMP/Hoare.thy
+    ID:
+    Author: 	Tobias Nipkow
+    Copyright   1995 TUM
+
+Semantic embedding of Hoare logic
+*)
+
+Hoare = Denotation +
+consts
+  spec:: "[state=>bool,com,state=>bool] => bool" ("{{(1_)}}/ (_)/ {{(1_)}}" 10)
+defs
+  spec_def "spec P c Q == !s t. <s,t> : C(c) --> P s --> Q t"
+end
