5898527a482a43a039be70a762c70bfd78b01f90
lcp <none@none>
1995-05-04 02:02:54 +0200

    Changed some definitions and proofs to use pattern-matching.

--- src/ZF/ex/Integ.thy
+++ src/ZF/ex/Integ.thy
@@ -30,25 +30,29 @@ defs
     
     znat_def	"$# m == intrel `` {<m,0>}"
     
-    zminus_def	"$~ Z == UN p:Z. split(%x y. intrel``{<y,x>}, p)"
+    zminus_def	"$~ Z == UN <x,y>:Z. intrel``{<y,x>}"
     
     znegative_def
 	"znegative(Z) == EX x y. x<y & y:nat & <x,y>:Z"
     
     zmagnitude_def
-	"zmagnitude(Z) == UN p:Z. split(%x y. (y#-x) #+ (x#-y), p)"
+	"zmagnitude(Z) == UN <x,y>:Z. (y#-x) #+ (x#-y)"
     
+    (*Cannot use UN<x1,y2> here or in zmult because of the form of congruent2.
+      Perhaps a "curried" or even polymorphic congruent predicate would be
+      better.*)
     zadd_def
      "Z1 $+ Z2 == \
-\       UN p1:Z1. UN p2:Z2. split(%x1 y1. split(%x2 y2. 		\
-\                                         intrel``{<x1#+x2, y1#+y2>}, p2), p1)"
+\       UN z1:Z1. UN z2:Z2. let <x1,y1>=z1; <x2,y2>=z2                 \
+\                           in intrel``{<x1#+x2, y1#+y2>}"
     
     zdiff_def   "Z1 $- Z2 == Z1 $+ zminus(Z2)"
     zless_def	"Z1 $< Z2 == znegative(Z1 $- Z2)"
     
+    (*This illustrates the primitive form of definitions (no patterns)*)
     zmult_def
      "Z1 $* Z2 == \
-\       UN p1:Z1. UN p2:Z2.  split(%x1 y1. split(%x2 y2. 	\
+\       UN p1:Z1. UN p2:Z2.  split(%x1 y1. split(%x2 y2.        \
 \                   intrel``{<x1#*x2 #+ y1#*y2, x1#*y2 #+ y1#*x2>}, p2), p1)"
     
  end
