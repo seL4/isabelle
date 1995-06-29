b98d7a6b0249675915ba5e38ed5da07c5580101f
lcp <none@none>
1995-06-29 16:50:14 +0200

    fixed comment

--- src/ZF/ex/BT.thy
+++ src/ZF/ex/BT.thy
@@ -1,4 +1,4 @@
-(*  Title: 	ZF/ex/bt-fn.thy
+(*  Title: 	ZF/ex/BT.thy
     ID:         $Id$
     Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1992  University of Cambridge
