2ef77faad2371516e89814cfcd9a384d4161a0e6
clasohm <none@none>
1995-10-04 13:10:03 +0100

    added local simpsets; removed IOA from 'make test'

--- src/HOL/Lfp.thy
+++ src/HOL/Lfp.thy
@@ -1,4 +1,4 @@
-(*  Title: 	HOL/lfp.thy
+(*  Title: 	HOL/Lfp.thy
     ID:         $Id$
     Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1992  University of Cambridge
--- src/HOL/mono.thy
+++ src/HOL/mono.thy
@@ -1,8 +1,8 @@
-(*  Title: 	HOL/mono
+(*  Title: 	HOL/mono.thy
     ID:         $Id$
     Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1991  University of Cambridge
 
 *)
 
-mono = subset
+mono = equalities
