d2865662df0f0f729236cd5fba2987cc3a959ce8
clasohm <none@none>
1995-10-26 13:53:00 +0100

    renamed logics.html to index.html; added command for cleaning the directory before transfering newer version

--- src/Tools/install_html.sh
+++ src/Tools/install_html.sh
@@ -3,6 +3,7 @@
 # files needed for the HTML version of Isabelle's theories to the HTTP
 # server.
 
-rcp logics.html www4:.html-data/isabelle
-rcp Tools/*arrow.gif www4:.html-data/isabelle/Tools
+rsh www4 "rm -r .html-data/isabelle/*; mkdir .html-data/isabelle/Tools"
+rcp index.html www4:.html-data/isabelle
+rcp Tools/*_arrow.gif www4:.html-data/isabelle/Tools
 rcp -r CCL CTT Cube FOL FOLP HOL HOLCF LCF LK Modal ZF www4:.html-data/isabelle
