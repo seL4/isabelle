37514879ca0ab323426d6028e0e21dd332aa513a
clasohm <none@none>
1995-11-07 13:15:43 +0100

    changed for new naming style of HTML files (leading ".")

--- src/Tools/rm_html.sh
+++ src/Tools/rm_html.sh
@@ -2,9 +2,4 @@
 # Executed from the main Isabelle directory, this script removes all files
 # made when Isabelle was used with set MAKE_HTML
 
-rm */theory_list.txt */index.html */Pure_sub.html */CPure_sub.html
-foreach f (*/*.thy */*/*.thy */*/*/*.thy)
-  if (-f $f:r.html) then
-    rm $f:r.html $f:r_sub.html $f:r_sup.html
-  endif
-end
+rm */.theory_list.txt */index.html */.*.html */*/.*.html */*/*/.*.html
