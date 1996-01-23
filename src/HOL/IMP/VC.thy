1898989dd0484422cb1e8ca0a745487d1b089f75
nipkow <none@none>
1996-01-23 14:25:55 +0100

    Added vcwp

--- src/HOL/IMP/VC.thy
+++ src/HOL/IMP/VC.thy
@@ -18,6 +18,7 @@ datatype  acom = Askip
 
 consts
   vc,wp :: acom => assn => assn
+  vcwp :: "acom => assn => assn * assn"
   astrip :: acom => com
 
 primrec wp acom
@@ -41,5 +42,25 @@ primrec astrip acom
   astrip_Asemi  "astrip (Asemi c d) = (astrip c;astrip d)"
   astrip_Aif    "astrip (Aif b c d) = ifC b (astrip c) (astrip d)"
   astrip_Awhile "astrip (Awhile b I c) = whileC b (astrip c)"
-  
+
+(* simultaneous computation of vc and wp: *)
+primrec vcwp acom
+  vcwp_Askip
+  "vcwp Askip Q = (%s.True, Q)"
+  vcwp_Aass
+  "vcwp (Aass x a) Q = (%s.True, %s.Q(s[A a s/x]))"
+  vcwp_Asemi
+  "vcwp (Asemi c d) Q = (let (vcd,wpd) = vcwp d Q;
+                            (vcc,wpc) = vcwp c wpd
+                         in (%s. vcc s & vcd s, wpc))"
+  vcwp_Aif
+  "vcwp (Aif b c d) Q = (let (vcd,wpd) = vcwp d Q;
+                            (vcc,wpc) = vcwp c Q
+                         in (%s. vcc s & vcd s,
+                             %s.(B b s-->wpc s) & (~B b s-->wpd s)))"
+  vcwp_Awhile
+  "vcwp (Awhile b I c) Q = (let (vcc,wpc) = vcwp c I
+                            in (%s. (I s & ~B b s --> Q s) &
+                                    (I s & B b s --> wpc s) & vcc s, I))"
+
 end
