a23b9fb6a7ef47d601c1e87f5b6751e1ef2e37d3
clasohm <none@none>
1996-01-30 15:24:36 +0100

    expanded tabs

--- src/HOL/equalities.ML
+++ src/HOL/equalities.ML
@@ -1,6 +1,6 @@
-(*  Title: 	HOL/equalities
+(*  Title:      HOL/equalities
     ID:         $Id$
-    Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1994  University of Cambridge
 
 Equalities involving union, intersection, inclusion, etc.
