0fa32dcf9215bd7eb46e99db80c23bce5c3d033f
clasohm <none@none>
1996-01-30 13:42:57 +0100

    expanded tabs

--- src/ZF/ex/Enum.ML
+++ src/ZF/ex/Enum.ML
@@ -1,6 +1,6 @@
-(*  Title: 	ZF/ex/Enum
+(*  Title:      ZF/ex/Enum
     ID:         $Id$
-    Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1993  University of Cambridge
 
 Example of a BIG enumeration type
