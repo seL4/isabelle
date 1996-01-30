e9210aedf81e7da04c26009ed256d12fc891355b
clasohm <none@none>
1996-01-30 15:31:04 +0100

    fixed typo

--- src/FOLP/ex/Prolog.thy
+++ src/FOLP/ex/Prolog.thy
@@ -1,4 +1,4 @@
-(*  Title: 	FOL/ex/prolog.thy
+(*  Title: 	FOLP/ex/Prolog.thy
     ID:         $Id$
     Author: 	Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1992  University of Cambridge
