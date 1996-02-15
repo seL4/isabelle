79fb0ae33ebd28a89bd0baf6aafe4e29ee961160
nipkow <none@none>
1996-02-15 08:10:36 +0100

    Added a few thms and the new theory RelPow.

---
+++ src/HOL/RelPow.thy
@@ -0,0 +1,15 @@
+(*  Title:      HOL/RelPow.thy
+    ID:         $Id$
+    Author:     Tobias Nipkow
+    Copyright   1996  TU Muenchen
+
+R^n = R O ... O R, the n-fold composition of R
+*)
+
+RelPow = Nat +
+
+consts
+  "^" :: "('a * 'a) set => nat => ('a * 'a) set" (infixr 100)
+defs
+  rel_pow_def "R^n == nat_rec n id (%m S. R O S)"
+end
