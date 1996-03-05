f0deb09fe5c259662d267708bd54cbe9bb09c6e2
paulson <none@none>
1996-03-05 11:38:41 +0100

    Example of declaring oracles

---
+++ src/FOL/ex/IffOracle.thy
@@ -0,0 +1,11 @@
+(*  Title:      FOL/ex/IffOracle
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1996  University of Cambridge
+
+Example of how to declare an oracle
+*)
+
+IffOracle = "declIffOracle" + FOL +
+oracle mk_iff_oracle
+end
---
+++ src/FOL/ex/declIffOracle.thy
@@ -0,0 +1,1 @@
+declIffOracle = Pure
