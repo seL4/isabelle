4d95a3ef39b90a1aff286477e50138a0c47abb8c
paulson <none@none>
1996-04-25 12:45:14 +0200

    Rearrangement and polishing to look for for publication

--- src/HOL/ex/Mutil.thy
+++ src/HOL/ex/Mutil.thy
@@ -3,19 +3,17 @@
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1996  University of Cambridge
 
-The Mutilated Checkerboard Problem, formalized inductively
+The Mutilated Chess Board Problem, formalized inductively
+  Originator is Max Black, according to J A Robinson.
+  Popularized as the Mutilated Checkerboard Problem by J McCarthy
 *)
 
 Mutil = Finite +
 consts
-  below   :: nat => nat set
-  evnodd  :: "[(nat*nat)set, nat] => (nat*nat)set"
   domino  :: "(nat*nat)set set"
   tiling  :: 'a set set => 'a set set
-
-defs
-  below_def  "below n    == nat_rec n {} insert"
-  evnodd_def "evnodd A b == A Int {(i,j). (i+j) mod 2 = b}"
+  below   :: nat => nat set
+  evnodd  :: "[(nat*nat)set, nat] => (nat*nat)set"
 
 inductive "domino"
   intrs
@@ -26,5 +24,9 @@ inductive "tiling A"
   intrs
     empty  "{} : tiling A"
     Un     "[| a: A;  t: tiling A;  a Int t = {} |] ==> a Un t : tiling A"
+
+defs
+  below_def  "below n    == nat_rec n {} insert"
+  evnodd_def "evnodd A b == A Int {(i,j). (i+j) mod 2 = b}"
 
 end
