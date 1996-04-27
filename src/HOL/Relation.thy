606e723f8514765e84cbc10eb22a8acaa3c5f3b3
nipkow <none@none>
1996-04-27 12:09:21 +0200

    Generalized types of some of the operators (thanks to Norbert Voelker)

--- src/HOL/Relation.thy
+++ src/HOL/Relation.thy
@@ -11,10 +11,10 @@ consts
     id          :: "('a * 'a)set"               (*the identity relation*)
     O           :: "[('b * 'c)set, ('a * 'b)set] => ('a * 'c)set" (infixr 60)
     trans       :: "('a * 'a)set => bool"       (*transitivity predicate*)
-    converse    :: "('a * 'b)set => ('b * 'a)set"
-    "^^"        :: "[('a * 'b) set, 'a set] => 'b set" (infixl 90)
-    Domain      :: "('a * 'b) set => 'a set"
-    Range       :: "('a * 'b) set => 'b set"
+    converse    :: "('a*'b) set => ('b*'a) set"
+    "^^"        :: "[('a*'b) set,'a set] => 'b set" (infixl 90)
+    Domain      :: "('a*'b) set => 'a set"
+    Range       :: "('a*'b) set => 'b set"
 defs
     id_def      "id == {p. ? x. p = (x,x)}"
     comp_def    "r O s == {(x,z). ? y. (x,y):s & (y,z):r}"
