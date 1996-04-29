eec739cbc7e43998d107503137701739c7ec85fd
nipkow <none@none>
1996-04-29 15:48:27 +0200

    Natural and Transition semantics.

---
+++ src/HOL/IMP/Natural.thy
@@ -0,0 +1,41 @@
+(*  Title:      HOL/IMP/Natural.thy
+    ID:         $Id$
+    Author:     Tobias Nipkow & Robert Sandner, TUM
+    Copyright   1996 TUM
+
+Natural semantics of commands
+*)
+
+Natural = Com +
+
+(** Execution of commands **)
+consts  evalc    :: "(com*state*state)set"
+        "@evalc" :: [com,state,state] => bool ("<_,_>/ -c-> _" [0,0,50] 50)
+
+translations  "<ce,sig> -c-> s" == "(ce,sig,s) : evalc"
+
+constdefs assign :: [state,val,loc] => state    ("_[_'/_]" [95,0,0] 95)
+  "s[m/x] ==  (%y. if y=x then m else s y)"
+
+
+inductive "evalc"
+  intrs
+    Skip    "<SKIP,s> -c-> s"
+
+    Assign  "<x := a,s> -c-> s[a(s)/x]"
+
+    Semi    "[| <c0,s> -c-> s2; <c1,s2> -c-> s1 |] 
+            ==> <c0 ; c1, s> -c-> s1"
+
+    IfTrue "[| b s; <c0,s> -c-> s1 |] 
+            ==> <IF b THEN c0 ELSE c1, s> -c-> s1"
+
+    IfFalse "[| ~b s; <c1,s> -c-> s1 |] 
+             ==> <IF b THEN c0 ELSE c1, s> -c-> s1"
+
+    WhileFalse "~b s ==> <WHILE b DO c,s> -c-> s"
+
+    WhileTrue  "[| b s;  <c,s> -c-> s2;  <WHILE b DO c, s2> -c-> s1 |] 
+               ==> <WHILE b DO c, s> -c-> s1"
+
+end
---
+++ src/HOL/IMP/Transition.thy
@@ -0,0 +1,40 @@
+(*  Title:      HOL/IMP/Transition.thy
+    ID:         $Id$
+    Author:     Tobias Nipkow & Robert Sandner, TUM
+    Copyright   1996 TUM
+
+Transition semantics of commands
+*)
+
+Transition = Natural + RelPow +
+
+consts  evalc1    :: "((com*state)*(com*state))set"        
+	"@evalc1" :: "[(com*state),(com*state)] => bool"   
+				("_ -1-> _" [81,81] 100)
+	"@evalcn" :: "[(com*state),(com*state)] => nat => bool"
+				("_ -(_)-> _" [81,81] 100)
+	"@evalc*" :: "[(com*state),(com*state)] => bool"   
+				("_ -*-> _" [81,81] 100)
+
+translations
+       	"cs0 -1-> cs1" == "(cs0,cs1) : evalc1"
+        "x -(n)-> (y,z)" == "(x,y,z) : evalc1 ^ n" 
+	"cs0 -(n)-> cs1" == "(cs0,cs1) : evalc1 ^ n"
+	"x -*-> (y,z)" == "(x,y,z) : evalc1 ^*" 
+	"cs0 -*-> cs1" == "(cs0,cs1) : evalc1 ^*"
+
+
+inductive "evalc1"
+  intrs
+    Assign "(x := a,s) -1-> (SKIP,s[a s / x])"
+
+    Semi1   "(SKIP;c,s) -1-> (c,s)"	
+    Semi2   "(c0,s) -1-> (c2,s1) ==> (c0;c1,s) -1-> (c2;c1,s1)"
+
+    IfTrue "b s ==> (IF b THEN c1 ELSE c2,s) -1-> (c1,s)"
+    IfFalse "~b s ==> (IF b THEN c1 ELSE c2,s) -1-> (c2,s)"
+
+    WhileFalse "~b s ==> (WHILE b DO c,s) -1-> (SKIP,s)"
+    WhileTrue "b s ==> (WHILE b DO c,s) -1-> (c;WHILE b DO c,s)"
+
+end
