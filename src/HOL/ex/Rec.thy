76815e561543b2b8b1003ddf3b8ce8c877468f9f
paulson <none@none>
1996-06-14 12:21:02 +0200

    Obsolete relics concerned with recursion

--- src/HOL/ex/Rec.thy
+++
@@ -1,9 +0,0 @@
-Rec = Fixedpt +
-consts
-fix     :: ('a=>'a) => 'a
-Dom     :: (('a=>'b) => ('a=>'b)) => 'a set
-Domf    :: (('a=>'b) => ('a=>'b)) => 'a set => 'a set
-rules
-Domf_def "Domf(F,D) == {y . !f g. (!x:D. f(x)=g(x)) --> F(f,y)=F(g,y)}"
-Dom_def  "Dom(F) == lfp(Domf(F))"
-end
