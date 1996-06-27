2eaf905e8eaa916e30b12a45e462365971439347
oheimb <none@none>
1996-06-27 12:53:08 +0200

    removed old version of LEAST operator

--- src/HOLCF/Holcfb.thy
+++ src/HOLCF/Holcfb.thy
@@ -8,24 +8,3 @@ Basic definitions for the embedding of LCF into HOL.
 *)
 
 Holcfb = Nat 
-
-(* + 
-
-consts
-        theleast     :: "(nat=>bool)=>nat"
-defs
-
-theleast_def    "theleast P == (@z.(P z & (!n.P n --> z<=n)))"
-
-(* start 8bit 1 *)
-
-syntax
-  "Gmu"        :: "[pttrn, bool] => nat"               ("(3´_./ _)" 10)
-
-translations
-  "´x.P"	== "theleast(%x.P)"
-
-(* end 8bit 1 *)
-
-end
-*)
