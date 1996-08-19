2bd48e7c66b55577305ee54b3610c6698df800a9
paulson <none@none>
1996-08-19 11:23:25 +0200

    Changed precedences to remove ambiguities in r^+ notation

--- src/HOL/Trancl.thy
+++ src/HOL/Trancl.thy
@@ -8,19 +8,22 @@ Relfexive and Transitive closure of a relation
 rtrancl is reflexive/transitive closure;
 trancl  is transitive closure
 reflcl  is reflexive closure
+
+These postfix operators have MAXIMUM PRIORITY, forcing their operands to be
+      atomic.
 *)
 
 Trancl = Lfp + Relation + 
 
 constdefs
-  rtrancl :: "('a * 'a)set => ('a * 'a)set"   ("(_^*)" [100] 100)
+  rtrancl :: "('a * 'a)set => ('a * 'a)set"   ("(_^*)" [1000] 1000)
   "r^*  ==  lfp(%s. id Un (r O s))"
 
-  trancl  :: "('a * 'a)set => ('a * 'a)set"   ("(_^+)" [100] 100)
+  trancl  :: "('a * 'a)set => ('a * 'a)set"   ("(_^+)" [1000] 1000)
   "r^+  ==  r O rtrancl(r)"
 
 syntax
-  reflcl  :: "('a*'a)set => ('a*'a)set"       ("(_^=)" [100] 100)
+  reflcl  :: "('a*'a)set => ('a*'a)set"       ("(_^=)" [1000] 1000)
 
 translations
   "r^=" == "r Un id"
