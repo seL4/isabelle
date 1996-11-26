9ec25d64099767b68163394e134e63390b556514
nipkow <none@none>
1996-11-26 14:28:17 +0100

    A bit of commutative ing theory, with a simplification tacxtic and an example.

---
+++ src/HOL/ex/Lagrange.thy
@@ -0,0 +1,18 @@
+(*  Title:      HOL/ex/Lagrange.thy
+    ID:         $Id$
+    Author:     Tobias Nipkow
+    Copyright   1996 TU Muenchen
+
+
+This theory only contains a single thm, which is a lemma in Lagrange's proof
+that every natural number is the sum of 4 squares.  It's sole purpose is to
+demonstrate ordered rewriting for commutative rings.
+
+The enterprising reader might consider proving all of Lagrange's thm.
+*)
+Lagrange = Ring +
+
+constdefs sq :: 'a::times => 'a
+         "sq x == x*x"
+
+end
