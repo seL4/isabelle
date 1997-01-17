effb715d606447e1e8ebb7dffb5538f5f9c1bdb9
nipkow <none@none>
1997-01-17 10:04:28 +0100

    Got rid of Alls in favour of !x:set_of_list

--- src/HOL/ex/Sorting.thy
+++ src/HOL/ex/Sorting.thy
@@ -21,7 +21,7 @@ sorted1_One  "sorted1 f [x]"
 sorted1_Cons "sorted1 f (Cons x (y#zs)) = (f x y & sorted1 f (y#zs))"
 
 sorted_Nil "sorted le []"
-sorted_Cons "sorted le (x#xs) = ((Alls y:xs. le x y) & sorted le xs)"
+sorted_Cons "sorted le (x#xs) = ((!y:set_of_list xs. le x y) & sorted le xs)"
 
 mset_Nil "mset [] y = 0"
 mset_Cons "mset (x#xs) y = (if x=y then Suc(mset xs y) else mset xs y)"
