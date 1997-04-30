74335bfb10df4f894d2de46d6d2213bdffe92d16
mueller <none@none>
1997-04-30 11:51:28 +0200

    remove -- new version in HOLCF/IOA/ABP;

--- src/HOL/IOA/ABP/Lemmas.thy
+++
@@ -1,9 +0,0 @@
-(*  Title:      HOL/IOA/example/Lemmas.thy
-    ID:         $Id$
-    Author:     Tobias Nipkow & Konrad Slind
-    Copyright   1994  TU Muenchen
-
-Arithmetic lemmas
-*)
-
-Lemmas = Arith
