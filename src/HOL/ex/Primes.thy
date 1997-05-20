366d365175a7970578ca025a38fb537049b6e63e
paulson <none@none>
1997-05-20 11:44:02 +0200

    Renamed egcd and gcd; defined the gcd function using TFL

--- src/HOL/ex/Primes.thy
+++ src/HOL/ex/Primes.thy
@@ -3,28 +3,27 @@
     Author:     Christophe Tabacznyj and Lawrence C Paulson
     Copyright   1996  University of Cambridge
 
-The "divides" relation, the greatest common divisor and Euclid's algorithm
+The "divides" relation, the Greatest Common Divisor and Euclid's algorithm
 *)
 
-Primes = Arith +
+Primes = Arith + WF_Rel +
 consts
   dvd     :: [nat,nat]=>bool              (infixl 70) 
-  gcd     :: [nat,nat,nat]=>bool          (* great common divisor *)
-  egcd    :: [nat,nat]=>nat               (* gcd by Euclid's algorithm *)
-  coprime :: [nat,nat]=>bool              (* coprime definition *)
-  prime   :: nat=>bool                    (* prime definition *)
+  is_gcd  :: [nat,nat,nat]=>bool          (*gcd as a relation*)
+  gcd     :: "nat*nat=>nat"               (*Euclid's algorithm *)
+  coprime :: [nat,nat]=>bool
+  prime   :: nat=>bool
   
+recdef gcd "measure ((%(x,y).y) ::nat*nat=>nat)"
+    "gcd (m, n) = (if n=0 then m else gcd(n, m mod n))"
+
 defs
   dvd_def     "m dvd n == EX k. n = m*k"
 
-  gcd_def     "gcd p m n == ((p dvd m) & (p dvd n))   &
-               (ALL d. (d dvd m) & (d dvd n) --> d dvd p)"
+  is_gcd_def  "is_gcd p m n == p dvd m  &  p dvd n  &
+                               (ALL d. d dvd m & d dvd n --> d dvd p)"
 
-  egcd_def    "egcd m n ==   
-               wfrec (pred_nat^+)
-                     (%f n m. if n=0 then m else f (m mod n) n) n m"
-
-  coprime_def "coprime m n == egcd m n = 1"
+  coprime_def "coprime m n == gcd(m,n) = 1"
 
   prime_def   "prime(n) == (1<n) & (ALL m. 1<m & m<n --> ~(m dvd n))"
 
