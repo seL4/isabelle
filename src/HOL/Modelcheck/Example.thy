b64072dd1917c7dd90587e470e3ccc4a514307e3
mueller <none@none>
1997-05-20 10:28:19 +0200

    little changes

--- src/HOL/Modelcheck/Example.thy
+++ src/HOL/Modelcheck/Example.thy
@@ -20,11 +20,10 @@ defs
   
   INIT_def "INIT x == ~(fst x)&~(fst (snd x))&~(snd (snd x))"
 
-  N_def   "N x y  == let x1 = fst(x); x2 = fst(snd(x)); x3 = snd(snd(x));   
-	                  y1 = fst(y); y2 = fst(snd(y)); y3 = snd(snd(y)) 
-                     in (~x1&~x2&~x3 & y1&~y2&~y3) |   
-	                (x1&~x2&~x3 & ~y1&~y2&~y3) |   
-	                (x1&~x2&~x3 & y1&y2&y3)    "
+   N_def   "N      == % (x1,x2,x3) (y1,y2,y3).
+                        (~x1 & ~x2 & ~x3 &   y1 & ~y2 & ~y3) |   
+	                ( x1 & ~x2 & ~x3 &  ~y1 & ~y2 & ~y3) |   
+	                ( x1 & ~x2 & ~x3 &   y1 &  y2 &  y3)    "
   
   reach_def "reach  == mu (%Q x. INIT x | (? y. Q y & N y x))"
   
