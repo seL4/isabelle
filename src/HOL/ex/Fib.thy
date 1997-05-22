0cf887b6b343e3a561cc1287310979ff7ccafc7f
paulson <none@none>
1997-05-22 15:11:23 +0200

    New example of recdef and permutative rewriting

---
+++ src/HOL/ex/Fib.thy
@@ -0,0 +1,18 @@
+(*  Title:      ex/Fib
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1997  University of Cambridge
+
+Fibonacci numbers and other simple examples of recursive definitions
+	(the TFL package)
+*)
+
+Fib = WF_Rel +
+
+consts fib  :: "nat => nat"
+recdef fib "less_than"
+    "fib 0 = 0"
+    "fib 1 = 1"
+    "fib (Suc(Suc x)) = (fib x + fib (Suc x))"
+
+end
