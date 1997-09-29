939fc70598872860a630e751fba9f144f765d773
wenzelm <none@none>
1997-09-29 15:11:27 +0200

    obsolete;

--- src/Tools/rm_html.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/csh
-# Executed from the main Isabelle directory, this script removes all files
-# made when Isabelle was used with set MAKE_HTML
-
-rm */.theory_list.txt */*/.theory_list.txt */*/*/.theory_list.txt \
-   */index.html */*/index.html */*/*/index.html \
-   */.*.html */*/.*.html */*/*/.*.html
