6612cf35064b44346a1909f09aa7126a51f86696
wenzelm <none@none>
1997-11-03 21:12:21 +0100

    nat datatype_info moved to Nat.thy;

--- src/HOL/Nat.thy
+++ src/HOL/Nat.thy
@@ -8,6 +8,20 @@ Nat is a partial order
 
 Nat = NatDef +
 
+(*install 'automatic' induction tactic, pretending nat is a datatype
+  except for induct_tac and exhaust_tac, everything is dummy*)
+
+MLtext {|
+|> Dtype.add_datatype_info
+("nat", {case_const = Bound 0, case_rewrites = [],
+  constructors = [], induct_tac = nat_ind_tac,
+  exhaustion = natE,
+  exhaust_tac = fn v => ALLNEWSUBGOALS (res_inst_tac [("n",v)] natE)
+                                       (rotate_tac ~1),
+  nchotomy = ProtoPure.flexpair_def, case_cong = ProtoPure.flexpair_def})
+|}
+
+
 instance nat :: order (le_refl,le_trans,le_anti_sym,nat_less_le)
 
 consts
