43292bd86393c486a1dd31d52b572f2d03d723a4
oheimb <none@none>
1997-11-04 20:48:38 +0100

    added the, option_map, and case analysis theorems

--- src/HOL/Option.thy
+++ src/HOL/Option.thy
@@ -7,5 +7,15 @@ Datatype 'a option
 *)
 
 Option = Arith +
+
 datatype 'a option = None | Some 'a
+
+constdefs
+
+  the		:: "'a option => 'a"
+ "the Ú %y. case y of None => arbitrary | Some x => x"
+
+  option_map	:: "('a => 'b) => ('a option => 'b option)"
+ "option_map Ú %f y. case y of None => None | Some x => Some (f x)"
+
 end
