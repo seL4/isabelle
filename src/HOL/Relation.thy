6d8d84fb639fcb93536abcbfc67bd6153c383736
paulson <none@none>
1998-03-16 16:50:50 +0100

    inverse -> converse [It is standard terminology and also used in ZF]

--- src/HOL/Relation.thy
+++ src/HOL/Relation.thy
@@ -8,7 +8,7 @@ Relation = Prod +
 consts
     id          :: "('a * 'a)set"               (*the identity relation*)
     O           :: "[('b * 'c)set, ('a * 'b)set] => ('a * 'c)set" (infixr 60)
-    inverse    :: "('a*'b) set => ('b*'a) set"     ("(_^-1)" [1000] 999)
+    converse    :: "('a*'b) set => ('b*'a) set"     ("(_^-1)" [1000] 999)
     "^^"        :: "[('a*'b) set,'a set] => 'b set" (infixl 90)
     Domain      :: "('a*'b) set => 'a set"
     Range       :: "('a*'b) set => 'b set"
@@ -17,7 +17,7 @@ consts
 defs
     id_def        "id == {p. ? x. p = (x,x)}"
     comp_def      "r O s == {(x,z). ? y. (x,y):s & (y,z):r}"
-    inverse_def   "r^-1 == {(y,x). (x,y):r}"
+    converse_def   "r^-1 == {(y,x). (x,y):r}"
     Domain_def    "Domain(r) == {x. ? y. (x,y):r}"
     Range_def     "Range(r) == Domain(r^-1)"
     Image_def     "r ^^ s == {y. ? x:s. (x,y):r}"
