e974532e155cce9644409b609cedd95f66587a76
nipkow <none@none>
1998-04-27 16:45:11 +0200

    Added a few lemmas. Renamed expand_const -> split_const.

--- src/HOL/Fun.thy
+++ src/HOL/Fun.thy
@@ -12,15 +12,15 @@ instance set :: (term) order
                        (subset_refl,subset_trans,subset_antisym,psubset_eq)
 consts
 
-  inj, surj     :: ('a => 'b) => bool                   (*inj/surjective*)
-  inj_onto      :: ['a => 'b, 'a set] => bool
-  inv           :: ('a => 'b) => ('b => 'a)
+  inj, surj   :: ('a => 'b) => bool                   (*inj/surjective*)
+  inj_on      :: ['a => 'b, 'a set] => bool
+  inv         :: ('a => 'b) => ('b => 'a)
 
 defs
 
-  inj_def       "inj f          == ! x y. f(x)=f(y) --> x=y"
-  inj_onto_def  "inj_onto f A   == ! x:A. ! y:A. f(x)=f(y) --> x=y"
-  surj_def      "surj f         == ! y. ? x. y=f(x)"
-  inv_def       "inv(f::'a=>'b) == (% y. @x. f(x)=y)"
+  inj_def     "inj f          == ! x y. f(x)=f(y) --> x=y"
+  inj_on_def  "inj_on f A     == ! x:A. ! y:A. f(x)=f(y) --> x=y"
+  surj_def    "surj f         == ! y. ? x. y=f(x)"
+  inv_def     "inv(f::'a=>'b) == (% y. @x. f(x)=y)"
 
 end
