694a2ac5852ff7c940ef935454643f176da1aa0e
paulson <none@none>
1998-05-06 13:01:30 +0200

    Removed some traces of UNITY

--- src/HOL/Update.thy
+++ src/HOL/Update.thy
@@ -1,9 +1,9 @@
-(*  Title:      HOL/UNITY/Update.thy
+(*  Title:      HOL/Update.thy
     ID:         $Id$
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1998  University of Cambridge
 
-Function updates: like the standard theory Map, but for ordinary functions
+Function updates: like theory Map, but for ordinary functions
 *)
 
 Update = Fun +
@@ -11,10 +11,6 @@ Update = Fun +
 consts
   update  :: "('a => 'b) => 'a => 'b => ('a => 'b)"
            ("_/[_/:=/_]" [900,0,0] 900)
-
-syntax (symbols)
-  update    :: "('a => 'b) => 'a => 'b => ('a => 'b)"
-               ("_/[_/\\<mapsto>/_]" [900,0,0] 900)
 
 defs
   update_def "f[a:=b] == %x. if x=a then b else f x"
