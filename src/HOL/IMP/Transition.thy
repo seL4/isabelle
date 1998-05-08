9913a616feea8666c9cfcfeedbc682a7b61c5b66
wenzelm <none@none>
1998-05-08 15:45:01 +0200

    fixed translations;

--- src/HOL/IMP/Transition.thy
+++ src/HOL/IMP/Transition.thy
@@ -8,25 +8,32 @@ Transition semantics of commands
 
 Transition = Natural + RelPow +
 
-consts  evalc1    :: "((com*state)*(com*state))set"        
-	"@evalc1" :: "[(com*state),(com*state)] => bool"   
-				("_ -1-> _" [81,81] 100)
-	"@evalcn" :: "[(com*state),(com*state)] => nat => bool"
-				("_ -_-> _" [81,81] 100)
-	"@evalc*" :: "[(com*state),(com*state)] => bool"   
-				("_ -*-> _" [81,81] 100)
+consts  evalc1    :: "((com*state)*(com*state))set"
+
+syntax
+        "@evalc1" :: "[(com*state),(com*state)] => bool"
+                                ("_ -1-> _" [81,81] 100)
+        "@evalcn" :: "[(com*state),(com*state)] => nat => bool"
+                                ("_ -_-> _" [81,81] 100)
+        "@evalc*" :: "[(com*state),(com*state)] => bool"
+                                ("_ -*-> _" [81,81] 100)
 
 translations
-       	"cs0 -1-> cs1" == "(cs0,cs1) : evalc1"
-	"cs0 -n-> cs1" == "(cs0,cs1) : evalc1^n"
-	"cs0 -*-> cs1" == "(cs0,cs1) : evalc1^*"
+  "cs0 -1-> cs1"	== "(cs0,cs1) : evalc1"
+  "cs0 -1-> (c1,s1)"	<= "cs0 -1-> (_args c1 s1)"
+
+  "cs0 -n-> cs1" 	== "(cs0,cs1) : evalc1^n"
+  "cs0 -n-> (c1,s1)" 	<= "cs0 -n-> (_args c1 s1)"
+
+  "cs0 -*-> cs1" 	== "(cs0,cs1) : evalc1^*"
+  "cs0 -*-> (c1,s1)" 	<= "cs0 -*-> (_args c1 s1)"
 
 
 inductive evalc1
   intrs
     Assign "(x := a,s) -1-> (SKIP,s[x := a s])"
 
-    Semi1   "(SKIP;c,s) -1-> (c,s)"	
+    Semi1   "(SKIP;c,s) -1-> (c,s)"     
     Semi2   "(c0,s) -1-> (c2,s1) ==> (c0;c1,s) -1-> (c2;c1,s1)"
 
     IfTrue "b s ==> (IF b THEN c1 ELSE c2,s) -1-> (c1,s)"
