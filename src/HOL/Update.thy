2ab5d3efac6b91999e790ef9b2cb23038540e5c6
nipkow <none@none>
1998-06-23 18:06:50 +0200

    Replaced [ := ] syntax by ( := ). Also allows ( := , := , ...) now.

--- src/HOL/Update.thy
+++ src/HOL/Update.thy
@@ -10,9 +10,24 @@ Update = Fun +
 
 consts
   update  :: "('a => 'b) => 'a => 'b => ('a => 'b)"
-           ("_/[_/:=/_]" [900,0,0] 900)
+
+nonterminals
+  updbinds  updbind
+
+syntax
+
+  (* Let expressions *)
+
+  "_updbind"       :: ['a, 'a] => updbind             ("(2_ :=/ _)")
+  ""               :: updbind => updbinds             ("_")
+  "_updbinds"      :: [updbind, updbinds] => updbinds ("_,/ _")
+  "_Update"        :: ['a, updbinds] => 'a            ("_/'((_)')" [900,0] 900)
+
+translations
+  "_Update f (_updbinds b bs)"  == "_Update (_Update f b) bs"
+  "f(x:=y)"                     == "update f x y"
 
 defs
-  update_def "f[a:=b] == %x. if x=a then b else f x"
+  update_def "f(a:=b) == %x. if x=a then b else f x"
 
 end
