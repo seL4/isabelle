ff0bf5456e454e7d99beda9c24386a4d4bc3f29f
paulson <none@none>
1998-08-05 10:56:58 +0200

    Union primitives and examples

---
+++ src/HOL/UNITY/Union.thy
@@ -0,0 +1,19 @@
+(*  Title:      HOL/UNITY/Union.thy
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1998  University of Cambridge
+
+Unions of programs
+
+From Misra's Chapter 5: Asynchronous Compositions of Programs
+*)
+
+Union = SubstAx + FP +
+
+constdefs
+
+   Join :: "['a program, 'a program] => 'a program"
+    "Join prgF prgG == (|Init = Init prgF Int Init prgG,
+			 Acts = Acts prgF Un Acts prgG|)"
+
+end
