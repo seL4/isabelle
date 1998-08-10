bec4f2b80efbdfb605b2a558ce2634b97d30071c
paulson <none@none>
1998-08-10 11:51:09 +0200

    Tidying of AC, especially of AC16_WO4 using a locale

--- src/ZF/AC/AC16_WO4.thy
+++ src/ZF/AC/AC16_WO4.thy
@@ -1,29 +1,52 @@
 (*  Title:      ZF/AC/AC16_WO4.thy
     ID:         $Id$
     Author:     Krzysztof Grabczewski
+
+
+(*for all_in_lepoll_m and OUN_eq_x*)
 *)
 
 AC16_WO4 = OrderType + AC16_lemmas + Cardinal_aux +
 
 consts
 
-  ww  :: [i, i] => i
   s_u :: [i, i, i, i] => i
-  MM  :: [i, i, i] => i
-  LL  :: [i, i, i] => i
-  GG  :: [i, i, i] => i
   
 defs
 
-  ww_def  "ww(x, k) == {u:Pow(x). u eqpoll k}"
-
   s_u_def "s_u(u, t_n, k, y) == {v:t_n. u:v & k lepoll v Int y}"
 
-  MM_def  "MM(t_n, k, y) == {v:t_n. k lepoll v Int y}"
 
-  LL_def  "LL(t_n, k, y) == {v Int y. v:MM(t_n, k, y)}"
 
-  GG_def  "GG(t_n, k, y) == lam v:LL(t_n, k, y). 
-                            (THE w. w:MM(t_n, k, y) & v <= w) - v"
+
+locale AC16 =
+  fixes 
+    x	:: i
+    y	:: i
+    k	:: i
+    l   :: i
+    m	:: i
+    t_n	:: i
+    R	:: i
+    MM  :: i
+    LL  :: i
+    GG  :: i
+  assumes
+    all_ex    "ALL z:{z: Pow(x Un y) . z eqpoll succ(k)}.
+	         EX! w. w:t_n & z <= w "
+    disjoint  "x Int y = 0"
+    includes  "t_n <= {v:Pow(x Un y). v eqpoll succ(k #+ m)}"
+    WO_R      "well_ord(y,R)"
+    knat      "k:nat"
+    kpos      "k = succ(l)"
+    lnat      "l:nat"
+    mnat      "m:nat"
+    mpos      "0<m"
+    Infinite  "~ Finite(y)"
+    noLepoll  "~ y lepoll {v:Pow(x). v eqpoll m}"
+  defines
+    MM_def    "MM == {v:t_n. succ(k) lepoll v Int y}"
+    LL_def    "LL == {v Int y. v:MM}"
+    GG_def    "GG == lam v:LL. (THE w. w:MM & v <= w) - v"
 
 end
