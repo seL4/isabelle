a0b151e1c8b52ba2661175baf8a3fcf22e9f7a97
slotosch <none@none>
1998-08-12 12:17:20 +0200

    eliminated fabs,fapp. changed all theorem names and functions into Rep_CFun, Abs_CFun

--- src/HOLCF/Cfun1.thy
+++ src/HOLCF/Cfun1.thy
@@ -16,12 +16,10 @@ typedef (CFun)  ('a, 'b) "->" (infixr 0) = "{f. cont f}" (CfunI)
 (* to make << defineable *)
 instance "->"  :: (cpo,cpo)sq_ord
 
-consts  
-        fapp      :: "('a -> 'b)=>('a => 'b)" ("_`_" [999,1000] 999)
-					        (* usually Rep_Cfun *)
+syntax
+	Rep_CFun  :: "('a -> 'b)=>('a => 'b)" ("_`_" [999,1000] 999)
                                                 (* application      *)
-        fabs      :: "('a => 'b)=>('a -> 'b)"     (binder "LAM " 10)
-                                                (* usually Abs_Cfun *)
+        Abs_CFun  :: "('a => 'b)=>('a -> 'b)"     (binder "LAM " 10)
                                                 (* abstraction      *)
         less_cfun :: "[('a -> 'b),('a -> 'b)]=>bool"
 
@@ -30,9 +28,6 @@ syntax (symbols)
   "LAM "	:: "[idts, 'a => 'b] => ('a -> 'b)"
 					("(3\\<Lambda>_./ _)" [0, 10] 10)
 defs 
-  fabs_def	"fabs==Abs_CFun"
-  fapp_def	"fapp==Rep_CFun"
-
-  less_cfun_def "(op <<) == (% fo1 fo2. fapp fo1 << fapp fo2 )"
+  less_cfun_def "(op <<) == (% fo1 fo2. Rep_CFun fo1 << Rep_CFun fo2 )"
 
 end
