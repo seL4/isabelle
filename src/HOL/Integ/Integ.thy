05eccd491c333b9f44c2451fa64ec23b82b5dc72
paulson <none@none>
1998-09-18 16:04:44 +0200

    Now defines "int" as a linear order; basic derivations moved to IntDef

--- src/HOL/Integ/Integ.thy
+++ src/HOL/Integ/Integ.thy
@@ -1,53 +1,18 @@
 (*  Title:      Integ.thy
     ID:         $Id$
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
-    Copyright   1996  University of Cambridge
+    Copyright   1998  University of Cambridge
 
-The integers as equivalence classes over nat*nat.
+Type "int" is a linear order
 *)
 
-Integ = Equiv + Arith +
-constdefs
-  intrel      :: "((nat * nat) * (nat * nat)) set"
-  "intrel == {p. ? x1 y1 x2 y2. p=((x1::nat,y1),(x2,y2)) & x1+y2 = x2+y1}"
+Integ = IntDef +
 
-typedef (Integ)
-  int = "{x::(nat*nat).True}/intrel"            (Equiv.quotient_def)
-
-instance
-  int :: {ord, plus, times, minus}
-
-defs
-  zminus_def
-    "- Z == Abs_Integ(UN p:Rep_Integ(Z). split (%x y. intrel^^{(y,x)}) p)"
+instance int :: order (zle_refl,zle_trans,zle_anti_sym,int_less_le)
+instance int :: linorder (zle_linear)
 
 constdefs
-
-  znat        :: nat => int                                  ("$# _" [80] 80)
-  "$# m == Abs_Integ(intrel ^^ {(m,0)})"
-
-  znegative   :: int => bool
-  "znegative(Z) == EX x y. x<y & (x,y::nat):Rep_Integ(Z)"
-
-  zmagnitude  :: int => int
-  "zmagnitude(Z) == Abs_Integ(UN p:Rep_Integ(Z).
-                              split (%x y. intrel^^{((y-x) + (x-y),0)}) p)"
-
-defs
-  zadd_def
-   "Z1 + Z2 == 
-       Abs_Integ(UN p1:Rep_Integ(Z1). UN p2:Rep_Integ(Z2).   
-           split (%x1 y1. split (%x2 y2. intrel^^{(x1+x2, y1+y2)}) p2) p1)"
-
-  zdiff_def "Z1 - Z2 == Z1 + -(Z2::int)"
-
-  zless_def "Z1<Z2 == znegative(Z1 - Z2)"
-
-  zle_def   "Z1 <= (Z2::int) == ~(Z2 < Z1)"
-
-  zmult_def
-   "Z1 * Z2 == 
-       Abs_Integ(UN p1:Rep_Integ(Z1). UN p2:Rep_Integ(Z2). split (%x1 y1.   
-           split (%x2 y2. intrel^^{(x1*x2 + y1*y2, x1*y2 + y1*x2)}) p2) p1)"
+  zmagnitude  :: int => nat
+  "zmagnitude(Z) == @m. Z = $# m | -Z = $# m"
 
 end
