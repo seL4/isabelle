7809d59c4350308ae9db505d13e9a0c9a3046fe8
paulson <none@none>
1998-09-24 15:20:29 +0200

    added correctness proofs for arithmetic

--- src/HOL/ex/BinEx.thy
+++ src/HOL/ex/BinEx.thy
@@ -1,2 +1,27 @@
+(*  Title:      HOL/ex/BinEx.thy
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1998  University of Cambridge
 
-BinEx = Bin
+Definition of normal form for proving that binary arithmetic on
+ormalized operands yields normalized results.
+
+Normal means no leading 0s on positive numbers and no leading 1s on negatives.
+*)
+
+BinEx = Bin +
+
+consts normal :: bin set
+  
+inductive "normal"
+  intrs 
+
+    Pls  "Pls: normal"
+
+    Min  "Min: normal"
+
+    BIT_F  "[| w: normal; w ~= Pls |] ==> w BIT False : normal"
+
+    BIT_T  "[| w: normal; w ~= Min |] ==> w BIT True : normal"
+
+end
