d7015fcc27e69a45b14f91867db46e71d5f25039
nipkow <none@none>
1998-10-01 18:30:44 +0200

    Improved definition of foldSet.

--- src/HOL/Induct/FoldSet.thy
+++ src/HOL/Induct/FoldSet.thy
@@ -1,16 +1,16 @@
 (*  Title:      HOL/FoldSet
     ID:         $Id$
-    Author:     Lawrence C Paulson
+    Author:     Lawrence C Paulson, Tobias Nipkow
     Copyright   1998  University of Cambridge
 
 A "fold" functional for finite sets.  For n non-negative we have
     fold f e {x1,...,xn} = f x1 (... (f xn e))
-where f is an associative-commutative function and e is its identity.
+where f is at least left-commutative.
 *)
 
 FoldSet = Finite +
 
-consts foldSet :: "[['a,'a] => 'a, 'a] => ('a set * 'a) set"
+consts foldSet :: "[['b,'a] => 'a, 'a] => ('b set * 'a) set"
 
 inductive "foldSet f e"
   intrs
@@ -19,10 +19,22 @@ inductive "foldSet f e"
     insertI  "[| x ~: A;  (A,y) : foldSet f e |]
 	      ==> (insert x A, f x y) : foldSet f e"
 
-constdefs fold :: "[['a,'a] => 'a, 'a, 'a set] => 'a"
+constdefs
+   fold :: "[['b,'a] => 'a, 'a, 'b set] => 'a"
   "fold f e A == @x. (A,x) : foldSet f e"
-  
-locale ACI =
+  (* A frequent instance: *)
+   setsum :: ('a => nat) => 'a set => nat
+  "setsum f == fold (%x n. f x + n) 0"
+
+locale LC =
+  fixes
+    f    :: ['b,'a] => 'a
+  assumes
+    lcomm    "!! x y z. f x (f y z) = f y (f x z)"
+  defines
+    (*nothing*)
+
+locale ACe =
   fixes 
     f    :: ['a,'a] => 'a
     e    :: 'a
@@ -32,6 +44,5 @@ locale ACI =
     assoc    "!! x y z. f (f x y) z = f x (f y z)"
   defines
     (*nothing*)
+
 end
-
-
