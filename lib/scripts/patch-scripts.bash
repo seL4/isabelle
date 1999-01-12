f520854fabe92b418396a5404b09aa09177665aa
wenzelm <none@none>
1999-01-12 13:37:40 +0100

    fixed AUTO_PERL;

--- lib/scripts/patch-scripts.bash
+++ lib/scripts/patch-scripts.bash
@@ -40,7 +40,7 @@ do
   if [ -x "$FILE" ]; then
     sed -e "s:^#!.*/bash:#!$BASH:" -e "s:^#!.*/perl:#!$PERL:" \
       -e "s:^AUTO_BASH=.*/bash:AUTO_BASH=$BASH:" \
-      -e "s:^AUTO_PERL=.*/bash:AUTO_PERL=$PERL:" $FILE >$FILE~~
+      -e "s:^AUTO_PERL=.*/perl:AUTO_PERL=$PERL:" $FILE >$FILE~~
     if cmp -s $FILE $FILE~~; then
       rm $FILE~~
     else
