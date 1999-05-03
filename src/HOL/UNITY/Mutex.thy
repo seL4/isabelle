ea475b338632717ec6fbe65c0653f682571d0404
paulson <none@none>
1999-05-03 11:18:44 +0200

    renamed state variables

--- src/HOL/UNITY/Mutex.thy
+++ src/HOL/UNITY/Mutex.thy
@@ -9,68 +9,69 @@ Based on "A Family of 2-Process Mutual Exclusion Algorithms" by J Misra
 Mutex = SubstAx +
 
 record state =
-  PP :: bool
-  MM :: int
-  NN :: int
-  UU :: bool
-  VV :: bool
+  p :: bool
+  m :: int
+  n :: int
+  u :: bool
+  v :: bool
+
+types command = "(state*state) set"
 
 constdefs
   
   (** The program for process U **)
   
-  cmd0U :: "(state*state) set"
-    "cmd0U == {(s,s'). s' = s (|UU:=True, MM:=#1|) & MM s = #0}"
+  U0 :: command
+    "U0 == {(s,s'). s' = s (|u:=True, m:=#1|) & m s = #0}"
 
-  cmd1U :: "(state*state) set"
-    "cmd1U == {(s,s'). s' = s (|PP:= VV s, MM:=#2|) & MM s = #1}"
+  U1 :: command
+    "U1 == {(s,s'). s' = s (|p:= v s, m:=#2|) & m s = #1}"
 
-  cmd2U :: "(state*state) set"
-    "cmd2U == {(s,s'). s' = s (|MM:=#3|) & ~ PP s & MM s = #2}"
+  U2 :: command
+    "U2 == {(s,s'). s' = s (|m:=#3|) & ~ p s & m s = #2}"
 
-  cmd3U :: "(state*state) set"
-    "cmd3U == {(s,s'). s' = s (|UU:=False, MM:=#4|) & MM s = #3}"
+  U3 :: command
+    "U3 == {(s,s'). s' = s (|u:=False, m:=#4|) & m s = #3}"
 
-  cmd4U :: "(state*state) set"
-    "cmd4U == {(s,s'). s' = s (|PP:=True, MM:=#0|) & MM s = #4}"
+  U4 :: command
+    "U4 == {(s,s'). s' = s (|p:=True, m:=#0|) & m s = #4}"
 
   (** The program for process V **)
   
-  cmd0V :: "(state*state) set"
-    "cmd0V == {(s,s'). s' = s (|VV:=True, NN:=#1|) & NN s = #0}"
+  V0 :: command
+    "V0 == {(s,s'). s' = s (|v:=True, n:=#1|) & n s = #0}"
 
-  cmd1V :: "(state*state) set"
-    "cmd1V == {(s,s'). s' = s (|PP:= ~ UU s, NN:=#2|) & NN s = #1}"
+  V1 :: command
+    "V1 == {(s,s'). s' = s (|p:= ~ u s, n:=#2|) & n s = #1}"
 
-  cmd2V :: "(state*state) set"
-    "cmd2V == {(s,s'). s' = s (|NN:=#3|) & PP s & NN s = #2}"
+  V2 :: command
+    "V2 == {(s,s'). s' = s (|n:=#3|) & p s & n s = #2}"
 
-  cmd3V :: "(state*state) set"
-    "cmd3V == {(s,s'). s' = s (|VV:=False, NN:=#4|) & NN s = #3}"
+  V3 :: command
+    "V3 == {(s,s'). s' = s (|v:=False, n:=#4|) & n s = #3}"
 
-  cmd4V :: "(state*state) set"
-    "cmd4V == {(s,s'). s' = s (|PP:=False, NN:=#0|) & NN s = #4}"
+  V4 :: command
+    "V4 == {(s,s'). s' = s (|p:=False, n:=#0|) & n s = #4}"
 
-  Mprg :: state program
-    "Mprg == mk_program ({s. ~ UU s & ~ VV s & MM s = #0 & NN s = #0},
-			 {cmd0U, cmd1U, cmd2U, cmd3U, cmd4U, 
-			  cmd0V, cmd1V, cmd2V, cmd3V, cmd4V})"
+  Mutex :: state program
+    "Mutex == mk_program ({s. ~ u s & ~ v s & m s = #0 & n s = #0},
+		 	  {U0, U1, U2, U3, U4, V0, V1, V2, V3, V4})"
 
 
   (** The correct invariants **)
 
-  invariantU :: "state set"
-    "invariantU == {s. (UU s = (#1 <= MM s & MM s <= #3)) &
-		       (MM s = #3 --> ~ PP s)}"
+  invariantU :: state set
+    "invariantU == {s. (u s = (#1 <= m s & m s <= #3)) &
+		       (m s = #3 --> ~ p s)}"
 
-  invariantV :: "state set"
-    "invariantV == {s. (VV s = (#1 <= NN s & NN s <= #3)) &
-		       (NN s = #3 --> PP s)}"
+  invariantV :: state set
+    "invariantV == {s. (v s = (#1 <= n s & n s <= #3)) &
+		       (n s = #3 --> p s)}"
 
   (** The faulty invariant (for U alone) **)
 
-  bad_invariantU :: "state set"
-    "bad_invariantU == {s. (UU s = (#1 <= MM s & MM s <= #3)) &
-		           (#3 <= MM s & MM s <= #4 --> ~ PP s)}"
+  bad_invariantU :: state set
+    "bad_invariantU == {s. (u s = (#1 <= m s & m s <= #3)) &
+		           (#3 <= m s & m s <= #4 --> ~ p s)}"
 
 end
