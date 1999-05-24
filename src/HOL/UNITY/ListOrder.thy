f48984e1cd8c935ad34c03298141d54f91972d76
paulson <none@none>
1999-05-24 15:47:57 +0200

    lists are partially ordered by the prefix relation

---
+++ src/HOL/UNITY/ListOrder.thy
@@ -0,0 +1,14 @@
+(*  Title:      HOL/UNITY/ListOrder
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1998  University of Cambridge
+
+Lists are partially ordered by the prefix relation
+*)
+
+ListOrder = Prefix +
+
+instance list :: (term) order
+    (prefix_refl,prefix_trans,prefix_antisym,prefix_less_le)
+
+end
