51f8ab7365a6edc9e602508869e1b2a4c40f42ea
wenzelm <none@none>
1999-06-05 20:37:29 +0200

    added Ord.order_antisym;

--- src/HOL/Calculation.thy
+++ src/HOL/Calculation.thy
@@ -7,15 +7,14 @@ Setup transitivity rules for calculational proofs.
 
 theory Calculation = Int:
 
-
-theorems[trans] = HOL.trans			(*  =  =  =  *)
-theorems[trans] = HOL.ssubst                    (*  =  *  *  *)
-theorems[trans] = HOL.subst[COMP swap_prems_rl]	(*  *  =  *  *)
+theorems[trans] = HOL.ssubst                    (*  =  x  x  *)
+theorems[trans] = HOL.subst[COMP swap_prems_rl]	(*  x  =  x  *)
 
 theorems[trans] = Ord.order_trans		(*  <= <= <= *)
 theorems[trans] = Ord.order_less_trans		(*  <  <  <  *)
 theorems[trans] = Ord.order_le_less_trans	(*  <= <  <  *)
 theorems[trans] = Ord.order_less_le_trans	(*  <  <= <  *)
+theorems[trans] = Ord.order_antisym		(*  <= <= =  *)
 
 theorems[trans] = Divides.dvd_trans		(* dvd dvd dvd *)
 
