ff3bd652d06364f769d9a9148bb65d95ad2da892
wenzelm <none@none>
1999-06-30 16:00:06 +0200

    antisym first;

--- src/HOL/Calculation.thy
+++ src/HOL/Calculation.thy
@@ -7,22 +7,22 @@ Setup transitivity rules for calculational proofs.
 
 theory Calculation = Int:;
 
+theorems [trans] = Ord.order_antisym;                   (*  <= <= =  *)
 theorems [trans] = Ord.order_trans;                     (*  <= <= <= *)
 theorems [trans] = Ord.order_less_trans;                (*  <  <  <  *)
 theorems [trans] = Ord.order_le_less_trans;             (*  <= <  <  *)
 theorems [trans] = Ord.order_less_le_trans;             (*  <  <= <  *)
-theorems [trans] = Ord.order_antisym;                   (*  <= <= =  *)
 
-theorem [trans]: "[| x <= y; y = z |] ==> x <= z";
+theorem [trans]: "[| x <= y; y = z |] ==> x <= z";	(*  <= =  <= *)
   by (rule HOL.subst[with y z]);
 
-theorem [trans]: "[| x = y; y <= z |] ==> x <= z";
+theorem [trans]: "[| x = y; y <= z |] ==> x <= z";	(*  =  <= <= *)
   by (rule HOL.ssubst[with x y]);
 
-theorem [trans]: "[| x < y; y = z |] ==> x < z";
+theorem [trans]: "[| x < y; y = z |] ==> x < z";	(*  <  =  <  *)
   by (rule HOL.subst[with y z]);
 
-theorem [trans]: "[| x = y; y < z |] ==> x < z";
+theorem [trans]: "[| x = y; y < z |] ==> x < z";	(*  =  <  <  *)
   by (rule HOL.ssubst[with x y]);
 
 theorems [trans] = HOL.subst[COMP swap_prems_rl];       (*  x  =  x  *)
