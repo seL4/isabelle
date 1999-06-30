e12390e495461327e83a1c1deae3b5b603d0fe25
wenzelm <none@none>
1999-06-30 13:42:47 +0200

    more robust trans rules;

--- src/HOL/Calculation.thy
+++ src/HOL/Calculation.thy
@@ -5,18 +5,30 @@
 Setup transitivity rules for calculational proofs.
 *)
 
-theory Calculation = Int:
+theory Calculation = Int:;
 
-theorems[trans] = HOL.ssubst                    (*  =  x  x  *)
-theorems[trans] = HOL.subst[COMP swap_prems_rl]	(*  x  =  x  *)
+theorems [trans] = Ord.order_trans;                     (*  <= <= <= *)
+theorems [trans] = Ord.order_less_trans;                (*  <  <  <  *)
+theorems [trans] = Ord.order_le_less_trans;             (*  <= <  <  *)
+theorems [trans] = Ord.order_less_le_trans;             (*  <  <= <  *)
+theorems [trans] = Ord.order_antisym;                   (*  <= <= =  *)
 
-theorems[trans] = Ord.order_trans		(*  <= <= <= *)
-theorems[trans] = Ord.order_less_trans		(*  <  <  <  *)
-theorems[trans] = Ord.order_le_less_trans	(*  <= <  <  *)
-theorems[trans] = Ord.order_less_le_trans	(*  <  <= <  *)
-theorems[trans] = Ord.order_antisym		(*  <= <= =  *)
+theorem [trans]: "[| x <= y; y = z |] ==> x <= z";
+  by (rule HOL.subst[with y z]);
 
-theorems[trans] = Divides.dvd_trans		(* dvd dvd dvd *)
+theorem [trans]: "[| x = y; y <= z |] ==> x <= z";
+  by (rule HOL.ssubst[with x y]);
+
+theorem [trans]: "[| x < y; y = z |] ==> x < z";
+  by (rule HOL.subst[with y z]);
+
+theorem [trans]: "[| x = y; y < z |] ==> x < z";
+  by (rule HOL.ssubst[with x y]);
+
+theorems [trans] = HOL.subst[COMP swap_prems_rl];       (*  x  =  x  *)
+theorems [trans] = HOL.ssubst;                          (*  =  x  x  *)
+
+theorems [trans] = Divides.dvd_trans;                   (* dvd dvd dvd *)
 
 
-end
+end;
