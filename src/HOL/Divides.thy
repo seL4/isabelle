f19f24fa0ab2dd4b36dfa7e2bd53bc5a2e6ebc80
paulson <none@none>
1999-07-01 10:33:50 +0200

    now div and mod are overloaded; dvd is polymorphic

--- src/HOL/Divides.thy
+++ src/HOL/Divides.thy
@@ -1,24 +1,39 @@
 (*  Title:      HOL/Divides.thy
     ID:         $Id$
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
-    Copyright   1993  University of Cambridge
+    Copyright   1999  University of Cambridge
 
 The division operators div, mod and the divides relation "dvd"
 *)
 
 Divides = Arith +
 
+(*We use the same sort for div and mod;
+  moreover, dvd is defined whenever multiplication is*)
+axclass
+  div < term
+
+instance
+  nat :: {div}
+
 consts
-  div, mod  :: [nat, nat] => nat          (infixl 70)
-  dvd     :: [nat,nat]=>bool              (infixl 70) 
+  div  :: ['a::div, 'a]  => 'a          (infixl 70)
+  mod  :: ['a::div, 'a]  => 'a          (infixl 70)
+  dvd  :: ['a::times, 'a] => bool       (infixl 70) 
 
 
+(*Remainder and quotient are defined here by algorithms and later proved to
+  satisfy the traditional definition (theorem mod_div_equality)
+*)
 defs
+
   mod_def   "m mod n == wfrec (trancl pred_nat)
                           (%f j. if j<n then j else f (j-n)) m"
+
   div_def   "m div n == wfrec (trancl pred_nat) 
                           (%f j. if j<n then 0 else Suc (f (j-n))) m"
 
+(*The definition of dvd is polymorphic!*)
   dvd_def   "m dvd n == EX k. n = m*k"
 
 end
