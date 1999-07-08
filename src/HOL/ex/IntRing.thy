b283262684444e4a8f62fd6f090ace6e72afcea9
paulson <none@none>
1999-07-08 13:48:11 +0200

    Renaming of theorems from _nat0 to _int0 and _nat1 to _int1

--- src/HOL/ex/IntRing.thy
+++ src/HOL/ex/IntRing.thy
@@ -10,7 +10,7 @@ With an application of Lagrange's lemma.
 IntRing = IntRingDefs + Lagrange +
 
 instance int :: add_semigroup (zadd_assoc)
-instance int :: add_monoid (zero_int_def,zadd_nat0,zadd_nat0_right)
+instance int :: add_monoid (zero_int_def,zadd_int0,zadd_int0_right)
 instance int :: add_group (left_inv_int,minus_inv_int)
 instance int :: add_agroup (zadd_commute)
 instance int :: ring (zmult_assoc,zadd_zmult_distrib2,zadd_zmult_distrib)
