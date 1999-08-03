fc3bb1f3dd0e965576c8d4ca3bff432a90441fa2
paulson <none@none>
1999-08-03 13:08:58 +0200

    biconditionals and the natural numbers

--- src/HOL/SVC_Oracle.thy
+++ src/HOL/SVC_Oracle.thy
@@ -9,5 +9,10 @@ Based upon the work of Søren T. Heilmann
 *)
 
 SVC_Oracle = NatBin + (**Real?? + **)
+
+consts
+  (*reserved for the oracle*)
+  iff_keep, iff_unfold :: [bool, bool] => bool 
+
 oracle svc_oracle = Svc.oracle
 end
