9dac6d1b7dc3deb2b9e46dbb6508a72fb0448ca3
paulson <none@none>
1999-08-06 17:29:18 +0200

    re-organization of theorems from Alloc and PPROD, partly into new theory Lift_prog

--- src/HOL/UNITY/PPROD.thy
+++ src/HOL/UNITY/PPROD.thy
@@ -3,36 +3,14 @@
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   1998  University of Cambridge
 
-General products of programs (Pi operation), for replicating components.
+Abstraction over replicated components (PLam)
+General products of programs (Pi operation)
 *)
 
-PPROD = Union + Comp +
+PPROD = Lift_prog +
 
 constdefs
 
-  lift_set :: "['a, 'b set] => ('a => 'b) set"
-    "lift_set i A == {f. f i : A}"
-
-  drop_set :: "['a, ('a=>'b) set] => 'b set"
-    "drop_set i A == (%f. f i) `` A"
-
-  lift_act :: "['a, ('b*'b) set] => (('a=>'b) * ('a=>'b)) set"
-    "lift_act i act == {(f,f'). f(i:= f' i) = f' & (f i, f' i) : act}"
-
-  drop_act :: "['a, (('a=>'b) * ('a=>'b)) set] => ('b*'b) set"
-    "drop_act i act == (%(f,f'). (f i, f' i)) `` act"
-
-  lift_prog :: "['a, 'b program] => ('a => 'b) program"
-    "lift_prog i F ==
-       mk_program (lift_set i (Init F),
-		   lift_act i `` Acts F)"
-
-  drop_prog :: "['a, ('a=>'b) program] => 'b program"
-    "drop_prog i F ==
-       mk_program (drop_set i (Init F),
-		   drop_act i `` (Acts F))"
-
-  (*products of programs*)
   PLam  :: ['a set, 'a => 'b program] => ('a => 'b) program
     "PLam I F == JN i:I. lift_prog i (F i)"
 
