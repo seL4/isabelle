63585418848ec45a749ed2c7dbb9980ebd410634
wenzelm <none@none>
1999-08-09 22:25:27 +0200

    added asym rule;

--- src/HOL/Calculation.thy
+++ src/HOL/Calculation.thy
@@ -9,30 +9,33 @@ list below later rules have priority.
 theory Calculation = Int:;
 
 
-theorems [trans] = HOL.ssubst;                          (*  =  x  x  *)
-theorems [trans] = HOL.subst[COMP swap_prems_rl];       (*  x  =  x  *)
+theorems [trans] = HOL.ssubst;                              (*  =  x  x  *)
+theorems [trans] = HOL.subst[COMP swap_prems_rl];           (*  x  =  x  *)
 
-theorems [trans] = Divides.dvd_trans;                   (* dvd dvd dvd *)
+theorems [trans] = Divides.dvd_trans;                       (* dvd dvd dvd *)
 
-theorems [trans] = Ord.order_less_trans;                (*  <  <  <  *)
-theorems [trans] = Ord.order_le_less_trans;             (*  <= <  <  *)
-theorems [trans] = Ord.order_less_le_trans;             (*  <  <= <  *)
-theorems [trans] = Ord.order_trans;                     (*  <= <= <= *)
-theorems [trans] = Ord.order_antisym;                   (*  <= <= =  *)
+theorem [trans]: "[| (x::'a::order) < y; y < x |] ==> P";   (*  <  >  P  *)
+  by (rule Ord.order_less_asym);
 
-theorem [trans]: "[| x <= y; y = z |] ==> x <= z";	(*  <= =  <= *)
+theorems [trans] = Ord.order_less_trans;                    (*  <  <  <  *)
+theorems [trans] = Ord.order_le_less_trans;                 (*  <= <  <  *)
+theorems [trans] = Ord.order_less_le_trans;                 (*  <  <= <  *)
+theorems [trans] = Ord.order_trans;                         (*  <= <= <= *)
+theorems [trans] = Ord.order_antisym;                       (*  <= >= =  *)
+
+theorem [trans]: "[| x <= y; y = z |] ==> x <= z";	    (*  <= =  <= *)
   by (rule HOL.subst);
 
-theorem [trans]: "[| x = y; y <= z |] ==> x <= z";	(*  =  <= <= *)
+theorem [trans]: "[| x = y; y <= z |] ==> x <= z";	    (*  =  <= <= *)
   by (rule HOL.ssubst);
 
-theorem [trans]: "[| x < y; y = z |] ==> x < z";	(*  <  =  <  *)
+theorem [trans]: "[| x < y; y = z |] ==> x < z";	    (*  <  =  <  *)
   by (rule HOL.subst);
 
-theorem [trans]: "[| x = y; y < z |] ==> x < z";	(*  =  <  <  *)
+theorem [trans]: "[| x = y; y < z |] ==> x < z";	    (*  =  <  <  *)
   by (rule HOL.ssubst);
 
-theorems [trans] = HOL.trans                            (*  =  =  =  *)
+theorems [trans] = HOL.trans                                (*  =  =  =  *)
 
 
 end;
