211633561a76720638305de23bf47efbdb3631f9
wenzelm <none@none>
1999-08-17 22:11:05 +0200

    turned SVC_Oracle into a new-style theory in order to get automatic   handling of its additional dependency on Tools/svc_funcs.ML;

--- src/HOL/SVC_Oracle.thy
+++ src/HOL/SVC_Oracle.thy
@@ -1,4 +1,4 @@
-(*  Title:      HOL/SVC_Oracle
+(*  Title:      HOL/SVC_Oracle.thy
     ID:         $Id$
     Author:     Lawrence C Paulson
     Copyright   1999  University of Cambridge
@@ -8,11 +8,15 @@ Installing the oracle for SVC (Stanford Validity Checker)
 Based upon the work of Søren T. Heilmann
 *)
 
-SVC_Oracle = NatBin + (**Real?? + **)
+theory SVC_Oracle = NatBin (** + Real??**)
+files "Tools/svc_funcs.ML":
 
 consts
   (*reserved for the oracle*)
-  iff_keep, iff_unfold :: [bool, bool] => bool 
+  iff_keep :: "[bool, bool] => bool"
+  iff_unfold :: "[bool, bool] => bool"
 
-oracle svc_oracle = Svc.oracle
+oracle
+  svc_oracle = Svc.oracle
+
 end
