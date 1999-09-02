6deca7eb114a7b572bf30125ad0603d4113467e3
wenzelm <none@none>
1999-09-02 15:25:19 +0200

    renamed NatSum to Summation;

--- src/HOL/Isar_examples/Summation.thy
+++ src/HOL/Isar_examples/Summation.thy
@@ -1,15 +1,14 @@
-(*  Title:      HOL/Isar_examples/NatSum.thy
+(*  Title:      HOL/Isar_examples/Summation.thy
     ID:         $Id$
     Author:     Markus Wenzel
 
 Summing natural numbers, squares and cubes (see HOL/ex/NatSum for the
-original scripts).
-
-Demonstrates mathematical induction together with calculational proof.
+original scripts).  Demonstrates mathematical induction together with
+calculational proof.
 *)
 
 
-theory NatSum = Main:;
+theory Summation = Main:;
 
 
 section {* Summing natural numbers *};
@@ -31,7 +30,7 @@ translations
 
 subsection {* Summation laws *};
 
-(* FIXME fails with binary numeral (why!?) *)
+(* FIXME binary arithmetic does not yet work here *)
 
 syntax
   "3" :: nat  ("3")
@@ -43,57 +42,53 @@ translations
   "4" == "Suc 3"
   "6" == "Suc (Suc 4)";
 
-
-(* FIXME bind_thms in Arith.ML *)
-theorems mult_distrib [simp] = add_mult_distrib add_mult_distrib2;
-theorems mult_ac [simp] = mult_assoc mult_commute mult_left_commute;
+theorems [simp] = add_mult_distrib add_mult_distrib2 mult_ac;
 
 
 theorem sum_of_naturals: "2 * (SUM i < n + 1. i) = n * (n + 1)"
-  (is "??P n" is "??L n = _");
+  (is "??P n" is "??S n = _");
 proof (induct n);
   show "??P 0"; by simp;
 
   fix n;
-  have "??L (n + 1) = ??L n + 2 * (n + 1)"; by simp;
-  also; assume "??L n = n * (n + 1)";
+  have "??S (n + 1) = ??S n + 2 * (n + 1)"; by simp;
+  also; assume "??S n = n * (n + 1)";
   also; have "... + 2 * (n + 1) = (n + 1) * (n + 2)"; by simp;
   finally; show "??P (Suc n)"; by simp;
 qed;
 
 theorem sum_of_odds: "(SUM i < n. 2 * i + 1) = n^2"
-  (is "??P n" is "??L n = _");
+  (is "??P n" is "??S n = _");
 proof (induct n);
   show "??P 0"; by simp;
 
   fix n;
-  have "??L (n + 1) = ??L n + 2 * n + 1"; by simp;
-  also; assume "??L n = n^2";
+  have "??S (n + 1) = ??S n + 2 * n + 1"; by simp;
+  also; assume "??S n = n^2";
   also; have "... + 2 * n + 1 = (n + 1)^2"; by simp;
   finally; show "??P (Suc n)"; by simp;
 qed;
 
 theorem sum_of_squares: "6 * (SUM i < n + 1. i^2) = n * (n + 1) * (2 * n + 1)"
-  (is "??P n" is "??L n = _");
+  (is "??P n" is "??S n = _");
 proof (induct n);
   show "??P 0"; by simp;
 
   fix n;
-  have "??L (n + 1) = ??L n + 6 * (n + 1)^2"; by simp;
-  also; assume "??L n = n * (n + 1) * (2 * n + 1)";
+  have "??S (n + 1) = ??S n + 6 * (n + 1)^2"; by simp;
+  also; assume "??S n = n * (n + 1) * (2 * n + 1)";
   also; have "... + 6 * (n + 1)^2 = (n + 1) * (n + 2) * (2 * (n + 1) + 1)"; by simp;
-     (* FIXME #6: arith and simp fail!! *)
   finally; show "??P (Suc n)"; by simp;
 qed;
 
 theorem sum_of_cubes: "4 * (SUM i < n + 1. i^3) = (n * (n + 1))^2"
-  (is "??P n" is "??L n = _");
+  (is "??P n" is "??S n = _");
 proof (induct n);
   show "??P 0"; by simp;
 
   fix n;
-  have "??L (n + 1) = ??L n + 4 * (n + 1)^3"; by simp;
-  also; assume "??L n = (n * (n + 1))^2";
+  have "??S (n + 1) = ??S n + 4 * (n + 1)^3"; by simp;
+  also; assume "??S n = (n * (n + 1))^2";
   also; have "... + 4 * (n + 1)^3 = ((n + 1) * ((n + 1) + 1))^2"; by simp;
   finally; show "??P (Suc n)"; by simp;
 qed;
