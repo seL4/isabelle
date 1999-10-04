350aa60c5b9c74c0309d63592e955a39b358662f
wenzelm <none@none>
1999-10-04 21:43:45 +0200

    load / setup datatype package;

--- src/HOL/Inductive.thy
+++ src/HOL/Inductive.thy
@@ -1,7 +1,19 @@
+(*  Title:      HOL/Inductive.thy
+    ID:         $Id$
+*)
 
 theory Inductive = Gfp + Prod + Sum
-files "Tools/inductive_package.ML":
+files
+  "Tools/inductive_package.ML"
+  "Tools/datatype_aux.ML"
+  "Tools/datatype_prop.ML"
+  "Tools/datatype_rep_proofs.ML"
+  "Tools/datatype_abs_proofs.ML"
+  "Tools/datatype_package.ML"
+  "Tools/primrec_package.ML":
 
 setup InductivePackage.setup
+setup DatatypePackage.setup
+
 
 end
