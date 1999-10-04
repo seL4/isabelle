c1bc46bc8248eabb5012a193af7cb7e282d9a2d6
wenzelm <none@none>
1999-10-04 21:44:07 +0200

    load / setup recdef package (TFL);

--- src/HOL/Recdef.thy
+++ src/HOL/Recdef.thy
@@ -1,6 +1,36 @@
+(*  Title:      HOL/Recdef.thy
+    ID:         $Id$
+    Author:     Konrad Slind
 
-theory Recdef = WF_Rel
-files "Tools/recdef_package.ML" "Tools/induct_method.ML":
+TFL: recursive function definitions.
+*)
+
+theory Recdef = WF_Rel + Datatype
+files
+  (*establish a base of common and/or helpful functions*)
+  "../TFL/utils.sig"
+
+  "../TFL/usyntax.sig"
+  "../TFL/rules.sig"
+  "../TFL/thry.sig"
+  "../TFL/thms.sig"
+  "../TFL/tfl.sig"
+  "../TFL/utils.sml"
+
+  (*supply implementations*)
+  "../TFL/usyntax.sml"
+  "../TFL/thms.sml"
+  "../TFL/dcterm.sml"
+  "../TFL/rules.sml"
+  "../TFL/thry.sml"
+
+  (*link system and specialize for Isabelle*)
+  "../TFL/tfl.sml"
+  "../TFL/post.sml"
+
+  (*theory extender wrapper module*)
+  "Tools/recdef_package.ML"
+  "Tools/induct_method.ML":
 
 setup RecdefPackage.setup
 setup InductMethod.setup
