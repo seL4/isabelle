5d554691f6c734b30d9e06d3c8d4f076a4201910
paulson <none@none>
1999-10-04 13:47:28 +0200

    working snapshot (even Alloc)

--- src/HOL/UNITY/Project.thy
+++ src/HOL/UNITY/Project.thy
@@ -8,15 +8,4 @@ Projections of state sets (also of actions and programs)
 Inheritance of GUARANTEES properties under extension
 *)
 
-Project = Extend +
-
-
-constdefs
-
-  restr_act :: "['c set, 'a*'b => 'c, ('a*'a) set] => ('a*'a) set"
-    "restr_act C h act == project_act C h (extend_act h act)"
-
-  restr :: "['c set, 'a*'b => 'c, 'a program] => 'a program"
-    "restr C h F == project C h (extend h F)"
-
-end
+Project = Extend
