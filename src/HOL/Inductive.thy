c58ba8ef51759b6c7451d41925d9a8ac65dda614
berghofe <none@none>
1999-10-05 15:23:22 +0200

    Monotonicity rules for inductive definitions can now be added to a theory via attribute "mono".

--- src/HOL/Inductive.thy
+++ src/HOL/Inductive.thy
@@ -15,5 +15,11 @@ files
 setup InductivePackage.setup
 setup DatatypePackage.setup
 
+theorems [mono] =
+   subset_refl imp_refl disj_mono conj_mono ex_mono all_mono
+   Collect_mono in_mono vimage_mono
+   imp_conv_disj not_not de_Morgan_disj de_Morgan_conj
+   not_all not_ex
+   Ball_def Bex_def
 
 end
