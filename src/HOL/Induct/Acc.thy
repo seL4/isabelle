bd16a656addd18947ad01f45c2feb8ec80c6bf5e
wenzelm <none@none>
1999-10-14 15:05:35 +0200

    fixed comment;

--- src/HOL/Induct/Acc.thy
+++ src/HOL/Induct/Acc.thy
@@ -9,7 +9,7 @@ See Ch. Paulin-Mohring, Inductive Definitions in the System Coq.
 Research Report 92-49, LIP, ENS Lyon.  Dec 1992.
 *)
 
-header {* The acessible part of a relation *};
+header {* The accessible part of a relation *};
 
 theory Acc = WF + Inductive:;
 
