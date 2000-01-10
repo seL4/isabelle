9f6888e2dbf54a647c2d7f374610c2d53ea2db2a
nipkow <none@none>
2000-01-10 16:07:29 +0100

    int:nat->int is pushed inwards.

--- src/HOL/MicroJava/J/JBasis.thy
+++ src/HOL/MicroJava/J/JBasis.thy
@@ -13,7 +13,4 @@ constdefs
   unique  :: "('a \\<times> 'b) list \\<Rightarrow> bool"
  "unique  \\<equiv> nodups \\<circ> map fst"
 
-  list_all2 :: "('a \\<Rightarrow> 'b \\<Rightarrow> bool) \\<Rightarrow> ('a list \\<Rightarrow> 'b list \\<Rightarrow> bool)"
- "list_all2 P xs ys \\<equiv> length xs = length ys \\<and> (\\<forall> (x,y)\\<in>set (zip xs ys). P x y)"
-
 end
