e5bfd153bf37550bdc4222a9d5401549dd6fc55f
nipkow <none@none>
2000-03-24 08:56:48 +0100

    comments

--- src/HOL/PreList.thy
+++ src/HOL/PreList.thy
@@ -1,5 +1,14 @@
+(*  Title:      HOL/List.thy
+    ID:         $Id$
+    Author:     Tobias Nipkow
+    Copyright   2000 TU Muenchen
+
+A basis for building theory List on. Is defined separately to serve as a
+basis for theory ToyList in the documentation.
+*)
 
 theory PreList =
-  Option + WF_Rel + NatBin + Recdef + Record + RelPow + Sexp + Calculation + SVC_Oracle:
+  Option + WF_Rel + NatBin + Recdef + Record + RelPow + Sexp + Calculation
+  + SVC_Oracle:
 
 end
