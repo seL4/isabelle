1eb1b2be62d4e9812c18fbbe4e4d3d9151ff94dc
paulson <none@none>
2000-03-27 17:04:03 +0200

    added an order-sorted version of quickSort

--- src/HOL/ex/Qsort.thy
+++ src/HOL/ex/Qsort.thy
@@ -7,6 +7,8 @@ Quicksort
 *)
 
 Qsort = Sorting +
+
+(*Version one: higher-order*)
 consts qsort :: "((['a,'a] => bool) * 'a list) => 'a list"
 
 recdef qsort "measure (size o snd)"
@@ -16,5 +18,16 @@ recdef qsort "measure (size o snd)"
     
     "qsort(le, x#xs) = qsort(le, [y:xs . ~ le x y])  
                        @ (x # qsort(le, [y:xs . le x y]))"
+
+
+(*Version two: type classes*)
+consts quickSort :: "('a::linorder) list => 'a list"
+
+recdef quickSort "measure size"
+    simpset "simpset() addsimps [length_filter RS le_less_trans]"
+    
+    "quickSort []   = []"
+    
+    "quickSort (x#l) = quickSort [y:l. ~ x<=y] @ (x # quickSort [y:l. x<=y])"
 
 end
