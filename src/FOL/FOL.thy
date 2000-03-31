58b7525b7f18c84becad4ff272b60e8fd2613039
wenzelm <none@none>
2000-03-31 22:22:23 +0200

    added cong atts;

--- src/FOL/FOL.thy
+++ src/FOL/FOL.thy
@@ -9,7 +9,7 @@ use "FOL_lemmas1.ML"
 use "cladata.ML"        setup Cla.setup setup clasetup
 use "blastdata.ML"      setup Blast.setup
 use "FOL_lemmas2.ML"
-use "simpdata.ML"       setup simpsetup
+use "simpdata.ML"       setup simpsetup setup cong_attrib_setup
                         setup "Simplifier.method_setup Splitter.split_modifiers"
                         setup Splitter.setup setup Clasimp.setup
 
