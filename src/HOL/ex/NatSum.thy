2b309c09a55cb94bb07ae348b834862de6ce9dec
paulson <none@none>
2000-05-08 16:59:18 +0200

    new example

--- src/HOL/ex/NatSum.thy
+++ src/HOL/ex/NatSum.thy
@@ -1,4 +1,4 @@
-(*  Title:      HOL/ex/natsum.thy
+(*  Title:      HOL/ex/NatSum.thy
     ID:         $Id$
     Author:     Tobias Nipkow
     Copyright   1994 TU Muenchen
@@ -9,7 +9,7 @@ A summation operator. sum(f,n+1) is the sum of all f(i), i=0...n.
 NatSum = Main +
 consts sum     :: [nat=>nat, nat] => nat
 primrec 
-  "sum f 0 = 0"
-  "sum f (Suc n) = f(n) + sum f n"
+  sum_0    "sum f 0 = 0"
+  sum_Suc  "sum f (Suc n) = f(n) + sum f n"
 
 end
