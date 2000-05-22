ae8b3f23853c0a53ddda0601f084ce1e45e866af
wenzelm <none@none>
2000-05-22 10:31:44 +0200

    tuned;

--- doc-src/AxClass/Group/Semigroups.thy
+++ doc-src/AxClass/Group/Semigroups.thy
@@ -1,19 +1,54 @@
+
+header {* Semigroups *};
+
 theory Semigroups = Main:;
 
-constdefs
-  is_assoc :: "('a \\<Rightarrow> 'a \\<Rightarrow> 'a) \\<Rightarrow> bool"
-  "is_assoc f \\<equiv> \\<forall>x y z. f (f x y) z = f x (f y z)";
+text {*
+ \medskip\noindent An axiomatic type class is simply a class of types
+ that all meet certain \emph{axioms}. Thus, type classes may be also
+ understood as type predicates --- i.e.\ abstractions over a single
+ type argument $\alpha$.  Class axioms typically contain polymorphic
+ constants that depend on this type $\alpha$.  These
+ \emph{characteristic constants} behave like operations associated
+ with the ``carrier'' type $\alpha$.
+
+ We illustrate these basic concepts by the following formulation of
+ semigroups.
+*};
 
 consts
-  plus :: "'a \\<Rightarrow> 'a \\<Rightarrow> 'a"    (infixl "\<Oplus>" 65);
+  times :: "'a \\<Rightarrow> 'a \\<Rightarrow> 'a"    (infixl "\<Otimes>" 70);
+axclass
+  semigroup < "term"
+  assoc: "(x \<Otimes> y) \<Otimes> z = x \<Otimes> (y \<Otimes> z)";
+
+text {*
+ \noindent Above we have first declared a polymorphic constant $\TIMES
+ :: \alpha \To \alpha \To \alpha$ and then defined the class
+ $semigroup$ of all types $\tau$ such that $\TIMES :: \tau \To \tau
+ \To \tau$ is indeed an associative operator.  The $assoc$ axiom
+ contains exactly one type variable, which is invisible in the above
+ presentation, though.  Also note that free term variables (like $x$,
+ $y$, $z$) are allowed for user convenience --- conceptually all of
+ these are bound by outermost universal quantifiers.
+
+ \medskip In general, type classes may be used to describe
+ \emph{structures} with exactly one carrier $\alpha$ and a fixed
+ \emph{signature}.  Different signatures require different classes.
+ Subsequently, class $plus_semigroup$ represents semigroups of the
+ form $(\tau, \PLUS^\tau)$, while $semigroup$ above would correspond
+ to semigroups $(\tau, \TIMES^\tau)$.
+*};
+
+consts
+  plus :: "'a \\<Rightarrow> 'a \\<Rightarrow> 'a"    (infixl "\<Oplus>" 70);
 axclass
   plus_semigroup < "term"
-  assoc: "is_assoc (op \<Oplus>)";
+  assoc: "(x \<Oplus> y) \<Oplus> z = x \<Oplus> (y \<Oplus> z)";
 
-consts
-  times :: "'a \\<Rightarrow> 'a \\<Rightarrow> 'a"    (infixl "\<Otimes>" 65);
-axclass
-  times_semigroup < "term"
-  assoc: "is_assoc (op \<Otimes>)";
+text {*
+ \noindent Even if classes $plus_semigroup$ and $times_semigroup$ both
+ represent semigroups in a sense, they are certainly not the same!
+*};
 
 end;
--- doc-src/AxClass/Nat/NatClass.thy
+++ doc-src/AxClass/Nat/NatClass.thy
@@ -1,4 +1,18 @@
+
+header {* Defining natural numbers in FOL \label{sec:ex-natclass} *};
+
 theory NatClass = FOL:;
+
+text {*
+ \medskip\noindent Axiomatic type classes abstract over exactly one
+ type argument. Thus, any \emph{axiomatic} theory extension where each
+ axiom refers to at most one type variable, may be trivially turned
+ into a \emph{definitional} one.
+
+ We illustrate this with the natural numbers in
+ Isabelle/FOL.\footnote{See also
+ \url{http://isabelle.in.tum.de/library/FOL/ex/NatClass.html}}
+*};
 
 consts
   zero :: 'a    ("0")
@@ -16,5 +30,31 @@ axclass
 constdefs
   add :: "'a::nat \\<Rightarrow> 'a \\<Rightarrow> 'a"    (infixl "+" 60)
   "m + n \\<equiv> rec(m, n, \\<lambda>x y. Suc(y))";
+
+text {*
+ This is an abstract version of the plain $Nat$ theory in
+ FOL.\footnote{See
+ \url{http://isabelle.in.tum.de/library/FOL/ex/Nat.html}}
+
+ Basically, we have just replaced all occurrences of type $nat$ by
+ $\alpha$ and used the natural number axioms to define class $nat$.
+ There is only a minor snag, that the original recursion operator
+ $rec$ had to be made monomorphic, in a sense.  Thus class $nat$
+ contains exactly those types $\tau$ that are isomorphic to ``the''
+ natural numbers (with signature $0$, $Suc$, $rec$).
+
+ \medskip What we have done here can be also viewed as \emph{type
+ specification}.  Of course, it still remains open if there is some
+ type at all that meets the class axioms.  Now a very nice property of
+ axiomatic type classes is, that abstract reasoning is always possible
+ --- independent of satisfiability.  The meta-logic won't break, even
+ if some classes (or general sorts) turns out to be empty
+ (``inconsistent'') later.
+
+ Theorems of the abstract natural numbers may be derived in the same
+ way as for the concrete version.  The original proof scripts may be
+ used with some trivial changes only (mostly adding some type
+ constraints).
+*};
 
 end;
