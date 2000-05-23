2d9196d574603a6351ae7029a30736ece6c35e17
paulson <none@none>
2000-05-23 12:32:24 +0200

    declared sum_below

--- src/HOL/Arith.thy
+++ src/HOL/Arith.thy
@@ -26,4 +26,11 @@ primrec
   mult_0   "0 * n = 0"
   mult_Suc "Suc m * n = n + (m * n)"
 
+(*We overload the constant for all + types, but unfortunately there's no
+  overloaded 0...*)
+consts sum_below :: [nat=>'a, nat] => ('a::plus) 
+primrec 
+  sum_below_0    "sum_below f 0 = 0"
+  sum_below_Suc  "sum_below f (Suc n) = f(n) + sum_below f n"
+
 end
