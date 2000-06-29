2e39feabc4ff73ad08f69c5372364bf6975128c7
wenzelm <none@none>
2000-06-29 22:48:08 +0200

    improved arrangement of files; tuned;

---
+++ src/FOL/ex/int.thy
@@ -0,0 +1,11 @@
+(*  Title:      FOL/ex/int.thy
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1991  University of Cambridge
+
+Intuitionistic First-Order Logic.
+*)
+
+theory int = IFOL:
+
+end
