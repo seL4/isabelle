018187bf436973c36e674f19c9e731a520f851af
wenzelm <none@none>
2000-08-17 10:33:13 +0200

    renamed 'RS' to 'THEN';

--- src/HOL/Arith.thy
+++ src/HOL/Arith.thy
@@ -12,7 +12,7 @@ setup arith_setup
 (*elimination of `-' on nat*)
 lemma nat_diff_split:
     "P(a - b::nat) = (ALL d. (a<b --> P 0) & (a = b + d --> P d))"
-  by (cases "a < b" rule: case_split) (auto simp add: diff_is_0_eq [RS iffD2])
+  by (cases "a < b" rule: case_split) (auto simp add: diff_is_0_eq [THEN iffD2])
 
 ML {* val nat_diff_split = thm "nat_diff_split" *}
 
