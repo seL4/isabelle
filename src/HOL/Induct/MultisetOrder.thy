dabc597956abade2687928d34820cc2c0f895445
wenzelm <none@none>
2000-08-19 12:45:11 +0200

    turned into new-style theory;

--- src/HOL/Induct/MultisetOrder.thy
+++ src/HOL/Induct/MultisetOrder.thy
@@ -1,15 +1,28 @@
-(*  Title:      HOL/UNITY/MultisetOrder
+(*  Title:      HOL/Induct/MultisetOrder.thy
     ID:         $Id$
     Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
     Copyright   2000  University of Cambridge
 
-Multisets are partially ordered
+Multisets are partially ordered.
 *)
 
-MultisetOrder = Multiset +
+theory MultisetOrder = Multiset:
 
 instance multiset :: (order) order
-    (mult_le_refl,mult_le_trans,mult_le_antisym,mult_less_le)
+  apply intro_classes
+     apply (rule mult_le_refl)
+    apply (erule mult_le_trans)
+    apply assumption
+   apply (erule mult_le_antisym)
+   apply assumption
+  apply (rule mult_less_le)
+  done
 
-instance multiset :: (term) plus_ac0 (union_commute,union_assoc) {|Auto_tac|}
+instance multiset :: ("term") plus_ac0
+  apply intro_classes
+    apply (rule union_commute)
+   apply (rule union_assoc)
+  apply simp
+  done
+
 end
