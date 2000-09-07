aa223cf5e56020cc4df5204d872cce524608b0a4
wenzelm <none@none>
2000-09-07 21:12:49 +0200

    tuned ML code (the_context, bind_thms(s));

--- src/ZF/upair.thy
+++ src/ZF/upair.thy
@@ -2,15 +2,12 @@
     ID:         $Id$
     Author:     Lawrence C Paulson and Martin D Coen, CU Computer Laboratory
     Copyright   1993  University of Cambridge
-
-Dummy theory, but holds the standard ZF simpset.
 *)
 
 theory upair = ZF
 files "Tools/typechk":
 
-setup
-  TypeCheck.setup
+setup TypeCheck.setup
+setup Rulify.setup
 
 end
-
