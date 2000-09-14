00017a3120306a9fe728c78ec8c8a84c233941cd
paulson <none@none>
2000-09-14 11:34:13 +0200

    a bit more of division

--- src/ZF/Integ/IntDiv.thy
+++ src/ZF/Integ/IntDiv.thy
@@ -6,7 +6,7 @@
 The division operators div, mod
 *)
 
-IntDiv = IntArith + 
+IntDiv = IntArith + OrderArith + 
 
 constdefs
   quorem :: "[i,i] => o"
@@ -18,5 +18,18 @@ constdefs
     "adjust(a,b) == %<q,r>. if #0 $<= r$-b then <#2$*q $+ #1,r$-b>
                             else <#2$*q,r>"
 
+
+(** the division algorithm **)
+
+constdefs posDivAlg :: "i => i"
+(*for the case a>=0, b>0*)
+(*recdef posDivAlg "inv_image less_than (%(a,b). nat_of(a $- b $+ #1))"*)
+    "posDivAlg(ab) ==
+       wfrec(measure(int*int, %<a,b>. nat_of (a $- b $+ #1)),
+	     ab,
+	     %<a,b> f. if (a$<b | b$<=#0) then <#0,a>
+                       else adjust(a, b, f ` <a,#2$*b>))"
+
+(**TO BE COMPLETED**)
 
 end
