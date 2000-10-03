280c13e3bd595d5f94e5f6225dc4a5d1b8188678
wenzelm <none@none>
2000-10-03 18:30:56 +0200

    moved axclass tutorial examples to top dir;

---
+++ src/HOL/AxClasses/Product.thy
@@ -0,0 +1,20 @@
+(*  Title:      HOL/AxClasses/Product.thy
+    ID:         $Id$
+    Author:     Markus Wenzel, TU Muenchen
+*)
+
+theory Product = Main:
+
+axclass product < "term"
+
+consts
+  product :: "'a::product => 'a => 'a"    (infixl "[*]" 70)
+
+
+instance bool :: product
+  by intro_classes
+
+defs (overloaded)
+  product_bool_def: "x [*] y == x & y"
+
+end
---
+++ src/HOL/AxClasses/Semigroups.thy
@@ -0,0 +1,21 @@
+(*  Title:      HOL/AxClasses/Semigroups.thy
+    ID:         $Id$
+    Author:     Markus Wenzel, TU Muenchen
+*)
+
+theory Semigroups = Main:
+
+consts
+  times :: "'a => 'a => 'a"    (infixl "[*]" 70)
+
+axclass semigroup < "term"
+  assoc: "(x [*] y) [*] z = x [*] (y [*] z)"
+
+
+consts
+  plus :: "'a => 'a => 'a"    (infixl "[+]" 70)
+
+axclass plus_semigroup < "term"
+  assoc: "(x [+] y) [+] z = x [+] (y [+] z)"
+
+end
