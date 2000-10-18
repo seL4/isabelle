a8db4694de372f3fa83d29204ac52a69fccf59cc
wenzelm <none@none>
2000-10-18 23:38:41 +0200

    moved to HOL/Library;

--- src/HOL/Induct/Multiset0.thy
+++
@@ -1,9 +0,0 @@
-(*  Title:      HOL/Induct/Multiset0.thy
-    ID:         $Id$
-    Author:     Tobias Nipkow
-    Copyright   1998 TUM
-
-This theory merely proves that the representation of multisets is nonempty.
-*)
-
-Multiset0 = Main
