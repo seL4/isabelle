d7481bdb331489da5a194d96ad745d282933eac3
wenzelm <none@none>
2000-11-03 21:33:15 +0100

    provide case names for rev_induct, rev_cases;

--- src/HOL/Main.thy
+++ src/HOL/Main.thy
@@ -7,5 +7,7 @@ theory Main = Map + String:
 (*belongs to theory List*)
 declare lists_mono [mono]
 declare map_cong [recdef_cong]
+lemmas rev_induct [case_names Nil snoc] = rev_induct
+  and rev_cases [case_names Nil snoc] = rev_exhaust
 
 end
