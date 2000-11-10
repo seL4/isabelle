989c9d3dee56b5be3b5df9f7d8e1bdc5094aa756
nipkow <none@none>
2000-11-10 15:05:09 +0100

    rule inversion

--- doc-src/TutorialI/Inductive/Advanced.thy
+++ doc-src/TutorialI/Inductive/Advanced.thy
@@ -1,5 +1,24 @@
 (* ID:         $Id$ *)
-theory Advanced = Main:
+theory Advanced = Even:
+
+text{* We completely forgot about "rule inversion", or whatever we may want
+to call it. Just as a demo I include the two forms that Markus has made available. First the one for binding the result to a name *}
+
+inductive_cases even_cases [elim!]:
+  "Suc(Suc n) \<in> even"
+
+thm even_cases;
+
+text{*and now the one for local usage:*}
+
+lemma "Suc(Suc n) \<in> even \<Longrightarrow> n \<in> even";
+by(ind_cases "Suc(Suc n) \<in> even");
+
+text{*Both forms accept lists of strings.
+
+Hope you can find some more exciting examples, although these may do
+*}
+
 
 datatype 'f "term" = Apply 'f "'f term list"
 
