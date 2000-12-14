3759f3b1fa2095e4ee73c37fbefe8fc9fbc7cd45
wenzelm <none@none>
2000-12-14 19:36:48 +0100

    added Summation;

--- src/HOL/PreList.thy
+++ src/HOL/PreList.thy
@@ -20,4 +20,21 @@ declare wf_induct [induct set: wf]
 (*belongs to theory Datatype_Universe; hides popular names *)
 hide const Node Atom Leaf Numb Lim Funs Split Case
 
+
+(*belongs to theory Nat, but requires Datatype*)
+consts
+  Summation :: "(nat => 'a::{zero,plus}) => nat => 'a"
+primrec
+  "Summation f 0 = 0"
+  "Summation f (Suc n) = Summation f n + f n"
+
+syntax
+  "_Summation" :: "idt => nat => 'a => nat"    ("\<Sum>_<_. _" [0, 51, 10] 10)
+translations
+  "\<Sum>i < n. b" == "Summation (\<lambda>i. b) n"
+
+theorem Summation_step:
+    "0 < n ==> (\<Sum>i < n. f i) = (\<Sum>i < n - 1. f i) + f (n - 1)"
+  by (induct n) simp_all
+
 end
