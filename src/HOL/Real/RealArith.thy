a7b9eca1dbcd1be166badb887a4fa5268c645063
nipkow <none@none>
2000-12-21 18:57:12 +0100

    rational linear arithmetic

--- src/HOL/Real/RealArith.thy
+++ src/HOL/Real/RealArith.thy
@@ -1,4 +1,4 @@
-theory RealArith = RealBin
+theory RealArith = RealArith0
 files "real_arith.ML":
 
 setup real_arith_setup
---
+++ src/HOL/Real/RealArith0.thy
@@ -0,0 +1,6 @@
+theory RealArith0 = RealBin
+files "real_arith0.ML":
+
+setup real_arith_setup
+
+end
