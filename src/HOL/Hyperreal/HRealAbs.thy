c2540fb2dc2ef6bbf05764b9e724b86766681682
paulson <none@none>
2000-12-30 22:03:46 +0100

    separation of HOL-Hyperreal from HOL-Real

---
+++ src/HOL/Hyperreal/HRealAbs.thy
@@ -0,0 +1,12 @@
+(*  Title       : HRealAbs.thy
+    Author      : Jacques D. Fleuriot
+    Copyright   : 1998  University of Cambridge
+    Description : Absolute value function for the hyperreals
+*) 
+
+HRealAbs = HyperArith + RealAbs + 
+
+defs
+    hrabs_def "abs r  == if (0::hypreal) <=r then r else -r" 
+
+end
