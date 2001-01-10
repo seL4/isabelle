84de909eceb905c8917e0067270e285f740d0707
paulson <none@none>
2001-01-10 11:07:11 +0100

    now using "by" for one-line proofs

--- doc-src/TutorialI/Rules/Blast.thy
+++ doc-src/TutorialI/Rules/Blast.thy
@@ -3,8 +3,7 @@ theory Blast = Main:
 
 lemma "((\<exists>x. \<forall>y. p(x)=p(y)) = ((\<exists>x. q(x))=(\<forall>y. p(y))))   =    
        ((\<exists>x. \<forall>y. q(x)=q(y)) = ((\<exists>x. p(x))=(\<forall>y. q(y))))"
-apply blast
-done
+by blast
 
 text{*\noindent Until now, we have proved everything using only induction and
 simplification.  Substantial proofs require more elaborate types of
@@ -16,13 +15,11 @@ lemma "(\<forall>x. honest(x) \<and> industrious(x) \<longrightarrow> healthy(x)
        (\<forall>x. cyclist(x) \<longrightarrow> industrious(x)) \<and> 
        (\<forall>x. \<not>healthy(x) \<and> cyclist(x) \<longrightarrow> \<not>honest(x))  
        \<longrightarrow> (\<forall>x. grocer(x) \<longrightarrow> \<not>cyclist(x))";
-apply blast
-done
+by blast
 
 lemma "(\<Union>i\<in>I. A(i)) \<inter> (\<Union>j\<in>J. B(j)) =
         (\<Union>i\<in>I. \<Union>j\<in>J. A(i) \<inter> B(j))"
-apply blast
-done
+by blast
 
 text {*
 @{thm[display] mult_is_0}
