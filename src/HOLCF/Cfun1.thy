f9c762cbd0416fdf17148d05f95192db7ee64f48
nipkow <none@none>
2001-01-10 20:41:14 +0100

    Added <cdot> syntax for continuous application $.

--- src/HOLCF/Cfun1.thy
+++ src/HOLCF/Cfun1.thy
@@ -17,9 +17,9 @@ typedef (CFun)  ('a, 'b) "->" (infixr 0) = "{f::'a => 'b. cont f}" (CfunI)
 instance "->"  :: (cpo,cpo)sq_ord
 
 syntax
-	Rep_CFun  :: "('a -> 'b)=>('a => 'b)" ("_$_" [999,1000] 999)
+	Rep_CFun  :: "('a -> 'b) => ('a => 'b)" ("_$_" [999,1000] 999)
                                                 (* application      *)
-        Abs_CFun  :: "('a => 'b)=>('a -> 'b)"     (binder "LAM " 10)
+        Abs_CFun  :: "('a => 'b) => ('a -> 'b)" (binder "LAM " 10)
                                                 (* abstraction      *)
         less_cfun :: "[('a -> 'b),('a -> 'b)]=>bool"
 
@@ -27,6 +27,11 @@ syntax (symbols)
   "->"		:: [type, type] => type	("(_ \\<rightarrow>/ _)" [1,0]0)
   "LAM "	:: "[idts, 'a => 'b] => ('a -> 'b)"
 					("(3\\<Lambda>_./ _)" [0, 10] 10)
+  Rep_CFun      :: "('a -> 'b) => ('a => 'b)"  ("(_\\<cdot>_)" [999,1000] 999)
+
+syntax (HTML output)
+  Rep_CFun      :: "('a -> 'b) => ('a => 'b)"  ("(_\\<cdot>_)" [999,1000] 999)
+
 defs 
   less_cfun_def "(op <<) == (% fo1 fo2. Rep_CFun fo1 << Rep_CFun fo2 )"
 
