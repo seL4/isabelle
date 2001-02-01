b0267dd643e4ca89d6ce1769c03ad44ab6e0d187
wenzelm <none@none>
2001-02-01 20:43:59 +0100

    tuned;

--- src/HOL/ex/MonoidGroup.thy
+++ src/HOL/ex/MonoidGroup.thy
@@ -11,23 +11,23 @@ header {* Monoids and Groups *}
 theory MonoidGroup = Main:
 
 record 'a monoid_sig =
-  times :: "['a, 'a] => 'a"
+  times :: "'a => 'a => 'a"
   one :: 'a
 
 record 'a group_sig = "'a monoid_sig" +
   inv :: "'a => 'a"
 
 constdefs
-  monoid :: "(| times :: ['a, 'a] => 'a, one :: 'a, ... :: 'b::more |) => bool"
-  "monoid M == ALL x y z.
-    times M (times M x y) z = times M x (times M y z) &
-    times M (one M) x = x & times M x (one M) = x"
+  monoid :: "(| times :: 'a => 'a => 'a, one :: 'a, ... :: 'b::more |) => bool"
+  "monoid M == \<forall>x y z.
+    times M (times M x y) z = times M x (times M y z) \<and>
+    times M (one M) x = x \<and> times M x (one M) = x"
 
-  group :: "(| times :: ['a, 'a] => 'a, one :: 'a, inv :: 'a => 'a, ... :: 'b::more |) => bool"
-  "group G == monoid G & (ALL x. times G (inv G x) x = one G)"
+  group :: "(| times :: 'a => 'a => 'a, one :: 'a, inv :: 'a => 'a, ... :: 'b::more |) => bool"
+  "group G == monoid G \<and> (\<forall>x. times G (inv G x) x = one G)"
 
-  reverse :: "(| times :: ['a, 'a] => 'a, one :: 'a, ... :: 'b::more |) =>
-    (| times :: ['a, 'a] => 'a, one :: 'a, ... :: 'b |)"
-  "reverse M == M (| times := %x y. times M y x |)"
+  reverse :: "(| times :: 'a => 'a => 'a, one :: 'a, ... :: 'b::more |) =>
+    (| times :: 'a => 'a => 'a, one :: 'a, ... :: 'b |)"
+  "reverse M == M (| times := \<lambda>x y. times M y x |)"
 
 end
