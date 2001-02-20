258c9575f8538a18a74cc4b0b825fb599e7f4fc3
oheimb <none@none>
2001-02-20 18:47:25 +0100

    added add_arith (just as hint by now)

--- src/HOL/NatArith.thy
+++ src/HOL/NatArith.thy
@@ -14,8 +14,14 @@ lemma nat_diff_split:
     "P(a - b::nat) = ((a<b --> P 0) & (ALL d. a = b + d --> P d))"
   by (cases "a < b" rule: case_split) (auto simp add: diff_is_0_eq [THEN iffD2])
 
-ML {* val nat_diff_split = thm "nat_diff_split" *}
+ML {*
+ val nat_diff_split = thm "nat_diff_split";
+
+(* TODO: use this for force_tac in Provers/clasip.ML *)
+fun add_arith cs = cs addaltern ("arith_tac", arith_tac);
+*}
 
 lemmas [arith_split] = nat_diff_split split_min split_max
+
 
 end
