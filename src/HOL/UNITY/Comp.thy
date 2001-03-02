20030c6d87f9249901869f7369cf1c71d606a743
ehmety <none@none>
2001-03-02 13:18:56 +0100

    *** empty log message ***

--- src/HOL/UNITY/Comp.thy
+++ src/HOL/UNITY/Comp.thy
@@ -4,8 +4,13 @@
     Copyright   1998  University of Cambridge
 
 Composition
+From Chandy and Sanders, "Reasoning About Program Composition",
+Technical Report 2000-003, University of Florida, 2000.
 
-From Chandy and Sanders, "Reasoning About Program Composition"
+Revised by Sidi Ehmety on January  2001 
+
+Added: a strong form of the <= relation (component_of) and localize 
+
 *)
 
 Comp = Union +
@@ -14,16 +19,26 @@ instance
   program :: (term)ord
 
 defs
-
   component_def   "F <= H == EX G. F Join G = H"
-
   strict_component_def   "(F < (H::'a program)) == (F <= H & F ~= H)"
 
+
 constdefs
+  component_of :: "'a program=>'a program=> bool"
+                                    (infixl "component'_of" 50)
+  "F component_of H == EX G. F ok G & F Join G = H"
+
+  strict_component_of :: "'a program\\<Rightarrow>'a program=> bool"
+                                    (infixl "strict'_component'_of" 50)
+  "F strict_component_of H == F component_of H & F~=H"
+  
   preserves :: "('a=>'b) => 'a program set"
     "preserves v == INT z. stable {s. v s = z}"
 
-  funPair      :: "['a => 'b, 'a => 'c, 'a] => 'b * 'c"
-    "funPair f g == %x. (f x, g x)"
+  localize  :: "('a=>'b) => 'a program => 'a program"
+  "localize v F == mk_program(Init F, Acts F,
+			      AllowedActs F Int (UN G:preserves v. Acts G))"
 
+  funPair      :: "['a => 'b, 'a => 'c, 'a] => 'b * 'c"
+  "funPair f g == %x. (f x, g x)"
 end
--- src/HOL/UNITY/Guar.thy
+++ src/HOL/UNITY/Guar.thy
@@ -5,7 +5,17 @@
 
 Guarantees, etc.
 
-From Chandy and Sanders, "Reasoning About Program Composition"
+From Chandy and Sanders, "Reasoning About Program Composition",
+Technical Report 2000-003, University of Florida, 2000.
+
+Revised by Sidi Ehmety on January 2001
+
+Added: Compatibility, weakest guarantees, etc.
+
+and Weakest existential property,
+from Charpentier and Chandy "Theorems about Composition",
+Fifth International Conference on Mathematics of Program, 2000.
+
 *)
 
 Guar = Comp +
@@ -20,33 +30,42 @@ constdefs
     case, proving equivalence with Chandy and Sanders's n-ary definitions*)
 
   ex_prop  :: 'a program set => bool
-   "ex_prop X == ALL F G. F:X | G: X --> (F Join G) : X"
+   "ex_prop X == ALL F G. F ok G -->F:X | G: X --> (F Join G) : X"
 
   strict_ex_prop  :: 'a program set => bool
-   "strict_ex_prop X == ALL F G. (F:X | G: X) = (F Join G : X)"
+   "strict_ex_prop X == ALL F G.  F ok G --> (F:X | G: X) = (F Join G : X)"
 
   uv_prop  :: 'a program set => bool
-   "uv_prop X == SKIP : X & (ALL F G. F:X & G: X --> (F Join G) : X)"
+   "uv_prop X == SKIP : X & (ALL F G. F ok G --> F:X & G: X --> (F Join G) : X)"
 
   strict_uv_prop  :: 'a program set => bool
-   "strict_uv_prop X == SKIP : X & (ALL F G. (F:X & G: X) = (F Join G : X))"
-
-  (*Ill-defined programs can arise through "Join"*)
-  welldef :: 'a program set  
-   "welldef == {F. Init F ~= {}}"
+   "strict_uv_prop X == SKIP : X & (ALL F G. F ok G -->(F:X & G: X) = (F Join G : X))"
 
   guar :: ['a program set, 'a program set] => 'a program set
           (infixl "guarantees" 55)  (*higher than membership, lower than Co*)
    "X guarantees Y == {F. ALL G. F ok G --> F Join G : X --> F Join G : Y}"
+  
 
+  (* Weakest guarantees *)
+   wg :: ['a program, 'a program set] =>  'a program set
+  "wg F Y == Union({X. F:X guarantees Y})"
+
+   (* Weakest existential property stronger than X *)
+   wx :: "('a program) set => ('a program)set"
+   "wx X == Union({Y. Y<=X & ex_prop Y})"
+  
+  (*Ill-defined programs can arise through "Join"*)
+  welldef :: 'a program set  
+  "welldef == {F. Init F ~= {}}"
+  
   refines :: ['a program, 'a program, 'a program set] => bool
 			("(3_ refines _ wrt _)" [10,10,10] 10)
-   "G refines F wrt X ==
-      ALL H. (F Join H) : welldef Int X --> G Join H : welldef Int X"
+  "G refines F wrt X ==
+   ALL H. (F ok H  & G ok H & F Join H:welldef Int X) --> (G Join H:welldef Int X)"
 
   iso_refines :: ['a program, 'a program, 'a program set] => bool
-			("(3_ iso'_refines _ wrt _)" [10,10,10] 10)
-   "G iso_refines F wrt X ==
-      F : welldef Int X --> G : welldef Int X"
+                              ("(3_ iso'_refines _ wrt _)" [10,10,10] 10)
+  "G iso_refines F wrt X ==
+   F : welldef Int X --> G : welldef Int X"
 
 end
