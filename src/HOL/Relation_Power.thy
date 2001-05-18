9f5133716f4f2466964bc75c7599c544d9e67a71
nipkow <none@none>
2001-05-18 12:09:13 +0200

    added comments

--- src/HOL/Relation_Power.thy
+++ src/HOL/Relation_Power.thy
@@ -4,7 +4,12 @@
     Copyright   1996  TU Muenchen
 
 R^n = R O ... O R, the n-fold composition of R
-Both for functions and relations.
+f^n = f o ... o f, the n-fold composition of f
+
+WARNING: due to the limits of Isabelle's type classes, ^ on functions and
+relations has too general a domain, namely ('a * 'b)set and 'a => 'b.
+This means that it may be necessary to attach explicit type constraints.
+Examples: range(f^n) = A and Range(R^n) = B need constraints.
 *)
 
 Relation_Power = Nat +
@@ -17,7 +22,8 @@ primrec (relpow)
   "R^(Suc n) = R O (R^n)"
 
 
-instance fun :: (term,term)power   (* only 'a \<Rightarrow> 'a should be in power! *)
+instance
+  fun :: (term,term)power   (* only 'a => 'a should be in power! *)
 
 primrec (funpow)
   "f^0 = id"
