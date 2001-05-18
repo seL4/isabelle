7d5f3ad771f13f4763d455be95a344f74b2b4cd1
nipkow <none@none>
2001-05-18 07:56:19 +0200

    added ^ on functions.

--- src/HOL/Relation_Power.thy
+++ src/HOL/Relation_Power.thy
@@ -4,6 +4,7 @@
     Copyright   1996  TU Muenchen
 
 R^n = R O ... O R, the n-fold composition of R
+Both for functions and relations.
 *)
 
 Relation_Power = Nat +
@@ -14,5 +15,12 @@ instance
 primrec (relpow)
   "R^0 = Id"
   "R^(Suc n) = R O (R^n)"
+
+
+instance fun :: (term,term)power   (* only 'a \<Rightarrow> 'a should be in power! *)
+
+primrec (funpow)
+  "f^0 = id"
+  "f^(Suc n) = f o (f^n)"
 
 end
