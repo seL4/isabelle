972c1282c793861a2fd89e6136c9aefbeaa2de9a
oheimb <none@none>
2001-05-31 16:52:54 +0200

    added Library/Nat_Infinity.thy and Library/Continuity.thy

--- src/HOL/Library/Library.thy
+++ src/HOL/Library/Library.thy
@@ -2,10 +2,12 @@
 theory Library =
   Quotient +
   Ring_and_Field + Ring_and_Field_Example +
+  Nat_Infinity +
   Rational_Numbers +
   List_Prefix +
   Nested_Environment +
   Accessible_Part +
+  Continuity +
   Multiset +
   Permutation +
   While_Combinator:
