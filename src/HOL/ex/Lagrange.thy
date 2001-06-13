0395c09a6df8e7eec2cefeec11f1c99cd3515fcf
paulson <none@none>
2001-06-13 16:30:12 +0200

    tidied

--- src/HOL/ex/Lagrange.thy
+++ src/HOL/ex/Lagrange.thy
@@ -1,14 +1,14 @@
-(*  Title:      HOL/Integ/Lagrange.thy
+(*  Title:      HOL/ex/Lagrange.thy
     ID:         $Id$
     Author:     Tobias Nipkow
     Copyright   1996 TU Muenchen
 
 
-This theory only contains a single thm, which is a lemma in Lagrange's proof
-that every natural number is the sum of 4 squares.  It's sole purpose is to
-demonstrate ordered rewriting for commutative rings.
+This theory only contains a single theorem, which is a lemma in Lagrange's
+proof that every natural number is the sum of 4 squares.  Its sole purpose is
+to demonstrate ordered rewriting for commutative rings.
 
-The enterprising reader might consider proving all of Lagrange's thm.
+The enterprising reader might consider proving all of Lagrange's theorem.
 *)
 Lagrange = Ring +
 
