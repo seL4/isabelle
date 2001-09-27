62c6f6d98165806625ef9fe4ac3dca5b273b140f
wenzelm <none@none>
2001-09-27 22:26:00 +0200

    renamed theory "subset" to "Typedef";

--- src/HOL/Typedef.thy
+++ src/HOL/Typedef.thy
@@ -1,13 +1,13 @@
-(*  Title:      HOL/subset.thy
+(*  Title:      HOL/Typedef.thy
     ID:         $Id$
-    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
-    Copyright   1994  University of Cambridge
+    Author:     Markus Wenzel, TU Munich
 
-Subset lemmas and HOL type definitions.
+Misc set-theory lemmas and HOL type definitions.
 *)
 
-theory subset = Set
-files "Tools/induct_attrib.ML" ("Tools/typedef_package.ML"):
+theory Typedef = Set
+files "subset.ML" "equalities.ML" "mono.ML"
+  "Tools/induct_attrib.ML" ("Tools/typedef_package.ML"):
 
 (** belongs to theory Ord **)
   
@@ -16,14 +16,14 @@ theorems linorder_cases [case_names less equal greater] =
 
 (* Courtesy of Stephan Merz *)
 lemma Least_mono: 
-"[| mono (f::'a::order => 'b::order); EX x:S. ALL y:S. x <= y |]  
-   ==> (LEAST y. y : f`S) = f(LEAST x. x : S)"
-apply clarify
-apply (erule_tac P = "%x. x : S" in LeastI2)
-apply  fast
-apply (rule LeastI2)
-apply (auto elim: monoD intro!: order_antisym)
-done
+  "mono (f::'a::order => 'b::order) ==> EX x:S. ALL y:S. x <= y
+    ==> (LEAST y. y : f ` S) = f (LEAST x. x : S)"
+  apply clarify
+  apply (erule_tac P = "%x. x : S" in LeastI2)
+   apply fast
+  apply (rule LeastI2)
+  apply (auto elim: monoD intro!: order_antisym)
+  done
 
 
 (*belongs to theory Set*)
@@ -35,25 +35,25 @@ section {* HOL type definitions *}
 constdefs
   type_definition :: "('a => 'b) => ('b => 'a) => 'b set => bool"
   "type_definition Rep Abs A ==
-    (\\<forall>x. Rep x \\<in> A) \\<and>
-    (\\<forall>x. Abs (Rep x) = x) \\<and>
-    (\\<forall>y \\<in> A. Rep (Abs y) = y)"
+    (\<forall>x. Rep x \<in> A) \<and>
+    (\<forall>x. Abs (Rep x) = x) \<and>
+    (\<forall>y \<in> A. Rep (Abs y) = y)"
   -- {* This will be stated as an axiom for each typedef! *}
 
 lemma type_definitionI [intro]:
-  "(!!x. Rep x \\<in> A) ==>
+  "(!!x. Rep x \<in> A) ==>
     (!!x. Abs (Rep x) = x) ==>
-    (!!y. y \\<in> A ==> Rep (Abs y) = y) ==>
+    (!!y. y \<in> A ==> Rep (Abs y) = y) ==>
     type_definition Rep Abs A"
   by (unfold type_definition_def) blast
 
-theorem Rep: "type_definition Rep Abs A ==> Rep x \\<in> A"
+theorem Rep: "type_definition Rep Abs A ==> Rep x \<in> A"
   by (unfold type_definition_def) blast
 
 theorem Rep_inverse: "type_definition Rep Abs A ==> Abs (Rep x) = x"
   by (unfold type_definition_def) blast
 
-theorem Abs_inverse: "type_definition Rep Abs A ==> y \\<in> A ==> Rep (Abs y) = y"
+theorem Abs_inverse: "type_definition Rep Abs A ==> y \<in> A ==> Rep (Abs y) = y"
   by (unfold type_definition_def) blast
 
 theorem Rep_inject: "type_definition Rep Abs A ==> (Rep x = Rep y) = (x = y)"
@@ -71,10 +71,10 @@ proof -
 qed
 
 theorem Abs_inject:
-  "type_definition Rep Abs A ==> x \\<in> A ==> y \\<in> A ==> (Abs x = Abs y) = (x = y)"
+  "type_definition Rep Abs A ==> x \<in> A ==> y \<in> A ==> (Abs x = Abs y) = (x = y)"
 proof -
   assume tydef: "type_definition Rep Abs A"
-  assume x: "x \\<in> A" and y: "y \\<in> A"
+  assume x: "x \<in> A" and y: "y \<in> A"
   show ?thesis
   proof
     assume "Abs x = Abs y"
@@ -89,10 +89,10 @@ proof -
 qed
 
 theorem Rep_cases:
-  "type_definition Rep Abs A ==> y \\<in> A ==> (!!x. y = Rep x ==> P) ==> P"
+  "type_definition Rep Abs A ==> y \<in> A ==> (!!x. y = Rep x ==> P) ==> P"
 proof -
   assume tydef: "type_definition Rep Abs A"
-  assume y: "y \\<in> A" and r: "(!!x. y = Rep x ==> P)"
+  assume y: "y \<in> A" and r: "(!!x. y = Rep x ==> P)"
   show P
   proof (rule r)
     from y have "Rep (Abs y) = y" by (rule Abs_inverse [OF tydef])
@@ -101,33 +101,33 @@ proof -
 qed
 
 theorem Abs_cases:
-  "type_definition Rep Abs A ==> (!!y. x = Abs y ==> y \\<in> A ==> P) ==> P"
+  "type_definition Rep Abs A ==> (!!y. x = Abs y ==> y \<in> A ==> P) ==> P"
 proof -
   assume tydef: "type_definition Rep Abs A"
-  assume r: "!!y. x = Abs y ==> y \\<in> A ==> P"
+  assume r: "!!y. x = Abs y ==> y \<in> A ==> P"
   show P
   proof (rule r)
     have "Abs (Rep x) = x" by (rule Rep_inverse [OF tydef])
     thus "x = Abs (Rep x)" ..
-    show "Rep x \\<in> A" by (rule Rep [OF tydef])
+    show "Rep x \<in> A" by (rule Rep [OF tydef])
   qed
 qed
 
 theorem Rep_induct:
-  "type_definition Rep Abs A ==> y \\<in> A ==> (!!x. P (Rep x)) ==> P y"
+  "type_definition Rep Abs A ==> y \<in> A ==> (!!x. P (Rep x)) ==> P y"
 proof -
   assume tydef: "type_definition Rep Abs A"
   assume "!!x. P (Rep x)" hence "P (Rep (Abs y))" .
-  moreover assume "y \\<in> A" hence "Rep (Abs y) = y" by (rule Abs_inverse [OF tydef])
+  moreover assume "y \<in> A" hence "Rep (Abs y) = y" by (rule Abs_inverse [OF tydef])
   ultimately show "P y" by (simp only:)
 qed
 
 theorem Abs_induct:
-  "type_definition Rep Abs A ==> (!!y. y \\<in> A ==> P (Abs y)) ==> P x"
+  "type_definition Rep Abs A ==> (!!y. y \<in> A ==> P (Abs y)) ==> P x"
 proof -
   assume tydef: "type_definition Rep Abs A"
-  assume r: "!!y. y \\<in> A ==> P (Abs y)"
-  have "Rep x \\<in> A" by (rule Rep [OF tydef])
+  assume r: "!!y. y \<in> A ==> P (Abs y)"
+  have "Rep x \<in> A" by (rule Rep [OF tydef])
   hence "P (Abs (Rep x))" by (rule r)
   moreover have "Abs (Rep x) = x" by (rule Rep_inverse [OF tydef])
   ultimately show "P x" by (simp only:)
