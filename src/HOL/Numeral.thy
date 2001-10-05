21415ceec84b8829ea700b225295182267e940f7
wenzelm <none@none>
2001-10-05 21:49:59 +0200

    "num" syntax;

--- src/HOL/Numeral.thy
+++ src/HOL/Numeral.thy
@@ -23,10 +23,27 @@ nonterminals
   numeral
 
 syntax
-  "_constify" :: "xnum => numeral"    ("_")
-  "_Numeral" :: "numeral => 'a"    ("_")
+  "_constify" :: "num => numeral"    ("_")
+  "_Numeral" :: "numeral => 'a"    ("#_")
+  Numeral0 :: 'a
+  Numeral1 :: 'a
+
+translations
+  "Numeral0" == "number_of Pls"
+  "Numeral1" == "number_of (Pls BIT True)"
 
 
 setup NumeralSyntax.setup
+
+
+(*Unfold all "let"s involving constants*)
+lemma Let_number_of [simp]: "Let (number_of v) f == f (number_of v)"
+  by (simp add: Let_def)
+
+(*The condition "True" is a hack to prevent looping.
+  Conditional rewrite rules are tried after unconditional ones, so a rule
+  like eq_nat_number_of will be tried first to eliminate #mm=#nn. *)
+lemma number_of_reorient [simp]: "True ==> (number_of w = x) = (x = number_of w)"
+  by auto
 
 end
