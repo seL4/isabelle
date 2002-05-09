a6131df27f4fcfa9e2b9f4338e9aa663df07f727
paulson <none@none>
2002-05-09 17:50:59 +0200

    ordinal addition now coerces its arguments to ordinals

--- src/ZF/OrderType.thy
+++ src/ZF/OrderType.thy
@@ -9,35 +9,38 @@ The order type of a well-ordering is the least ordinal isomorphic to it.
 *)
 
 OrderType = OrderArith + OrdQuant + 
-consts
-  ordermap  :: [i,i]=>i
-  ordertype :: [i,i]=>i
-
-  Ord_alt   :: i => o   
-
-  "**"      :: [i,i]=>i           (infixl 70)
-  "++"      :: [i,i]=>i           (infixl 65)
-  "--"      :: [i,i]=>i           (infixl 65)
- 
-
-defs
-  ordermap_def
-      "ordermap(A,r) == lam x:A. wfrec[A](r, x, %x f. f `` pred(A,x,r))"
-
-  ordertype_def "ordertype(A,r) == ordermap(A,r)``A"
-
-  Ord_alt_def    (*alternative definition of ordinal numbers*)
-  "Ord_alt(X) == well_ord(X, Memrel(X)) & (ALL u:X. u=pred(X, u, Memrel(X)))"
+constdefs
   
+  ordermap  :: [i,i]=>i
+   "ordermap(A,r) == lam x:A. wfrec[A](r, x, %x f. f `` pred(A,x,r))"
+
+  ordertype :: [i,i]=>i
+   "ordertype(A,r) == ordermap(A,r)``A"
+
+  (*alternative definition of ordinal numbers*)
+  Ord_alt   :: i => o   
+   "Ord_alt(X) == well_ord(X, Memrel(X)) & (ALL u:X. u=pred(X, u, Memrel(X)))"
+
+  (*coercion to ordinal: if not, just 0*)
+  ordify    :: i=>i
+    "ordify(x) == if Ord(x) then x else 0"
+
   (*ordinal multiplication*)
-  omult_def     "i ** j == ordertype(j*i, rmult(j,Memrel(j),i,Memrel(i)))"
+  omult      :: [i,i]=>i           (infixl "**" 70)
+   "i ** j == ordertype(j*i, rmult(j,Memrel(j),i,Memrel(i)))"
 
   (*ordinal addition*)
-  oadd_def      "i ++ j == ordertype(i+j, radd(i,Memrel(i),j,Memrel(j)))"
+  raw_oadd   :: [i,i]=>i
+    "raw_oadd(i,j) == ordertype(i+j, radd(i,Memrel(i),j,Memrel(j)))"
+
+  oadd      :: [i,i]=>i           (infixl "++" 65)
+    "i ++ j == raw_oadd(ordify(i),ordify(j))"
 
   (*ordinal subtraction*)
-  odiff_def     "i -- j == ordertype(i-j, Memrel(i))"
+  odiff      :: [i,i]=>i           (infixl "--" 65)
+    "i -- j == ordertype(i-j, Memrel(i))"
 
+  
 syntax (xsymbols)
   "op **"     :: [i,i] => i          (infixl "\\<times>\\<times>" 70)
 
