5ff5ed16ef102f2070333e08ca7c922368053064
wenzelm <none@none>
2002-07-04 16:48:21 +0200

    tuned;

--- src/HOL/NatArith.thy
+++ src/HOL/NatArith.thy
@@ -1,38 +1,59 @@
 (*  Title:      HOL/NatArith.thy
     ID:         $Id$
-
-Setup arithmetic proof procedures.
+    Author:     Tobias Nipkow and Markus Wenzel
+    License:    GPL (GNU GENERAL PUBLIC LICENSE)
 *)
+
+header {* More arithmetic on natural numbers *}
 
 theory NatArith = Nat
 files "arith_data.ML":
 
 setup arith_setup
 
+
 lemma pred_nat_trancl_eq_le: "((m, n) : pred_nat^*) = (m <= n)"
 apply (simp add: less_eq reflcl_trancl [symmetric]
             del: reflcl_trancl)
-by arith
+apply arith
+done
 
-(*elimination of `-' on nat*)
 lemma nat_diff_split:
     "P(a - b::nat) = ((a<b --> P 0) & (ALL d. a = b + d --> P d))"
-  by (cases "a<b" rule: case_split) (auto simp add: diff_is_0_eq [THEN iffD2])
+    -- {* elimination of @{text -} on @{text nat} *}
+  by (cases "a<b" rule: case_split)
+    (auto simp add: diff_is_0_eq [THEN iffD2])
 
-(*elimination of `-' on nat in assumptions*)
 lemma nat_diff_split_asm:
     "P(a - b::nat) = (~ (a < b & ~ P 0 | (EX d. a = b + d & ~ P d)))"
+    -- {* elimination of @{text -} on @{text nat} in assumptions *}
   by (simp split: nat_diff_split)
 
 ML {*
  val nat_diff_split = thm "nat_diff_split";
  val nat_diff_split_asm = thm "nat_diff_split_asm";
 
-(* TODO: use this for force_tac in Provers/clasip.ML *)
 fun add_arith cs = cs addafter ("arith_tac", arith_tac);
-*}
+*} (* TODO: use arith_tac for force_tac in Provers/clasip.ML *)
 
 lemmas [arith_split] = nat_diff_split split_min split_max
 
+
+subsubsection {* Generic summation indexed over natural numbers *}
+
+consts
+  Summation :: "(nat => 'a::{zero, plus}) => nat => 'a"
+primrec
+  "Summation f 0 = 0"
+  "Summation f (Suc n) = Summation f n + f n"
+
+syntax
+  "_Summation" :: "idt => nat => 'a => nat"    ("\<Sum>_<_. _" [0, 51, 10] 10)
+translations
+  "\<Sum>i < n. b" == "Summation (\<lambda>i. b) n"
+
+theorem Summation_step:
+    "0 < n ==> (\<Sum>i < n. f i) = (\<Sum>i < n - 1. f i) + f (n - 1)"
+  by (induct n) simp_all
 
 end
--- src/HOL/PreList.thy
+++ src/HOL/PreList.thy
@@ -19,22 +19,4 @@ lemmas less_induct = nat_less_induct [rule_format, case_names less]
 (*belongs to theory Wellfounded_Recursion*)
 lemmas wf_induct_rule = wf_induct [rule_format, case_names less, induct set: wf]
 
-
-(* generic summation indexed over nat *)
-
-consts
-  Summation :: "(nat => 'a::{zero, plus}) => nat => 'a"
-primrec
-  "Summation f 0 = 0"
-  "Summation f (Suc n) = Summation f n + f n"
-
-syntax
-  "_Summation" :: "idt => nat => 'a => nat"    ("\<Sum>_<_. _" [0, 51, 10] 10)
-translations
-  "\<Sum>i < n. b" == "Summation (\<lambda>i. b) n"
-
-theorem Summation_step:
-    "0 < n ==> (\<Sum>i < n. f i) = (\<Sum>i < n - 1. f i) + f (n - 1)"
-  by (induct n) simp_all
-
 end
