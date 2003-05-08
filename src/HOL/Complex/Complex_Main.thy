b96b890f5bf4b13a3735ae8fa2a8ea5f940a6aff
paulson <none@none>
2003-05-08 17:44:38 +0200

    new theory Complex_Main as basis for analysis developments

---
+++ src/HOL/Complex/Complex_Main.thy
@@ -0,0 +1,11 @@
+(*  Title:      HOL/Complex/Complex_Main.thy
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   2003  University of Cambridge
+*)
+
+header{*Comprehensive Complex Theory*}
+
+theory Complex_Main = CLim:
+
+end
