05c8688168dd8e46a1324589402c50132bb8b7d0
paulson <none@none>
2003-07-24 16:35:51 +0200

    tidied

--- src/HOL/PreList.thy
+++ src/HOL/PreList.thy
@@ -2,19 +2,14 @@
     ID:         $Id$
     Author:     Tobias Nipkow and Markus Wenzel
     Copyright   2000 TU Muenchen
-
-A basis for building theory List on. Is defined separately to serve as a
-basis for theory ToyList in the documentation.
 *)
 
-theory PreList =
-  Wellfounded_Relations + Presburger + Recdef + Relation_Power:
+header{*A Basis for Building the Theory of Lists*}
 
-(*belongs to theory Divides*)
-declare dvdI [intro?]  dvdE [elim?]  dvd_trans [trans]
+(*Is defined separately to serve as a basis for theory ToyList in the
+documentation.*)
 
-(*belongs to theory Nat*)
-lemmas less_induct = nat_less_induct [rule_format, case_names less]
+theory PreList = Wellfounded_Relations + Presburger + Recdef + Relation_Power:
 
 (*belongs to theory Wellfounded_Recursion*)
 lemmas wf_induct_rule = wf_induct [rule_format, case_names less, induct set: wf]
