39def31577bc0738e028ceba6e7b68dfe979cd5f
obua <none@none>
2004-05-10 16:40:54 +0200

    moved first lemma in LongDiv.ML to LongDiv.thy

--- src/HOL/Algebra/poly/LongDiv.thy
+++ src/HOL/Algebra/poly/LongDiv.thy
@@ -4,15 +4,27 @@
     Author: Clemens Ballarin, started 23 June 1999
 *)
 
-LongDiv = PolyHomo +
+theory LongDiv = PolyHomo:
 
 consts
   lcoeff :: "'a::ring up => 'a"
-  eucl_size :: 'a::ring => nat
+  eucl_size :: "'a::ring => nat"
 
 defs
-  lcoeff_def	"lcoeff p == coeff p (deg p)"
-  eucl_size_def "eucl_size p == if p = 0 then 0 else deg p+1"
+  lcoeff_def: "lcoeff p == coeff p (deg p)"
+  eucl_size_def: "eucl_size p == (if p = 0 then 0 else deg p+1)"
+
+lemma SUM_shrink_below_lemma:
+  "!! f::(nat=>'a::ring). (ALL i. i < m --> f i = 0) --> 
+  setsum (%i. f (i+m)) {..d} = setsum f {..m+d}"
+  apply (induct_tac d)
+  apply (induct_tac m)
+  apply (simp)
+  apply (force)
+  apply (simp)
+  apply (subst plus_ac0.commute[of m])
+  apply (simp)
+  done
 
 end
 
