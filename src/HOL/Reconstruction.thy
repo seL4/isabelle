5dae07094f6086b48934c575f66655470ebda252
quigley <none@none>
2005-04-07 17:45:51 +0200

    Reconstruction.thy and IsaMakefile updated

--- src/HOL/Reconstruction.thy
+++ src/HOL/Reconstruction.thy
@@ -14,6 +14,19 @@ theory Reconstruction
 	  "Tools/res_axioms.ML"
 	  "Tools/res_types_sorts.ML"
 
+          "Tools/ATP/recon_prelim.ML"
+	  "Tools/ATP/recon_gandalf_base.ML"
+ 	  "Tools/ATP/recon_order_clauses.ML"
+ 	  "Tools/ATP/recon_translate_proof.ML"
+ 	  "Tools/ATP/recon_parse.ML"
+ 	  "Tools/ATP/recon_transfer_proof.ML"
+	  "Tools/ATP/VampireCommunication.ML"
+	  "Tools/ATP/SpassCommunication.ML"
+	  "Tools/ATP/modUnix.ML"
+	  "Tools/ATP/watcher.sig"
+	  "Tools/ATP/watcher.ML"
+	  "Tools/res_atp.ML"
+
           "Tools/reconstruction.ML"
 
 begin
