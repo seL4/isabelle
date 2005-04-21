e7f80222324810aaad3d841f9c56c2f08078ab97
wenzelm <none@none>
2005-04-21 22:06:18 +0200

    The CPure theory -- Pure with alternative application syntax.

---
+++ src/Pure/CPure.thy
@@ -0,0 +1,16 @@
+(*  Title:      Pure/CPure.thy
+    ID:         $Id$
+    Author:     Makarius
+
+The CPure theory -- Pure with alternative application syntax.
+*)
+
+theory CPure
+imports Pure
+begin
+
+setup
+"[Theory.del_modesyntax Syntax.default_mode Syntax.pure_appl_syntax,
+  Theory.add_syntax Syntax.pure_applC_syntax]"
+
+end
