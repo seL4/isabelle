bb2f2a890c19a435a7013cdb317abb9917195540
wenzelm <none@none>
2005-07-14 19:28:23 +0200

    use all files in HOLCF.thy;

--- src/HOLCF/HOLCF.thy
+++ src/HOLCF/HOLCF.thy
@@ -2,11 +2,27 @@
     ID:         $Id$
     Author:     Franz Regensburger
 
-Top theory for HOLCF system.
+HOLCF -- a semantic extension of HOL by the LCF logic.
 *)
 
 theory HOLCF
 imports Sprod Ssum Up Lift Discrete One Tr Domain
+uses
+  "holcf_logic.ML"
+  "cont_consts.ML"
+  "domain/library.ML"
+  "domain/syntax.ML"
+  "domain/axioms.ML"
+  "domain/theorems.ML"
+  "domain/extender.ML"
+  "domain/interface.ML"
+  "adm_tac.ML"
+
 begin
+
+ML_setup {*
+  simpset_ref() := simpset() addSolver
+     (mk_solver "adm_tac" (fn thms => (adm_tac (cut_facts_tac thms THEN' cont_tacRs))));
+*}
 
 end
