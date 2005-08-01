77010390f2b4d8f82128e115ec04dc3805aa55e0
wenzelm <none@none>
2005-08-01 19:20:47 +0200

    tuned;

--- src/HOL/ex/NatSum.thy
+++ src/HOL/ex/NatSum.thy
@@ -1,4 +1,4 @@
-(* Title: HOL/ex/NatSum.ML
+(*  Title: HOL/ex/NatSum.thy
     ID:         $Id$
     Author:     Tobias Nipkow
 *)
@@ -9,9 +9,6 @@ theory NatSum imports Main begin
 
 text {*
   Summing natural numbers, squares, cubes, etc.
-
-  Originally demonstrated permutative rewriting, but @{thm [source]
-  add_ac} is no longer needed thanks to new simprocs.
 
   Thanks to Sloane's On-Line Encyclopedia of Integer Sequences,
   \url{http://www.research.att.com/~njas/sequences/}.
@@ -28,7 +25,7 @@ text {*
 *}
 
 lemma sum_of_odds: "(\<Sum>i=0..<n. Suc (i + i)) = n * n"
-  by (induct n, auto)
+  by (induct n) auto
 
 
 text {*
@@ -37,7 +34,7 @@ text {*
 
 lemma sum_of_odd_squares:
   "3 * (\<Sum>i=0..<n. Suc(2*i) * Suc(2*i)) = n * (4 * n * n - 1)"
-  by (induct n, auto)
+  by (induct n) auto
 
 
 text {*
@@ -47,7 +44,7 @@ text {*
 lemma sum_of_odd_cubes:
   "(\<Sum>i=0..<n. Suc (2*i) * Suc (2*i) * Suc (2*i)) =
     n * n * (2 * n * n - 1)"
-  by (induct n, auto)
+  by (induct n) auto
 
 text {*
   \medskip The sum of the first @{text n} positive integers equals
@@ -55,15 +52,15 @@ text {*
 
 lemma sum_of_naturals:
     "2 * (\<Sum>i=0..n. i) = n * Suc n"
-  by (induct n, auto)
+  by (induct n) auto
 
 lemma sum_of_squares:
     "6 * (\<Sum>i=0..n. i * i) = n * Suc n * Suc (2 * n)"
-  by (induct n, auto)
+  by (induct n) auto
 
 lemma sum_of_cubes:
     "4 * (\<Sum>i=0..n. i * i * i) = n * n * Suc n * Suc n"
-  by (induct n, auto)
+  by (induct n) auto
 
 
 text {*
@@ -87,13 +84,13 @@ lemma int_sum_of_fourth_powers:
   "30 * int (\<Sum>i=0..<m. i * i * i * i) =
     int m * (int m - 1) * (int(2 * m) - 1) *
     (int(3 * m * m) - int(3 * m) - 1)"
-  by (induct m, simp_all add: int_mult)
+  by (induct m) (simp_all add: int_mult)
 
 lemma of_nat_sum_of_fourth_powers:
   "30 * of_nat (\<Sum>i=0..<m. i * i * i * i) =
     of_nat m * (of_nat m - 1) * (of_nat (2 * m) - 1) *
     (of_nat (3 * m * m) - of_nat (3 * m) - (1::int))"
-  by (induct m, simp_all)
+  by (induct m) simp_all
 
 
 text {*
@@ -102,15 +99,12 @@ text {*
 *}
 
 lemma sum_of_2_powers: "(\<Sum>i=0..<n. 2^i) = 2^n - (1::nat)"
-  apply (induct n)
-   apply (auto split: nat_diff_split)
-  done
+  by (induct n) (auto split: nat_diff_split)
 
 lemma sum_of_3_powers: "2 * (\<Sum>i=0..<n. 3^i) = 3^n - (1::nat)"
-  by (induct n, auto)
+  by (induct n) auto
 
 lemma sum_of_powers: "0 < k ==> (k - 1) * (\<Sum>i=0..<n. k^i) = k^n - (1::nat)"
-  by (induct n, auto)
-
+  by (induct n) auto
 
 end
