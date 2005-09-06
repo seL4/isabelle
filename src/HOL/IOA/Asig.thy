759ed3d103ab7672fc9dc095f7d82f2c30d5b318
wenzelm <none@none>
2005-09-06 19:03:39 +0200

    converted to Isar theory format;

--- src/HOL/IOA/Asig.thy
+++ src/HOL/IOA/Asig.thy
@@ -2,44 +2,50 @@
     ID:         $Id$
     Author:     Tobias Nipkow & Konrad Slind
     Copyright   1994  TU Muenchen
-
-Action signatures
 *)
 
-Asig = Main +
+header {* Action signatures *}
 
-types 
+theory Asig
+imports Main
+begin
 
-'a signature = "('a set * 'a set * 'a set)"
+types
+  'a signature = "('a set * 'a set * 'a set)"
 
 consts
-  actions,inputs,outputs,internals,externals
-                ::"'action signature => 'action set"
+  "actions" :: "'action signature => 'action set"
+  "inputs" :: "'action signature => 'action set"
+  "outputs" :: "'action signature => 'action set"
+  "internals" :: "'action signature => 'action set"
+  externals :: "'action signature => 'action set"
+
   is_asig       ::"'action signature => bool"
   mk_ext_asig   ::"'action signature => 'action signature"
 
 
 defs
 
-asig_inputs_def    "inputs == fst"
-asig_outputs_def   "outputs == (fst o snd)"
-asig_internals_def "internals == (snd o snd)"
+asig_inputs_def:    "inputs == fst"
+asig_outputs_def:   "outputs == (fst o snd)"
+asig_internals_def: "internals == (snd o snd)"
 
-actions_def
+actions_def:
    "actions(asig) == (inputs(asig) Un outputs(asig) Un internals(asig))"
 
-externals_def
+externals_def:
    "externals(asig) == (inputs(asig) Un outputs(asig))"
 
-is_asig_def
-  "is_asig(triple) ==            
-      ((inputs(triple) Int outputs(triple) = {})    & 
-       (outputs(triple) Int internals(triple) = {}) & 
+is_asig_def:
+  "is_asig(triple) ==
+      ((inputs(triple) Int outputs(triple) = {})    &
+       (outputs(triple) Int internals(triple) = {}) &
        (inputs(triple) Int internals(triple) = {}))"
 
 
-mk_ext_asig_def
+mk_ext_asig_def:
   "mk_ext_asig(triple) == (inputs(triple), outputs(triple), {})"
 
+ML {* use_legacy_bindings (the_context ()) *}
 
-end 
+end
--- src/HOL/IOA/Solve.thy
+++ src/HOL/IOA/Solve.thy
@@ -2,21 +2,25 @@
     ID:         $Id$
     Author:     Tobias Nipkow & Konrad Slind
     Copyright   1994  TU Muenchen
-
-Weak possibilities mapping (abstraction)
 *)
 
-Solve = IOA +
+header {* Weak possibilities mapping (abstraction) *}
+
+theory Solve
+imports IOA
+begin
 
 constdefs
 
   is_weak_pmap :: "['c => 'a, ('action,'c)ioa,('action,'a)ioa] => bool"
-  "is_weak_pmap f C A ==                          
-   (!s:starts_of(C). f(s):starts_of(A)) &        
-   (!s t a. reachable C s &                      
-            (s,a,t):trans_of(C)                  
-            --> (if a:externals(asig_of(C)) then 
-                   (f(s),a,f(t)):trans_of(A)     
+  "is_weak_pmap f C A ==
+   (!s:starts_of(C). f(s):starts_of(A)) &
+   (!s t a. reachable C s &
+            (s,a,t):trans_of(C)
+            --> (if a:externals(asig_of(C)) then
+                   (f(s),a,f(t)):trans_of(A)
                  else f(s)=f(t)))"
+
+ML {* use_legacy_bindings (the_context ()) *}
 
 end
