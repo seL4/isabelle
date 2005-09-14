9fd70c9931a31583a46739cb5daa1dcef83a93aa
wenzelm <none@none>
2005-09-14 23:14:57 +0200

    renamed Guard/NS_Public, Guard/OtwayRees, Guard/Yahalom.thy to avoid clash with plain Auth versions;

--- src/HOL/Auth/Guard/Guard_Yahalom.thy
+++ src/HOL/Auth/Guard/Guard_Yahalom.thy
@@ -11,7 +11,7 @@ Cambridge CB3 0FD, United Kingdom
 
 header{*Yahalom Protocol*}
 
-theory Yahalom imports Guard_Shared begin
+theory Guard_Yahalom imports Guard_Shared begin
 
 subsection{*messages used in the protocol*}
 
