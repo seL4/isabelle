799a10a89dd2f91cc3ffdc50d99c2c5cba6fdc93
mengj <none@none>
2005-10-28 02:30:12 +0200

    Added a new file res_hol_clause.ML to Reconstruction.thy. This file is used to translate HOL formulae into FOL clauses.

--- src/HOL/Reconstruction.thy
+++ src/HOL/Reconstruction.thy
@@ -9,6 +9,7 @@ header{* Reconstructing external resolution proofs *}
 theory Reconstruction
 imports Hilbert_Choice Map Infinite_Set Extraction
 uses "Tools/res_clause.ML"
+         "Tools/res_hol_clause.ML"
 	 "Tools/res_axioms.ML"
 	 "Tools/ATP/recon_order_clauses.ML"
 	 "Tools/ATP/recon_translate_proof.ML"
