9f7d4f3fd86198f46b218f96cd8d1b4a811ef530
mengj <none@none>
2005-11-18 07:10:37 +0100

    -- changed the interface of functions vampire_oracle and eprover_oracle.

--- src/HOL/ResAtpMethods.thy
+++ src/HOL/ResAtpMethods.thy
@@ -13,8 +13,8 @@ uses
 
 begin
 
-oracle vampire_oracle ("string list * int") = {* ResAtpProvers.vampire_o *}
-oracle eprover_oracle ("string list * int") = {* ResAtpProvers.eprover_o *}
+oracle vampire_oracle ("(string list * string list) * int") = {* ResAtpProvers.vampire_o *}
+oracle eprover_oracle ("(string list * string list) * int") = {* ResAtpProvers.eprover_o *}
 
 use "Tools/res_atp_methods.ML"
 setup ResAtpMethods.ResAtps_setup
