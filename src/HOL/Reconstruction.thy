63bb79032b3fe1d5dc938f8992bb7616a920505b
paulson <none@none>
2005-12-21 12:06:08 +0100

    new hash table module in HOL/Too/s

--- src/HOL/Reconstruction.thy
+++ src/HOL/Reconstruction.thy
@@ -8,8 +8,9 @@ header{* Reconstructing external resolution proofs *}
 
 theory Reconstruction
 imports Hilbert_Choice Map Infinite_Set Extraction
-uses "Tools/res_clause.ML"
-         "Tools/res_hol_clause.ML"
+uses 	 "Tools/polyhash.ML"
+         "Tools/res_clause.ML"
+	 "Tools/res_hol_clause.ML"
 	 "Tools/res_axioms.ML"
 	 "Tools/ATP/recon_order_clauses.ML"
 	 "Tools/ATP/recon_translate_proof.ML"
