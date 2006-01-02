972c0bd95408e36b3d5f30f7f2c1c53410550505
wenzelm <none@none>
2006-01-02 20:42:12 +0100

    outline;

--- doc-src/IsarImplementation/Thy/ML.thy
+++ doc-src/IsarImplementation/Thy/ML.thy
@@ -1,2 +1,15 @@
 
-theory "ML" imports Pure begin end
+(* $Id$ *)
+
+theory "ML" imports base begin
+
+chapter {* Aesthetics of ML programming *}
+
+text {* FIXME style guide *}
+
+
+chapter {* Basic library functions *}
+
+text {* FIXME beyond the basis library definition *}
+
+end
