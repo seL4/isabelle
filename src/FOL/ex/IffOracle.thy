e3517f6b688a0f8fbff13bc23e34b975b36b42ea
wenzelm <none@none>
2006-01-14 17:14:06 +0100

    sane ERROR handling;

--- src/FOL/ex/IffOracle.thy
+++ src/FOL/ex/IffOracle.thy
@@ -40,12 +40,12 @@ ML {* #der (Thm.rep_thm it) *}
 text {* These oracle calls had better fail *}
 
 ML {*
-  (iff_oracle (the_context ()) 5; raise ERROR)
+  (iff_oracle (the_context ()) 5; error "?")
     handle Fail _ => warning "Oracle failed, as expected"
 *}
 
 ML {*
-  (iff_oracle (the_context ()) 1; raise ERROR)
+  (iff_oracle (the_context ()) 1; error "?")
     handle Fail _ => warning "Oracle failed, as expected"
 *}
 
