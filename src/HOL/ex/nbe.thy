1802664a77d1bf9209fff71012cfb27d1605455d
haftmann <none@none>
2006-03-01 13:47:42 +0100

    refined representation of codegen intermediate language

--- src/HOL/ex/nbe.thy
+++ src/HOL/ex/nbe.thy
@@ -7,28 +7,17 @@ theory nbe
 imports Main
 begin
 
-datatype n = Z | S n
-consts
- add :: "n \<Rightarrow> n \<Rightarrow> n"
- mul :: "n \<Rightarrow> n \<Rightarrow> n"
- exp :: "n \<Rightarrow> n \<Rightarrow> n"
-primrec
-"add Z = id"
-"add (S m) = S o add m"
-primrec
-"mul Z = (%n. Z)"
-"mul (S m) = (%n. add (mul m n) n)"
-primrec
-"exp m Z = S Z"
-"exp m (S n) = mul (exp m n) m"
-
 (*ML"set Toplevel.timing"*)
-;norm_by_eval "exp (S(S(S(S(S(S(S Z))))))) (S(S(S(S(S Z)))))";
-norm_by_eval "0 + (n::nat)";
-norm_by_eval "0 + Suc(n)";
-norm_by_eval "Suc(n) + Suc m";
-norm_by_eval "[] @ xs";
-norm_by_eval "(x#xs) @ ys";
-norm_by_eval "[x,y,z] @ [a,b,c]";
+norm_by_eval "exp (Suc (Suc (Suc (Suc (Suc (Suc (Suc 0))))))) (Suc (Suc  (Suc (Suc (Suc 0)))))"
+norm_by_eval "0 + (n::nat)"
+norm_by_eval "0 + Suc(n)"
+norm_by_eval "Suc(n) + Suc m"
+norm_by_eval "[] @ xs"
+norm_by_eval "(x#xs) @ ys"
+norm_by_eval "[x,y,z] @ [a,b,c]"
+norm_by_eval "%(xs, ys). xs @ ys"
+norm_by_eval "(%(xs, ys). xs @ ys) ([a, b, c], [d, e, f])"
+norm_by_eval "%x. case x of None \<Rightarrow> False | Some y \<Rightarrow> True"
+norm_by_eval "map (%x. case x of None \<Rightarrow> False | Some y \<Rightarrow> True) [None, Some ()]"
 
 end
