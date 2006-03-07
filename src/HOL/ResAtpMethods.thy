5629c07a189efa4a9161f643b2cae46e091d4446
mengj <none@none>
2006-03-07 03:49:26 +0100

    When ATP methods call ATPs, all input clauses from one subgoal are written to one file.

--- src/HOL/ResAtpMethods.thy
+++ src/HOL/ResAtpMethods.thy
@@ -7,14 +7,13 @@ header {* ATP setup (Vampire and E prover) *}
 theory ResAtpMethods
 imports Reconstruction
 uses
-  "Tools/res_atp_setup.ML"
   "Tools/res_atp_provers.ML"
   ("Tools/res_atp_methods.ML")
 
 begin
 
-oracle vampire_oracle ("(string list * string list) * int") = {* ResAtpProvers.vampire_o *}
-oracle eprover_oracle ("(string list * string list) * int") = {* ResAtpProvers.eprover_o *}
+oracle vampire_oracle ("string * int") = {* ResAtpProvers.vampire_o *}
+oracle eprover_oracle ("string * int") = {* ResAtpProvers.eprover_o *}
 
 use "Tools/res_atp_methods.ML"
 setup ResAtpMethods.ResAtps_setup
