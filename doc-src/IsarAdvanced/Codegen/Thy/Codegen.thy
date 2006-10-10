42d63663903ef0d4da74f1383df8c480e411dfcb
haftmann <none@none>
2006-10-10 12:08:12 +0200

    initial draft

---
+++ doc-src/IsarAdvanced/Codegen/Thy/Codegen.thy
@@ -0,0 +1,47 @@
+
+(* $Id$ *)
+
+theory Codegen
+imports Main
+begin
+
+chapter {* Code generation from Isabelle theories *}
+
+section {* Introduction *}
+
+text {*
+  \cite{isa-tutorial}
+*} (* add graphics here *)
+
+section {* Basics *}
+
+subsection {* Invoking the code generator *}
+
+subsection {* Theorem selection *}
+
+(* print_codethms, code func, default setup code nofunc *)
+
+subsection {* Type classes *}
+
+subsection {* Preprocessing *}
+
+(* preprocessing, print_codethms () *)
+
+subsection {* Incremental code generation *}
+
+(* print_codethms (\<dots>) *)
+
+
+section {* Customizing serialization  *}
+
+section {* Recipes and advanced topics *}
+
+(* understanding the type game, reflexive equations, code inline code_constsubst, code_abstype*)
+
+section {* ML interfaces *}
+
+subsection {* codegen\_data.ML *}
+
+subsection {* Implementing code generator applications *}
+
+end
