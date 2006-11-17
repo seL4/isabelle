dbfd2bc099cd765bdcc459cb74651257f5082efb
haftmann <none@none>
2006-11-18 00:20:29 +0100

    cleanup

--- src/HOL/ex/Codegenerator.thy
+++ src/HOL/ex/Codegenerator.thy
@@ -190,7 +190,6 @@ code_gen
   "Code_Generator.eq :: mut2 \<Rightarrow> mut2 \<Rightarrow> bool"
   "Code_Generator.eq :: ('a\<Colon>eq) point_scheme \<Rightarrow> 'a point_scheme \<Rightarrow> bool"
 
-code_gen (SML *)
-code_gen (Haskell -)
+code_gen (SML *) (Haskell -)
 
 end
