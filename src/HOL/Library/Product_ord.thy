dd7129eb0710acfceebbede0d123691b6dd81ba8
haftmann <none@none>
2006-11-22 10:20:18 +0100

    added code lemmas

--- src/HOL/Library/Product_ord.thy
+++ src/HOL/Library/Product_ord.thy
@@ -9,14 +9,16 @@ theory Product_ord
 imports Main
 begin
 
-instance "*" :: (ord, ord) ord ..
-
-defs (overloaded)
+instance "*" :: (ord, ord) ord
   prod_le_def: "(x \<le> y) \<equiv> (fst x < fst y) | (fst x = fst y & snd x \<le> snd y)"
-  prod_less_def: "(x < y) \<equiv> (fst x < fst y) | (fst x = fst y & snd x < snd y)"
-
+  prod_less_def: "(x < y) \<equiv> (fst x < fst y) | (fst x = fst y & snd x < snd y)" ..
 
 lemmas prod_ord_defs = prod_less_def prod_le_def
+
+lemma [code]:
+  "(x1, y1) \<le> (x2, y2) \<longleftrightarrow> x1 < x2 \<or> x1 = x2 \<and> y1 \<le> y2"
+  "(x1, y1) < (x2, y2) \<longleftrightarrow> x1 < x2 \<or> x1 = x2 \<and> y1 < y2"
+  unfolding prod_ord_defs by simp_all
 
 instance * :: (order, order) order
   by default (auto simp: prod_ord_defs intro: order_less_trans)
