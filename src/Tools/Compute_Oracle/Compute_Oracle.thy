29b0f464e2952830a038bf4e2e4d2004e3a60ac1
obua <none@none>
2007-07-09 17:36:25 +0200

    new version of computing oracle

--- src/Tools/Compute_Oracle/Compute_Oracle.thy
+++ src/Tools/Compute_Oracle/Compute_Oracle.thy
@@ -5,28 +5,10 @@
 Steven Obua's evaluator.
 *)
 
-theory Compute_Oracle
-imports CPure
-uses
-  "am_interpreter.ML"
-  "am_compiler.ML"
-  "am_util.ML"
-  "compute.ML"
+theory Compute_Oracle imports CPure
+uses "am.ML" "am_compiler.ML" "am_interpreter.ML" "am_ghc.ML" "am_sml.ML" "compute.ML" "linker.ML"
 begin
 
-oracle compute_oracle ("Compute.computer * (int -> string) * cterm") =
-  {* Compute.oracle_fn *}
-
-ML {*
-structure Compute =
-struct
-  open Compute
-
-  fun rewrite_param r n ct =
-    compute_oracle (Thm.theory_of_cterm ct) (r, n, ct)
-
-  fun rewrite r ct = rewrite_param r default_naming ct
-end
-*}
+setup {* Compute.setup; *}
 
 end
