5c1da4fb2a88f4e39fbe463794985bbc394b5001
haftmann <none@none>
2007-08-07 09:38:43 +0200

    changed import order

--- src/HOL/ATP_Linkup.thy
+++ src/HOL/ATP_Linkup.thy
@@ -7,7 +7,7 @@
 header{* The Isabelle-ATP Linkup *}
 
 theory ATP_Linkup
-imports Divides Hilbert_Choice Record
+imports Divides Record Hilbert_Choice
 uses
   "Tools/polyhash.ML"
   "Tools/res_clause.ML"
