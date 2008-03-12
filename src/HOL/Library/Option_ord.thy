476323510255fcc73b5914ac1ffa5985b0944d9b
haftmann <none@none>
2008-03-12 08:45:51 +0100

    *** empty log message ***

--- src/HOL/Library/Option_ord.thy
+++ src/HOL/Library/Option_ord.thy
@@ -18,34 +18,34 @@ definition less_eq_option where
 definition less_option where
   [code func del]: "x < y \<longleftrightarrow> (case y of None \<Rightarrow> False | Some y \<Rightarrow> (case x of None \<Rightarrow> True | Some x \<Rightarrow> x < y))"
 
-lemma less_eq_option_None [simp]: "None \<le> (x\<Colon>'a option)"
+lemma less_eq_option_None [simp]: "None \<le> x"
   by (simp add: less_eq_option_def)
 
-lemma less_eq_option_None_code [code]: "None \<le> (x\<Colon>'a option) \<longleftrightarrow> True"
+lemma less_eq_option_None_code [code]: "None \<le> x \<longleftrightarrow> True"
   by simp
 
-lemma less_eq_option_None_is_None: "(x\<Colon>'a option) \<le> None \<Longrightarrow> x = None"
+lemma less_eq_option_None_is_None: "x \<le> None \<Longrightarrow> x = None"
   by (cases x) (simp_all add: less_eq_option_def)
 
-lemma less_eq_option_Some_None [simp, code]: "Some (x\<Colon>'a) \<le> None \<longleftrightarrow> False"
+lemma less_eq_option_Some_None [simp, code]: "Some x \<le> None \<longleftrightarrow> False"
   by (simp add: less_eq_option_def)
 
-lemma less_eq_option_Some [simp, code]: "Some (x\<Colon>'a) \<le> Some y \<longleftrightarrow> x \<le> y"
+lemma less_eq_option_Some [simp, code]: "Some x \<le> Some y \<longleftrightarrow> x \<le> y"
   by (simp add: less_eq_option_def)
 
-lemma less_option_None [simp, code]: "(x\<Colon>'a option) < None \<longleftrightarrow> False"
+lemma less_option_None [simp, code]: "x < None \<longleftrightarrow> False"
   by (simp add: less_option_def)
 
-lemma less_option_None_is_Some: "None < (x\<Colon>'a option) \<Longrightarrow> \<exists>z. x = Some z"
+lemma less_option_None_is_Some: "None < x \<Longrightarrow> \<exists>z. x = Some z"
   by (cases x) (simp_all add: less_option_def)
 
-lemma less_option_None_Some [simp]: "None < Some (x\<Colon>'a)"
+lemma less_option_None_Some [simp]: "None < Some x"
   by (simp add: less_option_def)
 
-lemma less_option_None_Some_code [code]: "None < Some (x\<Colon>'a) \<longleftrightarrow> True"
+lemma less_option_None_Some_code [code]: "None < Some x \<longleftrightarrow> True"
   by simp
 
-lemma less_option_Some [simp, code]: "Some (x\<Colon>'a) < Some y \<longleftrightarrow> x < y"
+lemma less_option_Some [simp, code]: "Some x < Some y \<longleftrightarrow> x < y"
   by (simp add: less_option_def)
 
 instance by default
