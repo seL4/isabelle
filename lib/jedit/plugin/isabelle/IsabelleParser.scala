ae3b18144bfad8c134711a01349a1911e3ec7e02
wenzelm <none@none>
2008-07-20 20:23:49 +0200

    SideKickParsedData: minimal content;

--- lib/jedit/plugin/isabelle/IsabelleParser.scala
+++ lib/jedit/plugin/isabelle/IsabelleParser.scala
@@ -7,26 +7,54 @@ Isabelle parser setup for Sidekick plugin.
 
 package isabelle
 
+import javax.swing.text.Position
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeModel
+
 import org.gjt.sp.jedit.Buffer
 import org.gjt.sp.util.Log
 
+import sidekick.Asset
 import sidekick.SideKickParsedData
 import sidekick.SideKickParser
 import errorlist.DefaultErrorSource
 
 
-class IsabelleParser extends SideKickParser("isabelle") {
-  private var text: String = null
-  private var data: SideKickParsedData = null
-  private var buffer: Buffer = null
+private class IsabelleAsset(name: String, content: String) extends Asset(name)
+{
+  override def getShortString() = { name }
+  override def getLongString() = { content }
+  override def getIcon() = { null }
+}
 
-  // FIXME dummy -- no functionality yet
-  def parse(buf: Buffer, e: DefaultErrorSource): SideKickParsedData = {
-    buffer = buf
-    buffer.readLock()
-    text = buffer.getText(0, buffer.getLength())
-    data = new SideKickParsedData(buffer.getName())
-    buffer.readUnlock()
+
+class IsabelleParser extends SideKickParser("isabelle") {
+  private var stopped = false
+
+  override def stop () { stopped = true }
+
+  def parse(buffer: Buffer, e: DefaultErrorSource): SideKickParsedData = {
+    stopped = false
+
+    var text: String = null
+    var data: SideKickParsedData = null
+
+    try {
+      buffer.readLock()
+      text = buffer.getText(0, buffer.getLength())
+      data = new SideKickParsedData(buffer.getName())
+
+      val asset = new IsabelleAsset("theory", null)
+      asset.setStart(buffer.createPosition(0))
+      asset.setEnd(buffer.createPosition(buffer.getLength()))
+
+      val node = new DefaultMutableTreeNode(asset)
+      data.root.insert(node, node.getChildCount())
+
+    } finally {
+      buffer.readUnlock()
+    }
+
     data
   }
 }
