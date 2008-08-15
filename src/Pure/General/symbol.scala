818560a106682aa90a02e13cce62e444a23f33b0
wenzelm <none@none>
2008-08-15 21:53:40 +0200

    Basic support for Isabelle symbols.

---
+++ src/Pure/General/symbol.scala
@@ -0,0 +1,30 @@
+/*  Title:      Pure/General/symbol.scala
+    ID:         $Id$
+    Author:     Makarius
+
+Basic support for Isabelle symbols.
+*/
+
+package isabelle
+
+import java.util.regex.Pattern
+
+
+object Symbol {
+
+  /* Regular expressions */
+
+  private def compile(s: String) =
+    Pattern.compile(s, Pattern.COMMENTS | Pattern.DOTALL)
+
+  private val symbol_src = """ \\ \\? < (?:
+      \^? [A-Za-z][A-Za-z0-9_']* |
+      \^raw: [\x20-\x7e\u0100-\uffff && [^.>]]* ) >"""
+
+  private val bad_symbol_src = "(?!" + symbol_src + ")" +
+    """ \\ \\? < (?: (?! \p{Space} | ["`\\] | \(\* | \*\) | \{\* | \*\} ) . )*"""
+
+  val symbol_pattern = compile(symbol_src)
+  val bad_symbol_pattern = compile(bad_symbol_src)
+  val pattern = compile(symbol_src + "|" + bad_symbol_src + "| .")
+}
