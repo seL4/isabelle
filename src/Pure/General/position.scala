e72461c083cdbe96d1e220c2400cd58386a61d54
wenzelm <none@none>
2008-08-23 23:07:36 +0200

    Position properties.

---
+++ src/Pure/General/position.scala
@@ -0,0 +1,37 @@
+/*  Title:      Pure/General/position.scala
+    ID:         $Id$
+    Author:     Makarius
+
+Position properties.
+*/
+
+package isabelle
+
+import java.util.Properties
+
+
+object Position {
+
+  private def get_string(name: String, props: Properties) = {
+    val value = props.getProperty(name)
+    if (value != null) Some(value) else None
+  }
+
+  private def get_int(name: String, props: Properties) = {
+    get_string(name, props) match {
+      case None => None
+      case Some(value) =>
+        try { Some(Integer.parseInt(value)) }
+        catch { case e: NumberFormatException => None }
+    }
+  }
+
+  def line_of(props: Properties) = get_int(Markup.LINE, props)
+  def column_of(props: Properties) = get_int(Markup.COLUMN, props)
+  def offset_of(props: Properties) = get_int(Markup.OFFSET, props)
+  def end_line_of(props: Properties) = get_int(Markup.END_LINE, props)
+  def end_column_of(props: Properties) = get_int(Markup.END_COLUMN, props)
+  def end_offset_of(props: Properties) = get_int(Markup.END_OFFSET, props)
+  def file_of(props: Properties) = get_string(Markup.FILE, props)
+  def id_of(props: Properties) = get_string(Markup.ID, props)
+}
