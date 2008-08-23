3252da5d7063d7cda04c2c876dba6918d834f3f2
wenzelm <none@none>
2008-08-23 19:42:12 +0200

    Common markup elements.

---
+++ src/Pure/General/markup.scala
@@ -0,0 +1,15 @@
+/*  Title:      Pure/General/markup.scala
+    ID:         $Id$
+    Author:     Makarius
+
+Common markup elements.
+*/
+
+package isabelle
+
+object Markup {
+  val ROOT = "root"
+  val RAW = "raw"
+  val MALFORMED = "malformed"
+}
+
