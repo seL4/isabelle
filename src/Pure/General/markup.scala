5fa4ebf53d61093027e1baead838bdd19779cb3b
wenzelm <none@none>
2008-08-23 23:07:39 +0200

    added position, messages; renamed messages to content, malformed to bad;

--- src/Pure/General/markup.scala
+++ src/Pure/General/markup.scala
@@ -8,8 +8,28 @@ Common markup elements.
 package isabelle
 
 object Markup {
+
+  /* position */
+
+  val LINE = "line"
+  val COLUMN = "column"
+  val OFFSET = "offset"
+  val END_LINE = "end_line"
+  val END_COLUMN = "end_column"
+  val END_OFFSET = "end_offset"
+  val FILE = "file"
+  val ID = "id"
+
+
+  /* messages */
+
+  val PID = "pid"
+  val SESSION = "session"
+
+
+  /* content */
+
   val ROOT = "root"
   val RAW = "raw"
-  val MALFORMED = "malformed"
+  val BAD = "bad"
 }
-
