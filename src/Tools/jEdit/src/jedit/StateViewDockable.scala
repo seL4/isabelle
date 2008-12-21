fb7acbeab8c17c638fb95679818776d122eb7d60
wenzelm <none@none>
2008-12-21 19:51:56 +0100

    basic setup of anti-aliasing, according to jEdit property; tuned;

--- src/Tools/jEdit/src/jedit/StateViewDockable.scala
+++ src/Tools/jEdit/src/jedit/StateViewDockable.scala
@@ -13,27 +13,52 @@ import javax.swing.{JButton, JPanel, JScrollPane}
 
 import isabelle.IsabelleSystem.getenv
 
-import org.xhtmlrenderer.simple.XHTMLPanel
+import org.xhtmlrenderer.simple.{XHTMLPanel, FSScrollPane}
 import org.xhtmlrenderer.context.AWTFontResolver
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.extend.TextRenderer;
+import org.xhtmlrenderer.swing.Java2DTextRenderer;
 
+import org.gjt.sp.jedit.jEdit
 import org.gjt.sp.jedit.View
 import org.gjt.sp.jedit.gui.DockableWindowManager
+import org.gjt.sp.jedit.textarea.AntiAlias
 
 
 class StateViewDockable(view : View, position : String) extends JPanel {
-  val panel = new XHTMLPanel(new UserAgent())
 
+  // outer panel
   if (position == DockableWindowManager.FLOATING)
     setPreferredSize(new Dimension(500, 250))
-
   setLayout(new BorderLayout)
 
-  
-  //Copy-paste-support
-  private val cp = new SelectionActions
-  cp.install(panel)
 
-  add(new JScrollPane(panel), BorderLayout.CENTER)
+  // XHTML panel
+  val panel = new XHTMLPanel(new UserAgent)
+
+
+  // anti-aliasing
+  // TODO: proper EditBus event handling
+  {
+    val aa = jEdit.getProperty("view.antiAlias")
+    if (aa != null && aa != AntiAlias.NONE) {
+      panel.getSharedContext.setTextRenderer(
+        {
+          val renderer = new Java2DTextRenderer
+          renderer.setSmoothingThreshold(0)
+          renderer.setSmoothingLevel(TextRenderer.HIGH)
+          renderer
+        })
+    }
+  }
+
+  // copy & paste
+  (new SelectionActions).install(panel)
+
+
+  // scrolling
+  add(new FSScrollPane(panel), BorderLayout.CENTER)
+
 
   private val fontResolver =
     panel.getSharedContext.getFontResolver.asInstanceOf[AWTFontResolver]
