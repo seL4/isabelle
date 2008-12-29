b96e6561904bddb8e90138fd179f562a1771979b
wenzelm <none@none>
2008-12-29 15:13:53 +0100

    added methods "+" and "-"; event: non-synchronized execution of handlers, based on synchronized snapshot;

--- src/Pure/General/event_bus.scala
+++ src/Pure/General/event_bus.scala
@@ -6,15 +6,18 @@ Generic event bus.
 
 package isabelle
 
-import scala.collection.jcl.LinkedList
+import scala.collection.mutable.ListBuffer
 
 
 class EventBus[Event] {
   type Handler = Event => Unit
-  private val handlers = new LinkedList[Handler]
+  private val handlers = new ListBuffer[Handler]
 
-  def += (h: Handler) = synchronized { handlers -= h; handlers += h }
+  def += (h: Handler) = synchronized { handlers += h }
+  def + (h: Handler) = { this += h; this }
+
   def -= (h: Handler) = synchronized { handlers -= h }
+  def - (h: Handler) = { this -= h; this }
 
-  def event(e: Event) = synchronized { for(h <- handlers) h(e) }
+  def event(e: Event) = synchronized { handlers.toList } foreach (h => h(e))
 }
