a961a39ec30fec04e06ffaffa7e79628e2afeb07
haftmann <none@none>
2009-01-28 16:35:42 +0100

    explicit check for exactly one type variable in class specification elements

--- src/HOL/Algebra/abstract/PID.thy
+++ src/HOL/Algebra/abstract/PID.thy
@@ -1,6 +1,5 @@
 (*
     Principle ideal domains
-    $Id$
     Author: Clemens Ballarin, started 5 October 1999
 *)
 
@@ -8,7 +7,6 @@ theory PID imports Ideal2 begin
 
 instance pid < factorial
   apply intro_classes
-   apply (rule TrueI)
   apply (erule pid_irred_imp_prime)
   done
 
