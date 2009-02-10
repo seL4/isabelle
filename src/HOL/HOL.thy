39fa8b14fe5c28c4c4dc2ecaa9ddbb8ffb8b0d81
blanchet <none@none>
2009-02-10 14:02:45 +0100

    Renamed descriptions of Nitpick (and ATP) attributes, so that they fit well with the rest of the sentence in ProofGeneral.

--- src/HOL/HOL.thy
+++ src/HOL/HOL.thy
@@ -932,7 +932,7 @@ ML_Antiquote.value "claset"
 
 structure ResAtpset = NamedThmsFun(val name = "atp" val description = "ATP rules");
 
-structure ResBlacklist = NamedThmsFun(val name = "noatp" val description = "Theorems blacklisted for ATP");
+structure ResBlacklist = NamedThmsFun(val name = "noatp" val description = "theorems blacklisted for ATP");
 *}
 
 text {*ResBlacklist holds theorems blacklisted to sledgehammer. 
@@ -1707,17 +1707,17 @@ ML {*
 structure Nitpick_Const_Simp_Thms = NamedThmsFun
 (
   val name = "nitpick_const_simp"
-  val description = "Equational specification of constants as needed by Nitpick"
+  val description = "equational specification of constants as needed by Nitpick"
 )
 structure Nitpick_Const_Psimp_Thms = NamedThmsFun
 (
   val name = "nitpick_const_psimp"
-  val description = "Partial equational specification of constants as needed by Nitpick"
+  val description = "partial equational specification of constants as needed by Nitpick"
 )
 structure Nitpick_Ind_Intro_Thms = NamedThmsFun
 (
   val name = "nitpick_ind_intro"
-  val description = "Introduction rules for (co)inductive predicates as needed by Nitpick"
+  val description = "introduction rules for (co)inductive predicates as needed by Nitpick"
 )
 *}
 setup {* Nitpick_Const_Simp_Thms.setup
