0813d7c0ae711bd6cef079e735ea873d75a7eecd
wenzelm <none@none>
2009-03-20 11:26:25 +0100

    proper context for prove_cont/adm_tac;

--- src/HOLCF/HOLCF.thy
+++ src/HOLCF/HOLCF.thy
@@ -24,7 +24,8 @@ defaultsort pcpo
 declaration {* fn _ =>
   Simplifier.map_ss (fn simpset => simpset addSolver
     (mk_solver' "adm_tac" (fn ss =>
-      Adm.adm_tac (cut_facts_tac (Simplifier.prems_of_ss ss) THEN' cont_tacRs ss))));
+      Adm.adm_tac (Simplifier.the_context ss)
+        (cut_facts_tac (Simplifier.prems_of_ss ss) THEN' cont_tacRs ss))));
 *}
 
 end
