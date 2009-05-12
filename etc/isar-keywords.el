107fe3ca03632da273fd2c9c75e9bc7775aa7793
haftmann <none@none>
2009-05-12 21:39:19 +0200

    updated keywords

--- etc/isar-keywords.el
+++ etc/isar-keywords.el
@@ -168,6 +168,7 @@
     "print_cases"
     "print_claset"
     "print_classes"
+    "print_codeproc"
     "print_codesetup"
     "print_commands"
     "print_configs"
@@ -250,6 +251,7 @@
     "use_thy"
     "using"
     "value"
+    "values"
     "welcome"
     "with"
     "{"
@@ -369,6 +371,7 @@
     "print_cases"
     "print_claset"
     "print_classes"
+    "print_codeproc"
     "print_codesetup"
     "print_commands"
     "print_configs"
@@ -402,6 +405,7 @@
     "unused_thms"
     "use_thy"
     "value"
+    "values"
     "welcome"))
 
 (defconst isar-keywords-theory-begin
