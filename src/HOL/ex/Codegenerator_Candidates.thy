cca7267cb75e280ce3dd2c698effffd2825e669f
haftmann <none@none>
2009-06-02 15:53:07 +0200

    tuned code generator test theories
    --HG--
    rename : src/HOL/ex/ExecutableContent.thy => src/HOL/ex/Codegenerator_Candidates.thy
    rename : src/HOL/ex/Codegenerator_Pretty.thy => src/HOL/ex/Codegenerator_Pretty_Test.thy
    rename : src/HOL/ex/Codegenerator.thy => src/HOL/ex/Codegenerator_Test.thy

---
+++ src/HOL/ex/Codegenerator_Candidates.thy
@@ -0,0 +1,44 @@
+
+(* Author: Florian Haftmann, TU Muenchen *)
+
+header {* A huge collection of equations to generate code from *}
+
+theory Codegenerator_Candidates
+imports
+  Complex_Main
+  AssocList
+  Binomial
+  Commutative_Ring
+  Enum
+  List_Prefix
+  Nat_Infinity
+  Nested_Environment
+  Option_ord
+  Permutation
+  Primes
+  Product_ord
+  SetsAndFunctions
+  While_Combinator
+  Word
+  "~~/src/HOL/ex/Commutative_Ring_Complete"
+  "~~/src/HOL/ex/Records"
+begin
+
+(*temporary Haskell workaround*)
+declare typerep_fun_def [code inline]
+declare typerep_prod_def [code inline]
+declare typerep_sum_def [code inline]
+declare typerep_cpoint_ext_type_def [code inline]
+declare typerep_itself_def [code inline]
+declare typerep_list_def [code inline]
+declare typerep_option_def [code inline]
+declare typerep_point_ext_type_def [code inline]
+declare typerep_point'_ext_type_def [code inline]
+declare typerep_point''_ext_type_def [code inline]
+declare typerep_pol_def [code inline]
+declare typerep_polex_def [code inline]
+
+(*avoid popular infixes*)
+code_reserved SML union inter upto
+
+end
---
+++ src/HOL/ex/Codegenerator_Pretty_Test.thy
@@ -0,0 +1,14 @@
+
+(* Author: Florian Haftmann, TU Muenchen *)
+
+header {* Pervasive test of code generator using pretty literals *}
+
+theory Codegenerator_Pretty_Test
+imports Codegenerator_Pretty
+begin
+
+export_code * in SML module_name CodegenTest
+  in OCaml module_name CodegenTest file -
+  in Haskell file -
+
+end
