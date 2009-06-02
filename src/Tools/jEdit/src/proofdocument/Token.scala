19f303a9b989c061f6c515b982a91f4b088851c6
wenzelm <none@none>
2009-06-02 21:36:22 +0200

    use RichString.* to produce blanks;

--- src/Tools/jEdit/src/proofdocument/Token.scala
+++ src/Tools/jEdit/src/proofdocument/Token.scala
@@ -18,18 +18,13 @@ object Token {
     val OTHER = Value("OTHER")
   }
 
-  private def fill(n: Int) = {
-    val blanks = new Array[Char](n)
-    for(i <- 0 to n - 1) blanks(i) = ' '
-    new String(blanks)
-  }
   def string_from_tokens (tokens: List[Token], starts: Map[Token, Int]): String = {
     def stop(t: Token) = starts(t) + t.length
     tokens match {
       case Nil => ""
       case t::tokens => ( tokens.foldLeft
           (t.content, stop(t))
-          ((a, token) => (a._1 + fill(starts(token) - a._2) + token.content, stop(token)))
+          ((a, token) => (a._1 + " " * (starts(token) - a._2) + token.content, stop(token)))
         )._1
     }
   }
