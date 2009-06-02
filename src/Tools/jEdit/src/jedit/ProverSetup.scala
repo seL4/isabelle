b52e4c258a2936a6b03456fe5d5949b772fbe4d8
immler@in.tum.de <immler@in.tum.de>
2009-06-02 19:00:58 +0200

    split change on activate

--- src/Tools/jEdit/src/jedit/ProverSetup.scala
+++ src/Tools/jEdit/src/jedit/ProverSetup.scala
@@ -42,7 +42,11 @@ class ProverSetup(buffer: JEditBuffer)
     prover.set_document(theory_view.change_receiver,
       if (path.startsWith(Isabelle.VFS_PREFIX)) path.substring(Isabelle.VFS_PREFIX.length) else path)
     theory_view.activate
-    prover ! new isabelle.proofdocument.Text.Change(Isabelle.plugin.id(), 0,buffer.getText(0, buffer.getLength),0)
+    val MCL = TheoryView.MAX_CHANGE_LENGTH
+    for (i <- 0 to buffer.getLength / MCL) prover !
+      new isabelle.proofdocument.Text.Change(Isabelle.plugin.id(), i * MCL,
+                                             buffer.getText(i * MCL,
+                                                            MCL min buffer.getLength - i * MCL),0)
 
     //register output-view
     prover.output_info += (text =>
