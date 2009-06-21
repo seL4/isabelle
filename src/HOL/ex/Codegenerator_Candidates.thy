2467b902ee17192eb5f79959a2c3a1b5d031d621
haftmann <none@none>
2009-06-21 15:45:42 +0200

    removed temporariy workarounds

--- src/HOL/ex/Codegenerator_Candidates.thy
+++ src/HOL/ex/Codegenerator_Candidates.thy
@@ -25,20 +25,6 @@ imports
   "~~/src/HOL/ex/Records"
 begin
 
-(*temporary Haskell workaround*)
-declare typerep_fun_def [code inline]
-declare typerep_prod_def [code inline]
-declare typerep_sum_def [code inline]
-declare typerep_cpoint_ext_type_def [code inline]
-declare typerep_itself_def [code inline]
-declare typerep_list_def [code inline]
-declare typerep_option_def [code inline]
-declare typerep_point_ext_type_def [code inline]
-declare typerep_point'_ext_type_def [code inline]
-declare typerep_point''_ext_type_def [code inline]
-declare typerep_pol_def [code inline]
-declare typerep_polex_def [code inline]
-
 (*avoid popular infixes*)
 code_reserved SML union inter upto
 
