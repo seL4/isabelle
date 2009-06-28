f798bd71d465b9a2ce8f65c46143a5dbb6e1820a
wenzelm <none@none>
2009-06-28 22:50:37 +0200

    improved display;

--- src/Pure/System/gui_setup.scala
+++ src/Pure/System/gui_setup.scala
@@ -29,8 +29,8 @@ object GUI_Setup extends GUIApplication
     // components
     val text = new TextArea {
       editable = false
-      columns = 20
-      rows = 10
+      columns = 40
+      rows = 15
       xLayoutAlignment = 0.5
     }
     val ok = new Button {
@@ -43,15 +43,15 @@ object GUI_Setup extends GUIApplication
     }
 
     // values
-    Platform.defaults match {
-      case None =>
-      case Some((name, None)) => text.append("platform: " + name + "\n")
-      case Some((name1, Some(name2))) =>
-        text.append("main platform: " + name2 + "\n")
-        text.append("alternative platform: " + name2 + "\n")
-    }
     if (Platform.is_windows) {
       text.append("Cygwin root: " + Cygwin.config()._1 + "\n")
+    }
+    Platform.defaults match {
+      case None =>
+      case Some((name, None)) => text.append("Platform: " + name + "\n")
+      case Some((name1, Some(name2))) =>
+        text.append("Main platform: " + name1 + "\n")
+        text.append("Alternative platform: " + name2 + "\n")
     }
 
     // reactions
