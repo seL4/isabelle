1e305b7215e79ee991fdf7d166c6b478175a427a
haftmann <none@none>
2009-06-30 14:53:58 +0200

    tuned

--- src/HOL/Imperative_HOL/Imperative_HOL_ex.thy
+++ src/HOL/Imperative_HOL/Imperative_HOL_ex.thy
@@ -1,8 +1,9 @@
 (*  Title:      HOL/Imperative_HOL/Imperative_HOL_ex.thy
-    Author:     John Matthews, Galois Connections; Alexander Krauss, Lukas Bulwahn & Florian Haftmann, TU Muenchen
+    Author:     John Matthews, Galois Connections;
+                Alexander Krauss, Lukas Bulwahn & Florian Haftmann, TU Muenchen
 *)
 
-header {* Mmonadic imperative HOL with examples *}
+header {* Monadic imperative HOL with examples *}
 
 theory Imperative_HOL_ex
 imports Imperative_HOL "ex/Imperative_Quicksort"
