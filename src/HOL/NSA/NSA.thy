92e775bc5084cabe778e24b9d46c8528347ddd0b
wenzelm <none@none>
2009-07-23 23:12:21 +0200

    more @{theory} antiquotations;

--- src/HOL/NSA/NSA.thy
+++ src/HOL/NSA/NSA.thy
@@ -684,7 +684,7 @@ fun reorient_proc sg _ (_ $ t $ u) =
 
 in
 val approx_reorient_simproc =
-  Arith_Data.prep_simproc
+  Arith_Data.prep_simproc @{theory}
     ("reorient_simproc", ["0@=x", "1@=x", "number_of w @= x"], reorient_proc);
 end;
 
