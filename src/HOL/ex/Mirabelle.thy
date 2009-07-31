53bbb80c898c66b3dd2482d3b4d83d6e59e72c63
boehmes <none@none>
2009-07-31 23:31:11 +0200

    added Mirabelle

---
+++ src/HOL/ex/Mirabelle.thy
@@ -0,0 +1,14 @@
+(* Title: Mirabelle.thy
+   Author: Jasmin Blanchette and Sascha Boehme
+*)
+
+theory Mirabelle
+imports Main
+uses "mirabelle.ML"
+begin
+
+(* FIXME: use a logfile for each theory file *)
+
+setup Mirabelle.setup
+
+end
