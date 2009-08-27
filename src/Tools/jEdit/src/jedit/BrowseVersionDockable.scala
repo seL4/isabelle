272c1116b6249526c2883f82cb6db810826ebf61
immler@in.tum.de <immler@in.tum.de>
2009-08-27 16:41:36 +0200

    no busy waiting

--- src/Tools/jEdit/src/jedit/BrowseVersionDockable.scala
+++ src/Tools/jEdit/src/jedit/BrowseVersionDockable.scala
@@ -16,24 +16,21 @@ import org.gjt.sp.jedit.View
 import org.gjt.sp.jedit.gui.DockableWindowManager
 
 
-class BrowseVersionDockable(view : View, position : String) extends FlowPanel {
+class BrowseVersionDockable(view : View, position : String) extends FlowPanel
+{
+
+  def get_versions() =
+    Isabelle.prover_setup(view.getBuffer).map(_.theory_view.changes).getOrElse(Nil)
 
   if (position == DockableWindowManager.FLOATING)
     preferredSize = new Dimension(500, 250)
 
   val list = new ListView[Change]
   list.fixedCellWidth = 500
-
-  new javax.swing.Timer(1000, new java.awt.event.ActionListener {
-    override def actionPerformed(evt: java.awt.event.ActionEvent) {
-      list.listData = Isabelle.prover_setup(view.getBuffer).map(_.
-        theory_view.changes).getOrElse(Nil)
-    }
-  }).start()
-
+  list.listData = get_versions()
   contents += list
-  listenTo(list.selection)
 
+  listenTo(list.selection)
   reactions += {
     case ListSelectionChanged(source, range, false) =>
       Swing_Thread.now {
@@ -41,4 +38,10 @@ class BrowseVersionDockable(view : View, position : String) extends FlowPanel {
           theory_view.set_version(list.listData(range.start)))
       }
   }
+
+  private var num_changes = 0
+  Isabelle.plugin.document_change += {_ =>
+    list.listData = get_versions()
+  }
+
 }
