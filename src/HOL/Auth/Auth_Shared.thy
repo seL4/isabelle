78c1f203b91a55a116ed84b5da1f9becdae99115
haftmann <none@none>
2009-09-21 15:33:40 +0200

    added session entry point theories
    --HG--
    rename : src/HOL/Auth/ROOT.ML => src/HOL/Auth/Auth_Public.thy
    rename : src/HOL/Auth/ROOT.ML => src/HOL/Auth/Auth_Shared.thy
    rename : src/HOL/Auth/ROOT.ML => src/HOL/Auth/Guard/Auth_Guard_Public.thy
    rename : src/HOL/Auth/ROOT.ML => src/HOL/Auth/Guard/Auth_Guard_Shared.thy
    rename : src/HOL/Auth/ROOT.ML => src/HOL/Auth/Smartcard/Auth_Smartcard.thy

---
+++ src/HOL/Auth/Auth_Shared.thy
@@ -0,0 +1,27 @@
+(*  Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1996  University of Cambridge
+*)
+
+header {* Conventional protocols: rely on conventional Message, Event and Public -- Shared-key protocols *}
+
+theory Auth_Shared
+imports
+  "NS_Shared"
+  "Kerberos_BAN"
+  "Kerberos_BAN_Gets"
+  "KerberosIV"
+  "KerberosIV_Gets"
+  "KerberosV"
+  "OtwayRees"
+  "OtwayRees_AN"
+  "OtwayRees_Bad"
+  "OtwayReesBella"
+  "WooLam"
+  "Recur"
+  "Yahalom"
+  "Yahalom2"
+  "Yahalom_Bad"
+  "ZhouGollmann"
+begin
+
+end
---
+++ src/HOL/Auth/Smartcard/Auth_Smartcard.thy
@@ -0,0 +1,13 @@
+(*  Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1996  University of Cambridge
+*)
+
+header {* Smartcard protocols: rely on conventional Message and on new EventSC and Smartcard *}
+
+theory Auth_Smartcard
+imports
+  "ShoupRubin"
+  "ShoupRubinBella"
+begin
+
+end
