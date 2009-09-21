e38a5279dfe953fa935d511819761a03c692b46a
haftmann <none@none>
2009-09-21 16:01:30 +0200

    added session theory for Nominal_Examples
    --HG--
    rename : src/HOL/Nominal/Examples/ROOT.ML => src/HOL/Nominal/Examples/Nominal_Examples.thy

---
+++ src/HOL/Nominal/Examples/Nominal_Examples.thy
@@ -0,0 +1,25 @@
+(*  Author:  Christian Urban TU Muenchen *)
+
+header {* Various examples involving nominal datatypes. *}
+
+theory Nominal_Examples
+imports
+  CR
+  CR_Takahashi
+  Class
+  Compile
+  Fsub
+  Height
+  Lambda_mu
+  SN
+  Weakening
+  Crary
+  SOS
+  LocalWeakening
+  Support
+  Contexts
+  Standardization
+  W
+begin
+
+end
