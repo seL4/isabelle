8f678d991cdee03dc793f3d29bcb26b962031fdb
haftmann <none@none>
2010-02-08 17:12:18 +0100

    tuned header

--- src/HOL/Decision_Procs/Decision_Procs.thy
+++ src/HOL/Decision_Procs/Decision_Procs.thy
@@ -2,7 +2,8 @@ header {* Various decision procedures, typically involving reflection *}
 
 theory Decision_Procs
 imports
-  Commutative_Ring Cooper Ferrack MIR Approximation Dense_Linear_Order Parametric_Ferrante_Rackoff
+  Commutative_Ring Cooper Ferrack MIR Approximation Dense_Linear_Order
+  Parametric_Ferrante_Rackoff
   Commutative_Ring_Complete
   "ex/Commutative_Ring_Ex" "ex/Approximation_Ex" "ex/Dense_Linear_Order_Ex"
 begin
