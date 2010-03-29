a5f9c69737cc9fe3144155c3f81b1e5ceb887b13
blanchet <none@none>
2010-03-29 19:49:57 +0200

    added "modulus" and "sorts" options to control Sledgehammer's Isar proof output

--- src/HOL/Sledgehammer.thy
+++ src/HOL/Sledgehammer.thy
@@ -99,7 +99,6 @@ use "Tools/Sledgehammer/sledgehammer_fact_preprocessor.ML"
 setup Sledgehammer_Fact_Preprocessor.setup
 use "Tools/Sledgehammer/sledgehammer_hol_clause.ML"
 use "Tools/Sledgehammer/sledgehammer_proof_reconstruct.ML"
-setup Sledgehammer_Proof_Reconstruct.setup
 use "Tools/Sledgehammer/sledgehammer_fact_filter.ML"
 use "Tools/ATP_Manager/atp_manager.ML"
 use "Tools/ATP_Manager/atp_wrapper.ML"
