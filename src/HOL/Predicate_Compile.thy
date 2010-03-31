76764f94de0251f0310c896de0c2b8ea836e5b2f
bulwahn <none@none>
2010-03-31 16:44:41 +0200

    putting compilation setup of predicate compiler in a separate file

--- src/HOL/Predicate_Compile.thy
+++ src/HOL/Predicate_Compile.thy
@@ -8,6 +8,7 @@ theory Predicate_Compile
 imports New_Random_Sequence
 uses
   "Tools/Predicate_Compile/predicate_compile_aux.ML"
+  "Tools/Predicate_Compile/predicate_compile_compilations.ML"
   "Tools/Predicate_Compile/predicate_compile_core.ML"
   "Tools/Predicate_Compile/predicate_compile_data.ML"
   "Tools/Predicate_Compile/predicate_compile_fun.ML"
