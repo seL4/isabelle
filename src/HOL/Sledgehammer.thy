057278acb106807f4ed0ff6e9eb778b671da5423
blanchet <none@none>
2010-04-23 17:38:25 +0200

    move the minimizer to the Sledgehammer directory
    --HG--
    rename : src/HOL/Tools/ATP_Manager/atp_minimal.ML => src/HOL/Tools/Sledgehammer/sledgehammer_fact_minimizer.ML

--- src/HOL/Sledgehammer.thy
+++ src/HOL/Sledgehammer.thy
@@ -19,7 +19,7 @@ uses
   ("Tools/Sledgehammer/sledgehammer_fact_filter.ML")
   ("Tools/ATP_Manager/atp_manager.ML")
   ("Tools/ATP_Manager/atp_wrapper.ML")
-  ("Tools/ATP_Manager/atp_minimal.ML")
+  ("Tools/Sledgehammer/sledgehammer_fact_minimizer.ML")
   ("Tools/Sledgehammer/sledgehammer_isar.ML")
   ("Tools/Sledgehammer/meson_tactic.ML")
   ("Tools/Sledgehammer/metis_tactics.ML")
@@ -103,7 +103,7 @@ use "Tools/Sledgehammer/sledgehammer_fact_filter.ML"
 use "Tools/ATP_Manager/atp_manager.ML"
 use "Tools/ATP_Manager/atp_wrapper.ML"
 setup ATP_Wrapper.setup
-use "Tools/ATP_Manager/atp_minimal.ML"
+use "Tools/Sledgehammer/sledgehammer_fact_minimizer.ML"
 use "Tools/Sledgehammer/sledgehammer_isar.ML"
 
 
