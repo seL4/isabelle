d6a46b232a1fe3219fe8b8785f58f78c035fe461
Cezary Kaliszyk <kaliszyk@in.tum.de>
2010-04-29 09:06:35 +0200

    Tuning the quotient examples
    --HG--
    rename : src/HOL/Quotient_Examples/LarryDatatype.thy => src/HOL/Quotient_Examples/Quotient_Message.thy

--- src/HOL/Quotient_Examples/FSet.thy
+++ src/HOL/Quotient_Examples/FSet.thy
@@ -1,4 +1,4 @@
-(*  Title:      HOL/Quotient_Examples/Quotient.thy
+(*  Title:      HOL/Quotient_Examples/FSet.thy
     Author:     Cezary Kaliszyk, TU Munich
     Author:     Christian Urban, TU Munich
 
