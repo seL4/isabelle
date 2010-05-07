aeaa123154622ed02ef27fa41968bf42c87c5810
wenzelm <none@none>
2010-05-07 22:38:13 +0200

    sidekick: unformatted content, notably without newlines;

--- src/Tools/jEdit/src/jedit/isabelle_sidekick.scala
+++ src/Tools/jEdit/src/jedit/isabelle_sidekick.scala
@@ -47,7 +47,7 @@ class Isabelle_Sidekick extends SideKickParser("isabelle")
         for ((command, command_start) <- document.command_range(0) if !stopped) {
           root.add(document.current_state(command).get.markup_root.swing_tree((node: Markup_Node) =>
               {
-                val content = command.source(node.start, node.stop)
+                val content = Pretty.str_of(List(XML.Text(command.source(node.start, node.stop))))
                 val id = command.id
 
                 new DefaultMutableTreeNode(new IAsset {
