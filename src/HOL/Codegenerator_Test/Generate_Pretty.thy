515be4d63b84bc2f92172742121f3edfbc8bcf7b
haftmann <none@none>
2010-07-08 16:19:24 +0200

    checking generated code for various target languages

--- src/HOL/Codegenerator_Test/Generate_Pretty.thy
+++ src/HOL/Codegenerator_Test/Generate_Pretty.thy
@@ -12,9 +12,23 @@ lemma [code, code del]: "char_of_nat = char_of_nat" ..
 lemma [code, code del]: "(less_eq :: char \<Rightarrow> _) = less_eq" ..
 lemma [code, code del]: "(less :: char \<Rightarrow> _) = less" ..
 
-export_code * in SML module_name Code_Test
-  in OCaml module_name Code_Test file -
-  in Haskell file -
-  in Scala file -
+subsection {* Check whether generated code compiles *}
+
+text {*
+  If any of the @{text ML} ... check fails, inspect the code generated
+  by the previous @{text export_code} command.
+*}
+
+export_code "*" in SML module_name Code_Test file -
+ML {* Cache_IO.with_tmp_dir "Code_Test" (Code_ML.check_SML @{theory}) *}
+
+export_code "*" in OCaml module_name Code_Test file -
+ML {* Cache_IO.with_tmp_dir "Code_Test" (Code_ML.check_OCaml @{theory}) *}
+
+export_code "*" in Haskell module_name Code_Test file -
+ML {* Cache_IO.with_tmp_dir "Code_Test" (Code_Haskell.check @{theory}) *}
+
+export_code "*" in Scala module_name Code_Test file -
+ML {* Cache_IO.with_tmp_dir "Code_Test" (Code_Scala.check @{theory}) *}
 
 end
