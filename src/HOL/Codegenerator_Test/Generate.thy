efd79dee2fe4c32e00823a35a440b93abf05a239
haftmann <none@none>
2010-07-14 15:49:29 +0200

    added Isar syntax for code checking

--- src/HOL/Codegenerator_Test/Generate.thy
+++ src/HOL/Codegenerator_Test/Generate.thy
@@ -10,20 +10,10 @@ begin
 subsection {* Check whether generated code compiles *}
 
 text {*
-  If any of the @{text ML} ... check fails, inspect the code generated
-  by the previous @{text export_code} command.
+  If any of the checks fails, inspect the code generated
+  by a corresponding @{text export_code} command.
 *}
 
-export_code "*" in SML module_name Code_Test file -
-ML {* Code_ML.check_SML @{theory} *}
-
-export_code "*" in OCaml module_name Code_Test file -
-ML {* Code_ML.check_OCaml @{theory} *}
-
-export_code "*" in Haskell module_name Code_Test file -
-ML {* Code_Haskell.check @{theory} *}
-
-export_code "*" in Scala module_name Code_Test file -
-ML {* Code_Scala.check @{theory} *}
+export_code "*" checking SML OCaml Haskell Scala
 
 end
