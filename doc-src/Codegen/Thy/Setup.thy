5412a45ff30aa8dab464f76b851b36a25e75b2f9
haftmann <none@none>
2010-08-18 09:46:59 +0200

    pretty constraint syntax; tuned theory imports

--- doc-src/Codegen/Thy/Setup.thy
+++ doc-src/Codegen/Thy/Setup.thy
@@ -1,13 +1,29 @@
 theory Setup
-imports Complex_Main More_List
+imports Complex_Main More_List RBT Dlist Mapping
 uses
   "../../antiquote_setup.ML"
   "../../more_antiquote.ML"
 begin
 
 ML {* no_document use_thys
-  ["Efficient_Nat", "Code_Char_chr", "Product_ord", "~~/src/HOL/Imperative_HOL/Imperative_HOL",
+  ["Efficient_Nat", "Code_Char_chr", "Product_ord",
+   "~~/src/HOL/Imperative_HOL/Imperative_HOL",
    "~~/src/HOL/Decision_Procs/Ferrack"] *}
+
+setup {*
+let
+  val typ = Simple_Syntax.read_typ;
+  val typeT = Syntax.typeT;
+  val spropT = Syntax.spropT;
+in
+  Sign.del_modesyntax_i (Symbol.xsymbolsN, false) [
+    ("_constrain", typ "logic => type => logic", Mixfix ("_\<Colon>_", [4, 0], 3)),
+    ("_constrain", [spropT, typeT] ---> spropT, Mixfix ("_\<Colon>_", [4, 0], 3))]
+  #> Sign.add_modesyntax_i (Symbol.xsymbolsN, false) [
+      ("_constrain", typ "logic => type => logic", Mixfix ("_ \<Colon>  _", [4, 0], 3)),
+      ("_constrain", [spropT, typeT] ---> spropT, Mixfix ("_ \<Colon> _", [4, 0], 3))]
+end
+*}
 
 setup {* Code_Target.set_default_code_width 74 *}
 
