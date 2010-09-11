42ed199f9a9858a5b795047b31111cce7ce943f6
blanchet <none@none>
2010-09-11 10:28:44 +0200

    start renaming "Auto_Counterexample" to "Auto_Tools"; Auto Sledgehammer now uses so the name is clearly wrong
    --HG--
    rename : src/Tools/auto_counterexample.ML => src/Tools/auto_tools.ML

--- src/Tools/Code_Generator.thy
+++ src/Tools/Code_Generator.thy
@@ -8,8 +8,8 @@ theory Code_Generator
 imports Pure
 uses
   "~~/src/Tools/cache_io.ML"
+  "~~/src/Tools/auto_tools.ML"
   "~~/src/Tools/auto_solve.ML"
-  "~~/src/Tools/auto_counterexample.ML"
   "~~/src/Tools/quickcheck.ML"
   "~~/src/Tools/value.ML"
   "~~/src/Tools/Code/code_preproc.ML" 
