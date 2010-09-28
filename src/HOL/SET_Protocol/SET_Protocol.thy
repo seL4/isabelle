5d40d89cad2fe9684500356dec58c4603a7409da
haftmann <none@none>
2010-09-28 15:31:10 +0200

    modernized session

---
+++ src/HOL/SET_Protocol/SET_Protocol.thy
@@ -0,0 +1,12 @@
+(*  Title:      HOL/SET_Protocol/SET_Protocol.thy
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   2003  University of Cambridge
+
+Root file for the SET protocol proofs.
+*)
+
+theory SET_Protocol
+imports Cardholder_Registration Merchant_Registration Purchase
+begin
+
+end
