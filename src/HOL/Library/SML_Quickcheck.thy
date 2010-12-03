d70b6a677a2e7dd38ac93a4752644dbcba767e47
bulwahn <none@none>
2010-12-03 08:40:47 +0100

    adapting SML_Quickcheck to recent changes

--- src/HOL/Library/SML_Quickcheck.thy
+++ src/HOL/Library/SML_Quickcheck.thy
@@ -7,7 +7,21 @@ begin
 
 setup {*
   Inductive_Codegen.quickcheck_setup #>
-  Context.theory_map (Quickcheck.add_generator ("SML", Codegen.test_term))
+  Context.theory_map (Quickcheck.add_generator ("SML",
+    fn ctxt => fn t =>
+      let
+        val test_fun = Codegen.test_term ctxt t 
+        val iterations = Config.get ctxt Quickcheck.iterations
+        fun iterate size 0 = NONE
+          | iterate size j =
+            let
+              val result = test_fun size handle Match => 
+                (if Config.get ctxt Quickcheck.quiet then ()
+                 else warning "Exception Match raised during quickcheck"; NONE)
+            in
+              case result of NONE => iterate size (j - 1) | SOME q => SOME q
+            end
+     in fn size => (iterate size iterations, NONE) end))
 *}
 
 end
