cd179582ebeebd55aa91b1e581d47ae2aa96d0b4
haftmann <none@none>
2011-01-15 20:05:29 +0100

    experimental variant of interpretation with simultaneous definitions, plus example

---
+++ src/HOL/ex/Interpretation_with_Defs.thy
@@ -0,0 +1,12 @@
+(*  Title:      HOL/ex/Interpretation_with_Defs.thy
+    Author:     Florian Haftmann, TU Muenchen
+*)
+
+header {* Interpretation accompanied with mixin definitions.  EXPERIMENTAL. *}
+
+theory Interpretation_with_Defs
+imports Pure
+uses "~~/src/Tools/interpretation_with_defs.ML"
+begin
+
+end
