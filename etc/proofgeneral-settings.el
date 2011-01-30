847b23b7f70ea883bcdc88658191912b753d9b8c
wenzelm <none@none>
2011-01-30 13:02:18 +0100

    clarified example settings for Proof General;

--- etc/proofgeneral-settings.el
+++ etc/proofgeneral-settings.el
@@ -1,13 +1,13 @@
 ;;; Options for Proof General
+;;;
+;;; Examples for sensible settings:
 
-;; Examples for sensible settings:
-
+;; keep sources clean
 (custom-set-variables '(indent-tabs-mode nil))
 
+;; retain vital AltGr behaviour, e.g. on non-US Mac OS X
+;(custom-set-variables '(ns-alternate-modifier 'none))
+
+;; longer timeout for saving persistent session
 ;(custom-set-variables '(proof-shell-quit-timeout 60))
 
-;(custom-set-variables '(isar-eta-contract nil))
-
-;(custom-set-faces
-; '(proof-locked-face
-;   ((((type x) (class color) (background light)) (:background "lightsteelblue2")))))
