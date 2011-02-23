8593db4826449d17851114bc46af416d5267d2a7
noschinl <none@none>
2011-02-23 11:23:26 +0100

    add example for case_product usage

---
+++ src/HOL/ex/Case_Product.thy
@@ -0,0 +1,30 @@
+(*  Title:      HOL/ex/Case_Product.thy
+    Author:     Lars Noschinski
+    Copyright   2011 TU Muenchen
+*)
+
+header {* Examples for the 'case_product' attribute *}
+
+theory Case_Product
+imports Main
+begin
+
+text {*
+  The {@attribute case_product} attribute combines multiple case distinction
+  lemmas into a single case distinction lemma by building the product of all
+  these case distinctions.
+*}
+
+lemmas nat_list_exhaust = nat.exhaust[case_product list.exhaust]
+
+text {*
+  The attribute honors preconditions
+*}
+
+lemmas trancl_acc_cases= trancl.cases[case_product acc.cases]
+
+text {*
+  Also, case names are generated based on the old names
+*}
+
+end
