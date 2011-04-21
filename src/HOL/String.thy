7685bb05881cf600d71969607b16a4e5318b3911
wenzelm <none@none>
2011-04-21 12:56:27 +0200

    discontinuend obsolete Thm.definitionK, which was hardly ever well-defined;

--- src/HOL/String.thy
+++ src/HOL/String.thy
@@ -53,7 +53,7 @@ let
    (fn n => fn m => Drule.instantiate' [] [SOME n, SOME m] @{thm nibble_pair_of_char.simps})
       nibbles nibbles;
 in
-  Global_Theory.note_thmss Thm.definitionK
+  Global_Theory.note_thmss ""
     [((@{binding nibble_pair_of_char_simps}, []), [(thms, [])])]
   #-> (fn [(_, thms)] => fold_rev Code.add_eqn thms)
 end
