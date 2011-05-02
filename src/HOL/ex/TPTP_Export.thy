9eb23dd3012b24cadb497830298387118441e753
blanchet <none@none>
2011-05-02 13:29:47 +0200

    fix ROOT.ML and handle "readable_names" reference slightly more cleanly

--- src/HOL/ex/TPTP_Export.thy
+++ src/HOL/ex/TPTP_Export.thy
@@ -4,10 +4,16 @@ uses "tptp_export.ML"
 begin
 
 ML {*
+val readable_names = !Sledgehammer_ATP_Translate.readable_names;
+Sledgehammer_ATP_Translate.readable_names := false
+*}
+
+ML {*
 val thy = @{theory Complex_Main}
 val ctxt = @{context}
 *}
 
+(*
 ML {*
 TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy true
     "/tmp/infs_full_types.tptp"
@@ -21,6 +27,11 @@ TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy false
 ML {*
 TPTP_Export.generate_tptp_graph_file_for_theory ctxt thy
     "/tmp/graph.out"
+*}
+*)
+
+ML {*
+Sledgehammer_ATP_Translate.readable_names := readable_names
 *}
 
 end
