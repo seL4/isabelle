efa211dc9c6f1966ccabec4bfb7a20054df16b8a
blanchet <none@none>
2011-05-03 00:10:22 +0200

    replaced some Unsynchronized.refs with Config.Ts

--- src/HOL/ex/TPTP_Export.thy
+++ src/HOL/ex/TPTP_Export.thy
@@ -3,35 +3,36 @@ imports Complex_Main
 uses "tptp_export.ML"
 begin
 
-ML {*
-val readable_names = !Sledgehammer_ATP_Translate.readable_names;
-Sledgehammer_ATP_Translate.readable_names := false
-*}
+declare [[sledgehammer_atp_readable_names = false]]
 
 ML {*
-val thy = @{theory Complex_Main}
+val do_it = false; (* switch to true to generate files *)
+val thy = @{theory Complex_Main};
 val ctxt = @{context}
 *}
 
-(*
 ML {*
-TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy true
-    "/tmp/infs_full_types.tptp"
+if do_it then
+  TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy true
+      "/tmp/infs_full_types.tptp"
+else
+  ()
 *}
 
 ML {*
-TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy false
-    "/tmp/infs_partial_types.tptp"
+if do_it then
+  TPTP_Export.generate_tptp_inference_file_for_theory ctxt thy false
+      "/tmp/infs_partial_types.tptp"
+else
+  ()
 *}
 
 ML {*
-TPTP_Export.generate_tptp_graph_file_for_theory ctxt thy
-    "/tmp/graph.out"
-*}
-*)
-
-ML {*
-Sledgehammer_ATP_Translate.readable_names := readable_names
+if do_it then
+  TPTP_Export.generate_tptp_graph_file_for_theory ctxt thy
+      "/tmp/graph.out"
+else
+  ()
 *}
 
 end
