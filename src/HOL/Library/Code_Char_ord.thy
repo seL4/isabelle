e71e685f4ab7156869f97ad5f81698b45e457b9b
bulwahn <none@none>
2011-05-18 15:45:33 +0200

    adding theory to force code generation of ordering on characters to built-in ordering of characters in target languages

---
+++ src/HOL/Library/Code_Char_ord.thy
@@ -0,0 +1,25 @@
+(*  Title:      HOL/Library/Code_Char_ord.thy
+    Author:     Lukas Bulwahn, Florian Haftmann, Rene Thiemann
+*)
+
+header {* Code generation of orderings for pretty characters *}
+
+theory Code_Char_ord
+imports Code_Char Char_ord
+begin
+  
+code_const "Orderings.less_eq \<Colon> char \<Rightarrow> char \<Rightarrow> bool"
+  (SML "!((_ : char) <= _)")
+  (OCaml "<=")
+  (Haskell infix 4 "<=")
+  (Scala infixl 4 "<=")
+  (Eval infixl 6 "<=")
+
+code_const "Orderings.less \<Colon> char \<Rightarrow> char \<Rightarrow> bool"
+  (SML "!((_ : char) < _)")
+  (OCaml "<")
+  (Haskell infix 4 "<")
+  (Scala infixl 4 "<")
+  (Eval infixl 6 "<")
+
+end
