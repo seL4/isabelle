bb0c50fbec4e33a7d16066e4bf2544d213459e43
blanchet <none@none>
2011-06-06 20:56:06 +0200

    Metis code cleanup

--- src/HOL/Metis_Examples/Proxies.thy
+++ src/HOL/Metis_Examples/Proxies.thy
@@ -24,24 +24,24 @@ definition inc :: "nat \<Rightarrow> nat" where
 
 lemma "inc \<noteq> (\<lambda>y. 0)"
 sledgehammer [expect = some] (inc_def plus_1_not_0)
-by (new_metis_exhaust inc_def plus_1_not_0)
+by (metis_exhaust inc_def plus_1_not_0)
 
 lemma "inc = (\<lambda>y. y + 1)"
 sledgehammer [expect = some] (inc_def)
-by (new_metis_exhaust inc_def)
+by (metis_exhaust inc_def)
 
 definition add_swap :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
 "add_swap = (\<lambda>x y. y + x)"
 
 lemma "add_swap m n = n + m"
 sledgehammer [expect = some] (add_swap_def)
-by (new_metis_exhaust add_swap_def)
+by (metis_exhaust add_swap_def)
 
 definition "A = {xs\<Colon>'a list. True}"
 
 lemma "xs \<in> A"
 sledgehammer [expect = some]
-by (new_metis_exhaust A_def Collect_def mem_def)
+by (metis_exhaust A_def Collect_def mem_def)
 
 definition "B (y::int) \<equiv> y \<le> 0"
 definition "C (y::int) \<equiv> y \<le> 1"
@@ -51,7 +51,7 @@ by linarith
 
 lemma "B \<subseteq> C"
 sledgehammer [type_sys = poly_args, max_relevant = 200, expect = some]
-by (new_metis_exhaust B_def C_def int_le_0_imp_le_1 predicate1I)
+by (metis_exhaust B_def C_def int_le_0_imp_le_1 predicate1I)
 
 
 text {* Proxies for logical constants *}
@@ -78,7 +78,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "\<not> id False"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -90,7 +90,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "x = id True \<or> x = id False"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -102,7 +102,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id x = id True \<or> id x = id False"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -114,7 +114,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "P True \<Longrightarrow> P False \<Longrightarrow> P x"
 sledgehammer [type_sys = erased, expect = none] ()
@@ -139,7 +139,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (\<not> \<not> a) \<Longrightarrow> id a"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -151,7 +151,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (\<not> (id (\<not> a))) \<Longrightarrow> id a"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -163,7 +163,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (a \<and> b) \<Longrightarrow> id a"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -175,7 +175,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (a \<and> b) \<Longrightarrow> id b"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -187,7 +187,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id a \<Longrightarrow> id b \<Longrightarrow> id (a \<and> b)"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -199,7 +199,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id a \<Longrightarrow> id (a \<or> b)"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -211,7 +211,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id b \<Longrightarrow> id (a \<or> b)"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -223,7 +223,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (\<not> a) \<Longrightarrow> id (\<not> b) \<Longrightarrow> id (\<not> (a \<or> b))"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -235,7 +235,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (\<not> a) \<Longrightarrow> id (a \<longrightarrow> b)"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -247,7 +247,7 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 lemma "id (a \<longrightarrow> b) \<longleftrightarrow> id (\<not> a \<or> b)"
 sledgehammer [type_sys = erased, expect = some] (id_apply)
@@ -259,6 +259,6 @@ sledgehammer [type_sys = mangled_tags?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_tags, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds?, expect = some] (id_apply)
 sledgehammer [type_sys = mangled_preds, expect = some] (id_apply)
-by (new_metis_exhaust id_apply)
+by (metis_exhaust id_apply)
 
 end
--- src/HOL/Metis_Examples/Type_Encodings.thy
+++ src/HOL/Metis_Examples/Type_Encodings.thy
@@ -69,47 +69,47 @@ val type_syss =
    "mangled_tags_heavy!",
    "simple!"]
 
-fun new_metis_exhaust_tac ctxt ths =
+fun metis_exhaust_tac ctxt ths =
   let
     fun tac [] st = all_tac st
       | tac (type_sys :: type_syss) st =
         st (* |> tap (fn _ => tracing (PolyML.makestring type_sys)) *)
            |> ((if null type_syss then all_tac else rtac @{thm fork} 1)
-               THEN Metis_Tactics.new_metis_tac [type_sys] ctxt ths 1
+               THEN Metis_Tactics.metis_tac [type_sys] ctxt ths 1
                THEN COND (has_fewer_prems 2) all_tac no_tac
                THEN tac type_syss)
   in tac end
 *}
 
-method_setup new_metis_exhaust = {*
+method_setup metis_exhaust = {*
   Attrib.thms >>
-    (fn ths => fn ctxt => SIMPLE_METHOD (new_metis_exhaust_tac ctxt ths type_syss))
+    (fn ths => fn ctxt => SIMPLE_METHOD (metis_exhaust_tac ctxt ths type_syss))
 *} "exhaustively run the new Metis with all type encodings"
 
 
 text {* Miscellaneous tests *}
 
 lemma "x = y \<Longrightarrow> y = x"
-by new_metis_exhaust
+by metis_exhaust
 
 lemma "[a] = [1 + 1] \<Longrightarrow> a = 1 + (1::int)"
-by (new_metis_exhaust last.simps)
+by (metis_exhaust last.simps)
 
 lemma "map Suc [0] = [Suc 0]"
-by (new_metis_exhaust map.simps)
+by (metis_exhaust map.simps)
 
 lemma "map Suc [1 + 1] = [Suc 2]"
-by (new_metis_exhaust map.simps nat_1_add_1)
+by (metis_exhaust map.simps nat_1_add_1)
 
 lemma "map Suc [2] = [Suc (1 + 1)]"
-by (new_metis_exhaust map.simps nat_1_add_1)
+by (metis_exhaust map.simps nat_1_add_1)
 
 definition "null xs = (xs = [])"
 
 lemma "P (null xs) \<Longrightarrow> null xs \<Longrightarrow> xs = []"
-by (new_metis_exhaust null_def)
+by (metis_exhaust null_def)
 
 lemma "(0::nat) + 0 = 0"
-by (new_metis_exhaust arithmetic_simps(38))
+by (metis_exhaust arithmetic_simps(38))
 
 end
