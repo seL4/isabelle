d09273e47036010dc91d2eb225486d8faed27b39
haftmann <none@none>
2011-06-11 07:50:28 +0200

    tuned comment

--- src/HOL/Library/Executable_Set.thy
+++ src/HOL/Library/Executable_Set.thy
@@ -12,7 +12,7 @@ begin
 text {*
   This is just an ad-hoc hack which will rarely give you what you want.
   For the moment, whenever you need executable sets, consider using
-  type @{text fset} from theory @{text Cset}.
+  type @{text Cset.set} from theory @{text Cset}.
 *}
 
 declare mem_def [code del]
