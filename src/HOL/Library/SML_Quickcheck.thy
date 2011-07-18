d78309886c42592699dbe371585e7a848bdac20a
bulwahn <none@none>
2011-07-18 10:34:21 +0200

    parametrized test_term functions in quickcheck

--- src/HOL/Library/SML_Quickcheck.thy
+++ src/HOL/Library/SML_Quickcheck.thy
@@ -6,9 +6,8 @@ imports Main
 begin
 
 setup {*
-  Inductive_Codegen.quickcheck_setup #>
-  Context.theory_map (Quickcheck.add_generator ("SML",
-    fn ctxt => fn [(t, eval_terms)] =>
+  let
+    fun compile_generator_expr ctxt [(t, eval_terms)] =
       let
         val prop = list_abs_free (Term.add_frees t [], t)
         val test_fun = Codegen.test_term ctxt prop
@@ -22,8 +21,13 @@ setup {*
               in
                 case result of NONE => iterate size (j - 1) | SOME q => SOME q
               end
-     in fn [_, size] => (iterate size iterations, NONE) end))
-  #> Context.theory_map (Quickcheck.add_tester ("SML", Quickcheck.generator_test_goal_terms))  
+      in fn [_, size] => (iterate size iterations, NONE) end))
+    val test_goals = Quickcheck.generator_test_goal_terms compile_generator_expr
+  in
+    Inductive_Codegen.quickcheck_setup
+    #> Context.theory_map (Quickcheck.add_generator ("SML", compile_generator_expr))
+    #> Context.theory_map (Quickcheck.add_tester ("SML", test_goals))
+  end
 *}
 
 end
