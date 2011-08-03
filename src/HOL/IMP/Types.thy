4c3cae3147febee6f87f3c1d4b4b9f8b7e25b905
kleing <none@none>
2011-08-03 11:09:12 +0200

    fixed wrong isubs in IMP/Types
    --HG--
    extra : rebase_source : 48b62480fdff3ac9ebb4ef2f425b1905e552936a

--- src/HOL/IMP/Types.thy
+++ src/HOL/IMP/Types.thy
@@ -17,7 +17,7 @@ inductive taval :: "aexp \<Rightarrow> state \<Rightarrow> val \<Rightarrow> boo
 "taval (V x) s (s x)" |
 "taval a1 s (Iv i1) \<Longrightarrow> taval a2 s (Iv i2)
  \<Longrightarrow> taval (Plus a1 a2) s (Iv(i1+i2))" |
-"taval a\<^isub>1 s (Rv r1) \<Longrightarrow> taval a2 s (Rv r2)
+"taval a1 s (Rv r1) \<Longrightarrow> taval a2 s (Rv r2)
  \<Longrightarrow> taval (Plus a1 a2) s (Rv(r1+r2))"
 
 inductive_cases [elim!]:
@@ -32,9 +32,9 @@ datatype bexp = B bool | Not bexp | And bexp bexp | Less aexp aexp
 inductive tbval :: "bexp \<Rightarrow> state \<Rightarrow> bool \<Rightarrow> bool" where
 "tbval (B bv) s bv" |
 "tbval b s bv \<Longrightarrow> tbval (Not b) s (\<not> bv)" |
-"tbval b\<^isub>1 s bv1 \<Longrightarrow> tbval b2 s bv2 \<Longrightarrow> tbval (And b1 b2) s (bv1 & bv2)" |
-"taval a\<^isub>1 s (Iv i1) \<Longrightarrow> taval a2 s (Iv i2) \<Longrightarrow> tbval (Less a1 a2) s (i1 < i2)" |
-"taval a\<^isub>1 s (Rv r1) \<Longrightarrow> taval a2 s (Rv r2) \<Longrightarrow> tbval (Less a1 a2) s (r1 < r2)"
+"tbval b1 s bv1 \<Longrightarrow> tbval b2 s bv2 \<Longrightarrow> tbval (And b1 b2) s (bv1 & bv2)" |
+"taval a1 s (Iv i1) \<Longrightarrow> taval a2 s (Iv i2) \<Longrightarrow> tbval (Less a1 a2) s (i1 < i2)" |
+"taval a1 s (Rv r1) \<Longrightarrow> taval a2 s (Rv r2) \<Longrightarrow> tbval (Less a1 a2) s (r1 < r2)"
 
 subsection "Syntax of Commands"
 (* a copy of Com.thy - keep in sync! *)
