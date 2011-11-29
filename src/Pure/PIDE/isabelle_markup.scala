cc9cbc32a97005ed246db514616e51d51283f6a1
wenzelm <none@none>
2011-11-29 21:50:00 +0100

    clarified Time vs. Timing;
    --HG--
    rename : src/Pure/General/timing.scala => src/Pure/General/time.scala

--- src/Pure/PIDE/isabelle_markup.scala
+++ src/Pure/PIDE/isabelle_markup.scala
@@ -168,6 +168,32 @@ object Isabelle_Markup
   val LOADED_THEORY = "loaded_theory"
 
 
+  /* timing */
+
+  val TIMING = "timing"
+  val ELAPSED = "elapsed"
+  val CPU = "cpu"
+  val GC = "gc"
+
+  object Timing
+  {
+    def apply(timing: isabelle.Timing): Markup =
+      Markup(TIMING, List(
+        (ELAPSED, Properties.Value.Double(timing.elapsed.seconds)),
+        (CPU, Properties.Value.Double(timing.cpu.seconds)),
+        (GC, Properties.Value.Double(timing.gc.seconds))))
+    def unapply(markup: Markup): Option[isabelle.Timing] =
+      markup match {
+        case Markup(TIMING, List(
+          (ELAPSED, Properties.Value.Double(elapsed)),
+          (CPU, Properties.Value.Double(cpu)),
+          (GC, Properties.Value.Double(gc)))) =>
+            Some(new isabelle.Timing(Time.seconds(elapsed), Time.seconds(cpu), Time.seconds(gc)))
+        case _ => None
+      }
+  }
+
+
   /* toplevel */
 
   val SUBGOALS = "subgoals"
