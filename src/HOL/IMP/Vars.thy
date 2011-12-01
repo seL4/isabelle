e890f04c9b83e078462cffd6b1b3f7f1c4da0bfe
nipkow <none@none>
2011-12-01 20:52:16 +0100

    merged IMP/Util into IMP/Vars

--- src/HOL/IMP/Vars.thy
+++ src/HOL/IMP/Vars.thy
@@ -2,8 +2,27 @@
 
 header "Definite Assignment Analysis"
 
-theory Vars imports Util BExp
+theory Vars imports BExp
 begin
+
+subsection "Show sets of variables as lists"
+
+text {* Sets may be infinite and are not always displayed by element 
+  if computed as values. Lists do not have this problem. 
+
+  We define a function @{text show} that converts a set of
+  variable names into a list. To keep things simple, we rely on
+  the convention that we only use single letter names.
+*}
+definition 
+  letters :: "string list" where
+  "letters = map (\<lambda>c. [c]) chars"
+
+definition 
+  "show" :: "string set \<Rightarrow> string list" where
+  "show S = filter (\<lambda>x. x \<in> S) letters" 
+
+value "show {''x'', ''z''}"
 
 subsection "The Variables in an Expression"
 
