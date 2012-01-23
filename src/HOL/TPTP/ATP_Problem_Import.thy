cadbf5cb9197636d962499d0e18425a22828561d
blanchet <none@none>
2012-01-23 17:40:32 +0100

    added problem importer

---
+++ src/HOL/TPTP/ATP_Problem_Import.thy
@@ -0,0 +1,16 @@
+(*  Title:      HOL/TPTP/ATP_Problem_Import.thy
+    Author:     Jasmin Blanchette, TU Muenchen
+*)
+
+header {* ATP Problem Importer *}
+
+theory ATP_Problem_Import
+imports Complex_Main
+uses ("atp_problem_import.ML")
+begin
+
+typedecl iota (* for TPTP *)
+
+use "atp_problem_import.ML"
+
+end
