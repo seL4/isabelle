6b5c5b533dd03e25ce4b1823fd1bc0516dea4928
wenzelm <none@none>
2012-03-01 15:48:03 +0100

    more robust locking;

--- src/Tools/jEdit/src/document_model.scala
+++ src/Tools/jEdit/src/document_model.scala
@@ -61,15 +61,12 @@ class Document_Model(val session: Session, val buffer: Buffer, val name: Documen
   /* header */
 
   def node_header(): Document.Node_Header =
-  {
-    Swing_Thread.require()
-    Isabelle.buffer_lock(buffer) {
+    Isabelle.swing_buffer_lock(buffer) {
       Exn.capture {
         Isabelle.thy_load.check_header(name,
           Thy_Header.read(buffer.getSegment(0, buffer.getLength)))
       }
     }
-  }
 
 
   /* perspective */
