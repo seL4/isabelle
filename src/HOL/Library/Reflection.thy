8f94672540d8c44a384666c5ceb9bbbec75d40f2
haftmann <none@none>
2012-03-02 18:26:54 +0100

    tuned whitespace
    --HG--
    extra : rebase_source : 7f70c38139d100d2bdd77890264658f78b634274

--- src/HOL/Library/Reflection.thy
+++ src/HOL/Library/Reflection.thy
@@ -16,7 +16,7 @@ setup {* Reify_Data.setup *}
 method_setup reify = {*
   Attrib.thms --
     Scan.option (Scan.lift (Args.$$$ "(") |-- Args.term --| Scan.lift (Args.$$$ ")")) >>
-  (fn (eqs, to) => fn ctxt => SIMPLE_METHOD' (Reflection.genreify_tac ctxt (eqs @ (fst (Reify_Data.get ctxt))) to))
+  (fn (eqs, to) => fn ctxt => SIMPLE_METHOD' (Reflection.genreify_tac ctxt (eqs @ fst (Reify_Data.get ctxt)) to))
 *} "partial automatic reification"
 
 method_setup reflection = {*
@@ -31,11 +31,11 @@ in
   thms --
   Scan.optional (keyword rulesN |-- thms) [] --
   Scan.option (keyword onlyN |-- Args.term) >>
-  (fn ((eqs,ths),to) => fn ctxt =>
+  (fn ((eqs, ths), to) => fn ctxt =>
     let
-      val (ceqs,cths) = Reify_Data.get ctxt
-      val corr_thms = ths@cths
-      val raw_eqs = eqs@ceqs
+      val (ceqs, cths) = Reify_Data.get ctxt
+      val corr_thms = ths @ cths
+      val raw_eqs = eqs @ ceqs
     in SIMPLE_METHOD' (Reflection.reflection_tac ctxt corr_thms raw_eqs to) end)
 end
 *}
