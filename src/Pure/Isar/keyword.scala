8ea18aea08a70c4e2f4d3181cc37a2a1f220f3eb
wenzelm <none@none>
2012-03-16 20:33:33 +0100

    uniform keyword names within ML/Scala -- produce elisp names via external conversion; discontinued obsolete Keyword.thy_switch;

--- src/Pure/Isar/keyword.scala
+++ src/Pure/Isar/keyword.scala
@@ -14,27 +14,26 @@ object Keyword
   val MINOR = "minor"
   val CONTROL = "control"
   val DIAG = "diag"
-  val THY_BEGIN = "theory-begin"
-  val THY_SWITCH = "theory-switch"
-  val THY_END = "theory-end"
-  val THY_HEADING = "theory-heading"
-  val THY_DECL = "theory-decl"
-  val THY_SCRIPT = "theory-script"
-  val THY_GOAL = "theory-goal"
-  val THY_SCHEMATIC_GOAL = "theory-schematic-goal"
+  val THY_BEGIN = "thy_begin"
+  val THY_END = "thy_end"
+  val THY_HEADING = "thy_heading"
+  val THY_DECL = "thy_decl"
+  val THY_SCRIPT = "thy_script"
+  val THY_GOAL = "thy_goal"
+  val THY_SCHEMATIC_GOAL = "thy_schematic_goal"
   val QED = "qed"
-  val QED_BLOCK = "qed-block"
-  val QED_GLOBAL = "qed-global"
-  val PRF_HEADING = "proof-heading"
-  val PRF_GOAL = "proof-goal"
-  val PRF_BLOCK = "proof-block"
-  val PRF_OPEN = "proof-open"
-  val PRF_CLOSE = "proof-close"
-  val PRF_CHAIN = "proof-chain"
-  val PRF_DECL = "proof-decl"
-  val PRF_ASM = "proof-asm"
-  val PRF_ASM_GOAL = "proof-asm-goal"
-  val PRF_SCRIPT = "proof-script"
+  val QED_BLOCK = "qed_block"
+  val QED_GLOBAL = "qed_global"
+  val PRF_HEADING = "prf_heading"
+  val PRF_GOAL = "prf_goal"
+  val PRF_BLOCK = "prf_block"
+  val PRF_OPEN = "prf_open"
+  val PRF_CLOSE = "prf_close"
+  val PRF_CHAIN = "prf_chain"
+  val PRF_DECL = "prf_decl"
+  val PRF_ASM = "prf_asm"
+  val PRF_ASM_GOAL = "prf_asm_goal"
+  val PRF_SCRIPT = "prf_script"
 
 
   /* categories */
@@ -43,9 +42,8 @@ object Keyword
   val control = Set(CONTROL)
   val diag = Set(DIAG)
   val theory =
-    Set(THY_BEGIN, THY_SWITCH, THY_END, THY_HEADING, THY_DECL, THY_SCRIPT,
-      THY_GOAL, THY_SCHEMATIC_GOAL)
-  val theory1 = Set(THY_BEGIN, THY_SWITCH, THY_END)
+    Set(THY_BEGIN, THY_END, THY_HEADING, THY_DECL, THY_SCRIPT, THY_GOAL, THY_SCHEMATIC_GOAL)
+  val theory1 = Set(THY_BEGIN, THY_END)
   val theory2 = Set(THY_DECL, THY_GOAL)
   val proof =
     Set(QED, QED_BLOCK, QED_GLOBAL, PRF_HEADING, PRF_GOAL, PRF_BLOCK,
