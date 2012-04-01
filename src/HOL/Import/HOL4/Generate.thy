741b31665e3ae84ef7dffbc57d26034e46aea729
krauss <none@none>
2012-04-01 22:03:45 +0200

    removed old HOL4 import -- corresponding exporter is lost, code is broken, no users known, maintenance nightmare

--- src/HOL/Import/HOL4/Generate.thy
+++
@@ -1,5 +0,0 @@
-theory Generate
-imports "Template/GenHOL4Prob" "Template/GenHOL4Vec" "Template/GenHOL4Word32"
-begin
-
-end
--- src/HOL/Import/HOL4/Imported.thy
+++
@@ -1,6 +0,0 @@
-theory Imported
-imports "Generated/HOL4Vec" "Generated/HOL4Word32" "Generated/HOL4Real" "Generated/HOL4Prob"
-begin
-
-end
-
