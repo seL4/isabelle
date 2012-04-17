da005f88f3931b2553f0031721d3ea0617f53128
sultana <none@none>
2012-04-17 16:14:07 +0100

    simplified interpretation of '$i';

--- src/HOL/TPTP/TPTP_Interpret.thy
+++ src/HOL/TPTP/TPTP_Interpret.thy
@@ -8,8 +8,9 @@ interpreting them as HOL terms (i.e. importing types and type-checking the terms
 theory TPTP_Interpret
 imports Main TPTP_Parser
 keywords "import_tptp" :: thy_decl
-uses
-  "TPTP_Parser/tptp_interpret.ML"
+uses  ("TPTP_Parser/tptp_interpret.ML")
 
 begin
+typedecl "ind"
+use  "TPTP_Parser/tptp_interpret.ML"
 end
