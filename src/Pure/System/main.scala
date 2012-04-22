1dc3c795bda56ec33b93a79731be55314d427e68
wenzelm <none@none>
2012-04-22 15:50:29 +0200

    default Isabelle application wrapper -- JVM entry point for Isabelle.exe;

---
+++ src/Pure/System/main.scala
@@ -0,0 +1,33 @@
+/*  Title:      Pure/System/main.scala
+    Author:     Makarius
+
+Default Isabelle application wrapper.
+*/
+
+package isabelle
+
+import scala.swing.TextArea
+
+
+object Main
+{
+  def main(args: Array[String]) =
+  {
+    val (out, rc) =
+      try {
+        Platform.init_laf()
+        Isabelle_System.init()
+        Isabelle_System.isabelle_tool("jedit", args: _*)
+      }
+      catch { case exn: Throwable => (Exn.message(exn), 2) }
+
+    if (rc != 0) {
+      val text = new TextArea(out)
+      text.editable = false
+      Library.dialog(null, "Isabelle", "Isabelle output", text)
+    }
+
+    System.exit(rc)
+  }
+}
+
