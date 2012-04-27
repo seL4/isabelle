6ce0bd417fe0f5e00ef3a343eafb1dfcfd9a1d3a
blanchet <none@none>
2012-04-27 15:24:37 +0200

    thread theory cleanly and use "smt" method rather than Sledgehammer for Z3 (because of obscure debilitating bug)

--- src/HOL/TPTP/ATP_Problem_Import.thy
+++ src/HOL/TPTP/ATP_Problem_Import.thy
@@ -6,7 +6,7 @@ header {* ATP Problem Importer *}
 theory ATP_Problem_Import
 imports Complex_Main TPTP_Interpret
 uses "sledgehammer_tactics.ML"
-     "atp_problem_import.ML"
+     ("atp_problem_import.ML")
 begin
 
 ML {* Proofterm.proofs := 0 *}
@@ -14,8 +14,14 @@ ML {* Proofterm.proofs := 0 *}
 declare [[show_consts]] (* for Refute *)
 declare [[smt_oracle]]
 
+declare [[unify_search_bound = 60]]
+declare [[unify_trace_bound = 60]]
+
+use "atp_problem_import.ML"
+
 (*
-ML {* ATP_Problem_Import.isabelle_tptp_file 300 "$TPTP/Problems/PUZ/PUZ107^5.p" *}
+ML {* ATP_Problem_Import.isabelle_tptp_file @{theory} 50
+          "$TPTP/Problems/PUZ/PUZ107^5.p" *}
 *)
 
 end
