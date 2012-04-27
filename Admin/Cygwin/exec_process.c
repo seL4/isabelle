b2ce757745b200fe1c7291b9f14713f820aba960
wenzelm <none@none>
2012-04-27 19:31:03 +0200

    more direct exec with synchronous exit code;

--- Admin/Cygwin/exec_process.c
+++ Admin/Cygwin/exec_process.c
@@ -7,7 +7,6 @@ Bash process group invocation on Cygwin.
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <process.h>
 
 
 static void fail(const char *msg)
@@ -52,8 +51,7 @@ int main(int argc, char *argv[])
   cmd_line[2] = script;
   cmd_line[3] = NULL;
 
-  int pid = spawnv(_P_NOWAIT, "/bin/bash", cmd_line);
-  if (pid == -1) fail("Bad process");
-  waitpid(pid);
+  execv("/bin/bash", cmd_line);
+  fail("Cannot exec process");
 }
 
