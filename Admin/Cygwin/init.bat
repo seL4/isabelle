b1d2d744480d13bb77377bc6a10915ef67331a53
wenzelm <none@none>
2012-05-17 15:58:57 +0200

    some message;

--- Admin/Cygwin/init.bat
+++ Admin/Cygwin/init.bat
@@ -3,5 +3,6 @@
 cd "%~dp0"
 cd "..\.."
 
+echo Initializing ...
 "contrib\cygwin-1.7.9\bin\ash" /bin/rebaseall
 "contrib\cygwin-1.7.9\bin\bash" -c "PATH=/bin; chmod -wx $(find heaps -type f); mkpasswd -l >/etc/passwd; mkgroup -l >/etc/group"
