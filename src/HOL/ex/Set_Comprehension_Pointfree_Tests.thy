198276a7c40d57f988f0f55c26c5f6e80d92488c
Rafal Kolanski <rafal.kolanski@nicta.com.au>
2012-06-19 11:16:41 +0200

    Improved tactic for rewriting set comprehensions into pointfree form.
    Currently the simproc targets a term with an arbitrary number of conjuncts
    of form "x : S". The tactic should now handle all cases of this form.
    
    --HG--
    extra : rebase_source : 3f08f178681920ee415a3b49130c613778c79256

--- src/HOL/ex/Set_Comprehension_Pointfree_Tests.thy
+++ src/HOL/ex/Set_Comprehension_Pointfree_Tests.thy
@@ -1,5 +1,5 @@
 (*  Title:      HOL/ex/Set_Comprehension_Pointfree_Tests.thy
-    Author:     Lukas Bulwahn
+    Author:     Lukas Bulwahn, Rafal Kolanski
     Copyright   2012 TU Muenchen
 *)
 
@@ -10,36 +10,62 @@ imports Main
 uses "set_comprehension_pointfree.ML"
 begin
 
-simproc_setup finite_Collect ("finite (Collect P)") = {* Set_Comprehension_Pointfree.simproc *}
+simproc_setup finite_Collect ("finite (Collect P)") =
+  {* Set_Comprehension_Pointfree.simproc *}
+
+lemma
+  "finite {p. EX x. p = (x, x)}"
+  apply simp
+  oops
 
 lemma
   "finite {f a b| a b. a : A \<and> b : B}"
-apply simp (* works :) *)
-oops
+  apply simp
+  oops
 
 lemma
   "finite {f a b| a b. a : A \<and> a : A' \<and> b : B}"
-(* apply simp -- does not terminate *)
-oops
+  apply simp
+  oops
 
 lemma
   "finite {f a b| a b. a : A \<and> b : B \<and> b : B'}"
-(* apply simp -- does not terminate *)
-oops
+  apply simp
+  oops
 
 lemma
   "finite {f a b c| a b c. a : A \<and> b : B \<and> c : C}"
-(* apply simp -- failed *) 
-oops
+  apply simp
+  oops
 
 lemma
   "finite {f a b c d| a b c d. a : A \<and> b : B \<and> c : C \<and> d : D}"
-(* apply simp -- failed *)
-oops
+  apply simp
+  oops
 
 lemma
   "finite {f a b c d e | a b c d e. a : A \<and> b : B \<and> c : C \<and> d : D \<and> e : E}"
-(* apply simp -- failed *)
-oops
+  apply simp
+  oops
+
+lemma (* check arbitrary ordering *)
+  "finite {f a d c b e | e b c d a. b : B \<and> a : A \<and> e : E' \<and> c : C \<and> d : D \<and> e : E \<and> b : B'}"
+  apply simp
+  oops
+
+lemma
+  "\<lbrakk> finite A ; finite B ; finite C ; finite D \<rbrakk>
+  \<Longrightarrow> finite ({f a b c d| a b c d. a : A \<and> b : B \<and> c : C \<and> d : D})"
+  by simp
+
+lemma
+  "finite ((\<lambda>(a,b,c,d). f a b c d) ` (A \<times> B \<times> C \<times> D))
+  \<Longrightarrow> finite ({f a b c d| a b c d. a : A \<and> b : B \<and> c : C \<and> d : D})"
+  by simp
+
+schematic_lemma (* check interaction with schematics *)
+  "finite {x :: ?'A \<Rightarrow> ?'B \<Rightarrow> bool. \<exists>a b. x = Pair_Rep a b}
+   = finite ((\<lambda>(a:: ?'A, b :: ?'B). Pair_Rep a b) ` (UNIV \<times> UNIV))"
+  by simp
 
 end
