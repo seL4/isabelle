a6c2ea04455ece10ba4d17dc2a640e84b8833915
blanchet <none@none>
2012-07-09 23:23:12 +0200

    more precise dependencies -- eliminate tautologies

--- src/HOL/TPTP/ATP_Theory_Export.thy
+++ src/HOL/TPTP/ATP_Theory_Export.thy
@@ -5,7 +5,7 @@
 header {* ATP Theory Exporter *}
 
 theory ATP_Theory_Export
-imports (* Complex_Main *) "~~/src/HOL/Sledgehammer2d" (* ### *)
+imports "~~/src/HOL/Sledgehammer2d" Complex_Main (* ### *)
 uses "atp_theory_export.ML"
 begin
 
@@ -20,10 +20,13 @@ val thy = @{theory Nat}; (* @{theory Complex_Main}; ### *)
 val ctxt = @{context}
 *}
 
+
+(* MaSh *)
+
 ML {*
 if do_it then
   "/tmp/mash_sample_problem.out"
-  |> generate_mash_accessibility_file_for_theory thy
+  |> generate_mash_problem_file_for_theory thy
 else
   ()
 *}
@@ -52,11 +55,14 @@ else
    ()
 *}
 
+
+(* TPTP/DFG *)
+
 ML {*
 if do_it then
   "/tmp/infs_poly_guards_query_query.tptp"
   |> generate_tptp_inference_file_for_theory ctxt thy FOF
-                                             "poly_guards_query_query"
+         "poly_guards_query_query"
 else
   ()
 *}
@@ -65,7 +71,7 @@ ML {*
 if do_it then
   "/tmp/infs_poly_tags_query_query.tptp"
   |> generate_tptp_inference_file_for_theory ctxt thy FOF
-                                             "poly_tags_query_query"
+         "poly_tags_query_query"
 else
   ()
 *}
@@ -74,7 +80,7 @@ ML {*
 if do_it then
   "/tmp/axs_tc_native.dfg"
   |> generate_tptp_inference_file_for_theory ctxt thy (DFG Polymorphic)
-                                             "tc_native"
+         "tc_native"
 else
   ()
 *}
