0c0ebecd8a2b4257f909e08d5395fba1d94a711e
wenzelm <none@none>
2012-07-18 20:59:02 +0200

    more SHA1.digest operations;

--- src/Pure/General/sha1.scala
+++ src/Pure/General/sha1.scala
@@ -8,6 +8,7 @@ Digest strings according to SHA-1 (see RFC 3174).
 package isabelle
 
 
+import java.io.{File, FileInputStream}
 import java.security.MessageDigest
 
 
@@ -18,10 +19,10 @@ object SHA1
     override def toString: String = rep
   }
 
-  def digest_bytes(bytes: Array[Byte]): Digest =
+  private def make_result(digest: MessageDigest): Digest =
   {
     val result = new StringBuilder
-    for (b <- MessageDigest.getInstance("SHA").digest(bytes)) {
+    for (b <- digest.digest()) {
       val i = b.asInstanceOf[Int] & 0xFF
       if (i < 16) result += '0'
       result ++= Integer.toHexString(i)
@@ -29,6 +30,32 @@ object SHA1
     Digest(result.toString)
   }
 
-  def digest(s: String): Digest = digest_bytes(Standard_System.string_bytes(s))
+  def digest(file: File): Digest =
+  {
+    val stream = new FileInputStream(file)
+    val digest = MessageDigest.getInstance("SHA")
+
+    val buf = new Array[Byte](65536)
+    var m = 0
+    try {
+      do {
+        m = stream.read(buf, 0, buf.length)
+        if (m != -1) digest.update(buf, 0, m)
+      } while (m != -1)
+    }
+    finally { stream.close }
+
+    make_result(digest)
+  }
+
+  def digest(bytes: Array[Byte]): Digest =
+  {
+    val digest = MessageDigest.getInstance("SHA")
+    digest.update(bytes)
+
+    make_result(digest)
+  }
+
+  def digest(string: String): Digest = digest(Standard_System.string_bytes(string))
 }
 
