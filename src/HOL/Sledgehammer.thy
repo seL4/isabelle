f917ce87c5baa28c1ca8dbc6637d1a2ab41e3f9a
blanchet <none@none>
2012-07-18 08:44:03 +0200

    rationalize relevance filter, slowing moving code from Iter to MaSh

--- src/HOL/Sledgehammer.thy
+++ src/HOL/Sledgehammer.thy
@@ -12,11 +12,10 @@ keywords "sledgehammer" :: diag and "sledgehammer_params" :: thy_decl
 uses "Tools/Sledgehammer/async_manager.ML"
      "Tools/Sledgehammer/sledgehammer_util.ML"
      "Tools/Sledgehammer/sledgehammer_fact.ML"
-     "Tools/Sledgehammer/sledgehammer_filter_iter.ML"
      "Tools/Sledgehammer/sledgehammer_provers.ML"
      "Tools/Sledgehammer/sledgehammer_minimize.ML"
+     "Tools/Sledgehammer/sledgehammer_filter_iter.ML"
      "Tools/Sledgehammer/sledgehammer_filter_mash.ML"
-     "Tools/Sledgehammer/sledgehammer_filter.ML"
      "Tools/Sledgehammer/sledgehammer_run.ML"
      "Tools/Sledgehammer/sledgehammer_isar.ML"
 begin
