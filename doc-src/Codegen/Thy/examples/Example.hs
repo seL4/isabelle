f44e34bc5893170eab04f6a885ea0bf2f9c874fa
wenzelm <none@none>
2012-07-25 17:43:36 +0200

    updated generated files;

--- doc-src/Codegen/Thy/examples/Example.hs
+++ doc-src/Codegen/Thy/examples/Example.hs
@@ -2,6 +2,13 @@
 
 module Example where {
 
+import Prelude ((==), (/=), (<), (<=), (>=), (>), (+), (-), (*), (/),
+  (**), (>>=), (>>), (=<<), (&&), (||), (^), (^^), (.), ($), ($!), (++),
+  (!!), Eq, error, id, return, not, fst, snd, map, filter, concat,
+  concatMap, reverse, zip, null, takeWhile, dropWhile, all, any, Integer,
+  negate, abs, divMod, String, Bool(True, False), Maybe(Nothing, Just));
+import qualified Prelude;
+
 data Queue a = AQueue [a] [a];
 
 empty :: forall a. Queue a;
