9168c8cb481f2fde2c9ad00d2d5eb62157bc65c3
wenzelm <none@none>
2012-07-25 10:55:02 +0200

    more standard session setup for WWW_Find;

---
+++ src/Tools/WWW_Find/WWW_Find.thy
@@ -0,0 +1,20 @@
+theory WWW_Find
+imports Pure
+uses
+  "unicode_symbols.ML"
+  "html_unicode.ML"
+  "mime.ML"
+  "http_status.ML"
+  "http_util.ML"
+  "xhtml.ML"
+  "socket_util.ML"
+  "scgi_req.ML"
+  "scgi_server.ML"
+  "echo.ML"
+  "html_templates.ML"
+  "find_theorems.ML"
+  "yxml_find_theorems.ML"
+begin
+
+end
+
