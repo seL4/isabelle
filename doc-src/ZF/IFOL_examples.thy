1c33fda06f6b81ea3389e714ace92ea62b46b915
wenzelm <none@none>
2012-07-26 12:32:25 +0200

    proper imports;

--- doc-src/ZF/IFOL_examples.thy
+++ doc-src/ZF/IFOL_examples.thy
@@ -1,6 +1,6 @@
 header{*Examples of Intuitionistic Reasoning*}
 
-theory IFOL_examples imports IFOL begin
+theory IFOL_examples imports "~~/src/FOL/IFOL" begin
 
 text{*Quantifier example from the book Logic and Computation*}
 lemma "(EX y. ALL x. Q(x,y)) -->  (ALL x. EX y. Q(x,y))"
