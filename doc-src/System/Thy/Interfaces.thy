dfe97e252e89544c26894bb83cfb76b3f1a2e8fa
wenzelm <none@none>
2012-07-28 13:01:48 +0200

    top-down order of user interfaces;

--- doc-src/System/Thy/Interfaces.thy
+++ doc-src/System/Thy/Interfaces.thy
@@ -4,33 +4,44 @@ begin
 
 chapter {* User interfaces *}
 
-section {* Plain TTY interaction \label{sec:tool-tty} *}
+section {* Isabelle/jEdit Prover IDE \label{sec:tool-jedit} *}
 
-text {*
-  The @{tool_def tty} tool runs the Isabelle process interactively
-  within a plain terminal session:
+text {* The @{tool_def jedit} tool invokes a version of jEdit that has
+  been augmented with some components to provide a fully-featured
+  Prover IDE (based on Isabelle/Scala):
 \begin{ttbox}
-Usage: isabelle tty [OPTIONS]
+Usage: isabelle jedit [OPTIONS] [FILES ...]
 
   Options are:
+    -J OPTION    add JVM runtime option (default JEDIT_JAVA_OPTIONS)
+    -b           build only
+    -d           enable debugger
+    -f           fresh build
+    -j OPTION    add jEdit runtime option (default JEDIT_OPTIONS)
     -l NAME      logic image name (default ISABELLE_LOGIC)
     -m MODE      add print mode for output
-    -p NAME      line editor program name (default ISABELLE_LINE_EDITOR)
 
-  Run Isabelle process with plain tty interaction and line editor.
+Start jEdit with Isabelle plugin setup and opens theory FILES
+(default Scratch.thy).
 \end{ttbox}
 
   The @{verbatim "-l"} option specifies the logic image.  The
-  @{verbatim "-m"} option specifies additional print modes.  The
-  @{verbatim "-p"} option specifies an alternative line editor (such
-  as the @{executable_def rlwrap} wrapper for GNU readline); the
-  fall-back is to use raw standard input.
+  @{verbatim "-m"} option specifies additional print modes.
 
-  Regular interaction is via the standard Isabelle/Isar toplevel loop.
-  The Isar command @{command exit} drops out into the bare-bones ML
-  system, which is occasionally useful for debugging of the Isar
-  infrastructure itself.  Invoking @{ML Isar.loop}~@{verbatim "();"}
-  in ML will return to the Isar toplevel.  *}
+  The @{verbatim "-J"} and @{verbatim "-j"} options allow to pass
+  additional low-level options to the JVM or jEdit, respectively.  The
+  defaults are provided by the Isabelle settings environment.
+
+  The @{verbatim "-d"} option allows to connect to the runtime
+  debugger of the JVM.  Note that the Scala Console of Isabelle/jEdit
+  is more convenient in most practical situations.
+
+  The @{verbatim "-b"} and @{verbatim "-f"} options control the
+  self-build mechanism of Isabelle/jEdit.  This is only relevant for
+  building from sources, which also requires an auxiliary @{verbatim
+  jedit_build} component.  Official Isabelle releases already include
+  a version of Isabelle/jEdit that is built properly.
+*}
 
 
 section {* Proof General / Emacs *}
@@ -66,43 +77,32 @@ text {* The @{tool_def emacs} tool invokes a version of Emacs and
 *}
 
 
-section {* Isabelle/jEdit Prover IDE \label{sec:tool-jedit} *}
+section {* Plain TTY interaction \label{sec:tool-tty} *}
 
-text {* The @{tool_def jedit} tool invokes a version of jEdit that has
-  been augmented with some components to provide a fully-featured
-  Prover IDE (based on Isabelle/Scala):
+text {*
+  The @{tool_def tty} tool runs the Isabelle process interactively
+  within a plain terminal session:
 \begin{ttbox}
-Usage: isabelle jedit [OPTIONS] [FILES ...]
+Usage: isabelle tty [OPTIONS]
 
   Options are:
-    -J OPTION    add JVM runtime option (default JEDIT_JAVA_OPTIONS)
-    -b           build only
-    -d           enable debugger
-    -f           fresh build
-    -j OPTION    add jEdit runtime option (default JEDIT_OPTIONS)
     -l NAME      logic image name (default ISABELLE_LOGIC)
     -m MODE      add print mode for output
+    -p NAME      line editor program name (default ISABELLE_LINE_EDITOR)
 
-Start jEdit with Isabelle plugin setup and opens theory FILES
-(default Scratch.thy).
+  Run Isabelle process with plain tty interaction and line editor.
 \end{ttbox}
 
-The @{verbatim "-l"} option specifies the logic image.  The
-@{verbatim "-m"} option specifies additional print modes.
+  The @{verbatim "-l"} option specifies the logic image.  The
+  @{verbatim "-m"} option specifies additional print modes.  The
+  @{verbatim "-p"} option specifies an alternative line editor (such
+  as the @{executable_def rlwrap} wrapper for GNU readline); the
+  fall-back is to use raw standard input.
 
-The @{verbatim "-J"} and @{verbatim "-j"} options allow to pass
-additional low-level options to the JVM or jEdit, respectively.  The
-defaults are provided by the Isabelle settings environment.
-
-The @{verbatim "-d"} option allows to connect to the runtime debugger
-of the JVM.  Note that the Scala Console of Isabelle/jEdit is more
-convenient in most practical situations.
-
-The @{verbatim "-b"} and @{verbatim "-f"} options control the
-self-build mechanism of Isabelle/jEdit.  This is only relevant for
-building from sources, which also requires an auxiliary @{verbatim
-jedit_build} component.  Official Isabelle releases already include a
-version of Isabelle/jEdit that is built properly.
-*}
+  Regular interaction is via the standard Isabelle/Isar toplevel loop.
+  The Isar command @{command exit} drops out into the bare-bones ML
+  system, which is occasionally useful for debugging of the Isar
+  infrastructure itself.  Invoking @{ML Isar.loop}~@{verbatim "();"}
+  in ML will return to the Isar toplevel.  *}
 
 end
