8bb4c29b6ae03845ab62260445cd8139d930dfae
blanchet <none@none>
2012-08-30 09:47:46 +0200

    killed obsolete "bnf_of_typ" command

--- src/HOL/Codatatype/BNF_Comp.thy
+++ src/HOL/Codatatype/BNF_Comp.thy
@@ -9,8 +9,6 @@ header {* Composition of Bounded Natural Functors *}
 
 theory BNF_Comp
 imports Basic_BNFs
-keywords
-  "bnf_of_typ" :: thy_decl
 uses
   "Tools/bnf_comp_tactics.ML"
   "Tools/bnf_comp.ML"
