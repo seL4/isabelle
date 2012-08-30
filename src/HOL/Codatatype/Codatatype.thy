88381b341390e79345fe66bd733e96e253902cc4
blanchet <none@none>
2012-08-30 09:47:46 +0200

    started work on datatype sugar

--- src/HOL/Codatatype/Codatatype.thy
+++ src/HOL/Codatatype/Codatatype.thy
@@ -1,5 +1,7 @@
 (*  Title:      HOL/Codatatype/Codatatype.thy
     Author:     Dmitriy Traytel, TU Muenchen
+    Author:     Andrei Popescu, TU Muenchen
+    Author:     Jasmin Blanchette, TU Muenchen
     Copyright   2012
 
 The (co)datatype package.
@@ -9,6 +11,10 @@ header {* The (Co)datatype Package *}
 
 theory Codatatype
 imports BNF_LFP BNF_GFP
+keywords
+  "bnf_sugar" :: thy_goal
+uses
+  "Tools/bnf_sugar.ML"
 begin
 
 end
