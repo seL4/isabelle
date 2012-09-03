d0554622f57323032e3adf485132669aa9d8b514
wenzelm <none@none>
2012-09-03 09:15:40 +0200

    some parallel ML;

--- src/HOL/Decision_Procs/MIR.thy
+++ src/HOL/Decision_Procs/MIR.thy
@@ -5502,14 +5502,17 @@ definition
 definition
   "problem4 = E (And (Ge (Sub (Bound 1) (Bound 0))) (Eq (Add (Floor (Bound 1)) (Floor (Neg (Bound 0))))))"
 
-ML {* @{code mircfrqe} @{code problem1} *}
-ML {* @{code mirlfrqe} @{code problem1} *}  
-ML {* @{code mircfrqe} @{code problem2} *}
-ML {* @{code mirlfrqe} @{code problem2} *}  
-ML {* @{code mircfrqe} @{code problem3} *}
-ML {* @{code mirlfrqe} @{code problem3} *}  
-ML {* @{code mircfrqe} @{code problem4} *}
-ML {* @{code mirlfrqe} @{code problem4} *}
+ML {*
+  Par_List.map (fn e => e ())
+   [fn () => @{code mircfrqe} @{code problem1},
+    fn () => @{code mirlfrqe} @{code problem1},
+    fn () => @{code mircfrqe} @{code problem2},
+    fn () => @{code mirlfrqe} @{code problem2},
+    fn () => @{code mircfrqe} @{code problem3},
+    fn () => @{code mirlfrqe} @{code problem3},
+    fn () => @{code mircfrqe} @{code problem4},
+    fn () => @{code mirlfrqe} @{code problem4}]
+*}
 
 (*code_reflect Mir
   functions mircfrqe mirlfrqe
