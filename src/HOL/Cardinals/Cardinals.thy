608d899a2af10832ae539e00869ca0e56a228466
popescua <none@none>
2012-09-18 13:38:10 +0200

    added top-level theory for Cardinals

---
+++ src/HOL/Cardinals/Cardinals.thy
@@ -0,0 +1,15 @@
+(*  Title:      HOL/Cardinals/Cardinals.thy
+    Author:     Andrei Popescu, TU Muenchen
+    Author:     Dmitriy Traytel, TU Muenchen
+    Copyright   2012
+
+Theory of ordinals and cardinals.
+*)
+
+header {* Theory of Ordinals and Cardinals  *}
+
+theory Cardinals
+imports Cardinal_Order_Relation Cardinal_Arithmetic
+begin
+
+end
