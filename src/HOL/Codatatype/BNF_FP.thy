6a793db05fcb0b870679ac785bd3afaeaa99f1c5
blanchet <none@none>
2012-09-20 02:42:48 +0200

    renamed "bnf_fp_util.ML" to "bnf_fp.ML"
    --HG--
    rename : src/HOL/Codatatype/Tools/bnf_fp_util.ML => src/HOL/Codatatype/Tools/bnf_fp.ML

--- src/HOL/Codatatype/BNF_FP.thy
+++ src/HOL/Codatatype/BNF_FP.thy
@@ -119,7 +119,7 @@ lemma sum_set_simps:
 "setr (Inr x) = {x}"
 unfolding sum_set_defs by simp+
 
-ML_file "Tools/bnf_fp_util.ML"
+ML_file "Tools/bnf_fp.ML"
 ML_file "Tools/bnf_fp_sugar_tactics.ML"
 ML_file "Tools/bnf_fp_sugar.ML"
 
