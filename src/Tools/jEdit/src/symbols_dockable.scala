2e6d225538b81f79ad6a3658e929eae4fda72198
immler <none@none>
2012-11-21 16:04:00 +0100

    delayed search to improve reactivity

--- src/Tools/jEdit/src/symbols_dockable.scala
+++ src/Tools/jEdit/src/symbols_dockable.scala
@@ -36,7 +36,7 @@ class Symbols_Dockable(view: View, position: String) extends Dockable(view, posi
     tooltip = symbol + " - " + get_name(dec)
   }
 
-  val group_tabs = new TabbedPane {
+  val group_tabs: TabbedPane = new TabbedPane {
     pages ++= (for ((group, symbols) <- Symbol.groups) yield
       {
         new TabbedPane.Page(group,
@@ -49,10 +49,8 @@ class Symbols_Dockable(view: View, position: String) extends Dockable(view, posi
       add(search, BorderPanel.Position.North)
       add(results_panel, BorderPanel.Position.Center)
       listenTo(search)
-      var last = search.text
-      reactions += { case ValueChanged(`search`) =>
-        if (search.text != last) {
-          last = search.text
+      val delay_search =
+        Swing_Thread.delay_last(Time.seconds(Isabelle.options.real("editor_input_delay"))) {
           results_panel.contents.clear
           val results =
             (searchspace filter (search.text.toLowerCase.split("\\s+") forall _._2.contains)
@@ -60,10 +58,10 @@ class Symbols_Dockable(view: View, position: String) extends Dockable(view, posi
           for ((sym, _) <- results)
             results_panel.contents += new Symbol_Component(sym)
           if (results.length > max_results) results_panel.contents += new Label("...")
-          results_panel.revalidate
-          results_panel.repaint
+          revalidate
+          repaint
         }
-      }
+      reactions += { case ValueChanged(`search`) => delay_search.invoke() }
     }, "Search Symbols")
     pages map (p => p.title = (space_explode('_', p.title) map Library.capitalize).mkString(" ") )
   }
