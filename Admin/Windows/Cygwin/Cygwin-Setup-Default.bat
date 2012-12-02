f661573fbeb8e22dbcdf55bdbe3c56136858933a
wenzelm <none@none>
2012-12-02 22:20:12 +0100

    semi-automated Cygwin setup;

---
+++ Admin/Windows/Cygwin/Cygwin-Setup-Default.bat
@@ -0,0 +1,4 @@
+@echo off
+
+setup.exe --local-package-dir "%TEMP%" --root "%CD%\cygwin-root" -P libgmp3,perl,python,texlive-collection-basic,texlive-collection-fontsrecommended,texlive-collection-genericrecommended,texlive-collection-latex,texlive-collection-latexrecommended
+
