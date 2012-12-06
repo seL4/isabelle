b6f2adc18630c0ce50dc87fe51f962a5040ecb9f
blanchet <none@none>
2012-12-06 16:49:48 +0100

    export ATP and Isar commands separately

--- src/HOL/TPTP/MaSh_Export.thy
+++ src/HOL/TPTP/MaSh_Export.thy
@@ -11,8 +11,8 @@ begin
 ML_file "mash_export.ML"
 
 sledgehammer_params
-  [provers = spass, max_relevant = 40, strict, dont_slice, type_enc = mono_native,
-   lam_trans = combs_and_lifting, timeout = 1, dont_preplay, minimize]
+  [provers = spass, max_relevant = 32, strict, dont_slice, type_enc = mono_native,
+   lam_trans = combs_and_lifting, timeout = 2, dont_preplay, minimize]
 
 ML {*
 open MaSh_Export
@@ -57,7 +57,7 @@ else
 
 ML {*
 if do_it then
-  generate_commands @{context} params thys (prefix ^ "mash_commands")
+  generate_isar_commands @{context} prover thys (prefix ^ "mash_commands")
 else
   ()
 *}
@@ -72,6 +72,13 @@ else
 ML {*
 if do_it then
   generate_atp_dependencies @{context} params thys false (prefix ^ "mash_atp_dependencies")
+else
+  ()
+*}
+
+ML {*
+if do_it then
+  generate_atp_commands @{context} params thys (prefix ^ "mash_atp_commands")
 else
   ()
 *}
