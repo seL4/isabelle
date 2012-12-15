6cd18694a518a5c5547570dfa5bdcbdf01876276
wenzelm <none@none>
2012-12-15 21:07:52 +0100

    more general handling of graphics configurations, to increase chance of proper positioning of tooltips in multi-screen environment; more tooltip options via Rendering;

--- src/Tools/jEdit/src/isabelle_options.scala
+++ src/Tools/jEdit/src/isabelle_options.scala
@@ -41,8 +41,8 @@ class Isabelle_Options1 extends Isabelle_Options("isabelle-general")
 {
   // FIXME avoid hard-wired stuff
   private val relevant_options =
-    Set("jedit_logic", "jedit_font_scale", "jedit_text_overview_limit",
-      "jedit_tooltip_font_scale", "jedit_symbols_search_limit", "jedit_tooltip_margin",
+    Set("jedit_logic", "jedit_font_scale", "jedit_symbols_search_limit", "jedit_text_overview_limit",
+      "jedit_tooltip_bounds", "jedit_tooltip_font_scale", "jedit_tooltip_margin",
       "threads", "threads_trace", "parallel_proofs", "parallel_proofs_threshold", "ML_statistics",
       "editor_load_delay", "editor_input_delay", "editor_output_delay", "editor_reparse_limit",
       "editor_tracing_messages", "editor_update_delay")
