cdfd37426089973f507d8fc6c7c0a26f9a35485d
nipkow <none@none>
2013-01-11 13:24:36 +0100

    tuned

--- src/HOL/IMP/Collecting_Examples.thy
+++ src/HOL/IMP/Collecting_Examples.thy
@@ -15,25 +15,25 @@ definition C0 :: "state set acom" where "C0 = anno {} c"
 definition "show_acom xs = map_acom (\<lambda>S. show_state xs ` S)"
 
 text{* Collecting semantics: *}
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 1) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 2) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 3) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 4) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 5) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 6) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 7) C0)"
-value "show_acom [''x''] (((step {\<lambda>x. 0}) ^^ 8) C0)"
+
+value "show_acom [''x''] (((step {<>}) ^^ 1) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 2) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 3) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 4) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 5) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 6) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 7) C0)"
+value "show_acom [''x''] (((step {<>}) ^^ 8) C0)"
 
 text{* Small-step semantics: *}
-value "show_acom [''x''] (((step {}) ^^ 0) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 1) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 2) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 3) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 4) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 5) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 6) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 7) (step {\<lambda>x. 0} C0))"
-value "show_acom [''x''] (((step {}) ^^ 8) (step {\<lambda>x. 0} C0))"
-
+value "show_acom [''x''] (((step {}) ^^ 0) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 1) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 2) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 3) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 4) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 5) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 6) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 7) (step {<>} C0))"
+value "show_acom [''x''] (((step {}) ^^ 8) (step {<>} C0))"
 
 end
