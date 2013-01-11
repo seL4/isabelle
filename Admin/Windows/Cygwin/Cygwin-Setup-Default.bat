9a07ab0dfba2614d333ecae25ec6e9fd7d469436
wenzelm <none@none>
2013-01-11 13:24:22 +0100

    obsolete;

--- Admin/Windows/Cygwin/Cygwin-Setup-Default.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-setup.exe --local-package-dir "%TEMP%" --root "%CD%\cygwin-root" -P libgmp3,perl,python,texlive-collection-basic,texlive-collection-fontsrecommended,texlive-collection-genericrecommended,texlive-collection-latex,texlive-collection-latexrecommended
-
