55c411a47b409355f3500740acba9c2ea713b4fc
wenzelm <none@none>
2013-05-18 13:04:10 +0200

    discontinued odd workaround for scala-2.10.0-RC1;

--- src/Pure/Tools/build.scala
+++ src/Pure/Tools/build.scala
@@ -894,8 +894,7 @@ object Build
     val rc = (0 /: results)({ case (rc1, (_, res)) => rc1 max res.rc })
     if (rc != 0 && (verbose || !no_build)) {
       val unfinished =
-        (for ((name, res) <- results.iterator if res.rc != 0) yield name).toList
-          .sorted(scala.math.Ordering.String)  // FIXME scala-2.10.0-RC1
+        (for ((name, res) <- results.iterator if res.rc != 0) yield name).toList.sorted
       progress.echo("Unfinished session(s): " + commas(unfinished))
     }
     rc
