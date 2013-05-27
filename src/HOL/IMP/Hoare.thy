46992f3b758ae0040438f6f070e804bf49ed0be6
nipkow <none@none>
2013-05-27 09:15:26 +0200

    tuned

--- src/HOL/IMP/Hoare.thy
+++ src/HOL/IMP/Hoare.thy
@@ -10,7 +10,7 @@ type_synonym assn = "state \<Rightarrow> bool"
 
 definition
 hoare_valid :: "assn \<Rightarrow> com \<Rightarrow> assn \<Rightarrow> bool" ("\<Turnstile> {(1_)}/ (_)/ {(1_)}" 50) where
-"\<Turnstile> {P}c{Q} = (\<forall>s t. (c,s) \<Rightarrow> t  \<longrightarrow>  P s \<longrightarrow>  Q t)"
+"\<Turnstile> {P}c{Q} = (\<forall>s t. P s \<and> (c,s) \<Rightarrow> t  \<longrightarrow>  Q t)"
 
 abbreviation state_subst :: "state \<Rightarrow> aexp \<Rightarrow> vname \<Rightarrow> state"
   ("_[_'/_]" [1000,0,0] 999)
