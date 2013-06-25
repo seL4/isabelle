263544755c3aabe30b81403a5daab3cc29ae212e
wenzelm <none@none>
2013-06-25 20:35:50 +0200

    recover command tags from 4cf3f6153eb8 -- important for document preparation;

--- src/Pure/Pure.thy
+++ src/Pure/Pure.thy
@@ -13,8 +13,8 @@ theory Pure
     "identifier" "if" "imports" "in" "includes" "infix" "infixl"
     "infixr" "is" "keywords" "notes" "obtains" "open" "output"
     "overloaded" "pervasive" "shows" "structure" "unchecked" "where" "|"
-  and "theory" :: thy_begin
-  and "ML_file" :: thy_load
+  and "theory" :: thy_begin % "theory"
+  and "ML_file" :: thy_load % "ML"
   and "header" :: diag
   and "chapter" :: thy_heading1
   and "section" :: thy_heading2
