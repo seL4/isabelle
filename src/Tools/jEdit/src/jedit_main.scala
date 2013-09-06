5977bb8c8b83926feb64349ae80370c7d1e3c4e3
wenzelm <none@none>
2013-09-06 17:20:48 +0200

    prefer Isabelle/Scala over bash;

--- src/Tools/jEdit/src/jedit_main.scala
+++ src/Tools/jEdit/src/jedit_main.scala
@@ -20,10 +20,27 @@ object JEdit_Main
     GUI.init_laf()
     Isabelle_System.init()
 
-    val file_args =
-      if (args.isEmpty)
-        Array(Isabelle_System.platform_path(Path.explode("$USER_HOME/Scratch.thy")))
-      else args
+
+    /* settings directory */
+
+    val settings_dir = Path.explode("$JEDIT_SETTINGS")
+    Isabelle_System.mkdirs(settings_dir + Path.explode("DockableWindowManager"))
+
+    if (!(settings_dir + Path.explode("perspective.xml")).is_file) {
+      File.write(settings_dir + Path.explode("DockableWindowManager/perspective-view0.xml"),
+        """<DOCKING LEFT="" TOP="" RIGHT="" BOTTOM="isabelle-readme" LEFT_POS="0" TOP_POS="0" RIGHT_POS="250" BOTTOM_POS="250" />""")
+      File.write(settings_dir + Path.explode("perspective.xml"),
+        """<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE PERSPECTIVE SYSTEM "perspective.dtd">
+<PERSPECTIVE>
+<VIEW PLAIN="FALSE">
+<GEOMETRY X="0" Y="35" WIDTH="1072" HEIGHT="787" EXT_STATE="0" />
+</VIEW>
+</PERSPECTIVE>""")
+    }
+
+
+    /* args */
 
     val jedit_options =
       Isabelle_System.getenv_strict("JEDIT_OPTIONS").split(" +")
@@ -31,10 +48,18 @@ object JEdit_Main
     val jedit_settings =
       Array("-settings=" + Isabelle_System.platform_path(Path.explode("$JEDIT_SETTINGS")))
 
+    val more_args =
+      if (args.isEmpty)
+        Array(Isabelle_System.platform_path(Path.explode("$USER_HOME/Scratch.thy")))
+      else args
+
+
+    /* startup */
+
     System.setProperty("jedit.home",
       Isabelle_System.platform_path(Path.explode("$JEDIT_HOME/dist")))
 
-    jEdit.main(jedit_options ++ jedit_settings ++ file_args)
+    jEdit.main(jedit_options ++ jedit_settings ++ more_args)
   }
 }
 
