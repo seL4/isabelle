67077acb4e6260dcdd4a09a550d829d623fb1451
noschinl <none@none>
2013-09-06 10:56:40 +0200

    added simps_of_case and case_of_simps to convert between simps and case rules

---
+++ src/HOL/Library/Simps_Case_Conv.thy
@@ -0,0 +1,12 @@
+(*  Title:    HOL/Library/Simps_Case_Conv.thy
+    Author:   Lars Noschinski
+*)
+
+theory Simps_Case_Conv
+  imports Main
+  keywords "simps_of_case" "case_of_simps" :: thy_decl
+begin
+
+ML_file "simps_case_conv.ML"
+
+end
