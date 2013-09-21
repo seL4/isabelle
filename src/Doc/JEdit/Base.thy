f736ecb86ffa7c1d709d5dcb73cd3013338d7bae
wenzelm <none@none>
2013-09-21 13:05:54 +0200

    basic setup for Isabelle/jEdit documentation;

---
+++ src/Doc/JEdit/Base.thy
@@ -0,0 +1,8 @@
+theory Base
+imports Pure
+begin
+
+ML_file "../antiquote_setup.ML"
+setup Antiquote_Setup.setup
+
+end
---
+++ src/Doc/JEdit/JEdit.thy
@@ -0,0 +1,21 @@
+theory JEdit
+imports Base
+begin
+
+chapter {* Introduction *}
+
+text {* FIXME
+
+parallel proof checking \cite{Wenzel:2009} \cite{Wenzel:2013:ITP}
+
+asynchronous user interaction \cite{Wenzel:2010}, \cite{Wenzel:2012:UITP-EPTCS}
+
+document-oriented proof processing and Prover IDE \cite{Wenzel:2011:CICM} \cite{Wenzel:2012}
+
+*}
+
+section {* Concepts and terminology *}
+
+text {* FIXME *}
+
+end
