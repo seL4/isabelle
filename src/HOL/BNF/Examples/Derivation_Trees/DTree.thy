8602af1479b703319d1fd0e7fcb7ab3e2cede48c
blanchet <none@none>
2013-11-20 18:09:23 +0100

    compile

--- src/HOL/BNF/Examples/Derivation_Trees/DTree.thy
+++ src/HOL/BNF/Examples/Derivation_Trees/DTree.thy
@@ -22,8 +22,8 @@ subsection{* Transporting the Characteristic Lemmas from @{text "fset"} to @{tex
 
 definition "Node n as \<equiv> NNode n (the_inv fset as)"
 definition "cont \<equiv> fset o ccont"
-definition "unfold rt ct \<equiv> dtree_unfold rt (the_inv fset o ct)"
-definition "corec rt ct \<equiv> dtree_corec rt (the_inv fset o ct)"
+definition "unfold rt ct \<equiv> unfold_dtree rt (the_inv fset o ct)"
+definition "corec rt ct \<equiv> corec_dtree rt (the_inv fset o ct)"
 
 lemma finite_cont[simp]: "finite (cont tr)"
   unfolding cont_def o_apply by (cases tr, clarsimp)
