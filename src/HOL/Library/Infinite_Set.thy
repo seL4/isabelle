7b69fbceb924d9ec8efdf0c3b57badcc6ab54b2b
traytel <none@none>
2013-11-29 14:24:21 +0100

    Backed out changeset: a8ad7f6dd217---bypassing Main breaks theories that use \<inf> or \<sup>

--- src/HOL/Library/Infinite_Set.thy
+++ src/HOL/Library/Infinite_Set.thy
@@ -5,7 +5,7 @@
 header {* Infinite Sets and Related Concepts *}
 
 theory Infinite_Set
-imports Set_Interval
+imports Main
 begin
 
 subsection "Infinite Sets"
@@ -554,3 +554,4 @@ lemma atmost_one_unique [elim]: "atmost_one S \<Longrightarrow> x \<in> S \<Long
   by (simp add: atmost_one_def)
 
 end
+
