215faebcdc954364712203c17136121212480958
blanchet <none@none>
2013-12-20 20:36:38 +0100

    reconstruct SPASS-Pirate steps of the form 'x ~= C x' (or more complicated)

--- src/HOL/Sledgehammer.thy
+++ src/HOL/Sledgehammer.thy
@@ -11,6 +11,9 @@ imports ATP SMT
 keywords "sledgehammer" :: diag and "sledgehammer_params" :: thy_decl
 begin
 
+lemma size_ne_size_imp_ne: "size x \<noteq> size y \<Longrightarrow> x \<noteq> y"
+by (erule contrapos_nn) (rule arg_cong)
+
 ML_file "Tools/Sledgehammer/async_manager.ML"
 ML_file "Tools/Sledgehammer/sledgehammer_util.ML"
 ML_file "Tools/Sledgehammer/sledgehammer_annotate.ML"
