6ca4b51cf7ca594df1a73fefde5ea216ac017007
blanchet <none@none>
2014-01-20 21:45:08 +0100

    hide BNF notation

--- src/HOL/Main.thy
+++ src/HOL/Main.thy
@@ -30,7 +30,8 @@ no_notation
   card_of ("|_|") and
   csum (infixr "+c" 65) and
   cprod (infixr "*c" 80) and
-  cexp (infixr "^c" 90)
+  cexp (infixr "^c" 90) and
+  convol ("<_ , _>")
 
 no_syntax (xsymbols)
   "_INF1"     :: "pttrns \<Rightarrow> 'b \<Rightarrow> 'b"           ("(3\<Sqinter>_./ _)" [0, 10] 10)
