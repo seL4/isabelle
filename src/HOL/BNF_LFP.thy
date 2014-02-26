a6bc1928935e770ddfb1dc41910ba578087b3b22
traytel <none@none>
2014-02-26 23:09:29 +0100

    intermediate typedef for the type of the bound (local to lfp)

--- src/HOL/BNF_LFP.thy
+++ src/HOL/BNF_LFP.thy
@@ -236,6 +236,9 @@ lemma predicate2D_vimage2p: "\<lbrakk>R \<le> vimage2p f g S; R x y\<rbrakk> \<L
 lemma id_transfer: "fun_rel A A id id"
 unfolding fun_rel_def by simp
 
+lemma ssubst_Pair_rhs: "\<lbrakk>(r, s) \<in> R; s' = s\<rbrakk> \<Longrightarrow> (r, s') \<in> R"
+  by simp
+
 ML_file "Tools/BNF/bnf_lfp_util.ML"
 ML_file "Tools/BNF/bnf_lfp_tactics.ML"
 ML_file "Tools/BNF/bnf_lfp.ML"
