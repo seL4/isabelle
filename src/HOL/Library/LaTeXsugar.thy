fe74750fc4c621c7f0ff1743679d4da49a39d318
wenzelm <none@none>
2014-03-06 13:44:01 +0100

    more uniform check_const/read_const;

--- src/HOL/Library/LaTeXsugar.thy
+++ src/HOL/Library/LaTeXsugar.thy
@@ -100,7 +100,7 @@ syntax (IfThenNoBox output)
 setup{*
   let
     fun pretty ctxt c =
-      let val tc = Proof_Context.read_const_proper ctxt false c
+      let val tc = Proof_Context.read_const ctxt {proper = true, strict = false} dummyT c
       in Pretty.block [Thy_Output.pretty_term ctxt tc, Pretty.str " ::",
             Pretty.brk 1, Syntax.pretty_typ ctxt (fastype_of tc)]
       end
