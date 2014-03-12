86d5dd696309cfc415209292b75ca793c075b04e
wenzelm <none@none>
2014-03-12 21:58:48 +0100

    simplified programming interface to define ML antiquotations -- NB: the transformed context ignores updates of the context parser; added command 'print_ML_antiquotations';

--- src/Pure/Pure.thy
+++ src/Pure/Pure.thy
@@ -78,14 +78,15 @@ theory Pure
   and "finally" "ultimately" :: prf_chain % "proof"
   and "back" :: prf_script % "proof"
   and "Isabelle.command" :: control
-  and "help" "print_commands" "print_options"
-    "print_context" "print_theory" "print_syntax" "print_abbrevs" "print_defn_rules"
+  and "help" "print_commands" "print_options" "print_context"
+    "print_theory" "print_syntax" "print_abbrevs" "print_defn_rules"
     "print_theorems" "print_locales" "print_classes" "print_locale"
     "print_interps" "print_dependencies" "print_attributes"
     "print_simpset" "print_rules" "print_trans_rules" "print_methods"
-    "print_antiquotations" "thy_deps" "locale_deps" "class_deps" "thm_deps"
-    "print_binds" "print_facts" "print_cases" "print_statement" "thm"
-    "prf" "full_prf" "prop" "term" "typ" "print_codesetup" "unused_thms"
+    "print_antiquotations" "print_ML_antiquotations" "thy_deps"
+    "locale_deps" "class_deps" "thm_deps" "print_binds" "print_facts"
+    "print_cases" "print_statement" "thm" "prf" "full_prf" "prop"
+    "term" "typ" "print_codesetup" "unused_thms"
     :: diag
   and "cd" :: control
   and "pwd" :: diag
