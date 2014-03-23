ae43ca5ec479e7c7245c175391f996f3ab3faf46
wenzelm <none@none>
2014-03-23 16:40:35 +0100

    modernized approximation, avoiding bad name binding;

--- src/Doc/Tutorial/Misc/fakenat.thy
+++ src/Doc/Tutorial/Misc/fakenat.thy
@@ -4,10 +4,11 @@ theory fakenat imports Main begin;
 
 text{*\noindent
 The type \tydx{nat} of natural
-numbers is predefined to have the constructors \cdx{0} and~\cdx{Suc}.  It  behaves as if it were declared like this:
+numbers is predefined to have the constructors \cdx{0} and~\cdx{Suc}.
+It behaves approximately as if it were declared like this:
 *}
 
-datatype nat = 0 | Suc nat
+datatype nat = zero ("0") | Suc nat
 (*<*)
 end
 (*>*)
