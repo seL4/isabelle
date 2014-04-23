72596a3ca97bc0e5f051b436a4a0d4d186199f50
blanchet <none@none>
2014-04-23 10:23:26 +0200

    generate 'rec_o_map' and 'size_o_map' in size extension

--- src/HOL/BNF_FP_Base.thy
+++ src/HOL/BNF_FP_Base.thy
@@ -89,34 +89,34 @@ unfolding sum_set_defs by simp+
 lemma spec2: "\<forall>x y. P x y \<Longrightarrow> P x y"
 by blast
 
-lemma rewriteR_comp_comp: "\<lbrakk>g o h = r\<rbrakk> \<Longrightarrow> f o g o h = f o r"
+lemma rewriteR_comp_comp: "\<lbrakk>g \<circ> h = r\<rbrakk> \<Longrightarrow> f \<circ> g \<circ> h = f \<circ> r"
   unfolding comp_def fun_eq_iff by auto
 
-lemma rewriteR_comp_comp2: "\<lbrakk>g o h = r1 o r2; f o r1 = l\<rbrakk> \<Longrightarrow> f o g o h = l o r2"
+lemma rewriteR_comp_comp2: "\<lbrakk>g \<circ> h = r1 \<circ> r2; f \<circ> r1 = l\<rbrakk> \<Longrightarrow> f \<circ> g \<circ> h = l \<circ> r2"
   unfolding comp_def fun_eq_iff by auto
 
-lemma rewriteL_comp_comp: "\<lbrakk>f o g = l\<rbrakk> \<Longrightarrow> f o (g o h) = l o h"
+lemma rewriteL_comp_comp: "\<lbrakk>f \<circ> g = l\<rbrakk> \<Longrightarrow> f \<circ> (g \<circ> h) = l \<circ> h"
   unfolding comp_def fun_eq_iff by auto
 
-lemma rewriteL_comp_comp2: "\<lbrakk>f o g = l1 o l2; l2 o h = r\<rbrakk> \<Longrightarrow> f o (g o h) = l1 o r"
+lemma rewriteL_comp_comp2: "\<lbrakk>f \<circ> g = l1 \<circ> l2; l2 \<circ> h = r\<rbrakk> \<Longrightarrow> f \<circ> (g \<circ> h) = l1 \<circ> r"
   unfolding comp_def fun_eq_iff by auto
 
-lemma convol_o: "<f, g> o h = <f o h, g o h>"
+lemma convol_o: "<f, g> \<circ> h = <f \<circ> h, g \<circ> h>"
   unfolding convol_def by auto
 
-lemma map_prod_o_convol: "map_prod h1 h2 o <f, g> = <h1 o f, h2 o g>"
+lemma map_prod_o_convol: "map_prod h1 h2 \<circ> <f, g> = <h1 \<circ> f, h2 \<circ> g>"
   unfolding convol_def by auto
 
 lemma map_prod_o_convol_id: "(map_prod f id \<circ> <id , g>) x = <id \<circ> f , g> x"
   unfolding map_prod_o_convol id_comp comp_id ..
 
-lemma o_case_sum: "h o case_sum f g = case_sum (h o f) (h o g)"
+lemma o_case_sum: "h \<circ> case_sum f g = case_sum (h \<circ> f) (h \<circ> g)"
   unfolding comp_def by (auto split: sum.splits)
 
-lemma case_sum_o_map_sum: "case_sum f g o map_sum h1 h2 = case_sum (f o h1) (g o h2)"
+lemma case_sum_o_map_sum: "case_sum f g \<circ> map_sum h1 h2 = case_sum (f \<circ> h1) (g \<circ> h2)"
   unfolding comp_def by (auto split: sum.splits)
 
-lemma case_sum_o_map_sum_id: "(case_sum id g o map_sum f id) x = case_sum (f o id) g x"
+lemma case_sum_o_map_sum_id: "(case_sum id g \<circ> map_sum f id) x = case_sum (f \<circ> id) g x"
   unfolding case_sum_o_map_sum id_comp comp_id ..
 
 lemma rel_fun_def_butlast:
@@ -144,7 +144,7 @@ lemma vimage2p_refl: "(\<And>x. R x x) \<Longrightarrow> vimage2p f f R x x"
 
 lemma
   assumes "type_definition Rep Abs UNIV"
-  shows type_copy_Rep_o_Abs: "Rep \<circ> Abs = id" and type_copy_Abs_o_Rep: "Abs o Rep = id"
+  shows type_copy_Rep_o_Abs: "Rep \<circ> Abs = id" and type_copy_Abs_o_Rep: "Abs \<circ> Rep = id"
   unfolding fun_eq_iff comp_apply id_apply
     type_definition.Abs_inverse[OF assms UNIV_I] type_definition.Rep_inverse[OF assms] by simp_all
 
@@ -152,7 +152,7 @@ lemma type_copy_map_comp0_undo:
   assumes "type_definition Rep Abs UNIV"
           "type_definition Rep' Abs' UNIV"
           "type_definition Rep'' Abs'' UNIV"
-  shows "Abs' o M o Rep'' = (Abs' o M1 o Rep) o (Abs o M2 o Rep'') \<Longrightarrow> M1 o M2 = M"
+  shows "Abs' \<circ> M \<circ> Rep'' = (Abs' \<circ> M1 \<circ> Rep) \<circ> (Abs \<circ> M2 \<circ> Rep'') \<Longrightarrow> M1 \<circ> M2 = M"
   by (rule sym) (auto simp: fun_eq_iff type_definition.Abs_inject[OF assms(2) UNIV_I UNIV_I]
     type_definition.Abs_inverse[OF assms(1) UNIV_I]
     type_definition.Abs_inverse[OF assms(3) UNIV_I] dest: spec[of _ "Abs'' x" for x])
@@ -160,7 +160,7 @@ lemma type_copy_map_comp0_undo:
 lemma vimage2p_id: "vimage2p id id R = R"
   unfolding vimage2p_def by auto
 
-lemma vimage2p_comp: "vimage2p (f1 o f2) (g1 o g2) = vimage2p f2 g2 o vimage2p f1 g1"
+lemma vimage2p_comp: "vimage2p (f1 \<circ> f2) (g1 \<circ> g2) = vimage2p f2 g2 \<circ> vimage2p f1 g1"
   unfolding fun_eq_iff vimage2p_def o_apply by simp
 
 ML_file "Tools/BNF/bnf_fp_util.ML"
--- src/HOL/BNF_LFP.thy
+++ src/HOL/BNF_LFP.thy
@@ -186,11 +186,28 @@ lemma id_transfer: "rel_fun A A id id"
 lemma ssubst_Pair_rhs: "\<lbrakk>(r, s) \<in> R; s' = s\<rbrakk> \<Longrightarrow> (r, s') \<in> R"
   by (rule ssubst)
 
+lemma fun_cong_unused_0: "f = (\<lambda>x. g) \<Longrightarrow> f (\<lambda>x. 0) = g"
+  by (erule arg_cong)
+
 lemma snd_o_convol: "(snd \<circ> (\<lambda>x. (f x, g x))) = g"
   by (rule ext) simp
 
 lemma inj_on_convol_id: "inj_on (\<lambda>x. (x, f x)) X"
   unfolding inj_on_def by simp
+
+lemma case_prod_app: "case_prod f x y = case_prod (\<lambda>l r. f l r y) x"
+  by (case_tac x) simp
+
+lemma case_sum_o_map_sum: "case_sum l r (map_sum f g x) = case_sum (l \<circ> f) (r \<circ> g) x"
+  by (case_tac x) simp+
+
+lemma case_prod_o_map_prod: "case_prod h (map_prod f g x) = case_prod (\<lambda>l r. h (f l) (g r)) x"
+  by (case_tac x) simp+
+
+lemma prod_inj_map: "inj f \<Longrightarrow> inj g \<Longrightarrow> inj (map_prod f g)"
+  by (simp add: inj_on_def)
+
+declare [[ML_print_depth = 10000]] (*###*)
 
 ML_file "Tools/BNF/bnf_lfp_util.ML"
 ML_file "Tools/BNF/bnf_lfp_tactics.ML"
@@ -200,5 +217,59 @@ ML_file "Tools/BNF/bnf_lfp_rec_sugar_more.ML"
 ML_file "Tools/BNF/bnf_lfp_size.ML"
 
 hide_fact (open) id_transfer
+
+datatype_new ('a, 'b) j = J0 | J 'a "('a, 'b) j"
+thm j.size j.rec_o_map j.size_o_map
+
+datatype_new 'a l = N nat nat | C 'a "'a l"
+thm l.size l.rec_o_map l.size_o_map
+
+datatype_new ('a, 'b) x = XN 'b | XC 'a "('a, 'b) x"
+thm x.size x.rec_o_map x.size_o_map
+
+datatype_new
+  'a tl = TN | TC "'a mt" "'a tl" and
+  'a mt = MT 'a "'a tl"
+thm tl.size tl.rec_o_map tl.size_o_map
+thm mt.size mt.rec_o_map mt.size_o_map
+
+datatype_new 'a t = T nat 'a "'a t l"
+thm t.size t.rec_o_map t.size_o_map
+
+datatype_new 'a fset = FSet0 | FSet 'a "'a fset"
+thm fset.size fset.rec_o_map fset.size_o_map
+
+datatype_new 'a u = U 'a "'a u fset"
+thm u.size u.rec_o_map u.size_o_map
+
+datatype_new
+  ('a, 'b) v = V "nat l" | V' 'a "('a, 'b) w" and
+  ('a, 'b) w = W 'b "('a, 'b) v fset l"
+thm v.size v.rec_o_map v.size_o_map
+thm w.size w.rec_o_map w.size_o_map
+
+(*TODO:
+* deal with *unused* dead variables and other odd cases (e.g. recursion through fun)
+* what happens if recursion through arbitrary bnf, like 'fsize'?
+  * by default
+  * offer possibility to register size function and theorems
+* non-recursive types use 'case' instead of 'rec', causes trouble (revert?)
+* compat with old size?
+  * recursion of old through new (e.g. through list)?
+  * recursion of new through old?
+  * should they share theory data?
+* code generator setup?
+*)
+
+
+end
+datatype_new 'a x = X0 | X 'a (*###*)
+thm x.size
+thm x.size_o_map
+datatype_new 'a x = X0 | X 'a "'a x" (*###*)
+thm x.size
+thm x.size_o_map
+datatype_new 'a l = N | C 'a "'a l"
+datatype_new ('a, 'b) tl = TN 'b | TC 'a "'a l"
 
 end
