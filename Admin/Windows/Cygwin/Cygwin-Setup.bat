de1f54c00a3ec19535b7a323ea24f55963fc2a64
wenzelm <none@none>
2014-05-20 21:13:21 +0200

    updated cygwin;

--- Admin/Windows/Cygwin/Cygwin-Setup.bat
+++ Admin/Windows/Cygwin/Cygwin-Setup.bat
@@ -1,4 +1,4 @@
 @echo off
 
-"%CD%\contrib\cygwin\isabelle\cygwin" --site http://isabelle.in.tum.de/cygwin_2013-1 --no-verify --only-site --local-package-dir "%TEMP%" --root "%CD%\contrib\cygwin"
+"%CD%\contrib\cygwin\isabelle\cygwin" --site http://isabelle.in.tum.de/cygwin_2014 --no-verify --only-site --local-package-dir "%TEMP%" --root "%CD%\contrib\cygwin"
 
