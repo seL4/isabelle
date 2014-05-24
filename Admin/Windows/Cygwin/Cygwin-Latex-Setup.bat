2dfac1e6164e4d7e4153133697b939d32c354de0
wenzelm <none@none>
2014-05-24 20:24:43 +0200

    support for regular Windows TeX installation;

--- Admin/Windows/Cygwin/Cygwin-Latex-Setup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-"%CD%\contrib\cygwin\isabelle\cygwin" --site http://isabelle.in.tum.de/cygwin_2014 --no-verify --only-site --local-package-dir "%TEMP%" --root "%CD%\contrib\cygwin" --packages texlive-collection-latexextra,texlive-collection-fontutils,texlive-collection-mathextra,libsasl2 --quiet-mode
-
