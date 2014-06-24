104e77f3feb891e3ce5f6a290ee4cb86f92bc26a
desharna <none@none>
2014-06-24 13:48:14 +0200

    generate 'rel_coinduct' theorem for codatatypes

--- src/HOL/BNF_FP_Base.thy
+++ src/HOL/BNF_FP_Base.thy
@@ -16,6 +16,9 @@ begin
 lemma mp_conj: "(P \<longrightarrow> Q) \<and> R \<Longrightarrow> P \<Longrightarrow> R \<and> Q"
 by auto
 
+lemma predicate2D_conj: "(P \<le> Q) \<and> R \<Longrightarrow> P x y \<Longrightarrow> R \<and> Q x y"
+  by auto
+
 lemma eq_sym_Unity_conv: "(x = (() = ())) = x"
 by blast
 
