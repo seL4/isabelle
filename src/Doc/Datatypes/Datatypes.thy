3d51a59e56fcd91cf401c5b507ef930182d5b20e
blanchet <none@none>
2014-07-15 00:35:07 +0200

    took out 'rel_cases' for now because of failing tactic

--- src/Doc/Datatypes/Datatypes.thy
+++ src/Doc/Datatypes/Datatypes.thy
@@ -886,8 +886,9 @@ a set function, the map function, or the relator:
 @{thm list.rel_intros(1)[no_vars]} \\
 @{thm list.rel_intros(2)[no_vars]}
 
-\item[@{text "t."}\hthm{rel_cases} @{text "[consumes 1, case_names t\<^sub>1 \<dots> t\<^sub>m, cases pred]"}\rm:] ~ \\
-@{thm list.rel_cases[no_vars]}
+% FIXME (and add @ before antiquotation below)
+%\item[@{text "t."}\hthm{rel_cases} @{text "[consumes 1, case_names t\<^sub>1 \<dots> t\<^sub>m, cases pred]"}\rm:] ~ \\
+%{thm list.rel_cases[no_vars]}
 
 \end{description}
 \end{indentblock}
