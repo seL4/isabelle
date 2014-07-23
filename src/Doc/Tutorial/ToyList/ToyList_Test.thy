ee755a19cc6f5b55893e791f3b97e580bee8f93e
wenzelm <none@none>
2014-07-23 21:02:45 +0200

    more official Thy_Info.script_thy;

--- src/Doc/Tutorial/ToyList/ToyList_Test.thy
+++ src/Doc/Tutorial/ToyList/ToyList_Test.thy
@@ -2,14 +2,11 @@ theory ToyList_Test
 imports Datatype
 begin
 
-ML {*  (* FIXME somewhat non-standard, fragile *)
-  let
-    val texts =
-      map (File.read o Path.append (Resources.master_directory @{theory}) o Path.explode)
-        ["ToyList1.txt", "ToyList2.txt"];
-    val trs = Outer_Syntax.parse Position.start (implode texts);
-    val end_state = fold (Toplevel.command_exception false) trs Toplevel.toplevel;
-  in @{assert} (Toplevel.is_toplevel end_state) end;
+ML {*
+  map (File.read o Path.append (Resources.master_directory @{theory}) o Path.explode)
+    ["ToyList1.txt", "ToyList2.txt"]
+  |> implode
+  |> Thy_Info.script_thy Position.start
 *}
 
 end
