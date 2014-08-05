ea6dbc362744f11950515aa7c601ea5e8733380e
blanchet <none@none>
2014-08-05 15:54:47 +0200

    correctly interpret arithmetic types

--- src/HOL/TPTP/TPTP_Interpret.thy
+++ src/HOL/TPTP/TPTP_Interpret.thy
@@ -6,12 +6,12 @@ interpreting them as HOL terms (i.e. importing types and type-checking the terms
 *)
 
 theory TPTP_Interpret
-imports Main TPTP_Parser
+imports Complex_Main TPTP_Parser
 keywords "import_tptp" :: thy_decl
 begin
 
-typedecl "ind"
+typedecl ind
 
-ML_file  "TPTP_Parser/tptp_interpret.ML"
+ML_file "TPTP_Parser/tptp_interpret.ML"
 
 end
