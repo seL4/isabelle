14f437f55c750d4d6e764f3e55318d1c18565b91
Andreas Lochbihler <none@none>
2014-08-25 09:40:50 +0200

    add testing framework for generated code

---
+++ src/HOL/Codegenerator_Test/Code_Test_SMLNJ.thy
@@ -0,0 +1,13 @@
+(*  Title:      Code_Test_SMLNJ.thy
+    Author:     Andreas Lochbihler, ETH Zurich
+
+Test case for test_code on SMLNJ
+*)
+
+theory Code_Test_SMLNJ imports Code_Test begin
+
+test_code "14 + 7 * -12 = (140 div -2 :: integer)" in SMLNJ
+
+eval_term "14 + 7 * -12 :: integer" in SMLNJ
+
+end
