8666297f8fbb2d8aa4ed2342975dae36e919260f
steckerm <none@none>
2014-09-02 16:38:26 +0200

    Some work on the new waldmeister integration

--- src/HOL/ATP.thy
+++ src/HOL/ATP.thy
@@ -135,7 +135,7 @@ subsection {* Waldmeister helpers *}
 
 (* Has all needed simplification lemmas for logic.
    "HOL.simp_thms(35-42)" are about \<exists> and \<forall>. These lemmas are left out for now. *)
-lemmas waldmeister_fol = simp_thms(1-34) disj_absorb disj_left_absorb conj_absorb conj_left_absorb
+lemmas waldmeister_fol = simp_thms(1,2,4-8,11-34) disj_absorb disj_left_absorb conj_absorb conj_left_absorb
   eq_ac disj_comms disj_assoc conj_comms conj_assoc
 
 
