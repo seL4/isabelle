e7ec95afb3afeb5179d907ef8391d0b248321909
David Greenaway <david.greenaway@nicta.com.au>
2014-09-12 11:09:14 +1000

    Add "skip_proofs" UI to top of theories panel.
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- src/Tools/jEdit/src/theories_dockable.scala
+++ src/Tools/jEdit/src/theories_dockable.scala
@@ -39,9 +39,11 @@ class Theories_Dockable(view: View, position: String) extends Dockable(view, pos
   continuous_checking.focusable = false
 
   private val logic = JEdit_Sessions.logic_selector(PIDE.options, standalone = true)
+  private val skip_proofs = new JEdit_Options.skip_proofs.GUI
+  skip_proofs.focusable = false
 
   private val controls =
-    Wrap_Panel(List(purge, continuous_checking, session_phase, logic))
+    Wrap_Panel(List(purge, skip_proofs, continuous_checking, session_phase, logic))
 
   add(controls.peer, BorderLayout.NORTH)
 
@@ -58,6 +60,7 @@ class Theories_Dockable(view: View, position: String) extends Dockable(view, pos
 
       case _: Session.Global_Options =>
         GUI_Thread.later {
+          skip_proofs.load()
           continuous_checking.load()
           logic.load()
           theories.refresh()
