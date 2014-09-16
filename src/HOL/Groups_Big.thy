8a25bc558641cbe85adce7848fe8dc805b619b74
nipkow <none@none>
2014-09-16 18:42:33 +0200

    added lemma

--- src/HOL/Groups_Big.thy
+++ src/HOL/Groups_Big.thy
@@ -948,6 +948,9 @@ apply (erule finite_induct)
 apply (auto simp add: algebra_simps)
 done
 
+lemma setsum_Suc: "setsum (%x. Suc(f x)) A = setsum f A + card A"
+using setsum.distrib[of f "%_. 1" A] by(simp)
+
 lemma setsum_bounded:
   assumes le: "\<And>i. i\<in>A \<Longrightarrow> f i \<le> (K::'a::{semiring_1, ordered_ab_semigroup_add})"
   shows "setsum f A \<le> of_nat (card A) * K"
