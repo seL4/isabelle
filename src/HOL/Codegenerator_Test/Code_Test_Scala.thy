31e1aaa1a6bf920c1d441cf07da653fbc8bcd504
haftmann <none@none>
2014-10-02 17:51:04 +0200

    accomplish potentially case-insenstive file systems for Scala
    --HG--
    extra : rebase_source : 01775a1b8de5b9b7744107f7a87f3c79d88af6d0

--- src/HOL/Codegenerator_Test/Code_Test_Scala.thy
+++ src/HOL/Codegenerator_Test/Code_Test_Scala.thy
@@ -6,6 +6,8 @@ Test case for test_code on Scala
 
 theory Code_Test_Scala imports Code_Test begin 
 
+declare [[scala_case_insensitive]]
+
 test_code "14 + 7 * -12 = (140 div -2 :: integer)" in Scala
 
 value [Scala] "14 + 7 * -12 :: integer"
