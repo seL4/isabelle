6d8451cfc42b7b249c92f64f6f1e2f1d8823347c
wenzelm <none@none>
2014-12-20 00:05:20 +0100

    afford full test, with slightly improved scheduling order;

--- src/HOL/Nominal/Examples/Nominal_Examples.thy
+++
@@ -1,9 +0,0 @@
-(*  Author:  Christian Urban TU Muenchen *)
-
-section {* Various examples involving nominal datatypes. *}
-
-theory Nominal_Examples
-imports Nominal_Examples_Base Class3
-begin
-
-end
