d9fa5894d2da6551c61d5ffd7377eed6a450ff29
nipkow <none@none>
2015-05-01 08:45:30 +0200

    new simp rule

--- src/HOL/Set.thy
+++ src/HOL/Set.thy
@@ -1623,6 +1623,9 @@ text {* \medskip @{text Pow} *}
 lemma Pow_empty [simp]: "Pow {} = {{}}"
   by (auto simp add: Pow_def)
 
+lemma Pow_singleton_iff [simp]: "Pow X = {Y} \<longleftrightarrow> X = {} \<and> Y = {}"
+by blast
+
 lemma Pow_insert: "Pow (insert a A) = Pow A \<union> (insert a ` Pow A)"
   by (blast intro: image_eqI [where ?x = "u - {a}" for u])
 
