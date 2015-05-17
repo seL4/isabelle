2a31bd88fb5098b8e4e25916e5e9a91bfb26ffb8
wenzelm <none@none>
2015-05-17 23:03:49 +0200

    added Eisbach manual, using version 8845c4cb28b6 of its Bitbucket repository;

---
+++ src/Doc/Eisbach/Base.thy
@@ -0,0 +1,39 @@
+section \<open>Basic setup that is not included in the document\<close>
+
+theory Base
+imports Main
+begin
+
+ML_file "~~/src/Doc/antiquote_setup.ML"
+
+ML\<open>
+fun get_split_rule ctxt target =
+  let
+    val (head, args) = strip_comb (Envir.eta_contract target);
+    val (const_name, _) = dest_Const head;
+    val const_name_components = Long_Name.explode const_name;
+
+    val _ =
+      if String.isPrefix "case_" (List.last const_name_components) then ()
+      else raise TERM ("Not a case statement", [target]);
+
+    val type_name = Long_Name.implode (rev (tl (rev const_name_components)));
+    val split = Proof_Context.get_thm ctxt (type_name ^ ".split");
+    val vars = Term.add_vars (Thm.prop_of split) [];
+
+    val datatype_name = nth (rev const_name_components) 1;
+
+    fun is_datatype (Type (a, _)) = Long_Name.base_name a = Long_Name.base_name datatype_name
+      | is_datatype _ = false;
+
+    val datatype_var =
+      (case find_first (fn (_, T') => is_datatype T') vars of
+        SOME var => Thm.cterm_of ctxt (Term.Var var)
+      | NONE => error ("Couldn't find datatype in thm: " ^ datatype_name));
+  in
+    SOME (Drule.cterm_instantiate [(datatype_var, Thm.cterm_of ctxt (List.last args))] split)
+  end
+  handle TERM _ => NONE;
+\<close>
+
+end
---
+++ src/Doc/Eisbach/Preface.thy
@@ -0,0 +1,35 @@
+(*:wrap=hard:maxLineLen=78:*)
+
+theory Preface
+imports Base "../Eisbach_Tools"
+begin
+
+text \<open>
+  \emph{Eisbach} is a collection of tools which form the basis for defining
+  new proof methods in Isabelle/Isar~@{cite "Wenzel-PhD"}. It can be thought
+  of as a ``proof method language'', but is more precisely an infrastructure
+  for defining new proof methods out of existing ones.
+
+  The core functionality of Eisbach is provided by the Isar @{command method}
+  command. Here users may define new methods by combining existing ones with
+  the usual Isar syntax These methods can be abstracted over terms, facts and
+  other methods, as one might expect in any higher-order functional language.
+
+  Additional functionality is provided by extending the space of methods and
+  attributes. The new @{method match} method allows for explicit control-flow,
+  by taking a match target and a list of pattern-method pairs. By using the
+  functionality provided by Eisbach, additional support methods can be easily
+  written. For example, the @{method catch} method, which provides basic
+  try-catch functionality, only requires a few lines of ML.
+
+  Eisbach is meant to allow users to write automation using only Isar syntax.
+  Traditionally proof methods have been written in Isabelle/ML, which poses a
+  high barrier-to-entry for many users.
+
+  \medskip This manual is written for users familiar with Isabelle/Isar, but
+  not necessarily Isabelle/ML. It covers the usage of the @{command method} as
+  well as the @{method match} method, as well as discussing their integration
+  with existing Isar concepts such as @{command named_theorems}.
+\<close>
+
+end
