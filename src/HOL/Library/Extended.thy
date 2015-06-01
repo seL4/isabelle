975b88893cf611abde98fae17a74d72009887c15
haftmann <none@none>
2015-06-01 18:59:20 +0200

    correct sort constraints for abbreviations in type classes * * * yet another bunch of corrections

--- src/HOL/Library/Extended.thy
+++ src/HOL/Library/Extended.thy
@@ -25,7 +25,7 @@ fun less_eq_extended :: "'a extended \<Rightarrow> 'a extended \<Rightarrow> boo
 case_of_simps less_eq_extended_case: less_eq_extended.simps
 
 definition less_extended :: "'a extended \<Rightarrow> 'a extended \<Rightarrow> bool" where
-"((x::'a extended) < y) = (x \<le> y & \<not> x \<ge> y)"
+"((x::'a extended) < y) = (x \<le> y & \<not> y \<le> x)"
 
 instance
   by intro_classes (auto simp: less_extended_def less_eq_extended_case split: extended.splits)
