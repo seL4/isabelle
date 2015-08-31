f8b6675d06650072b905bb8aeb3c2a36f7e0aab8
wenzelm <none@none>
2015-08-31 19:34:26 +0200

    misc tuning and modernization;

--- src/HOL/Option.thy
+++ src/HOL/Option.thy
@@ -17,89 +17,86 @@ datatype_compat option
 lemma [case_names None Some, cases type: option]:
   -- \<open>for backward compatibility -- names of variables differ\<close>
   "(y = None \<Longrightarrow> P) \<Longrightarrow> (\<And>a. y = Some a \<Longrightarrow> P) \<Longrightarrow> P"
-by (rule option.exhaust)
+  by (rule option.exhaust)
 
 lemma [case_names None Some, induct type: option]:
   -- \<open>for backward compatibility -- names of variables differ\<close>
   "P None \<Longrightarrow> (\<And>option. P (Some option)) \<Longrightarrow> P option"
-by (rule option.induct)
+  by (rule option.induct)
 
 text \<open>Compatibility:\<close>
-
 setup \<open>Sign.mandatory_path "option"\<close>
-
 lemmas inducts = option.induct
 lemmas cases = option.case
-
 setup \<open>Sign.parent_path\<close>
 
-lemma not_None_eq [iff]: "(x ~= None) = (EX y. x = Some y)"
+lemma not_None_eq [iff]: "x \<noteq> None \<longleftrightarrow> (\<exists>y. x = Some y)"
   by (induct x) auto
 
-lemma not_Some_eq [iff]: "(ALL y. x ~= Some y) = (x = None)"
+lemma not_Some_eq [iff]: "(\<forall>y. x \<noteq> Some y) \<longleftrightarrow> x = None"
   by (induct x) auto
 
-text\<open>Although it may appear that both of these equalities are helpful
+text \<open>Although it may appear that both of these equalities are helpful
 only when applied to assumptions, in practice it seems better to give
 them the uniform iff attribute.\<close>
 
 lemma inj_Some [simp]: "inj_on Some A"
-by (rule inj_onI) simp
+  by (rule inj_onI) simp
 
 lemma case_optionE:
-  assumes c: "(case x of None => P | Some y => Q y)"
+  assumes c: "(case x of None \<Rightarrow> P | Some y \<Rightarrow> Q y)"
   obtains
     (None) "x = None" and P
   | (Some) y where "x = Some y" and "Q y"
   using c by (cases x) simp_all
 
 lemma split_option_all: "(\<forall>x. P x) \<longleftrightarrow> P None \<and> (\<forall>x. P (Some x))"
-by (auto intro: option.induct)
+  by (auto intro: option.induct)
 
 lemma split_option_ex: "(\<exists>x. P x) \<longleftrightarrow> P None \<or> (\<exists>x. P (Some x))"
-using split_option_all[of "\<lambda>x. \<not>P x"] by blast
+  using split_option_all[of "\<lambda>x. \<not> P x"] by blast
 
 lemma UNIV_option_conv: "UNIV = insert None (range Some)"
-by(auto intro: classical)
+  by (auto intro: classical)
 
 lemma rel_option_None1 [simp]: "rel_option P None x \<longleftrightarrow> x = None"
-by(cases x) simp_all
+  by (cases x) simp_all
 
 lemma rel_option_None2 [simp]: "rel_option P x None \<longleftrightarrow> x = None"
-by(cases x) simp_all
+  by (cases x) simp_all
 
-lemma rel_option_inf: "inf (rel_option A) (rel_option B) = rel_option (inf A B)" (is "?lhs = ?rhs")
-proof(rule antisym)
-  show "?lhs \<le> ?rhs" by(auto elim!: option.rel_cases)
-qed(auto elim: option.rel_mono_strong)
+lemma rel_option_inf: "inf (rel_option A) (rel_option B) = rel_option (inf A B)"
+  (is "?lhs = ?rhs")
+proof (rule antisym)
+  show "?lhs \<le> ?rhs" by (auto elim: option.rel_cases)
+  show "?rhs \<le> ?lhs" by (auto elim: option.rel_mono_strong)
+qed
 
 lemma rel_option_reflI:
   "(\<And>x. x \<in> set_option y \<Longrightarrow> P x x) \<Longrightarrow> rel_option P y y"
-by(cases y) auto
+  by (cases y) auto
 
 
 subsubsection \<open>Operations\<close>
 
-lemma ospec [dest]: "(ALL x:set_option A. P x) ==> A = Some x ==> P x"
+lemma ospec [dest]: "(\<forall>x\<in>set_option A. P x) \<Longrightarrow> A = Some x \<Longrightarrow> P x"
   by simp
 
 setup \<open>map_theory_claset (fn ctxt => ctxt addSD2 ("ospec", @{thm ospec}))\<close>
 
-lemma elem_set [iff]: "(x : set_option xo) = (xo = Some x)"
+lemma elem_set [iff]: "(x \<in> set_option xo) = (xo = Some x)"
   by (cases xo) auto
 
 lemma set_empty_eq [simp]: "(set_option xo = {}) = (xo = None)"
   by (cases xo) auto
 
-lemma map_option_case: "map_option f y = (case y of None => None | Some x => Some (f x))"
+lemma map_option_case: "map_option f y = (case y of None \<Rightarrow> None | Some x \<Rightarrow> Some (f x))"
   by (auto split: option.split)
 
-lemma map_option_is_None [iff]:
-    "(map_option f opt = None) = (opt = None)"
+lemma map_option_is_None [iff]: "(map_option f opt = None) = (opt = None)"
   by (simp add: map_option_case split add: option.split)
 
-lemma map_option_eq_Some [iff]:
-    "(map_option f xo = Some y) = (EX z. xo = Some z & f z = y)"
+lemma map_option_eq_Some [iff]: "(map_option f xo = Some y) = (\<exists>z. xo = Some z \<and> f z = y)"
   by (simp add: map_option_case split add: option.split)
 
 lemma map_option_o_case_sum [simp]:
@@ -107,121 +104,114 @@ lemma map_option_o_case_sum [simp]:
   by (rule o_case_sum)
 
 lemma map_option_cong: "x = y \<Longrightarrow> (\<And>a. y = Some a \<Longrightarrow> f a = g a) \<Longrightarrow> map_option f x = map_option g y"
-by (cases x) auto
+  by (cases x) auto
 
 functor map_option: map_option
-by(simp_all add: option.map_comp fun_eq_iff option.map_id)
+  by (simp_all add: option.map_comp fun_eq_iff option.map_id)
 
-lemma case_map_option [simp]:
-  "case_option g h (map_option f x) = case_option g (h \<circ> f) x"
+lemma case_map_option [simp]: "case_option g h (map_option f x) = case_option g (h \<circ> f) x"
   by (cases x) simp_all
 
 lemma rel_option_iff:
   "rel_option R x y = (case (x, y) of (None, None) \<Rightarrow> True
     | (Some x, Some y) \<Rightarrow> R x y
     | _ \<Rightarrow> False)"
-by (auto split: prod.split option.split)
+  by (auto split: prod.split option.split)
 
 definition is_none :: "'a option \<Rightarrow> bool"
-where [code_post]: "is_none x \<longleftrightarrow> x = None"
+  where [code_post]: "is_none x \<longleftrightarrow> x = None"
 
 lemma is_none_simps [simp]:
   "is_none None"
   "\<not> is_none (Some x)"
-by(simp_all add: is_none_def)
+  by (simp_all add: is_none_def)
 
 lemma is_none_code [code]:
   "is_none None = True"
   "is_none (Some x) = False"
-by simp_all
+  by simp_all
 
 lemma rel_option_unfold:
   "rel_option R x y \<longleftrightarrow>
    (is_none x \<longleftrightarrow> is_none y) \<and> (\<not> is_none x \<longrightarrow> \<not> is_none y \<longrightarrow> R (the x) (the y))"
-by(simp add: rel_option_iff split: option.split)
+  by (simp add: rel_option_iff split: option.split)
 
 lemma rel_optionI:
   "\<lbrakk> is_none x \<longleftrightarrow> is_none y; \<lbrakk> \<not> is_none x; \<not> is_none y \<rbrakk> \<Longrightarrow> P (the x) (the y) \<rbrakk>
   \<Longrightarrow> rel_option P x y"
-by(simp add: rel_option_unfold)
+  by (simp add: rel_option_unfold)
 
 lemma is_none_map_option [simp]: "is_none (map_option f x) \<longleftrightarrow> is_none x"
-by(simp add: is_none_def)
+  by (simp add: is_none_def)
 
 lemma the_map_option: "\<not> is_none x \<Longrightarrow> the (map_option f x) = f (the x)"
-by(clarsimp simp add: is_none_def)
+  by (auto simp add: is_none_def)
 
 
-primrec bind :: "'a option \<Rightarrow> ('a \<Rightarrow> 'b option) \<Rightarrow> 'b option" where
-bind_lzero: "bind None f = None" |
-bind_lunit: "bind (Some x) f = f x"
+primrec bind :: "'a option \<Rightarrow> ('a \<Rightarrow> 'b option) \<Rightarrow> 'b option"
+where
+  bind_lzero: "bind None f = None"
+| bind_lunit: "bind (Some x) f = f x"
 
 lemma is_none_bind: "is_none (bind f g) \<longleftrightarrow> is_none f \<or> is_none (g (the f))"
-by(cases f) simp_all
+  by (cases f) simp_all
 
 lemma bind_runit[simp]: "bind x Some = x"
-by (cases x) auto
+  by (cases x) auto
 
 lemma bind_assoc[simp]: "bind (bind x f) g = bind x (\<lambda>y. bind (f y) g)"
-by (cases x) auto
+  by (cases x) auto
 
 lemma bind_rzero[simp]: "bind x (\<lambda>x. None) = None"
-by (cases x) auto
+  by (cases x) auto
 
 lemma bind_cong: "x = y \<Longrightarrow> (\<And>a. y = Some a \<Longrightarrow> f a = g a) \<Longrightarrow> bind x f = bind y g"
-by (cases x) auto
+  by (cases x) auto
 
-lemma bind_split: "P (bind m f) 
-  \<longleftrightarrow> (m = None \<longrightarrow> P None) \<and> (\<forall>v. m=Some v \<longrightarrow> P (f v))"
-    by (cases m) auto
+lemma bind_split: "P (bind m f) \<longleftrightarrow> (m = None \<longrightarrow> P None) \<and> (\<forall>v. m = Some v \<longrightarrow> P (f v))"
+  by (cases m) auto
 
-lemma bind_split_asm: "P (bind m f) = (\<not>(
-    m=None \<and> \<not>P None 
-  \<or> (\<exists>x. m=Some x \<and> \<not>P (f x))))"
+lemma bind_split_asm: "P (bind m f) \<longleftrightarrow> \<not> (m = None \<and> \<not> P None \<or> (\<exists>x. m = Some x \<and> \<not> P (f x)))"
   by (cases m) auto
 
 lemmas bind_splits = bind_split bind_split_asm
 
 lemma bind_eq_Some_conv: "bind f g = Some x \<longleftrightarrow> (\<exists>y. f = Some y \<and> g y = Some x)"
-by(cases f) simp_all
+  by (cases f) simp_all
 
 lemma map_option_bind: "map_option f (bind x g) = bind x (map_option f \<circ> g)"
-by(cases x) simp_all
+  by (cases x) simp_all
 
 lemma bind_option_cong:
   "\<lbrakk> x = y; \<And>z. z \<in> set_option y \<Longrightarrow> f z = g z \<rbrakk> \<Longrightarrow> bind x f = bind y g"
-by(cases y) simp_all
+  by (cases y) simp_all
 
 lemma bind_option_cong_simp:
   "\<lbrakk> x = y; \<And>z. z \<in> set_option y =simp=> f z = g z \<rbrakk> \<Longrightarrow> bind x f = bind y g"
-unfolding simp_implies_def by(rule bind_option_cong)
+  unfolding simp_implies_def by (rule bind_option_cong)
 
-lemma bind_option_cong_code: "x = y \<Longrightarrow> bind x f = bind y f" by simp
+lemma bind_option_cong_code: "x = y \<Longrightarrow> bind x f = bind y f"
+  by simp
 setup \<open>Code_Simp.map_ss (Simplifier.add_cong @{thm bind_option_cong_code})\<close>
 
 
 definition these :: "'a option set \<Rightarrow> 'a set"
-where
-  "these A = the ` {x \<in> A. x \<noteq> None}"
+  where "these A = the ` {x \<in> A. x \<noteq> None}"
 
-lemma these_empty [simp]:
-  "these {} = {}"
+lemma these_empty [simp]: "these {} = {}"
   by (simp add: these_def)
 
-lemma these_insert_None [simp]:
-  "these (insert None A) = these A"
+lemma these_insert_None [simp]: "these (insert None A) = these A"
   by (auto simp add: these_def)
 
-lemma these_insert_Some [simp]:
-  "these (insert (Some x) A) = insert x (these A)"
+lemma these_insert_Some [simp]: "these (insert (Some x) A) = insert x (these A)"
 proof -
   have "{y \<in> insert (Some x) A. y \<noteq> None} = insert (Some x) {y \<in> A. y \<noteq> None}"
     by auto
   then show ?thesis by (simp add: these_def)
 qed
 
-lemma in_these_eq:
-  "x \<in> these A \<longleftrightarrow> Some x \<in> A"
+lemma in_these_eq: "x \<in> these A \<longleftrightarrow> Some x \<in> A"
 proof
   assume "Some x \<in> A"
   then obtain B where "A = insert (Some x) B" by auto
@@ -231,20 +221,16 @@ next
   then show "Some x \<in> A" by (auto simp add: these_def)
 qed
 
-lemma these_image_Some_eq [simp]:
-  "these (Some ` A) = A"
+lemma these_image_Some_eq [simp]: "these (Some ` A) = A"
   by (auto simp add: these_def intro!: image_eqI)
 
-lemma Some_image_these_eq:
-  "Some ` these A = {x\<in>A. x \<noteq> None}"
+lemma Some_image_these_eq: "Some ` these A = {x\<in>A. x \<noteq> None}"
   by (auto simp add: these_def image_image intro!: image_eqI)
 
-lemma these_empty_eq:
-  "these B = {} \<longleftrightarrow> B = {} \<or> B = {None}"
+lemma these_empty_eq: "these B = {} \<longleftrightarrow> B = {} \<or> B = {None}"
   by (auto simp add: these_def)
 
-lemma these_not_empty_eq:
-  "these B \<noteq> {} \<longleftrightarrow> B \<noteq> {} \<and> B \<noteq> {None}"
+lemma these_not_empty_eq: "these B \<noteq> {} \<longleftrightarrow> B \<noteq> {} \<and> B \<noteq> {None}"
   by (auto simp add: these_empty_eq)
 
 hide_const (open) bind these
@@ -255,6 +241,7 @@ subsection \<open>Transfer rules for the Transfer package\<close>
 
 context
 begin
+
 interpretation lifting_syntax .
 
 lemma option_bind_transfer [transfer_rule]:
@@ -264,7 +251,7 @@ lemma option_bind_transfer [transfer_rule]:
 
 lemma pred_option_parametric [transfer_rule]:
   "((A ===> op =) ===> rel_option A ===> op =) pred_option pred_option"
-by(rule rel_funI)+(auto simp add: rel_option_unfold is_none_def dest: rel_funD)
+  by (rule rel_funI)+ (auto simp add: rel_option_unfold is_none_def dest: rel_funD)
 
 end
 
@@ -276,7 +263,7 @@ lemma finite_option_UNIV [simp]:
   by (auto simp add: UNIV_option_conv elim: finite_imageD intro: inj_Some)
 
 instance option :: (finite) finite
-  by default (simp add: UNIV_option_conv)
+  by standard (simp add: UNIV_option_conv)
 
 
 subsubsection \<open>Code generator setup\<close>
