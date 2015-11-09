04243d3be8a0e0c5de95d432bf86fe8f7e206fd3
wenzelm <none@none>
2015-11-09 13:49:29 +0100

    suppress already persistent state output as well;

--- src/Tools/jEdit/src/rendering.scala
+++ src/Tools/jEdit/src/rendering.scala
@@ -678,7 +678,7 @@ class Rendering private(val snapshot: Document.Snapshot, val options: Options)
     val (states, other) =
       results.iterator.map(_._2).filterNot(Protocol.is_result(_)).toList
         .partition(Protocol.is_state(_))
-    states ::: other
+    if (options.bool("editor_output_state")) states ::: other else other
   }
 
 
