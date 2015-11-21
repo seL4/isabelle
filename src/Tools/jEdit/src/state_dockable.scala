082896854b4d896d05bf5dc14d755258d47ffd44
wenzelm <none@none>
2015-11-21 16:07:29 +0100

    clarified default;

--- src/Tools/jEdit/src/state_dockable.scala
+++ src/Tools/jEdit/src/state_dockable.scala
@@ -57,7 +57,7 @@ class State_Dockable(view: View, position: String) extends Dockable(view, positi
 
   /* update */
 
-  private var do_update = true
+  private var do_update = false
 
   private def maybe_update(): Unit = GUI_Thread.require { if (do_update) update() }
 
