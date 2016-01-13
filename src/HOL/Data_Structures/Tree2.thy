30b4a859c735635c8ef0d8f239c091446227d131
nipkow <none@none>
2016-01-13 09:38:16 +0100

    tuned layout

--- src/HOL/Data_Structures/Tree2.thy
+++ src/HOL/Data_Structures/Tree2.thy
@@ -4,7 +4,7 @@ begin
 
 datatype ('a,'b) tree =
   Leaf ("\<langle>\<rangle>") |
-  Node 'b "('a,'b)tree" 'a "('a,'b) tree" ("\<langle>_, _, _, _\<rangle>")
+  Node 'b "('a,'b)tree" 'a "('a,'b) tree" ("(1\<langle>_,/ _,/ _,/ _\<rangle>)")
 
 fun inorder :: "('a,'b)tree \<Rightarrow> 'a list" where
 "inorder Leaf = []" |
