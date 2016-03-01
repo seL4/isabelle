f9dc040d72c9a52182421e25b8377d55cac9398a
wenzelm <none@none>
2016-03-01 10:32:55 +0100

    missing file;

---
+++ src/HOL/Nonstandard_Analysis/Nonstandard_Analysis.thy
@@ -0,0 +1,13 @@
+(*  Title:      HOL/Nonstandard_Analysis/Nonstandard_Analysis.thy
+    Author:     Jacques D. Fleuriot, University of Cambridge
+    Author:     Lawrence C Paulson, University of Cambridge
+    Author:     Brian Huffman
+
+Nonstandard analysis.
+*)
+
+theory Nonstandard_Analysis
+imports Hypercomplex
+begin
+
+end
