4f908cd22cd821aa10e4448478e220737cdd1dca
nipkow <none@none>
2016-05-23 22:43:11 +0200

    renamed prefix* in Library/Sublist

--- src/HOL/Library/Prefix_Order.thy
+++ src/HOL/Library/Prefix_Order.thy
@@ -11,7 +11,7 @@ begin
 instantiation list :: (type) order
 begin
 
-definition "(xs::'a list) \<le> ys \<equiv> prefixeq xs ys"
+definition "(xs::'a list) \<le> ys \<equiv> prefix xs ys"
 definition "(xs::'a list) < ys \<equiv> xs \<le> ys \<and> \<not> (ys \<le> xs)"
 
 instance
@@ -19,23 +19,26 @@ instance
 
 end
 
-lemmas prefixI [intro?] = prefixeqI [folded less_eq_list_def]
-lemmas prefixE [elim?] = prefixeqE [folded less_eq_list_def]
-lemmas strict_prefixI' [intro?] = prefixI' [folded less_list_def]
-lemmas strict_prefixE' [elim?] = prefixE' [folded less_list_def]
-lemmas strict_prefixI [intro?] = prefixI [folded less_list_def]
-lemmas strict_prefixE [elim?] = prefixE [folded less_list_def]
-lemmas Nil_prefix [iff] = Nil_prefixeq [folded less_eq_list_def]
-lemmas prefix_Nil [simp] = prefixeq_Nil [folded less_eq_list_def]
-lemmas prefix_snoc [simp] = prefixeq_snoc [folded less_eq_list_def]
-lemmas Cons_prefix_Cons [simp] = Cons_prefixeq_Cons [folded less_eq_list_def]
-lemmas same_prefix_prefix [simp] = same_prefixeq_prefixeq [folded less_eq_list_def]
-lemmas same_prefix_nil [iff] = same_prefixeq_nil [folded less_eq_list_def]
-lemmas prefix_prefix [simp] = prefixeq_prefixeq [folded less_eq_list_def]
-lemmas prefix_Cons = prefixeq_Cons [folded less_eq_list_def]
-lemmas prefix_length_le = prefixeq_length_le [folded less_eq_list_def]
-lemmas strict_prefix_simps [simp, code] = prefix_simps [folded less_list_def]
+lemma less_list_def': "(xs::'a list) < ys \<longleftrightarrow> strict_prefix xs ys"
+by (simp add: less_eq_list_def order.strict_iff_order prefix_order.less_le)
+
+lemmas prefixI [intro?] = prefixI [folded less_eq_list_def]
+lemmas prefixE [elim?] = prefixE [folded less_eq_list_def]
+lemmas strict_prefixI' [intro?] = strict_prefixI' [folded less_list_def']
+lemmas strict_prefixE' [elim?] = strict_prefixE' [folded less_list_def']
+lemmas strict_prefixI [intro?] = strict_prefixI [folded less_list_def']
+lemmas strict_prefixE [elim?] = strict_prefixE [folded less_list_def']
+lemmas Nil_prefix [iff] = Nil_prefix [folded less_eq_list_def]
+lemmas prefix_Nil [simp] = prefix_Nil [folded less_eq_list_def]
+lemmas prefix_snoc [simp] = prefix_snoc [folded less_eq_list_def]
+lemmas Cons_prefix_Cons [simp] = Cons_prefix_Cons [folded less_eq_list_def]
+lemmas same_prefix_prefix [simp] = same_prefix_prefix [folded less_eq_list_def]
+lemmas same_prefix_nil [iff] = same_prefix_nil [folded less_eq_list_def]
+lemmas prefix_prefix [simp] = prefix_prefix [folded less_eq_list_def]
+lemmas prefix_Cons = prefix_Cons [folded less_eq_list_def]
+lemmas prefix_length_le = prefix_length_le [folded less_eq_list_def]
+lemmas strict_prefix_simps [simp, code] = strict_prefix_simps [folded less_list_def']
 lemmas not_prefix_induct [consumes 1, case_names Nil Neq Eq] =
-  not_prefixeq_induct [folded less_eq_list_def]
+  not_prefix_induct [folded less_eq_list_def]
 
 end
