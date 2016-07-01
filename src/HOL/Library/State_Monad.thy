8486ea1828f73c8d7dbdd8af45bafd429a2ec951
wenzelm <none@none>
2016-07-01 16:52:54 +0200

    clarified;

--- src/HOL/Library/State_Monad.thy
+++ src/HOL/Library/State_Monad.thy
@@ -145,7 +145,7 @@ translations
   "_sdo_block (_sdo_final e)" => "e"
 
 text \<open>
-  For an example, see @{file "~~/src/HOL/Proofs/Extraction/Higman.thy"}.
+  For an example, see @{file "~~/src/HOL/Proofs/Extraction/Higman_Extraction.thy"}.
 \<close>
 
 end
