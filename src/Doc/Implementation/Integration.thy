666cf920f4006cb01035c8a8ae28ce79cc6d757c
wenzelm <none@none>
2016-08-12 11:54:36 +0200

    liberal name as in document antiquotations;

--- src/Doc/Implementation/Integration.thy
+++ src/Doc/Implementation/Integration.thy
@@ -132,7 +132,7 @@ text %mlref \<open>
 \<close>
 
 text %mlex \<open>
-  The file @{"file" "~~/src/HOL/ex/Commands.thy"} shows some example Isar
+  The file @{file "~~/src/HOL/ex/Commands.thy"} shows some example Isar
   command definitions, with the all-important theory header declarations for
   outer syntax keywords.
 \<close>
