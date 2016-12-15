cec9b97881e437d9ece4d47d2317ee057cdd1c69
wenzelm <none@none>
2016-12-15 22:22:45 +0100

    tuned signature;

--- src/HOL/Proofs/ex/Proof_Terms.thy
+++ src/HOL/Proofs/ex/Proof_Terms.thy
@@ -30,7 +30,7 @@ ML_val \<open>
   (*all theorems used in the graph of nested proofs*)
   val all_thms =
     Proofterm.fold_body_thms
-      (fn (name, _, _) => insert (op =) name) [body] [];
+      (fn {name, ...} => insert (op =) name) [body] [];
 \<close>
 
 text \<open>
