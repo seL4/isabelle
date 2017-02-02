2f27793482bfb0f285d5d93c0b6d3e003ce20579
nipkow <none@none>
2017-02-02 09:32:11 +0100

    tuned proof

--- src/HOL/Data_Structures/Leftist_Heap.thy
+++ src/HOL/Data_Structures/Leftist_Heap.thy
@@ -222,12 +222,13 @@ by(simp add: t_insert_def split: tree.split)
 lemma ld_ld_1_less:
   assumes "x > 0" "y > 0" shows "1 + log 2 x + log 2 y < 2 * log 2 (x+y)"
 proof -
-  have 1: "2*x*y < (x+y)^2" using assms
+  have "2 powr (1 + log 2 x + log 2 y) = 2*x*y"
+    using assms by(simp add: powr_add)
+  also have "\<dots> < (x+y)^2" using assms
     by(simp add: numeral_eq_Suc algebra_simps add_pos_pos)
-  show ?thesis
-    apply(rule powr_less_cancel_iff[of 2, THEN iffD1])
-     apply simp
-    using assms 1 by(simp add: powr_add log_powr[symmetric] powr_numeral)
+  also have "\<dots> = 2 powr (2 * log 2 (x+y))"
+    using assms by(simp add: powr_add log_powr[symmetric] powr_numeral)
+  finally show ?thesis by simp
 qed
 
 corollary t_del_min_log: assumes "ltree t"
