91b6b9d229d2fb2dc039cb3f3f35a87534a7ea4d
wenzelm <none@none>
2017-03-10 14:16:45 +0100

    default cygwin_root from Isabelle distribution;

--- src/Tools/VSCode/extension/src/extension.ts
+++ src/Tools/VSCode/extension/src/extension.ts
@@ -2,6 +2,7 @@
 
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as os from 'os';
 import * as decorations from './decorations';
 import { Decoration } from './decorations'
@@ -13,13 +14,11 @@ export function activate(context: vscode.ExtensionContext)
 {
   const is_windows = os.type().startsWith("Windows")
 
-  const cygwin_root = vscode.workspace.getConfiguration("isabelle").get<string>("cygwin_root");
-  const isabelle_home = vscode.workspace.getConfiguration("isabelle").get<string>("home");
-  const isabelle_args = vscode.workspace.getConfiguration("isabelle").get<Array<string>>("args");
+  const isabelle_home = vscode.workspace.getConfiguration("isabelle").get<string>("home")
+  const isabelle_args = vscode.workspace.getConfiguration("isabelle").get<Array<string>>("args")
+  const cygwin_root = vscode.workspace.getConfiguration("isabelle").get<string>("cygwin_root")
 
-  if (is_windows && cygwin_root === "")
-    vscode.window.showErrorMessage("Missing user settings: isabelle.cygwin_root")
-  else if (isabelle_home === "")
+  if (isabelle_home === "")
     vscode.window.showErrorMessage("Missing user settings: isabelle.home")
   else {
     const isabelle_tool = isabelle_home + "/bin/isabelle"
@@ -27,7 +26,9 @@ export function activate(context: vscode.ExtensionContext)
 
     const server_options: ServerOptions =
       is_windows ?
-        { command: cygwin_root + "/bin/bash",
+        { command:
+            (cygwin_root === "" ? path.join(isabelle_home, "contrib", "cygwin") : cygwin_root) +
+            "/bin/bash",
           args: ["-l", isabelle_tool, "vscode_server"].concat(standard_args, isabelle_args) } :
         { command: isabelle_tool,
           args: ["vscode_server"].concat(standard_args, isabelle_args) };
