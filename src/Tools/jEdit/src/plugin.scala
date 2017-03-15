ebcf478004ef24cdab2a68fe5c6da40bb7ce3001
wenzelm <none@none>
2017-03-15 17:24:48 +0100

    clarified message;

--- src/Tools/jEdit/src/plugin.scala
+++ src/Tools/jEdit/src/plugin.scala
@@ -192,9 +192,9 @@ class Plugin extends EBPlugin
 
   val session_phase_changed: Session.Phase => Unit =
   {
-    case Session.Terminated(_) =>
+    case Session.Terminated(rc) if rc != 0 =>
       GUI_Thread.later {
-        GUI.error_dialog(jEdit.getActiveView, "Prover process terminated",
+        GUI.error_dialog(jEdit.getActiveView, "Prover process terminated with error",
           "Isabelle Syslog", GUI.scrollable_text(session.syslog_content()))
       }
 
