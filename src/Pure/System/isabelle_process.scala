b1dfca8d998bc9a516a611dfcdf34997e83cf2c8
wenzelm <none@none>
2017-03-18 20:24:12 +0100

    more process arguments;

--- src/Pure/System/isabelle_process.scala
+++ src/Pure/System/isabelle_process.scala
@@ -7,6 +7,9 @@ Isabelle process wrapper.
 package isabelle
 
 
+import java.io.{File => JFile}
+
+
 object Isabelle_Process
 {
   def start(session: Session,
@@ -15,6 +18,9 @@ object Isabelle_Process
     args: List[String] = Nil,
     dirs: List[Path] = Nil,
     modes: List[String] = Nil,
+    cwd: JFile = null,
+    env: Map[String, String] = Isabelle_System.settings(),
+    tree: Option[Sessions.Tree] = None,
     store: Sessions.Store = Sessions.store(),
     phase_changed: Session.Phase => Unit = null)
   {
@@ -23,7 +29,8 @@ object Isabelle_Process
 
     session.start(receiver =>
       Isabelle_Process(options, logic = logic, args = args, dirs = dirs, modes = modes,
-        receiver = receiver, xml_cache = session.xml_cache, store = store))
+        cwd = cwd, env = env, receiver = receiver, xml_cache = session.xml_cache,
+        tree = tree, store = store))
   }
 
   def apply(
@@ -32,15 +39,18 @@ object Isabelle_Process
     args: List[String] = Nil,
     dirs: List[Path] = Nil,
     modes: List[String] = Nil,
+    cwd: JFile = null,
+    env: Map[String, String] = Isabelle_System.settings(),
     receiver: Prover.Receiver = Console.println(_),
     xml_cache: XML.Cache = new XML.Cache(),
+    tree: Option[Sessions.Tree] = None,
     store: Sessions.Store = Sessions.store()): Isabelle_Process =
   {
     val channel = System_Channel()
     val process =
       try {
-        ML_Process(options, logic = logic, args = args, dirs = dirs,
-          modes = modes, store = store, channel = Some(channel))
+        ML_Process(options, logic = logic, args = args, dirs = dirs, modes = modes,
+          cwd = cwd, env = env, tree = tree, store = store, channel = Some(channel))
       }
       catch { case exn @ ERROR(_) => channel.accepted(); throw exn }
     process.stdin.close
