d958606d263f77cafbb24d68e170c23b2ccd4504
wenzelm <none@none>
2017-04-04 21:37:26 +0200

    tuned signature;

--- src/Pure/Thy/present.scala
+++ src/Pure/Thy/present.scala
@@ -18,19 +18,19 @@ object Present
 
   def session_graph(
     parent_session: String,
-    parent_loaded: String => Boolean,
+    parent_loaded: Document.Node.Name => Boolean,
     deps: List[Thy_Info.Dep]): Graph_Display.Graph =
   {
     val parent_session_node =
       Graph_Display.Node("[" + parent_session + "]", "session." + parent_session)
 
     def node(name: Document.Node.Name): Graph_Display.Node =
-      if (parent_loaded(name.theory)) parent_session_node
+      if (parent_loaded(name)) parent_session_node
       else Graph_Display.Node(Long_Name.base_name(name.theory), "theory." + name.theory)
 
     (Graph_Display.empty_graph /: deps) {
       case (g, dep) =>
-        if (parent_loaded(dep.name.theory)) g
+        if (parent_loaded(dep.name)) g
         else {
           val a = node(dep.name)
           val bs = dep.header.imports.map({ case (name, _) => node(name) })
