c9f2b44a793bb3ac9c957e8188fba3310a4ab823
wenzelm <none@none>
2017-04-23 14:15:09 +0200

    added missing file (amending f533820e7248);

---
+++ src/HOL/Main.thy
@@ -0,0 +1,12 @@
+section \<open>Main HOL\<close>
+
+text \<open>
+  Classical Higher-order Logic -- only ``Main'', excluding real and
+  complex numbers etc.
+\<close>
+
+theory Main
+imports Pre_Main GCD Binomial
+begin
+
+end
