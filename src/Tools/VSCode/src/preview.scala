2656ecbbbc582bddbb5df6ee207963e12a639255
wenzelm <none@none>
2017-06-01 21:14:38 +0200

    use css from Isabelle distribution;

--- src/Tools/VSCode/src/preview.scala
+++ src/Tools/VSCode/src/preview.scala
@@ -45,10 +45,11 @@ class Preview(resources: VSCode_Resources)
   {
     val label = "Preview " + quote(model.node_name.toString)
     val content =
-      HTML.output_document(head = Nil, css = "", body =
+      HTML.output_document(Nil,
         List(
           HTML.chapter("Theory " + quote(model.node_name.theory_base_name)),
-          HTML.source(Symbol.decode(snapshot.node.commands.iterator.map(_.source).mkString))))
+          HTML.source(Symbol.decode(snapshot.node.commands.iterator.map(_.source).mkString))),
+        css = Url.print_file(Path.explode("~~/etc/isabelle.css").file))
     (label, content)
   }
 }
