aa1f66527d6b8ff8000700931f86b624ad5b4045
wenzelm <none@none>
2017-06-09 13:56:32 +0200

    more robust: store important meta info before potential failure;

--- src/Pure/Admin/isabelle_devel.scala
+++ src/Pure/Admin/isabelle_devel.scala
@@ -72,6 +72,7 @@ object Isabelle_Devel
     val store = Build_Log.store(options)
     using(store.open_database())(db =>
     {
+      store.update_database(db, log_dirs)
       store.update_database(db, log_dirs, ml_statistics = true)
       store.snapshot_database(db, root + Path.explode(BUILD_LOG_DB))
     })
