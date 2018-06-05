bce252996a696bd4d6bd460c700c134f4573abc6
wenzelm <none@none>
2018-06-05 23:26:15 +0200

    more parallelism to cope with 8h30 CPU time;

--- src/Pure/Admin/isabelle_cronjob.scala
+++ src/Pure/Admin/isabelle_cronjob.scala
@@ -260,7 +260,7 @@ object Isabelle_Cronjob
       List(Remote_Build("Linux B", "lxbroy10", historic = true, history = 90,
         options = "-m32 -B -M1x4,2,4,6", args = "-N -g timing")),
       List(Remote_Build("Linux Benchmarks", "lxbroy5", historic = true, history = 90,
-        options = "-m32 -B -M1,2 -t Benchmarks" +
+        options = "-m32 -B -M1x2,2 -t Benchmarks" +
             " -e ISABELLE_GHC=ghc -e ISABELLE_MLTON=mlton -e ISABELLE_OCAML=ocaml" +
             " -e ISABELLE_OCAMLC=ocamlc -e ISABELLE_SMLNJ=sml -e ISABELLE_SWIPL=swipl",
           args = "-N -a -d '~~/src/Benchmarks'",
