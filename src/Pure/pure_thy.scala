f68330c0c9495c2566af7863e6d0dde449f1e429
wenzelm <none@none>
2018-08-03 14:08:33 +0200

    Pure theory content;

---
+++ src/Pure/pure_thy.scala
@@ -0,0 +1,19 @@
+/*  Title:      Pure/pure_thy.scala
+    Author:     Makarius
+
+Pure theory content.
+*/
+
+package isabelle
+
+
+object Pure_Thy
+{
+  val FUN: String = "fun"
+  val PROP: String = "prop"
+  val ITSELF: String = "itself"
+
+  val ALL: String = "Pure.all"
+  val IMP: String = "Pure.imp"
+  val EQ: String = "Pure.eq"
+}
