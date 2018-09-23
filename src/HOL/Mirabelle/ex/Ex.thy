98ab4e81b9038d6d6b6c341801fbe148e7b7d156
wenzelm <none@none>
2018-09-23 20:33:35 +0200

    more robust: logic image might be absent in PIDE session;

--- src/HOL/Mirabelle/ex/Ex.thy
+++ src/HOL/Mirabelle/ex/Ex.thy
@@ -3,7 +3,7 @@ begin
 
 ML \<open>
   val rc = Isabelle_System.bash
-    "cd \"$ISABELLE_HOME/src/HOL/Analysis\"; isabelle mirabelle arith Inner_Product.thy";
+    "cd \"$ISABELLE_HOME/src/HOL/Analysis\"; if isabelle build -n \"$MIRABELLE_LOGIC\"; then isabelle mirabelle arith Inner_Product.thy; fi";
   if rc <> 0 then error ("Mirabelle example failed: " ^ string_of_int rc)
   else ();
 \<close> \<comment> \<open>some arbitrary small test case\<close>
