2f4c5785161318358bd9e758ddd5560892873872
wenzelm <none@none>
2018-11-05 15:04:31 +0100

    support sub-directories, i.e. structure module names;

--- src/Tools/Haskell/Build.thy
+++ src/Tools/Haskell/Build.thy
@@ -7,18 +7,17 @@ section \<open>Build Isabelle/Haskell modules\<close>
 theory Build imports Haskell
 begin
 
-ML_command \<open>
+ML \<open>
   Isabelle_System.with_tmp_dir "ghc" (fn dir =>
     let
+      val _ = Haskell.install_sources dir;
       val (out, rc) =
         Isabelle_System.bash_output
          (cat_lines
            ["set -e",
             "cd " ^ File.bash_path dir,
-            "cp " ^ File.bash_paths Haskell.source_modules ^ " .",
-            "\"$ISABELLE_GHC\" " ^ File.bash_paths (map Path.base Haskell.source_modules)]);
-    in if rc = 0 then writeln out else error out end
-  )
+            "\"$ISABELLE_GHC\" " ^ File.bash_paths Haskell.sources]);
+    in if rc = 0 then writeln out else error out end)
 \<close>
 
 end
