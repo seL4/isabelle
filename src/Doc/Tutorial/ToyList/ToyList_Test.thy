cf26916dbd35761e45dc6b24c581217e8ce3413b
wenzelm <none@none>
2018-11-10 19:39:38 +0100

    added ML antiquotation @{master_dir};

--- src/Doc/Tutorial/ToyList/ToyList_Test.thy
+++ src/Doc/Tutorial/ToyList/ToyList_Test.thy
@@ -4,8 +4,7 @@ begin
 
 ML \<open>
   let val text =
-    map (File.read o Path.append (Resources.master_directory @{theory}) o Path.explode)
-      ["ToyList1.txt", "ToyList2.txt"]
+    map (File.read o Path.append \<^master_dir>) [\<^path>\<open>ToyList1.txt\<close>, \<^path>\<open>ToyList2.txt\<close>]
     |> implode
   in Thy_Info.script_thy Position.start text @{theory} end
 \<close>
