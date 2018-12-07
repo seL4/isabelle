9a3f10e217540de49a107918d04bfe21fb5b1510
wenzelm <none@none>
2018-12-07 12:31:37 +0100

    more robust;

--- src/Pure/System/components.scala
+++ src/Pure/System/components.scala
@@ -36,7 +36,8 @@ object Components
   {
     val name = Archive.get_name(archive.file_name)
     progress.echo("Unpacking " + name)
-    Isabelle_System.gnutar("-C " + File.bash_path(dir) + " -xzf " + File.bash_path(archive)).check
+    Isabelle_System.gnutar(
+      "-C " + File.bash_path(dir) + " -xzf " + File.bash_path(archive.absolute)).check
     name
   }
 
