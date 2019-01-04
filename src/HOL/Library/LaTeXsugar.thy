0ed930390cf29364a73ed01f65032890b77bcee3
wenzelm <none@none>
2019-01-04 21:49:06 +0100

    support for isabelle update -u control_cartouches;

--- src/HOL/Library/LaTeXsugar.thy
+++ src/HOL/Library/LaTeXsugar.thy
@@ -97,7 +97,8 @@ syntax (IfThenNoBox output)
   "_asm" :: "prop \<Rightarrow> asms" ("_")
 
 setup \<open>
-  Thy_Output.antiquotation_pretty_source \<^binding>\<open>const_typ\<close> (Scan.lift Args.embedded_inner_syntax)
+  Thy_Output.antiquotation_pretty_source_embedded \<^binding>\<open>const_typ\<close>
+    (Scan.lift Args.embedded_inner_syntax)
     (fn ctxt => fn c =>
       let val tc = Proof_Context.read_const {proper = false, strict = false} ctxt c in
         Pretty.block [Thy_Output.pretty_term ctxt tc, Pretty.str " ::",
