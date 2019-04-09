c836d733974a8b34dbb5528f7ba7670b1a33bbe7
wenzelm <none@none>
2019-04-09 10:51:35 +0200

    tuned -- prefer Isar command 'compile_generated_files';

--- src/Tools/Haskell/Test.thy
+++ src/Tools/Haskell/Test.thy
@@ -4,29 +4,24 @@
 
 section \<open>Test build of Isabelle/Haskell modules\<close>
 
-theory Test imports Haskell
+theory Test
+  imports Haskell
 begin
 
-ML \<open>
-  Isabelle_System.with_tmp_dir "ghc" (fn tmp_dir =>
+compile_generated_files _ (in Haskell)
+  where \<open>fn dir =>
     let
-      val src_dir = Path.append tmp_dir (Path.explode "src");
-      val files = Generated_Files.get_files \<^theory>\<open>Haskell\<close>;
-      val _ = List.app (Generated_Files.write_file src_dir) files;
-
-      val modules = files
+      val modules =
+        Generated_Files.get_files \<^theory>\<open>Haskell\<close>
         |> map (#path #> Path.implode #> unsuffix ".hs" #> space_explode "/" #> space_implode ".");
       val _ =
-        GHC.new_project tmp_dir
+        GHC.new_project dir
           {name = "isabelle",
            depends =
             ["bytestring", "containers", "network", "split", "threads", "utf8-string", "uuid"],
            modules = modules};
-
-      val (out, rc) =
-        Isabelle_System.bash_output
-          (cat_lines ["set -e", "cd " ^ File.bash_path tmp_dir, "isabelle ghc_stack build 2>&1"]);
-    in if rc = 0 then writeln out else error out end)
-\<close>
+    in
+      writeln (Generated_Files.execute dir \<open>Build\<close> "mv Isabelle src && isabelle ghc_stack build")
+    end\<close>
 
 end
