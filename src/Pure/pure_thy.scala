86b894e3a7a2b8b807912b75982f91d2f2da561a
wenzelm <none@none>
2019-06-24 16:26:25 +0200

    support abstract syntax for proof terms (see src/Pure/Proofs/proof_syntax.ML);

--- src/Pure/pure_thy.scala
+++ src/Pure/pure_thy.scala
@@ -9,6 +9,8 @@ package isabelle
 
 object Pure_Thy
 {
+  /* Pure logic */
+
   val DUMMY: String = "dummy"
   val FUN: String = "fun"
   val PROP: String = "prop"
@@ -17,4 +19,18 @@ object Pure_Thy
   val ALL: String = "Pure.all"
   val IMP: String = "Pure.imp"
   val EQ: String = "Pure.eq"
+
+
+  /* proof terms (abstract syntax) */
+
+  val PROOF: String = "proof"
+
+  val APPT: String = "Appt"
+  val APPP: String = "AppP"
+  val ABST: String = "Abst"
+  val ABSP: String = "AbsP"
+  val HYP: String = "Hyp"
+  val ORACLE: String = "Oracle"
+  val OFCLASS: String = "OfClass"
+  val MINPROOF: String = "MinProof"
 }
