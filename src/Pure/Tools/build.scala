18af9f992a1737d172b3593e034aab538e04af25
wenzelm <none@none>
2019-08-07 11:09:37 +0200

    removed junk (cf. fa933b98d64d);

--- src/Pure/Tools/build.scala
+++ src/Pure/Tools/build.scala
@@ -289,7 +289,6 @@ object Build
             }
 
           process.result(
-            progress_stderr = Output.writeln(_),
             progress_stdout = (line: String) =>
               Library.try_unprefix("\floading_theory = ", line) match {
                 case Some(theory) =>
