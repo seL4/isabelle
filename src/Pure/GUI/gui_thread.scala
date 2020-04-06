ff5cc13c4ad2d0c40d0f94f9688dd24cacc08c41
wenzelm <none@none>
2020-04-06 21:22:42 +0200

    tuned;

--- src/Pure/GUI/gui_thread.scala
+++ src/Pure/GUI/gui_thread.scala
@@ -16,13 +16,13 @@ object GUI_Thread
 
   def assert[A](body: => A): A =
   {
-    Predef.assert(SwingUtilities.isEventDispatchThread())
+    Predef.assert(SwingUtilities.isEventDispatchThread)
     body
   }
 
   def require[A](body: => A): A =
   {
-    Predef.require(SwingUtilities.isEventDispatchThread())
+    Predef.require(SwingUtilities.isEventDispatchThread)
     body
   }
 
@@ -31,23 +31,23 @@ object GUI_Thread
 
   def now[A](body: => A): A =
   {
-    if (SwingUtilities.isEventDispatchThread()) body
+    if (SwingUtilities.isEventDispatchThread) body
     else {
-      lazy val result = { assert { Exn.capture(body) } }
-      SwingUtilities.invokeAndWait(new Runnable { def run = result })
+      lazy val result = assert { Exn.capture(body) }
+      SwingUtilities.invokeAndWait(() => result)
       Exn.release(result)
     }
   }
 
   def later(body: => Unit)
   {
-    if (SwingUtilities.isEventDispatchThread()) body
-    else SwingUtilities.invokeLater(new Runnable { def run = body })
+    if (SwingUtilities.isEventDispatchThread) body
+    else SwingUtilities.invokeLater(() => body)
   }
 
   def future[A](body: => A): Future[A] =
   {
-    if (SwingUtilities.isEventDispatchThread()) Future.value(body)
+    if (SwingUtilities.isEventDispatchThread) Future.value(body)
     else {
       val promise = Future.promise[A]
       later { promise.fulfill_result(Exn.capture(body)) }
