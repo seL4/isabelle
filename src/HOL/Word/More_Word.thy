217b9a1589fe1dc6376c94f04f82f6c43d031684
haftmann <none@none>
2020-07-01 17:32:11 +0000

    moved to Word_Lib
    --HG--
    extra : rebase_source : 0b47d98382eac7de6ac5ca3c3a19c9a36b336c27

--- src/HOL/Word/More_Word.thy
+++ src/HOL/Word/More_Word.thy
@@ -1,12 +1,11 @@
 (*  Title:      HOL/Word/More_Word.thy
 *)
 
-section \<open>Comprehensive Word Library\<close>
+section \<open>Ancient comprehensive Word Library\<close>
 
 theory More_Word
 imports
   Word
-  Word_Bitwise
 begin
 
 end
--- src/HOL/Word/Word_Examples.thy
+++ src/HOL/Word/Word_Examples.thy
@@ -7,7 +7,7 @@ Examples demonstrating and testing various word operations.
 section "Examples of word operations"
 
 theory Word_Examples
-  imports Word_Bitwise
+  imports Word
 begin
 
 type_synonym word32 = "32 word"
@@ -164,43 +164,5 @@ proof -
     by simp
   finally show ?thesis .
 qed
-
-text "alternative proof using bitwise expansion"
-
-lemma "(x AND 0xff00) OR (x AND 0x00ff) = (x::16 word)"
-  by word_bitwise
-
-text "more proofs using bitwise expansion"
-
-lemma "(x AND NOT 3) >> 4 << 2 = ((x >> 2) AND NOT 3)"
-  for x :: "10 word"
-  by word_bitwise
-
-lemma "((x AND -8) >> 3) AND 7 = (x AND 56) >> 3"
-  for x :: "12 word"
-  by word_bitwise
-
-text "some problems require further reasoning after bit expansion"
-
-lemma "x \<le> 42 \<Longrightarrow> x \<le> 89"
-  for x :: "8 word"
-  apply word_bitwise
-  apply blast
-  done
-
-lemma "(x AND 1023) = 0 \<Longrightarrow> x \<le> -1024"
-  for x :: word32
-  apply word_bitwise
-  apply clarsimp
-  done
-
-text "operations like shifts by non-numerals will expose some internal list
- representations but may still be easy to solve"
-
-lemma shiftr_overflow: "32 \<le> a \<Longrightarrow> b >> a = 0"
-  for b :: word32
-  apply word_bitwise
-  apply simp
-  done
 
 end
