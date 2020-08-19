2155b9fd3768935dafab003771b38cbc12d6e6f5
wenzelm <none@none>
2020-08-19 13:29:53 +0200

    update to kodkodi-1.5.4; more realistic kodkod invocation;

--- src/HOL/Tools/Nitpick/kodkod.scala
+++ src/HOL/Tools/Nitpick/kodkod.scala
@@ -8,8 +8,10 @@ package isabelle.nitpick
 
 import isabelle._
 
+import java.util.concurrent.Executors
+
 import org.antlr.runtime.{ANTLRInputStream, Lexer, RecognitionException}
-import de.tum.in.isabelle.Kodkodi.{KodkodiLexer, KodkodiParser}
+import de.tum.in.isabelle.Kodkodi.{Context, KodkodiLexer, KodkodiParser}
 
 
 object Kodkod
@@ -19,17 +21,40 @@ object Kodkod
     prove: Boolean = false,
     max_solutions: Int = Integer.MAX_VALUE,
     cleanup_inst: Boolean = false,
-    time_limit: Time = Time.zero,
     max_threads: Int = 1): String =
   {
-    val in = Bytes(source).stream
-    val stream = new ANTLRInputStream(in)
-    val lexer = new KodkodiLexer(stream)
-
+    val context =
+      new Context {
+        private val out = new StringBuilder
+        private val err = new StringBuilder
+        def result: (String, String) = synchronized { (out.toString, err.toString) }
+        override def output(s: String): Unit = synchronized { out ++= s; out += '\n' }
+        override def error(s: String): Unit = synchronized { err ++= s; err += '\n' }
+        override def exit(rc: Int) { if (rc == 0) () else error("exit " + rc) }
+      }
+    val executor = Executors.newFixedThreadPool(max_threads)
+    val lexer = new KodkodiLexer(new ANTLRInputStream(Bytes(source).stream))
     val parser =
-      KodkodiParser.create(true, false, solve_all, prove, max_solutions,
-        cleanup_inst, max_threads, lexer)
+      KodkodiParser.create(context, executor,
+        false, solve_all, prove, max_solutions, cleanup_inst, lexer)
 
-    "FIXME"
+    val solution =
+        try { parser.problems() }
+        catch {
+          case exn: RecognitionException =>
+            parser.reportError(exn)
+            error("Parser error")
+        }
+    if (parser.getTokenStream.LA(1) != KodkodiParser.EOF) {
+      error("Trailing tokens")
+    }
+    if (lexer.getNumberOfSyntaxErrors > 0) error("Lexical error")
+    if (parser.getNumberOfSyntaxErrors > 0) error("Syntax error")
+
+    val (out, err) = context.result
+    Output.writeln(out)
+    Output.warning(err)
+
+    solution
   }
 }
