58c7bb2d9d5b1f1cd83740b2046acb894896715a
wenzelm <none@none>
2020-11-02 16:50:22 +0100

    tuned;

--- Admin/components/index.php
+++ Admin/components/index.php
@@ -1,11 +1,11 @@
 <html>
 <body>
-<h2>Components:</h2>
+<h2>Isabelle Components</h2>
 <table>
 <tr>
-<th> Name </th>
-<th> &nbsp;&nbsp;&nbsp; </th>
-<th> Size</th>
+<td><b>Name</b></td>
+<td>&nbsp;&nbsp;&nbsp;</td>
+<td><b>Size</b></td>
 </tr>
 <?php
  $component_pattern="/^.+\.tar\.gz$/";
@@ -14,7 +14,7 @@
    if (preg_match($component_pattern, $file)
        && is_file($file)
        && is_readable($file)) {
-     print "<tr><td><a href=\"$file\">$file</a></td><td></td><td>".filesize($file)."</td></tr>\n";
+     print "<tr><td><a href=\"$file\"><code>$file</code></a></td><td></td><td>".filesize($file)."</td></tr>\n";
   }
  }
 ?>
