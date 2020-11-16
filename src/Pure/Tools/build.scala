d762a1c2008a087f093c3b9bd5c5dbded79c64ac
wenzelm <none@none>
2020-11-16 22:46:02 +0100

    clarified signature: master_dir is just Path.current;

--- src/Pure/Tools/build.scala
+++ src/Pure/Tools/build.scala
@@ -175,21 +175,20 @@ object Build
             {
               import XML.Encode._
               pair(list(pair(string, int)), pair(list(properties), pair(string, pair(string,
-                pair(string, pair(Path.encode,
-                pair(list(pair(Options.encode, list(pair(string, properties)))),
+                pair(string, pair(list(pair(Options.encode, list(pair(string, properties)))),
                 pair(list(pair(string, properties)),
                 pair(list(pair(string, string)),
                 pair(list(pair(string, string)),
                 pair(list(string),
                 pair(list(pair(string, string)),
-                pair(list(string), list(pair(string, list(string))))))))))))))))(
+                pair(list(string), list(pair(string, list(string)))))))))))))))(
               (Symbol.codes, (command_timings0, (parent, (info.chapter,
-                (session_name, (Path.current, (info.theories,
+                (session_name, (info.theories,
                 (sessions_structure.session_positions,
                 (sessions_structure.dest_session_directories,
                 (sessions_structure.session_chapters,
                 (base.doc_names, (base.global_theories.toList,
-                (base.loaded_theories.keys, sessions_structure.bibtex_entries))))))))))))))
+                (base.loaded_theories.keys, sessions_structure.bibtex_entries)))))))))))))
             })
 
         val env =
