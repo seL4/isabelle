b51a10c8530dde1ca9e55cc39f514896f662356d
wenzelm <none@none>
2020-12-23 23:19:09 +0100

    more robust defaults: spurious problems with parallel invocations and interrupts;

--- src/Pure/Tools/build.scala
+++ src/Pure/Tools/build.scala
@@ -202,6 +202,7 @@ object Build
       options +
         "completion_limit=0" +
         "editor_tracing_messages=0" +
+        "kodkod_scala=false" +
         ("pide_reports=" + options.bool("build_pide_reports"))
 
     val store = Sessions.store(build_options)
