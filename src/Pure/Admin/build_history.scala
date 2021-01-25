f18fd05b3636ab9616ec3c0380886c66c37ccddc
wenzelm <none@none>
2021-01-25 22:30:39 +0100

    more robust: defer error in sessions structure to build process;

--- src/Pure/Admin/build_history.scala
+++ src/Pure/Admin/build_history.scala
@@ -161,8 +161,12 @@ object Build_History
     val (afp_build_args, afp_sessions) =
       if (afp_rev.isEmpty) (Nil, Nil)
       else {
-        val afp = AFP.init(options, afp_repos)
-        (List("-d", "~~/AFP/thys"), afp.partition(afp_partition))
+        val (opt, sessions) =
+          try {
+            val afp = AFP.init(options, afp_repos)
+            ("-d", afp.partition(afp_partition))
+          } catch { case ERROR(_) => ("-D", Nil) }
+        (List(opt, "~~/AFP/thys"), sessions)
       }
 
 
