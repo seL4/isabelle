f17f8934c8e977dacfcd93b0bb022544eda280b7
wenzelm <none@none>
2021-01-26 20:06:38 +0100

    more robust;

--- Admin/Linux/Isabelle.c
+++ Admin/Linux/Isabelle.c
@@ -24,18 +24,19 @@ int main(int argc, char *argv[])
   int i = 0;
 
   dcmd = strdup(argv[0]);
+  if (dcmd == NULL) fail("Failed to allocate memory");
+
   dname = dirname(dcmd);
 
   cmd_line = malloc(sizeof(char *) * (argc + 1));
   if (cmd_line == NULL) fail("Failed to allocate memory");
 
-  cmd = cmd_line[0];
   cmd = malloc(strlen(dname) + strlen("/lib/scripts/Isabelle_app") + 1);
   if (cmd == NULL) fail("Failed to allocate memory");
   sprintf(cmd, "%s/lib/scripts/Isabelle_app", dname);
 
+  cmd_line[0] = cmd;
   for (i = 1; i < argc; i++) cmd_line[i] = argv[i];
-
   cmd_line[argc] = NULL;
 
   execvp(cmd, cmd_line);
