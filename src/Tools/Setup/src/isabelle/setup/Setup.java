d55cba7da5c2d35ee5241a27683a32b181ae3613
wenzelm <none@none>
2021-05-11 21:57:43 +0200

    basic setup of Isabelle setup tool --- pure Java, no dependencies;

---
+++ src/Tools/Setup/src/isabelle/setup/Setup.java
@@ -0,0 +1,15 @@
+/*  Title:      Tools/Setup/isabelle/setup/Setup.java
+    Author:     Makarius
+
+Isabelle setup tool: bootstrap from generic Java environment.
+*/
+
+package isabelle.setup;
+
+class Setup
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Isabelle setup");
+    }
+}
