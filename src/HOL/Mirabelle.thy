a587a1762dfd312445cb019ca4d0ad7097487f57
wenzelm <none@none>
2021-05-15 17:40:36 +0200

    reactive "sledgehammer_filter": statically correct, but untested (no proof_file);

--- src/HOL/Mirabelle.thy
+++ src/HOL/Mirabelle.thy
@@ -1,5 +1,6 @@
 (*  Title:      HOL/Mirabelle.thy
     Author:     Jasmin Blanchette and Sascha Boehme, TU Munich
+    Author:     Makarius
 *)
 
 theory Mirabelle
@@ -12,8 +13,8 @@ ML_file \<open>Tools/Mirabelle/mirabelle_metis.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_quickcheck.ML\<close>
 (*
 ML_file \<open>Tools/Mirabelle/mirabelle_sledgehammer.ML\<close>
-ML_file \<open>Tools/Mirabelle/mirabelle_sledgehammer_filter.ML\<close>
 *)
+ML_file \<open>Tools/Mirabelle/mirabelle_sledgehammer_filter.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_try0.ML\<close>
 
 end
