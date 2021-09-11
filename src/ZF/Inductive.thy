37a4a49247f5d2b0daf625c3b6aa7d1fde913324
wenzelm <none@none>
2021-09-11 22:28:01 +0200

    clarified antiquotation;

--- src/ZF/Inductive.thy
+++ src/ZF/Inductive.thy
@@ -37,8 +37,8 @@ ML_file \<open>Tools/primrec_package.ML\<close>
 ML \<open>
 structure Lfp =
   struct
-  val oper      = \<^const>\<open>lfp\<close>
-  val bnd_mono  = \<^const>\<open>bnd_mono\<close>
+  val oper      = \<^Const>\<open>lfp\<close>
+  val bnd_mono  = \<^Const>\<open>bnd_mono\<close>
   val bnd_monoI = @{thm bnd_monoI}
   val subs      = @{thm def_lfp_subset}
   val Tarski    = @{thm def_lfp_unfold}
@@ -47,8 +47,8 @@ structure Lfp =
 
 structure Standard_Prod =
   struct
-  val sigma     = \<^const>\<open>Sigma\<close>
-  val pair      = \<^const>\<open>Pair\<close>
+  val sigma     = \<^Const>\<open>Sigma\<close>
+  val pair      = \<^Const>\<open>Pair\<close>
   val split_name = \<^const_name>\<open>split\<close>
   val pair_iff  = @{thm Pair_iff}
   val split_eq  = @{thm split}
@@ -61,10 +61,10 @@ structure Standard_CP = CartProd_Fun (Standard_Prod);
 
 structure Standard_Sum =
   struct
-  val sum       = \<^const>\<open>sum\<close>
-  val inl       = \<^const>\<open>Inl\<close>
-  val inr       = \<^const>\<open>Inr\<close>
-  val elim      = \<^const>\<open>case\<close>
+  val sum       = \<^Const>\<open>sum\<close>
+  val inl       = \<^Const>\<open>Inl\<close>
+  val inr       = \<^Const>\<open>Inr\<close>
+  val elim      = \<^Const>\<open>case\<close>
   val case_inl  = @{thm case_Inl}
   val case_inr  = @{thm case_Inr}
   val inl_iff   = @{thm Inl_iff}
@@ -84,8 +84,8 @@ structure Ind_Package =
 
 structure Gfp =
   struct
-  val oper      = \<^const>\<open>gfp\<close>
-  val bnd_mono  = \<^const>\<open>bnd_mono\<close>
+  val oper      = \<^Const>\<open>gfp\<close>
+  val bnd_mono  = \<^Const>\<open>bnd_mono\<close>
   val bnd_monoI = @{thm bnd_monoI}
   val subs      = @{thm def_gfp_subset}
   val Tarski    = @{thm def_gfp_unfold}
@@ -94,8 +94,8 @@ structure Gfp =
 
 structure Quine_Prod =
   struct
-  val sigma     = \<^const>\<open>QSigma\<close>
-  val pair      = \<^const>\<open>QPair\<close>
+  val sigma     = \<^Const>\<open>QSigma\<close>
+  val pair      = \<^Const>\<open>QPair\<close>
   val split_name = \<^const_name>\<open>qsplit\<close>
   val pair_iff  = @{thm QPair_iff}
   val split_eq  = @{thm qsplit}
@@ -108,10 +108,10 @@ structure Quine_CP = CartProd_Fun (Quine_Prod);
 
 structure Quine_Sum =
   struct
-  val sum       = \<^const>\<open>qsum\<close>
-  val inl       = \<^const>\<open>QInl\<close>
-  val inr       = \<^const>\<open>QInr\<close>
-  val elim      = \<^const>\<open>qcase\<close>
+  val sum       = \<^Const>\<open>qsum\<close>
+  val inl       = \<^Const>\<open>QInl\<close>
+  val inr       = \<^Const>\<open>QInr\<close>
+  val elim      = \<^Const>\<open>qcase\<close>
   val case_inl  = @{thm qcase_QInl}
   val case_inr  = @{thm qcase_QInr}
   val inl_iff   = @{thm QInl_iff}
