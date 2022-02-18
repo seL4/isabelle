2ac2206e6e79d1b283f25510f65a0335cc0905c0
wenzelm <none@none>
2022-02-18 11:34:30 +0100

    clarified options;

--- src/Tools/VSCode/src/vscode_setup.scala
+++ src/Tools/VSCode/src/vscode_setup.scala
@@ -17,7 +17,6 @@ object VSCode_Setup
   def vscode_home: Path = Path.variable("ISABELLE_VSCODE_HOME")
   def vscode_settings: Path = Path.variable("ISABELLE_VSCODE_SETTINGS")
   def vscode_version: String = Isabelle_System.getenv_strict("ISABELLE_VSCODE_VERSION")
-  def vscode_platform: Platform.Family.Value = Platform.family
 
   def vscode_installation(version: String, platform: Platform.Family.Value): (Boolean, Path) =
     {
@@ -35,6 +34,7 @@ object VSCode_Setup
   /* vscode setup */
 
   val default_download_url: String = "https://github.com/VSCodium/vscodium/releases/download"
+  def default_platform: Platform.Family.Value = Platform.family
 
   def download_name(version: String, platform: Platform.Family.Value): String =
   {
@@ -53,7 +53,7 @@ object VSCode_Setup
     check: Boolean = false,
     download_url: String = default_download_url,
     version: String = vscode_version,
-    platform: Platform.Family.Value = vscode_platform,
+    platform: Platform.Family.Value = default_platform,
     verbose: Boolean = false,
     progress: Progress = new Progress): Unit =
   {
@@ -110,7 +110,7 @@ object VSCode_Setup
       var check = false
       var download_url = default_download_url
       var version = vscode_version
-      var platform = vscode_platform
+      var platforms = List(default_platform)
       var verbose = false
 
       val getopts = Getopts("""
@@ -121,9 +121,10 @@ Usage: vscode_setup [OPTIONS]
     -U URL       download URL
                  (default: """" + default_download_url + """")
     -V VERSION   version (default: """" + vscode_version + """")
-    -p NAME      platform family: """ + commas_quote(Platform.Family.list.map(_.toString)) + """
-                 (default: """ + quote(vscode_platform.toString) + """)
-    -v           verbose
+    -p NAMES     platform families: comma-separated names
+                 (""" + commas_quote(Platform.Family.list.map(_.toString)) + """, default: """ +
+        quote(default_platform.toString) + """)
+    -v           verbose mode
 
   Maintain local installation of VSCode, see also https://vscodium.com
 
@@ -133,14 +134,16 @@ Usage: vscode_setup [OPTIONS]
         "C" -> (_ => check = true),
         "U:" -> (arg => download_url = arg),
         "V:" -> (arg => version = arg),
-        "p:" -> (arg => platform = Platform.Family.parse(arg)),
+        "p:" -> (arg => platforms = Library.space_explode(',', arg).map(Platform.Family.parse)),
         "v" -> (_ => verbose = true))
 
       val more_args = getopts(args)
       if (more_args.nonEmpty) getopts.usage()
 
       val progress = new Console_Progress()
-      vscode_setup(check = check, download_url = download_url, version = version,
-        platform = platform, verbose = verbose, progress = progress)
+      for (platform <- platforms) {
+        vscode_setup(check = check, download_url = download_url, version = version,
+          platform = platform, verbose = verbose, progress = progress)
+      }
     })
 }
