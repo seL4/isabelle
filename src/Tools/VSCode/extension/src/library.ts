4aca96761a75fe3cce4ff58d31102ced8e1c1b15
wenzelm <none@none>
2022-02-22 21:30:39 +0100

    more operations;

--- src/Tools/VSCode/extension/src/library.ts
+++ src/Tools/VSCode/extension/src/library.ts
@@ -14,6 +14,11 @@ export function escape_regex(s: string): string
 
 /* strings */
 
+export function quote(s: string): string
+{
+  return "\"" + s + "\""
+}
+
 export function reverse(s: string): string
 {
   return s.split("").reverse().join("")
@@ -23,6 +28,23 @@ export function has_newline(text: string)
 {
   return text.includes("\n") || text.includes("\r")
 }
+
+
+/* settings environment */
+
+export function getenv(name: string): string
+{
+  const s = process.env[name]
+  return s || ""
+}
+
+export function getenv_strict(name: string): string
+{
+  const s = getenv(name)
+  if (s) return s
+  else throw new Error("Undefined Isabelle environment variable: " + quote(name))
+}
+
 
 /* platform information */
 
