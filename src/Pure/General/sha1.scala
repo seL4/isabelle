7b22741289904641b2470efbffba606720ba65f3
wenzelm <none@none>
2022-03-23 12:02:56 +0100

    clarified signature;

--- src/Pure/General/sha1.scala
+++ src/Pure/General/sha1.scala
@@ -1,7 +1,7 @@
 /*  Title:      Pure/General/sha1.scala
     Author:     Makarius
 
-Digest strings according to SHA-1 (see RFC 3174).
+SHA-1 message digest according to RFC 3174.
 */
 
 package isabelle
@@ -9,8 +9,8 @@ package isabelle
 
 import java.io.{File => JFile, FileInputStream}
 import java.security.MessageDigest
-import java.util.Locale
-import java.math.BigInteger
+
+import isabelle.setup.{Build => Setup_Build}
 
 
 object SHA1
@@ -26,38 +26,27 @@ object SHA1
     override def toString: String = rep
   }
 
-  private def sha1_digest(sha: MessageDigest): Digest =
-    new Digest(String.format(Locale.ROOT, "%040x", new BigInteger(1, sha.digest())))
+  def fake_digest(rep: String): Digest = new Digest(rep)
 
-  def fake(rep: String): Digest = new Digest(rep)
+  def make_digest(body: MessageDigest => Unit): Digest =
+  {
+    val digest_body = new Setup_Build.Digest_Body { def apply(sha: MessageDigest): Unit = body(sha)}
+    new Digest(Setup_Build.make_digest(digest_body))
+  }
 
   def digest(file: JFile): Digest =
-  {
-    val digest = MessageDigest.getInstance("SHA")
-
-    using(new FileInputStream(file))(stream =>
-    {
-      val buf = new Array[Byte](65536)
-      var m = 0
-      do {
-        m = stream.read(buf, 0, buf.length)
-        if (m != -1) digest.update(buf, 0, m)
-      } while (m != -1)
-    })
-
-    sha1_digest(digest)
-  }
+    make_digest(sha => using(new FileInputStream(file))(stream =>
+      {
+        val buf = new Array[Byte](65536)
+        var m = 0
+        do {
+          m = stream.read(buf, 0, buf.length)
+          if (m != -1) sha.update(buf, 0, m)
+        } while (m != -1)
+      }))
 
   def digest(path: Path): Digest = digest(path.file)
-
-  def digest(bytes: Array[Byte]): Digest =
-  {
-    val digest = MessageDigest.getInstance("SHA")
-    digest.update(bytes)
-
-    sha1_digest(digest)
-  }
-
+  def digest(bytes: Array[Byte]): Digest = make_digest(_.update(bytes))
   def digest(bytes: Bytes): Digest = bytes.sha1_digest
   def digest(string: String): Digest = digest(Bytes(string))
   def digest_set(digests: List[Digest]): Digest =
