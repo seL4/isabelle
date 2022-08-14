7faa30b387c4b019d282404740237c98aef2a441
wenzelm <none@none>
2022-08-14 12:18:06 +0200

    clarified theory_names with exported content;

--- src/Pure/Thy/export_theory.scala
+++ src/Pure/Thy/export_theory.scala
@@ -29,7 +29,7 @@ object Export_Theory {
     progress: Progress = new Progress
   ): Session = {
     val thys =
-      for (theory <- session_context.theory_names()) yield {
+      for (theory <- theory_names(session_context)) yield {
         progress.echo("Reading theory " + theory)
         read_theory(session_context.theory(theory))
       }
@@ -103,6 +103,12 @@ object Export_Theory {
   def read_theory_parents(theory_context: Export.Theory_Context): Option[List[String]] =
     theory_context.get(Export.THEORY_PREFIX + "parents")
       .map(entry => Library.trim_split_lines(entry.uncompressed.text))
+
+  def theory_names(session_context: Export.Session_Context): List[String] =
+    for {
+      theory <- session_context.theory_names()
+      if read_theory_parents(session_context.theory(theory)).isDefined
+    } yield theory
 
   def no_theory: Theory =
     Theory("", Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Nil, Map.empty)
