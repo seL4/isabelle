ff58d32c775639f5bda26c0f8649b03b1e74c930
wenzelm <none@none>
2022-12-04 14:15:12 +0100

    more direct access to jEdit jar resources, without unzip;

---
+++ src/Tools/jEdit/src/jedit_jar.scala
@@ -0,0 +1,28 @@
+/*  Title:      Tools/jEdit/src/jedit_jar.scala
+    Author:     Makarius
+
+Offline access to jEdit jar resources.
+*/
+
+package isabelle.jedit
+
+
+import isabelle._
+
+
+object JEdit_JAR {
+  /* jEdit jar resources */
+
+  def get_resource(name: String): String = {
+    val s = classOf[org.gjt.sp.jedit.jEdit].getResourceAsStream(name)
+    if (s == null) error("Bad jEdit resource: " + quote(name))
+    else using(s)(File.read_stream)
+  }
+
+  object JEdit_Resources extends Scala.Fun_Strings("jEdit.resources") {
+    val here = Scala_Project.here
+    def apply(args: List[String]): List[String] = args.map(get_resource)
+  }
+
+  class Scala_Functions extends Scala.Functions(JEdit_Resources)
+}
