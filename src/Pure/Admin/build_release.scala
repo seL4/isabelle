a9d29753dc5b64da8a67a8b0f62e0db179cf2245
wenzelm <none@none>
2023-01-25 21:49:08 +0100

    tuned messages: less verbosity;

--- src/Pure/Admin/build_release.scala
+++ src/Pure/Admin/build_release.scala
@@ -610,7 +610,7 @@ exec "$ISABELLE_JDK_HOME/bin/java" \
 
         // application bundling
 
-        Components.clean_base(contrib_dir, platforms = List(platform), progress = progress)
+        Components.clean_base(contrib_dir, platforms = List(platform))
 
         platform match {
           case Platform.Family.linux_arm | Platform.Family.linux =>
