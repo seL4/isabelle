18eb84893eda61ccc558ee37427f81391622e9dd
wenzelm <none@none>
2023-02-19 13:43:38 +0100

    unused;

--- src/Pure/PIDE/document.scala
+++ src/Pure/PIDE/document.scala
@@ -417,7 +417,6 @@ object Document {
       } yield cmd).toList
 
     def descendants(names: List[Node.Name]): List[Node.Name] = graph.all_succs(names)
-    def requirements(names: List[Node.Name]): List[Node.Name] = graph.all_preds_rev(names)
     def topological_order: List[Node.Name] = graph.topological_order
 
     override def toString: String = topological_order.mkString("Nodes(", ",", ")")
