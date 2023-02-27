d56a1e92a9fa4ac3454c7a9808c59a5a2645f6e7
paulson <lp15@cam.ac.uk>
2023-02-27 17:09:59 +0000

    Importation of basic group theory results, due to Jakob von Raumer from his AFP entry Jordan-Hölder Theorem

--- src/HOL/Algebra/Algebra.thy
+++ src/HOL/Algebra/Algebra.thy
@@ -3,6 +3,6 @@
 theory Algebra
   imports Sylow Chinese_Remainder Zassenhaus Galois_Connection Generated_Fields Free_Abelian_Groups
      Divisibility Embedded_Algebras IntRing Sym_Groups Exact_Sequence Polynomials Algebraic_Closure
-     Left_Coset
+     Left_Coset SimpleGroups SndIsomorphismGrp
 begin
 end
