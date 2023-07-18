5088e31c1fb1cdb2d6ce8efc32a61f02e6234f96
wenzelm <none@none>
2023-07-18 20:14:57 +0200

    support for management of build cluster;

---
+++ src/Pure/Tools/build_cluster.scala
@@ -0,0 +1,12 @@
+/*  Title:      Pure/Tools/build_cluster.scala
+    Author:     Makarius
+
+Management of build cluster: independent ssh hosts with access to shared
+PostgreSQL server.
+*/
+
+package isabelle
+
+
+object Build_Cluster {
+}
