9fd95e0052fc1eec9be6fd9547ba1f050c9599ec
wenzelm <none@none>
2023-07-23 21:23:18 +0200

    proper check;

--- src/Pure/Tools/build_cluster.scala
+++ src/Pure/Tools/build_cluster.scala
@@ -161,7 +161,7 @@ object Build_Cluster {
           build_id = build_context.build_uuid,
           isabelle_home = remote_isabelle_home,
           afp_root = remote_afp_root)
-      remote_isabelle.bash(script)
+      remote_isabelle.bash(script).check
     }
 
     override def close(): Unit = ssh.close()
