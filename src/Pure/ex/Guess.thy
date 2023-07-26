4adcc6e6d8b1ec59ffcf13c06ae26b621cf67f5d
wenzelm <none@none>
2023-07-26 20:15:31 +0200

    prefer Output.writeln for theory "results", as opposed to Output.state for genuine proof states (see f8c412a45af8, c668735fb8b5, ecf80e37ed1a);

--- src/Pure/ex/Guess.thy
+++ src/Pure/ex/Guess.thy
@@ -149,7 +149,8 @@ fun gen_guess prep_var raw_vars int state =
     val goal = Var guess;
     val pos = Position.thread_data ();
     fun print_result ctxt' (k, [(s, [_, th])]) =
-      Proof_Display.print_results int pos ctxt' (k, [(s, [th])]);
+      Proof_Display.print_results {interactive = int, pos = pos, proof_state = true}
+        ctxt' (k, [(s, [th])]);
     val before_qed =
       Method.primitive_text (fn ctxt =>
         Goal.conclude #> Raw_Simplifier.norm_hhf ctxt #>
