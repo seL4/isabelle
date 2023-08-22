c6b596d363c9216e44065b2be06e187199f39594
wenzelm <none@none>
2023-08-22 10:31:15 +0200

    tuned output;

--- src/Pure/General/sql.scala
+++ src/Pure/General/sql.scala
@@ -676,7 +676,8 @@ object PostgreSQL {
     val url = "jdbc:postgresql://" + server.host + ":" + server.port + "/" + name
     val ssh = server.ssh_system.ssh_session
     val print =
-      "server " + user + "@" + server + "/" + name + if_proper(ssh, " via ssh " + ssh.get)
+      "server " + quote(user + "@" + server + "/" + name) +
+        if_proper(ssh, " via ssh " + quote(ssh.get.toString))
 
     val connection = DriverManager.getConnection(url, user, password)
     val db = new Database(connection, print, server, server_close)
