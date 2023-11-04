f3adceaadf8ce6da3f0715add357f319a8e86f08
wenzelm <none@none>
2023-11-04 16:56:54 +0100

    tuned whitespace;

--- src/Pure/Admin/build_status.scala
+++ src/Pure/Admin/build_status.scala
@@ -62,7 +62,8 @@ object Build_Status {
 
       Build_Log.private_data.universal_table.select(columns, distinct = true, sql =
         SQL.where_and(
-          Build_Log.private_data.pull_date(afp).ident + " > " + Build_Log.private_data.recent_time(days(options)),
+          Build_Log.private_data.pull_date(afp).ident + " > " +
+            Build_Log.private_data.recent_time(days(options)),
           Build_Log.private_data.status.member(
             List(
               Build_Log.Session_Status.finished.toString,
