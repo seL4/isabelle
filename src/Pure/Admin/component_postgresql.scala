001953b1aafab84963f8ca0104db90056c778fc3
wenzelm <none@none>
2024-01-20 22:26:35 +0100

    updated to postgresql-42.7.1;

--- src/Pure/Admin/component_postgresql.scala
+++ src/Pure/Admin/component_postgresql.scala
@@ -13,7 +13,7 @@ object Component_PostgreSQL {
   val notable_urls =
     List("https://jdbc.postgresql.org", "https://jdbc.postgresql.org/download")
 
-  val default_download_url = "https://jdbc.postgresql.org/download/postgresql-42.6.0.jar"
+  val default_download_url = "https://jdbc.postgresql.org/download/postgresql-42.7.1.jar"
 
 
   /* build postgresql */
