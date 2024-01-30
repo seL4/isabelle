3bb11e10d6f1ef58acd906f6d2b977428d7a14a0
wenzelm <none@none>
2024-01-30 17:36:14 +0100

    clarified symbolic host name;

--- src/Pure/Admin/isabelle_cronjob.scala
+++ src/Pure/Admin/isabelle_cronjob.scala
@@ -296,7 +296,7 @@ object Isabelle_Cronjob {
 
   val remote_builds1: List[List[Remote_Build]] = {
     List(
-      List(Remote_Build("Linux (ARM)", "server-arm",
+      List(Remote_Build("Linux (ARM)", "linux-arm",
         history_base = "build_history_base_arm",
         shared_isabelle_self = true,
         options = "-m32 -B -M1x2 -p timeout_scale=2" +
