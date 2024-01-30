27f912212c27c32d170ca41a606fe95220b0609c
wenzelm <none@none>
2024-01-30 21:54:33 +0100

    more informative message (amending b8a6b2ec85a2);

--- src/Pure/Admin/isabelle_cronjob.scala
+++ src/Pure/Admin/isabelle_cronjob.scala
@@ -480,7 +480,8 @@ object Isabelle_Cronjob {
         res match {
           case Exn.Res(_) => None
           case Exn.Exn(exn) =>
-            Output.writeln("Exception trace for " + quote(task.name) + ":\n" + Exn.trace(exn))
+            Output.writeln("Exception trace for " + quote(task.name) + ":\n" +
+              Exn.message(exn) + "\n" + Exn.trace(exn))
             val first_line = split_lines(Exn.message(exn)).headOption getOrElse "exception"
             Some(first_line)
         }
