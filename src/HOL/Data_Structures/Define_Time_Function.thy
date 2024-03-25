e20f5fc3f5fe843255b6d65a417dcf42bd4bca08
nipkow <none@none>
2024-03-25 14:08:25 +0100

    documented running time function framework by Jonas Stahl

--- src/HOL/Data_Structures/Define_Time_Function.thy
+++ src/HOL/Data_Structures/Define_Time_Function.thy
@@ -1,9 +1,10 @@
 (*
 Author: Jonas Stahl
 
-Automatic definition of running time functions from HOL functions
+Automatic definition of step-counting running-time functions from HOL functions
 following the translation described in Section 1.5, Running Time, of the book
 Functional Data Structures and Algorithms. A Proof Assistant Approach.
+See https://functional-algorithms-verified.org
 *)
 
 theory Define_Time_Function
@@ -20,17 +21,33 @@ ML_file Define_Time_Function.ML
 
 declare [[time_prefix = "T_"]]
 
-text \<open>The pre-defined functions below are assumed to have constant running time.
+text \<open>
+This theory provides commands for the automatic definition of step-counting running-time functions
+from HOL functions following the translation described in Section 1.5, Running Time, of the book
+"Functional Data Structures and Algorithms. A Proof Assistant Approach."
+See @{url "https://functional-algorithms-verified.org"}
+
+Command \<open>time_fun f\<close> retrieves the definition of \<open>f\<close> and defines a corresponding step-counting
+running-time function \<open>T_f\<close>. For all auxiliary functions used by \<open>f\<close> (excluding constructors),
+running time functions must already have been defined.
+If the definition of the function requires a manual termination proof,
+use \<open>time_function\<close> accompanied by a \<open>termination\<close> command.
+Limitation: The commands do not work properly in locales yet.
+
+The pre-defined functions below are assumed to have constant running time.
 In fact, we make that constant 0.
 This does not change the asymptotic running time of user-defined functions using the
 pre-defined functions because 1 is added for every user-defined function call.
 
-Note: Many of the functions below are polymorphic and reside in type classes.
+Many of the functions below are polymorphic and reside in type classes.
 The constant-time assumption is justified only for those types where the hardware offers
 suitable support, e.g. numeric types. The argument size is implicitly bounded, too.
 
 The constant-time assumption for \<open>(=)\<close> is justified for recursive data types such as lists and trees
-as long as the comparison is of the form \<open>t = c\<close> where \<open>c\<close> is a constant term, for example \<open>xs = []\<close>.\<close>
+as long as the comparison is of the form \<open>t = c\<close> where \<open>c\<close> is a constant term, for example \<open>xs = []\<close>.
+
+Users of this running time framework need to ensure that 0-time functions are used only
+within the above restrictions.\<close>
 
 time_fun_0 "(+)"
 time_fun_0 "(-)"
