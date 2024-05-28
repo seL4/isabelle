5e8950732e7a72132d1712fef5f2d7cea9d02402
Fabian Huch <huch@in.tum.de>
2024-05-28 19:51:49 +0200

    proper html script tag: source code must not be escaped;
    --HG--
    extra : histedit_source : 56d0123594be19101ef1f487f98c5b1b419a15ea

--- src/Pure/General/html.scala
+++ src/Pure/General/html.scala
@@ -93,7 +93,7 @@ object HTML {
     XML.Elem(Markup("link", List("rel" -> "stylesheet", "type" -> "text/css", "href" -> href)), Nil)
   def style_file(path: Path): XML.Elem = style_file(Url.print_file(path.file))
 
-  def script(s: String): XML.Elem = XML.elem("script", text(s))
+  def script(s: String): XML.Elem = XML.elem("script", List(raw(text(s))))
   def script_file(href: String): XML.Elem = XML.Elem(Markup("script", List("src" -> href)), Nil)
   def script_file(path: Path): XML.Elem = script_file(Url.print_file(path.file))
 
