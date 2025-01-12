fe9c9d5791afff16484e95ad3e96932813de72cc
wenzelm <none@none>
2025-01-12 14:16:21 +0100

    clarified names;
    --HG--
    rename : src/Tools/Find_Facts/Tools/find_facts => src/Tools/Find_Facts/Tools/find_facts_server

--- src/Tools/Find_Facts/src/find_facts_tools.scala
+++ src/Tools/Find_Facts/src/find_facts_tools.scala
@@ -7,4 +7,4 @@ JVM entry points for command-line tools in $FIND_FACTS_HOME/Tools/.
 package isabelle.find_facts
 
 object Find_Facts_Index_Tool { def main(args: Array[String]): Unit = Find_Facts.main_tool1(args) }
-object Find_Facts_Tool { def main(args: Array[String]): Unit = Find_Facts.main_tool3(args) }
+object Find_Facts_Server_Tool { def main(args: Array[String]): Unit = Find_Facts.main_tool3(args) }
