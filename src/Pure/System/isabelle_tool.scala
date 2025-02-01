0ca410b8cbc82bc35cfe02e21b27cfac236dd987
wenzelm <none@none>
2025-02-01 22:13:49 +0100

    updated to flatlaf-3.5.4, with fallback on 2.6 for arm64-linux;

--- src/Pure/System/isabelle_tool.scala
+++ src/Pure/System/isabelle_tool.scala
@@ -178,6 +178,7 @@ class Admin_Tools extends Isabelle_Scala_Tools(
   Component_EPTCS.isabelle_tool,
   Component_Easychair.isabelle_tool,
   Component_Elm.isabelle_tool,
+  Component_FlatLaf.isabelle_tool,
   Component_Foiltex.isabelle_tool,
   Component_Fonts.isabelle_tool,
   Component_HOL_Light.isabelle_tool,
