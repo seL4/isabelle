46ab1202ef29fe143996ca123349aee61ec32a38
wenzelm <none@none>
2025-02-03 13:12:44 +0100

    update to scala-3.3.5; enforce rebuild of Isabelle/ML and Isabelle/Scala;

--- src/Pure/Admin/component_scala.scala
+++ src/Pure/Admin/component_scala.scala
@@ -34,11 +34,11 @@ object Component_Scala {
   }
 
   val main_download: Download =
-    Download("scala", "3.3.4", base_version = "",
+    Download("scala", "3.3.5", base_version = "",
       url = "https://github.com/lampepfl/dotty/releases/download/{V}/scala3-{V}.tar.gz")
 
   val lib_downloads: List[Download] = List(
-    Download("scala-parallel-collections", "1.0.4",
+    Download("scala-parallel-collections", "1.2.0",
       "https://mvnrepository.com/artifact/org.scala-lang.modules/scala-parallel-collections_{B}/{V}",
       physical_url = "https://repo1.maven.org/maven2/org/scala-lang/modules/scala-parallel-collections_{B}/{V}/scala-parallel-collections_{B}-{V}.jar"),
     Download("scala-parser-combinators", "2.4.0",
