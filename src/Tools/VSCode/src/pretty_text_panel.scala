3581c5923687853325e50b1fbf5a32eef32085c0
wenzelm <none@none>
2025-02-18 17:16:55 +0100

    proper File.standard_path for Windows: result of resources.source_file is platform_path (amending da1108a6d249);

--- src/Tools/VSCode/src/pretty_text_panel.scala
+++ src/Tools/VSCode/src/pretty_text_panel.scala
@@ -51,8 +51,8 @@ class Pretty_Text_Panel private(
               for {
                 thy_file <- Position.Def_File.unapply(props)
                 def_line <- Position.Def_Line.unapply(props)
-                source <- resources.source_file(thy_file)
-                uri = File.uri(Path.explode(source).absolute_file)
+                platform_path <- resources.source_file(thy_file)
+                uri = File.uri(Path.explode(File.standard_path(platform_path)).absolute_file)
               } yield HTML.link(uri.toString + "#" + def_line, body)
           }
         val elements = Browser_Info.extra_elements.copy(entity = Markup.Elements.full)
