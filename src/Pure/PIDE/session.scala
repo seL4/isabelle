e5bd5da00314d3d171aa743a22e8dcfe4212c323
wenzelm <none@none>
2025-09-20 19:36:59 +0200

    more robust: post after state change;

--- src/Pure/PIDE/session.scala
+++ src/Pure/PIDE/session.scala
@@ -536,10 +536,10 @@ abstract class Session extends Document.Session {
           if (!handled) {
             msg.properties match {
               case Protocol.Command_Timing(state_id, props) if prover.defined =>
-                command_timings.post(Session.Command_Timing(props))
                 val markup = Markup(Markup.TIMING, props)
                 val message = XML.elem(Markup.STATUS, List(XML.Elem(markup, Nil)))
                 change_command(_.accumulate(state_id, cache.elem(message), cache))
+                command_timings.post(Session.Command_Timing(props))
 
               case Markup.Theory_Timing(props) =>
                 theory_timings.post(Session.Theory_Timing(props))
