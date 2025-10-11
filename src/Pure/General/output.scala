784b19757c5d49e0af409b9a4cd1f24f313c7f00
wenzelm <none@none>
2025-10-11 16:19:16 +0200

    clarified signature: more explicit operations;

--- src/Pure/General/output.scala
+++ src/Pure/General/output.scala
@@ -6,6 +6,8 @@ Console output channels.
 
 package isabelle
 
+import java.io.PrintStream
+
 
 object Output {
   def writeln_text(msg: String): String = Protocol_Message.clean_output(msg)
@@ -17,10 +19,11 @@ object Output {
   def error_message_text(msg: String): String =
     error_message_prefix(Protocol_Message.clean_output(msg))
 
+  def stream(stdout: Boolean = false): PrintStream =
+    if (stdout) Console.out else Console.err
+
   def output(s: String, stdout: Boolean = false, include_empty: Boolean = false): Unit =
-    if (s.nonEmpty || include_empty) {
-      if (stdout) Console.print(s + "\n") else Console.err.print(s + "\n")
-    }
+    if (s.nonEmpty || include_empty) stream(stdout = stdout).print(s + "\n")
 
   def writeln(msg: String, stdout: Boolean = false, include_empty: Boolean = false): Unit =
     output(writeln_text(msg), stdout = stdout, include_empty = include_empty)
