50a4656efeca1f96194407eb8c20088cea712241
wenzelm <none@none>
2025-12-04 15:55:04 +0100

    more uniform event handling (see also 209a97e1981f);

--- src/Tools/jEdit/src/documentation_dockable.scala
+++ src/Tools/jEdit/src/documentation_dockable.scala
@@ -39,7 +39,7 @@ class Documentation_Dockable(view: View, position: String) extends Dockable(view
 
   tree.addKeyListener(new KeyAdapter {
     override def keyPressed(e: KeyEvent): Unit = {
-      if (e.getKeyCode == KeyEvent.VK_ENTER) {
+      if (!e.isConsumed() && e.getKeyCode == KeyEvent.VK_ENTER) {
         e.consume()
         val path = tree.getSelectionPath
         if (path != null) action(path.getLastPathComponent)
