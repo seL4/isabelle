d3f666ea9e2d82755330c548322910d92a2bc2e9
wenzelm <none@none>
2026-01-16 17:18:49 +0100

    clarified modules;

--- src/Pure/PIDE/editor.scala
+++ src/Pure/PIDE/editor.scala
@@ -41,6 +41,9 @@ abstract class Editor {
 
   def ml_settings: ML_Settings = session.store.ml_settings
 
+  def doc_entry(name: String): Option[Doc.Entry] =
+    Doc.contents(ml_settings).entries(name = _ == name).headOption
+
 
   /* document editor */
 
