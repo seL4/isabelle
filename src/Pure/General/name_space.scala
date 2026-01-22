333ce81f433a00490e70676f78617db54da848da
wenzelm <none@none>
2026-01-22 22:28:48 +0100

    clarified markup: explicit label for interpreted entities;

--- src/Pure/General/name_space.scala
+++ src/Pure/General/name_space.scala
@@ -10,9 +10,10 @@ object Name_Space {
   sealed case class Entry(properties: Properties.T) {
     def name: String = Markup.Name.get(properties)
     def kind: String = Markup.Kind.get(properties)
+    def def_label: String = Position.Def_Label.get(properties)
 
     def print(style: GUI.Style = GUI.Style_Plain): String =
-      GUI.Name(name, kind = Word.informal(kind), style = style).toString
+      GUI.Name(name, kind = Word.informal(kind), prefix = def_label, style = style).toString
     def print_xml(style: GUI.Style = GUI.Style_Plain): XML.Elem =
       XML.Elem(Markup.Entity(this), XML.string(print(style = style)))
   }
