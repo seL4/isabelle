6667e6822a0b9583f66ea377b90db91a945ccc62
haftmann <none@none>
2026-02-19 15:47:24 +0100

    refine code assertion mechanism

--- src/HOL/ex/Code_Assertion.thy
+++ src/HOL/ex/Code_Assertion.thy
@@ -4,9 +4,11 @@
 
 theory Code_Assertion
   imports Main
-  keywords "code_assertion" :: thy_decl
 begin
 
 ML_file \<open>Tools/code_assertion.ML\<close>
+
+attribute_setup code_assert = Code_Assertion.attribute
+  \<open>check code generation for each global interpretation using the given definitions and target language\<close>
 
 end
