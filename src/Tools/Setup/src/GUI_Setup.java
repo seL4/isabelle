47bce33cd95ba8a54e6f3b7f4b1bdd59e4f7cc9b
wenzelm <none@none>
2026-02-21 16:29:59 +0100

    discontinue odd workaround for macOS 11 Big Sur (see 96d87b9c2b42): looks fine with macOS 12 Monterey and jdk-21;

--- src/Tools/Setup/src/GUI_Setup.java
+++ src/Tools/Setup/src/GUI_Setup.java
@@ -24,20 +24,7 @@ public class GUI_Setup
                 if (scale > 1) { msg = "export GDK_SCALE=" + scale; }
             }
         }
-        else if (Environment.is_macos()) {
-            String java_domain = "com.azul.zulu." + System.getProperty("java.version") + ".java";
-            if (!apple_tabbing_mode("read", java_domain)) {
-                apple_tabbing_mode("write", java_domain);
-            }
-        }
 
         return msg;
-    }
-
-    public static boolean apple_tabbing_mode(String op, String domain)
-    {
-        List<String> cmd = java.util.List.of("defaults", op, domain, "AppleWindowTabbingMode");
-        try { return Environment.exec_process(cmd, null, null, false).ok(); }
-        catch (Exception e) { return false; }
     }
 }
