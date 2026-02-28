470d0f525e10ceed65cc8b99d5d5289afdcb5c46
wenzelm <none@none>
2026-02-28 20:20:22 +0100

    prefer focus-textarea.shortcut=S+F1 to avoid conflict with vfs.browser.rename.shortcut, which is also hardwired in jEdit VFSDirectoryEntryTable.processKeyEvent and Swing Table.ancestorInputMap; proper find-prev.shortcut (not find-previous.shortcut) and additional find-prev.shortcut2=S+F3;

--- src/Pure/GUI/tree_view.scala
+++ src/Pure/GUI/tree_view.scala
@@ -110,8 +110,6 @@ class Tree_View(
 
   /* init */
 
-  if (!isEditable) GUI.suppress_input(this, k => k.getKeyCode == KeyEvent.VK_F2)
-
   setCellRenderer(new Tree_View.Cell_Renderer)
 
   setRowHeight(0)
