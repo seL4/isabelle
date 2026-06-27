88ae350cd09078eb52cbd67ad1ffdb899d3d6f52
wenzelm <none@none>
2026-06-27 13:21:53 +0200

    update secondary library;

--- src/Pure/Admin/component_scala.scala
+++ src/Pure/Admin/component_scala.scala
@@ -50,7 +50,7 @@ object Component_Scala {
     Download("scala-swing", "3.0.0",
       "https://mvnrepository.com/artifact/org.scala-lang.modules/scala-swing_{B}/{V}",
       physical_url = "https://repo1.maven.org/maven2/org/scala-lang/modules/scala-swing_{B}/{V}/scala-swing_{B}-{V}.jar"),
-    Download("scala-xml", "2.3.0",
+    Download("scala-xml", "2.4.0",
       "https://mvnrepository.com/artifact/org.scala-lang.modules/scala-xml_{B}/{V}",
       physical_url = "https://repo1.maven.org/maven2/org/scala-lang/modules/scala-xml_{B}/{V}/scala-xml_{B}-{V}.jar"),
     Download("scalajs-linker", scalajs_version,
