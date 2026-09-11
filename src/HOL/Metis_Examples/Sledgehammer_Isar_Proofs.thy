abb82e457f53d8b8bb3aa22cf2484d33001dab75
desharna <none@none>
2026-09-11 14:51:14 +0200

    added end-to-end tests of Sledgehammer: a case split, whose Vampire proof goes through AVATAR's "introduced" sources (see 3e25400fc785), and an if-then-else, which exercises $ite and, for Vampire 5.1, its avoidance (see 7ecbe125c5a9); dropped isar_proof, which turns off Vampire's AVATAR

--- src/HOL/Metis_Examples/Sledgehammer_Isar_Proofs.thy
+++ src/HOL/Metis_Examples/Sledgehammer_Isar_Proofs.thy
@@ -1,5 +1,5 @@
 (*  Title:      src/HOL/Metis_Example/Sledgehammer_Isar_Proofs.thy
-    Author:     Martin Desharnais, MPI-INF Saarbruecken
+    Author:     Martin Desharnais, LMU Muenchen
 
 Tests of proof reconstruction (a.k.a. preplay) in Sledgehammer.
 *)
@@ -11,6 +11,28 @@ external_file \<open>Sledgehammer_Isar_Proofs.certs\<close>
 declare [[smt_certificates = "Sledgehammer_Isar_Proofs.certs"]]
 declare [[smt_read_only_certificates = true]]
 
-sledgehammer_params [verit, isar_proof, minimize = false, slices = 1, compress = 1]
+sledgehammer_params [expect = some_preplayed, minimize = false, slices = 1, compress = 1]
+
+lemma
+  assumes "A \<or> B" and "A \<Longrightarrow> C" and "B \<Longrightarrow> C"
+  shows "C"
+  sledgehammer [cvc5] (assms)
+  sledgehammer [e, type_enc = mono_native] (assms)
+  sledgehammer [spass] (assms)
+  sledgehammer [vampire, type_enc = mono_native] (assms)
+  sledgehammer [verit] (assms)
+  sledgehammer [zipperposition, type_enc = mono_native] (assms)
+  sledgehammer [z3] (assms)
+  using assms by satx
+
+lemma "(if P then a else b) = (if \<not> P then b else a)"
+  sledgehammer [cvc5] ()
+  sledgehammer [e, type_enc = mono_native_fool] ()
+  sledgehammer [spass] ()
+  sledgehammer [vampire, type_enc = mono_native_fool] ()
+  sledgehammer [verit] ()
+  sledgehammer [zipperposition, type_enc = mono_native_fool] ()
+  sledgehammer [z3] ()
+  by metis
 
 end
